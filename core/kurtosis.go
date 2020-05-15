2fc59243b7a0d648f4262ce917f5e8e26c3fb826
Galen Marchetti <galenmarchetti@Galens-MacBook-Pro.local>
2020-05-15 05:49:32 -0300

    cleaning up utils and adding comments in CLI

--- core/kurtosis.go
+++ core/kurtosis.go
@@ -14,61 +14,53 @@ import (
 )
 
 const GECKO_IMAGE_NAME = "gecko-f290f73"
-var GECKO_START_COMMAND = [5]string{
-	"/gecko/build/ava",
-	"--public-ip=127.0.0.1",
-	"--snow-sample-size=1",
-	"--snow-quorum-size=1",
-	"--staking-tls-enabled=false",
-}
 
 func main() {
 	fmt.Println("Welcome to Kurtosis E2E Testing for Ava.")
 
 	ctx := context.Background()
 	
-	fmt.Println("Here are your containers that are currently running:")
+	// Initialize a Docker client and panic if any error occurs in the process.
 	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
 		panic(err)
 	}
 
-	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{})
-	if err != nil {
-		panic(err)
-	}
+	fmt.Println("I'm going to run a Gecko node, and hang while it's running! Kill me and then clear your docker containers.")
 
-	for _, container := range containers {
-		fmt.Printf("%s %s\n", container.ID[:10], container.Image)
-	}
+	// Creates a configuration object representing the container itself, based on a prebuilt image.
+	nodeConfig := utils.GetGeckoNodeConfig(GECKO_IMAGE_NAME)
 
-	fmt.Println("I'm going to run a Gecko node, and hang while it's running!")
-
-	nodeConfig := utils.GetNodeConfig(GECKO_IMAGE_NAME, GECKO_START_COMMAND)
+	// Creates a configuration object representing the mappings between the container and the host.
 	nodeToHostConfig := utils.GetNodeToHostConfig("9650", "9651")
 
+	// Create the container based on the configurations, but don't start it yet.
 	resp, err := cli.ContainerCreate(ctx, nodeConfig, nodeToHostConfig, nil, "")
 	if err != nil {
 		panic(err)
 	}
 
+	// Start the container.
 	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
 		panic(err)
 	}
 
+	// Wait on the container to return
 	statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
 	select {
-	case err := <-errCh:
-		if err != nil {
-			panic(err)
-		}
-	case <-statusCh:
+		case err := <-errCh:
+			if err != nil {
+				panic(err)
+			}
+		case <-statusCh:
 	}
 
+	// Once container has returned, grab the logs.
 	out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
 	if err != nil {
 		panic(err)
 	}
 
+	// Copy the logs to stdout.
 	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
 }
--- core/utils/containers.go
+++ core/utils/containers.go
@@ -15,6 +15,17 @@ const DEFAULT_GECKO_HTTP_PORT = nat.Port("9650/tcp")
 const DEFAULT_GECKO_STAKING_PORT = nat.Port("9651/tcp")
 const LOCAL_HOST_IP = "0.0.0.0"
 
+func GetGeckoNodeConfig(nodeImageName string) *container.Config {
+	var geckoStartCommand = [5]string{
+		"/gecko/build/ava",
+		"--public-ip=127.0.0.1",
+		"--snow-sample-size=1",
+		"--snow-quorum-size=1",
+		"--staking-tls-enabled=false",
+	}
+	return GetNodeConfig(nodeImageName, geckoStartCommand)
+}
+
 func GetNodeConfig(nodeImageName string, nodeStartCommand [5]string) *container.Config {
 	nodeConfig := &container.Config{
 		Image: nodeImageName,
