8f3ad37cecb40aa5540c9765bc75f48701e4db3c
Galen Marchetti <galenmarchetti@Galens-MacBook-Pro.local>
2020-05-14 21:07:50 -0300

    successfully running a gecko node in a docker container from pre-built image

--- core/go.mod
+++ core/go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/containerd/containerd v1.3.4 // indirect
 	github.com/docker/distribution v2.7.1+incompatible // indirect
 	github.com/docker/docker v17.12.0-ce-rc1.0.20200514193020-5da88705cccc+incompatible
-	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-connections v0.4.0
 	github.com/docker/go-units v0.4.0 // indirect
 	github.com/gogo/protobuf v1.3.1 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
--- core/kurtosis.go
+++ core/kurtosis.go
@@ -2,22 +2,36 @@ package main
 
 import (
 	"context"
+	"os"
 	"fmt"
 	
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
+	"github.com/docker/go-connections/nat"
 )
+
+var GECKO_IMAGE_NAME = "gecko-f290f73"
+var GECKO_START_COMMAND = [5]string{
+	"/gecko/build/ava",
+	"--public-ip=127.0.0.1",
+	"--snow-sample-size=1",
+	"--snow-quorum-size=1",
+	"--staking-tls-enabled=false",
+}
 
 func main() {
 	fmt.Println("Welcome to Kurtosis E2E Testing for Ava.")
 
+	ctx := context.Background()
 	fmt.Println("Here are your containers that are currently running:")
-	cli, err := client.NewClientWithOpts(client.WithVersion("1.40"))
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
 		panic(err)
 	}
 
-	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{})
 	if err != nil {
 		panic(err)
 	}
@@ -25,4 +39,58 @@ func main() {
 	for _, container := range containers {
 		fmt.Printf("%s %s\n", container.ID[:10], container.Image)
 	}
+
+	fmt.Println("I'm going to run a Gecko node, and hang while it's running!")
+
+	config := &container.Config{
+		Image: GECKO_IMAGE_NAME,
+		ExposedPorts: nat.PortSet{
+			"9650/tcp": struct{}{},
+			"9651/tcp": struct{}{},
+		},
+		Cmd:   GECKO_START_COMMAND[:5],
+		Tty: false,
+	}
+	
+	hostConfig := &container.HostConfig{
+		PortBindings: nat.PortMap{
+			"9650/tcp": []nat.PortBinding{
+				{
+					HostIP: "0.0.0.0",
+					HostPort: "9650",
+				},
+			},
+			"9651/tcp": []nat.PortBinding{
+				{
+					HostIP: "0.0.0.0",
+					HostPort: "9651",
+				},
+			},
+		},
+	}
+
+	resp, err := cli.ContainerCreate(ctx, config, hostConfig, nil, "")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		panic(err)
+	}
+
+	statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
+	select {
+	case err := <-errCh:
+		if err != nil {
+			panic(err)
+		}
+	case <-statusCh:
+	}
+
+	out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
+	if err != nil {
+		panic(err)
+	}
+
+	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
 }
