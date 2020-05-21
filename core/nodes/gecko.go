e9467c37d0ec2f72b56b1ab3a9d69bb6751c4607
mieubrisse <mieubrisse@gmail.com>
2020-05-20 21:46:15 -0300

    Clean up gecko.go

--- core/nodes/gecko.go
+++ core/nodes/gecko.go
@@ -6,20 +6,8 @@ Contains types to represent nodes contained in Docker containers.
 
 package nodes
 
-
+// TODO make this an implementation of JsonRpcServiceConfig
 // Type representing a Gecko Node and which ports on the host machine it will use for HTTP and Staking.
 type GeckoNode struct {
 	GeckoImageName, HttpPortOnHost, StakingPortOnHost string
-	respID string
 }
-
-func (node *GeckoNode) GetRespID() string {
-	return node.respID
-}
-
-// Creates a Docker container for the Gecko Node.
-func (node *GeckoNode) Create() {
-
-}
-
-// Waits on the Docker container, and if it exits, exposes logs to stdout.
