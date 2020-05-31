7b47f1196d11c9d8b8a9212839b3da44d9d9bb01
galenmarchetti <galenmarchetti@gmail.com>
2020-05-31 09:51:34 -0300

    modifying build script to accept custom image tag and adding run and validate pchain script

---
+++ core/.ci/run_and_validate_pchain_test.sh
@@ -0,0 +1,37 @@
+SCRIPTS_PATH=$(cd $(dirname "${BASH_SOURCE[0]}"); pwd)
+KURTOSIS_PATH=$(dirname "${SCRIPTS_PATH}")
+
+LATEST_KURTOSIS_TAG="kurtosistech/kurtosis:latest"
+LATEST_CONTROLLER_TAG="kurtosistech/ava-test-controller:latest"
+DEFAULT_GECKO_IMAGE="gecko-60668c3"
+
+#bash "${KURTOSIS_PATH}"/scripts/build_image.sh ${LATEST_KURTOSIS_TAG}
+#docker pull ${LATEST_CONTROLLER_TAG}
+
+(docker run -v /var/run/docker.sock:/var/run/docker.sock \
+--env DEFAULT_GECKO_IMAGE="${DEFAULT_GECKO_IMAGE}" \
+--env TEST_CONTROLLER_IMAGE="${LATEST_CONTROLLER_TAG}" \
+${LATEST_KURTOSIS_TAG}) &
+
+kurtosis_pid=$!
+
+sleep 15
+kill ${kurtosis_pid}
+
+ACTUAL_EXIT_STATUS=$(docker ps -a --latest --filter ancestor=kurtosistech/ava-test-controller:latest --format="{{.Status}}")
+EXPECTED_EXIT_STATUS="Exited \(0\).*"
+
+echo "${ACTUAL_EXIT_STATUS}"
+
+# Clear containers.
+echo "Clearing kurtosis testnet containers."
+docker rm $(docker stop $(docker ps -a -q --filter ancestor="${DEFAULT_GECKO_IMAGE}" --format="{{.ID}}")) >/dev/null
+
+if [[ ${ACTUAL_EXIT_STATUS} =~ ${EXPECTED_EXIT_STATUS} ]]
+then
+  echo "Kurtosis test succeeded."
+  exit 0
+else
+  echo "Kurtosis test failed."
+  exit 1
+fi
--- core/scripts/build_image.sh
+++ core/scripts/build_image.sh
@@ -4,10 +4,19 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+if [ $# -eq 0 ]
+  then
+    COMMIT="$(git --git-dir="${KURTOSIS_PATH}/.git" rev-parse --short HEAD)"
+    TAG="kurtosis-$COMMIT"
+  else
+    TAG=$1
+fi
+
+IMAGE_TAG=$1
 SCRIPTS_PATH=$(cd $(dirname "${BASH_SOURCE[0]}"); pwd)
 KURTOSIS_PATH=$(dirname "${SCRIPTS_PATH}")
 DOCKER="${DOCKER:-docker}"
 
 COMMIT="$(git --git-dir="${KURTOSIS_PATH}/.git" rev-parse --short HEAD)"
 
-"${DOCKER}" build -t "kurtosis-$COMMIT" "${KURTOSIS_PATH}"
+"${DOCKER}" build -t "$TAG" "${KURTOSIS_PATH}"
