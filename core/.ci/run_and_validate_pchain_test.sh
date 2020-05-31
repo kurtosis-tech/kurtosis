6a083c9f978ad76264d810cc49bdb8daf013f134
galenmarchetti <galenmarchetti@gmail.com>
2020-05-31 10:53:02 -0300

    inspecting docker containers before killing kurtosis

--- core/.ci/run_and_validate_pchain_test.sh
+++ core/.ci/run_and_validate_pchain_test.sh
@@ -19,6 +19,7 @@ ${LATEST_KURTOSIS_TAG}) &
 kurtosis_pid=$!
 
 sleep 15
+docker ps -a
 kill ${kurtosis_pid}
 
 ACTUAL_EXIT_STATUS=$(docker ps -a --latest --filter ancestor=kurtosistech/ava-test-controller:latest --format="{{.Status}}")
