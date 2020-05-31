7294d864593745425d9cd799ab2a06d219c20ad0
galenmarchetti <galenmarchetti@gmail.com>
2020-05-31 14:55:27 -0300

    changing script path call at top

--- core/.ci/run_and_validate_pchain_test.sh
+++ core/.ci/run_and_validate_pchain_test.sh
@@ -1,28 +1,29 @@
-SCRIPTS_PATH=$(cd $(dirname "${BASH_SOURCE[0]}"); pwd)
-KURTOSIS_PATH=$(dirname "${SCRIPTS_PATH}")
+set -euo pipefail
+SCRIPT_PATH="$(cd "$(dirname "${0}")" && pwd)"
+KURTOSIS_PATH="$(dirname "${SCRIPTS_PATH}")"
 
 LATEST_KURTOSIS_TAG="kurtosistech/kurtosis:latest"
 LATEST_CONTROLLER_TAG="kurtosistech/ava-test-controller:latest"
 DEFAULT_GECKO_IMAGE="kurtosistech/gecko:latest"
 
-bash "${KURTOSIS_PATH}"/scripts/build_image.sh ${LATEST_KURTOSIS_TAG}
-docker pull ${LATEST_CONTROLLER_TAG}
-docker pull ${DEFAULT_GECKO_IMAGE}
+bash "${KURTOSIS_PATH}"/scripts/build_image.sh "${LATEST_KURTOSIS_TAG}"
+docker pull "${LATEST_CONTROLLER_TAG}"
+docker pull "${DEFAULT_GECKO_IMAGE}"
 
 (docker run -v /var/run/docker.sock:/var/run/docker.sock \
 --env DEFAULT_GECKO_IMAGE="${DEFAULT_GECKO_IMAGE}" \
 --env TEST_CONTROLLER_IMAGE="${LATEST_CONTROLLER_TAG}" \
-${LATEST_KURTOSIS_TAG}) &
+"${LATEST_KURTOSIS_TAG}") &
 
-kurtosis_pid=$!
+kurtosis_pid=${!}
 
 # Sleep while Kurtosis spins up testnet and runs controller to execute tests.
 sleep 90
 docker image ls
 docker ps -a
-kill ${kurtosis_pid}
+kill "${kurtosis_pid}"
 
-ACTUAL_EXIT_STATUS=$(docker ps -a --latest --filter ancestor=kurtosistech/ava-test-controller:latest --format="{{.Status}}")
+ACTUAL_EXIT_STATUS="$(docker ps -a --latest --filter ancestor=kurtosistech/ava-test-controller:latest --format="{{.Status}}")"
 EXPECTED_EXIT_STATUS="Exited \(0\).*"
 
 echo "Exit status: ${ACTUAL_EXIT_STATUS}"
