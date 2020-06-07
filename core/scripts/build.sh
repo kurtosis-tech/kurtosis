9b2df576dae75b0a1fffffa894b9800ac93bdfa8
galenmarchetti <galenmarchetti@gmail.com>
2020-06-07 18:47:12 -0300

    removing unnecessary variables in build script

--- core/scripts/build.sh
+++ core/scripts/build.sh
@@ -8,10 +8,6 @@ set -o pipefail
 SCRIPTS_PATH=$(cd $(dirname "${BASH_SOURCE[0]}"); pwd)
 KURTOSIS_PATH=$(dirname "${SCRIPTS_PATH}")
 
-BUILD_DIR="build"
-MAIN_BINARY_OUTPUT_FILE="kurtosis"
-MAIN_BINARY_OUTPUT_PATH="${KURTOSIS_PATH}/${BUILD_DIR}/${MAIN_BINARY_OUTPUT_FILE}"
-
 echo "Running unit tests..."
 if ! go test "${KURTOSIS_PATH}/..."; then
     echo "Tests failed!"
