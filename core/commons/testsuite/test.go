bcaf21ef1cf58a6bc21eda555648736df8098fba
mieubrisse <mieubrisse@gmail.com>
2020-06-21 13:30:36 -0300

    Make Test.Run() function return an error

--- core/commons/testsuite/test.go
+++ core/commons/testsuite/test.go
@@ -5,7 +5,7 @@ import "time"
 type Test interface {
 	// NOTE: if Go had generics, interface{} would be a parameterized type representing the network that this test consumes
 	// as produced by the TestNetworkLoader
-	Run(network interface{}, context TestContext)
+	Run(network interface{}, context TestContext) error
 
 	GetNetworkLoader() (TestNetworkLoader, error)
 
