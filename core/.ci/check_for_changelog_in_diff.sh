2db14fa853494885feb81eeba2dbf5e68cf9791b
galenmarchetti <galenmarchetti@gmail.com>
2020-07-22 08:37:55 -0300

    whitespace

--- core/.ci/check_for_changelog_in_diff.sh
+++ core/.ci/check_for_changelog_in_diff.sh
@@ -5,7 +5,6 @@ if [[ "${current_branch}" == "${TRAVIS_BRANCH}" ]]; then
   exit 0
 fi
 
-
 return_code=1
 if ! git diff --name-only HEAD.."${TRAVIS_BRANCH}" | grep CHANGELOG.md; then
   echo "PR has no CHANGELOG entry. Please update the CHANGELOG!"
