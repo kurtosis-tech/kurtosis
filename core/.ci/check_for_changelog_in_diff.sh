601daba29862c6c1d548e4e6cf95d582b3194c61
galenmarchetti <galenmarchetti@gmail.com>
2020-07-22 08:37:37 -0300

    adding check for branch check

--- core/.ci/check_for_changelog_in_diff.sh
+++ core/.ci/check_for_changelog_in_diff.sh
@@ -1,3 +1,11 @@
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+# If current branch and travis branch are the same, we're running the "branch" check and it should pass.
+if [[ "${current_branch}" == "${TRAVIS_BRANCH}" ]]; then
+  exit 0
+fi
+
+
 return_code=1
 if ! git diff --name-only HEAD.."${TRAVIS_BRANCH}" | grep CHANGELOG.md; then
   echo "PR has no CHANGELOG entry. Please update the CHANGELOG!"
