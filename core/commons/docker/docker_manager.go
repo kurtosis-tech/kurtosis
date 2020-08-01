8ac415abd5466552c85a12ebe6dd4dad25dfd55b
galenmarchetti <galenmarchetti@gmail.com>
2020-08-01 16:53:01 -0300

    debugging

--- core/commons/docker/docker_manager.go
+++ core/commons/docker/docker_manager.go
@@ -88,6 +88,7 @@ func (manager DockerManager) CreateNetwork(context context.Context, name string,
 	if err != nil {
 		return "", stacktrace.Propagate( err, "Failed to create network %s with subnet %s", name, subnetMask)
 	}
+	manager.log.Debugf("Created docker network with name %v and got response %+v", resp)
 	return resp.ID, nil
 }
 
