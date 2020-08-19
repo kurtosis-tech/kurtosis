926336461f352117a5ed644ca412468ffa16a128
mieubrisse <mieubrisse@gmail.com>
2020-08-19 09:06:28 -0300

    Very basic RPC call CLI

--- core/api-container/main.go
+++ core/api-container/main.go
@@ -1,18 +1,34 @@
-package api_container
+package main
 
 import (
+	"github.com/gorilla/rpc/v2"
 	"github.com/gorilla/rpc/v2/json2"
 	"log"
 	"net/http"
-	"github.com/gorilla/rpc"
 )
+
+type TestHandler struct {}
+
+type InputArg struct {
+	Saying string		`json:"saying"`
+}
+
+type OutputArg struct {
+	Response string 	`json:"response"`
+}
+
+func (obj *TestHandler) Parrot(httpReq *http.Request, args *InputArg, result *OutputArg) error {
+	result.Response = args.Saying
+	log.Printf("Result: %v", result)
+	return nil
+}
 
 func main() {
 	server := rpc.NewServer()
 	jsonCodec := json2.NewCodec()
 	server.RegisterCodec(jsonCodec, "application/json")
-	server.RegisterService(KevinTest{}, "")
+	server.RegisterService(new(TestHandler), "")
 
-	http.Handle("/jsonrpc/", server)
-	log.Fatal(http.ListenAndServe(":80", nil))
+	http.Handle("/jsonrpc", server)
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
