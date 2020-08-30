032feba5738c69ad432f3476146cc13653a8d196
mieubrisse <mieubrisse@gmail.com>
2020-08-30 12:17:03 -0300

    Debugging changelog check

--- core/.circleci/check_for_changelog_in_diff.sh
+++ core/.circleci/check_for_changelog_in_diff.sh
@@ -1,4 +1,7 @@
-return_code=1
+set -euo pipefail
+# TODO debugging
+echo "${CIRCLE_BRANCH}"
+git diff --name-only HEAD.."${CIRCLE_BRANCH}"
 if ! git diff --name-only HEAD.."${CIRCLE_BRANCH}" | grep CHANGELOG.md; then
   echo "PR has no CHANGELOG entry. Please update the CHANGELOG!"
   return_code=1
