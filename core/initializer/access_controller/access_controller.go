42175eaa894cbd197a6905650e657cfd8b5784c3
galenmarchetti <galenmarchetti@gmail.com>
2020-09-06 13:00:43 -0300

    adding descriptive comments to auth functions

--- core/initializer/access_controller/access_controller.go
+++ core/initializer/access_controller/access_controller.go
@@ -13,6 +13,11 @@ const (
 	paidLicense = "paid-license"
 )
 
+/*
+	Verifies that the license is a credential for a registered user,
+	Returns true if the license associated with the user has ever
+	been registered in Kurtosis, even if that user has no permissions.
+ */
 func AuthenticateLicense(license string) (bool, error) {
 	switch license {
 	case freeTrialLicense, paidLicense, expiredTrialLicense:
@@ -22,6 +27,10 @@ func AuthenticateLicense(license string) (bool, error) {
 	}
 }
 
+/*
+	Verifies that the user associated with the license is authorized
+	to run kurtosis (either has a free trial or has purchased a full license).
+*/
 func AuthorizeLicense(license string) (bool, error) {
 	switch license {
 	case freeTrialLicense, paidLicense:
