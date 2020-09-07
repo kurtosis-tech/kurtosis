348664064b1158c85cf73caf84f67ae184f827b8
galenmarchetti <galenmarchetti@gmail.com>
2020-09-07 14:11:51 -0300

    cleaning up authenticate and authorize flow

--- core/initializer/access_controller/access_controller.go
+++ core/initializer/access_controller/access_controller.go
@@ -10,27 +10,32 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+const (
+	requiredScopeToExecute = "execute:kurtosis-core"
+)
+
 /*
 	Verifies that the license is a credential for a registered user,
 	Returns true if the license associated with the user has ever
 	been registered in Kurtosis, even if that user has no permissions.
  */
-func Authenticate() (authenticated bool, err error) {
+func AuthenticateAndAuthorize() (authenticated bool, authorized bool, err error) {
 	tokenResponse, alreadyAuthenticated, err := loadToken()
 	if err != nil {
-		return false, stacktrace.Propagate(err, "")
+		return false, false, stacktrace.Propagate(err, "")
 	}
 	if alreadyAuthenticated {
 		logrus.Debugf("Already authenticated on this device! Access token: %s", tokenResponse.AccessToken)
-		return true, nil
+		return true, tokenResponse.Scope == requiredScopeToExecute, nil
 	}
 	tokenResponse, err = authorizeUserDevice()
 	if err != nil {
-		return false, stacktrace.Propagate(err, "")
+		return false, false, stacktrace.Propagate(err, "")
 	}
 	logrus.Debugf("Access token: %s", tokenResponse.AccessToken)
 	err = persistToken(tokenResponse)
 	if err != nil {
-		return false, stacktrace.Propagate(err, "")
+		return false, false, stacktrace.Propagate(err, "")
 	}
+	return true, tokenResponse.Scope == requiredScopeToExecute, nil
 }
