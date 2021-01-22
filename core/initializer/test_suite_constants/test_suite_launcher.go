54d6db2a0a46d778e16881bea66aebafe0b5e4ca
mieubrisse <mieubrisse@gmail.com>
2021-01-22 13:23:28 -0600

    Fix all compile errors on TestSuiteContainerLauncher

--- core/initializer/test_suite_constants/test_suite_launcher.go
+++ core/initializer/test_suite_constants/test_suite_launcher.go
@@ -10,7 +10,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/docker/go-connections/nat"
-	"github.com/gogo/protobuf/protoc-gen-gogo/testdata/import_public/sub"
 	"github.com/google/uuid"
 	"github.com/kurtosis-tech/kurtosis/api_container/api_container_docker_consts/api_container_env_vars"
 	"github.com/kurtosis-tech/kurtosis/api_container/api_container_docker_consts/api_container_modes"
@@ -30,6 +29,17 @@ const (
 
 	dockerSocket = "/var/run/docker.sock"
 )
+
+// TODO TODO this is a hack! The metadata-acquiring testsuite container needs to know the IP of the corresponding
+//  API container, but because both testsuite & API containers will be started in the bridge network (so that we
+//  don't have to burn the time to create a network just for them), we don't know what IPs are free so we can't
+//  use the freeIpAddrProvider to give a static IP for the API container. We hack around this by picking
+//  a hardcoded IP for the API container, and praying that nobody else is using it.
+//  The correct fix would be one of:
+//   - Inspect the API container after starting in the bridge network to get its IP (requires adding a new function to DockerManager)
+//   - Using a hostname rather than an IP for the API container (requires adding a new function argument to CreateAndStartContainer)
+//   - Creating a separate network for the metadata-acquiring testsuite & API containers
+var metadataAcquiringApiContainerIp net.IP = []byte{172, 17, 256, 256}
 
 type TestsuiteContainerLauncher struct {
 	executionInstanceId uuid.UUID
@@ -53,8 +63,11 @@ type TestsuiteContainerLauncher struct {
 }
 
 func NewTestsuiteContainerLauncher(
+		executionInstanceId uuid.UUID,
+		kurtosisApiImage string,
+		kurtosisApiLogLevel logrus.Level,
 		testsuiteImage string,
-		logLevel string,
+		testsuiteLogLevel string,
 		customEnvVars map[string]string,
 		debuggerPort int) (*TestsuiteContainerLauncher, error) {
 	debuggerPortObj, err := nat.NewPort(debuggerPortProtocol, strconv.Itoa(debuggerPort))
@@ -62,13 +75,13 @@ func NewTestsuiteContainerLauncher(
 		return nil, stacktrace.Propagate(err, "An error occurred creating the debugger port object from port int '%v'", debuggerPort)
 	}
 	return &TestsuiteContainerLauncher{
-		// TODO add all the fields
-		kurtosisApiImage: kurtosisApiImage,
+		executionInstanceId: executionInstanceId,
+		kurtosisApiImage:    kurtosisApiImage,
 		kurtosisApiLogLevel: kurtosisApiLogLevel,
-		testsuiteImage: testsuiteImage,
-		suiteLogLevel:  logLevel,
-		customEnvVars:  customEnvVars,
-		debuggerPort:   debuggerPortObj,
+		testsuiteImage:      testsuiteImage,
+		suiteLogLevel:       testsuiteLogLevel,
+		customEnvVars:       customEnvVars,
+		debuggerPort:        debuggerPortObj,
 	}, nil
 }
 
@@ -76,20 +89,48 @@ func NewTestsuiteContainerLauncher(
 Launches a new testsuite container to acquire testsuite metadata
  */
 func (launcher TestsuiteContainerLauncher) LaunchMetadataAcquiringContainer(
-		context context.Context,
+		ctx context.Context,
 		dockerManager *docker_manager.DockerManager,
 		bridgeNetworkId string,
 		suiteExecutionVolume string,
-		debuggerPortBinding nat.PortBinding,
-		kurtosisApiIpAddr net.IP) (containerId string, err error) {
-	envVars, err := launcher.generateTestSuiteEnvVars(kurtosisApiIpAddr)
+		debuggerPortBinding nat.PortBinding) (testsuiteContainerId string, kurtosisApiContainerId string, err error) {
+	apiContainerEnvVars, err := launcher.genSuiteMetadataSerializationApiContainerEnvVars()
 	if err != nil {
-		return "", stacktrace.Propagate(err, "An error occurred generating the testsuite container env vars")
+		return "", "", stacktrace.Propagate(err, "An error occurred generating the API container env vars")
 	}
 
+	logrus.Info("Launching Kurtosis API container...")
+	kurtosisApiPort := nat.Port(fmt.Sprintf("%v/%v", api_container_server_consts.ListenPort, api_container_server_consts.ListenProtocol))
+	kurtosisApiContainerId, err = dockerManager.CreateAndStartContainer(
+		ctx,
+		launcher.kurtosisApiImage,
+		bridgeNetworkId,
+		metadataAcquiringApiContainerIp,
+		map[docker_manager.ContainerCapability]bool{}, // No extra capabilities needed for the API container
+		docker_manager.DefaultNetworkMode,
+		map[nat.Port]*nat.PortBinding{
+			kurtosisApiPort: nil,
+		},
+		nil,
+		apiContainerEnvVars,
+		map[string]string{},   // We don't need to bind mount the Docker socket because this API container won't interact with Docker
+		map[string]string{
+			suiteExecutionVolume: api_container_mountpoints.SuiteExecutionVolumeMountDirpath,
+		},
+	)
+	if err != nil {
+		return "", "", stacktrace.Propagate(err, "An error occurred launching the Kurtosis API container")
+	}
+	logrus.Infof("Successfully launched the Kurtosis API container")
 
-	resultContainerId, err := dockerManager.CreateAndStartContainer(
-		context,
+	testsuiteEnvVars, err := launcher.generateTestSuiteEnvVars(metadataAcquiringApiContainerIp)
+	if err != nil {
+		return "", "", stacktrace.Propagate(err, "An error occurred generating the testsuite container env vars")
+	}
+
+	logrus.Infof("Launching testsuite container to send metadata to Kurotsis API container...")
+	testsuiteContainerId, err = dockerManager.CreateAndStartContainer(
+		ctx,
 		launcher.testsuiteImage,
 		bridgeNetworkId,
 		nil,                                           // Nil because the bridge network will assign IPs on its own (and won't know what IPs are already used)
@@ -99,15 +140,17 @@ func (launcher TestsuiteContainerLauncher) LaunchMetadataAcquiringContainer(
 			launcher.debuggerPort: &debuggerPortBinding,
 		},
 		nil, // Nil start command args because we expect the test suite image to be parameterized with variables
-		envVars,
+		testsuiteEnvVars,
 		map[string]string{},
 		map[string]string{
 			suiteExecutionVolume: TestsuiteContainerSuiteExVolMountpoint,
 		})
 	if err != nil {
-		return "", stacktrace.Propagate(err, "An error occurred creating the test suite container to acquire testsuite metadata")
+		return "", "", stacktrace.Propagate(err, "An error occurred launching the testsuite container to send metadata to the Kurtosis API container")
 	}
-	return resultContainerId, nil
+	logrus.Infof("Successfully launched testsuite container to send metadata to Kurtosis API container")
+
+	return testsuiteContainerId, kurtosisApiContainerId, nil
 }
 
 /*
