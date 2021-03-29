c8b4f152b8988654d381ef918e9073a01d8bbc06
mieubrisse <mieubrisse@gmail.com>
2021-03-29 11:52:50 -0600

    Moved PrintContainerLogs function out of banner_printer module, where it doesn't belong

--- core/initializer/test_suite_metadata_acquirer/test_suite_metadata_acquirer.go
+++ core/initializer/test_suite_metadata_acquirer/test_suite_metadata_acquirer.go
@@ -10,6 +10,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
 	"github.com/docker/go-connections/nat"
 	"github.com/kurtosis-tech/kurtosis/api_container/api_container_docker_consts/api_container_exit_codes"
 	"github.com/kurtosis-tech/kurtosis/commons/docker_manager"
@@ -18,8 +19,10 @@ import (
 	"github.com/kurtosis-tech/kurtosis/initializer/test_suite_launcher"
 	"github.com/palantir/stacktrace"
 	"github.com/sirupsen/logrus"
+	"io"
 	"io/ioutil"
 	"os"
+	"strings"
 )
 
 const (
@@ -141,6 +144,52 @@ func printBothContainerLogs(
 		dockerManager *docker_manager.DockerManager,
 		apiContainerId string,
 		testsuiteContainerId string) {
-	banner_printer.PrintContainerLogsWithBanners(dockerManager, ctx, apiContainerId, logrus.StandardLogger(), metadataAcquiringApiContainerTitle)
-	banner_printer.PrintContainerLogsWithBanners(dockerManager, ctx, testsuiteContainerId, logrus.StandardLogger(), metadataSendingTestsuiteContainerTitle)
+	printContainerLogsWithBanners(dockerManager, ctx, apiContainerId, logrus.StandardLogger(), metadataAcquiringApiContainerTitle)
+	printContainerLogsWithBanners(dockerManager, ctx, testsuiteContainerId, logrus.StandardLogger(), metadataSendingTestsuiteContainerTitle)
 }
+
+/*
+Little helper function to print a container's logs with with banners indicating the start and end of the logs
+
+Args:
+	dockerManager: Docker manager to use when retrieving container logs
+	context: The context in which to run the log retrieval Docker function
+	containerId: ID of the Docker container from which to retrieve logs
+	containerDescription: Short, human-readable description of the container whose logs are being printed
+	logFilepath: Filepath of the file containing the container's logs
+*/
+func printContainerLogsWithBanners(
+		dockerManager *docker_manager.DockerManager,
+		context context.Context,
+		containerId string,
+		log *logrus.Logger,
+		containerDescription string) {
+	var logReader io.Reader
+	var useDockerLogDemultiplexing bool
+	logReadCloser, err := dockerManager.GetContainerLogs(context, containerId)
+	if err != nil {
+		errStr := fmt.Sprintf("Could not print container's logs due to the following error: %v", err)
+		logReader = strings.NewReader(errStr)
+		useDockerLogDemultiplexing = false
+	} else {
+		defer logReadCloser.Close()
+		logReader = logReadCloser
+		useDockerLogDemultiplexing = true
+	}
+
+	banner_printer.PrintSection(log, containerDescription + " Logs", false)
+	var copyErr error
+	if useDockerLogDemultiplexing {
+		// Docker logs multiplex STDOUT and STDERR into a single stream, and need to be demultiplexed
+		// See https://github.com/moby/moby/issues/32794
+		_, copyErr = stdcopy.StdCopy(log.Out, log.Out, logReader)
+	} else {
+		_, copyErr = io.Copy(log.Out, logReader)
+	}
+	if copyErr != nil {
+		log.Errorf("Could not print the test suite container's logs due to the following error when copying log contents:")
+		fmt.Fprintln(log.Out, err)
+	}
+	banner_printer.PrintSection(log, "End " + containerDescription + " Logs", false)
+}
+
