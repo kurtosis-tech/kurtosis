2ebd2d4cd69e5ae2012e9f08ad934334b2aaff0e
mieubrisse <mieubrisse@gmail.com>
2021-04-30 14:09:34 -0500

    Expanded Protobuf binding generation script to work on all Protobuf bindings in the project

--- core/scripts/regenerate-protobuf-output.sh
+++ core/scripts/regenerate-protobuf-output.sh
@@ -1,4 +1,4 @@
-# This script regenerates Go bindings corresponding to the .proto files that define the API container's API
+# This script regenerates Go bindings corresponding to all .proto files inside this project
 # It requires the Golang Protobuf extension to the 'protoc' compiler, as well as the Golang gRPC extension
 
 set -euo pipefail
@@ -8,10 +8,6 @@ root_dirpath="$(dirname "${script_dirpath}")"
 # ================================ CONSTANTS =======================================================
 GO_MOD_FILENAME="go.mod"
 GO_MOD_FILE_MODULE_KEYWORD="module"
-
-API_CONTAINER_DIRNAME="api_container"
-API_CONTAINER_API_DIRNAME="api"
-API_CONTAINER_API_BINDINGS_DIRNAME="bindings"
 
 # The name of the directory to contain generated Protobuf Go bindings
 BINDINGS_OUTPUT_DIRNAME="bindings"
@@ -45,8 +41,13 @@ for input_relative_dirpath in "${PROTOBUF_RELATIVE_DIRPATHS[@]}"; do
     output_relative_dirpath="${input_relative_dirpath}/${BINDINGS_OUTPUT_DIRNAME}"
 
     output_abs_dirpath="${root_dirpath}/${output_relative_dirpath}"
-    if [ "${output_abs_dirpath}/" != "/" ]; then
+    if [ "${output_abs_dirpath}/" == "/" ]; then
         echo "Error: Binding output dirpath cannot be empty!" >&2
+        exit 1
+    fi
+
+    if ! mkdir -p "${output_abs_dirpath}"; then
+        echo "Error: An error occurred creating output directory '${output_abs_dirpath}'" >&2
         exit 1
     fi
 
@@ -76,45 +77,6 @@ for input_relative_dirpath in "${PROTOBUF_RELATIVE_DIRPATHS[@]}"; do
             echo "Error: An error occurred converting Protobuf file '${protobuf_filepath}' to bindings in directory '${output_abs_dirpath}'" >&2
             exit 1
         fi
+        echo "Successfully generated bindings for Protobuf file '${protobuf_filepath}' in directory '${output_abs_dirpath}'"
     done
 done
-
-
-
-# api_container_api_bindings_go_pkg="${go_module}/${API_CONTAINER_DIRNAME}/${API_CONTAINER_API_DIRNAME}/${API_CONTAINER_API_BINDINGS_DIRNAME}"
-# 
-# api_container_dirpath="${root_dirpath}/${API_CONTAINER_DIRNAME}"
-# api_protobufs_input_dirpath="${api_container_dirpath}/${API_CONTAINER_API_DIRNAME}"
-# api_protobufs_output_dirpath="${api_protobufs_input_dirpath}/${API_CONTAINER_API_BINDINGS_DIRNAME}"
-# 
-# if [ "${api_protobufs_output_dirpath}/" != "/" ]; then
-#     if ! find ${api_protobufs_output_dirpath} -name '*.go' -delete; then
-#         echo "Error: An error occurred removing the existing protobuf-generated code" >&2
-#         exit 1
-#     fi
-# else
-#     echo "Error: lib core generated API code dirpath must not be empty!" >&2
-#     exit 1
-# fi
-# 
-# for protobuf_filepath in $(find "${api_protobufs_input_dirpath}" -name "*.proto"); do
-#     protobuf_filename="$(basename "${protobuf_filepath}")"
-# 
-#     # NOTE: When multiple people start developing on this, we won't be able to rely on using the user's local protoc because they might differ. We'll need to standardize by:
-#     #  1) Using protoc inside the API container Dockerfile to generate the output Go files (standardizes the output files for Docker)
-#     #  2) Using the user's protoc to generate the output Go files on the local machine, so their IDEs will work
-#     #  3) Tying the protoc inside the Dockerfile and the protoc on the user's machine together using a protoc version check
-#     #  4) Adding the locally-generated Go output files to .gitignore
-#     #  5) Adding the locally-generated Go output files to .dockerignore (since they'll get generated inside Docker)
-#     if ! protoc \
-#             -I="${api_protobufs_input_dirpath}" \
-#             --go_out="plugins=grpc:${api_protobufs_output_dirpath}" \
-#             `# Rather than specify the go_package in source code (which means all consumers of these protobufs would get it),` \
-#             `#  we specify the go_package here per https://developers.google.com/protocol-buffers/docs/reference/go-generated` \
-#             `# See also: https://github.com/golang/protobuf/issues/1272` \
-#             --go_opt="M${protobuf_filename}=${api_container_api_bindings_go_pkg};$(basename "${api_container_api_bindings_go_pkg}")" \
-#             "${protobuf_filepath}"; then
-#         echo "Error: An error occurred generating lib core files from protobuf file: ${protobuf_filepath}" >&2
-#         exit 1
-#     fi
-# done
