ce3cd781e20ff7bf662444322f14149e36364708
mieubrisse <mieubrisse@gmail.com>
2021-08-28 16:25:50 -0500

    More progress on GoReleaser

--- core/scripts/launch-interactive.sh
+++ core/scripts/launch-interactive.sh
@@ -11,8 +11,6 @@ root_dirpath="$(dirname "${script_dirpath}")"
 # ==================================================================================================
 source "${script_dirpath}/_constants.sh"
 
-
-
 # ==================================================================================================
 #                                             Main Logic
 # ==================================================================================================
@@ -21,10 +19,14 @@ if ! docker_tag="$("${script_dirpath}/${GET_DOCKER_IMAGES_TAG_SCRIPT_FILENAME}")
     exit 1
 fi
 
+goarch="$(go env GOARCH)"
+goos="$(go env GOOS)"
+cli_binary_filepath="${root_dirpath}/${GORELEASER_OUTPUT_DIRNAME}/${GORELEASER_CLI_BUILD_ID}_${goos}_${goarch}/${CLI_BINARY_FILENAME}"
+
 # The funky ${1+"${@}"} incantation is how you you feed arguments exactly as-is to a child script in Bash
 # ${*} loses quoting and ${@} trips set -e if no arguments are passed, so this incantation says, "if and only if
 #  ${1} exists, evaluate ${@}"
-"${root_dirpath}/${CLI_BINARY_OUTPUT_REL_FILEPATH}" \
+"${cli_binary_filepath}" \
     "--kurtosis-api-image=${API_IMAGE}:${docker_tag}" \
     "--javascript-repl-image=${JAVASCRIPT_REPL_IMAGE}:${docker_tag}" \
     ${1+"${@}"}
--- core/scripts/new-build.sh
+++ core/scripts/new-build.sh
@@ -10,34 +10,73 @@ root_dirpath="$(dirname "${script_dirpath}")"
 # ==================================================================================================
 #                                             Constants
 # ==================================================================================================
-# TODO Fill constants here with UPPER_SNAKE_CASE, noting that the only variables constants may use are:
-# TODO  1) other constants (with the "${OTHER_CONSTANT}" syntax)
-# TODO  2) script_dirpath/root_dirpath from above
-DEFAULT_SOME_OPTIONAL_ARG_VALUE="A default value"   # TODO Replace with your own constants
+source "${script_dirpath}/_constants.sh"
 
+INITIALIZER_DIRNAME="initializer"
+
+GET_DOCKER_IMAGES_TAG_SCRIPT_FILENAME="get-docker-images-tag.sh"
+
+WRAPPER_GENERATOR_DIRNAME="wrapper_generator"
+WRAPPER_GENERATOR_BINARY_OUTPUT_FILENAME="wrapper-generator"
+WRAPPER_TEMPLATE_REL_FILEPATH="${WRAPPER_GENERATOR_DIRNAME}/kurtosis.template.sh"
+
+GOOS_ENV_VAR_NAME="GOOS"
+GOARCH_ENV_VAR_NAME="GOARCH"
+
+WRAPPER_SCRIPT_GENERATOR_GORELEASER_BUILD_ID="wrapper-generator"
 
 # ==================================================================================================
 #                                             Main Logic
 # ==================================================================================================
-# docker run --rm --privileged \
-#     -v "${root_dirpath}:/go/src/github.com/user/repo" \
-#     -v /var/run/docker.sock:/var/run/docker.sock \
-#     -w /go/src/github.com/user/repo \
-#     -e GITHUB_TOKEN \
-#     -e DOCKER_USERNAME \
-#     -e DOCKER_PASSWORD \
-#     -e DOCKER_REGISTRY \
-#     goreleaser/goreleaser \
-#     build --rm-dist --snapshot --id wrapper-generator --single-target
+build_dirpath="${root_dirpath}/${BUILD_DIRNAME}"
+if ! mkdir -p "${build_dirpath}"; then
+    echo "Error: Couldn't create build output dir '${build_dirpath}'" >&2
+    exit 1
+fi
 
-docker run --rm --privileged \
-    -v "${root_dirpath}:/go/src/github.com/user/repo" \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -w /go/src/github.com/user/repo \
-    -e GITHUB_TOKEN \
-    -e DOCKER_USERNAME \
-    -e DOCKER_PASSWORD \
-    -e DOCKER_REGISTRY \
-    goreleaser/goreleaser \
-    release --rm-dist --snapshot
-    # build --rm-dist --snapshot
+if ! docker_image_tag="$(bash "${script_dirpath}/${GET_DOCKER_IMAGES_TAG_SCRIPT_FILENAME}")"; then
+    echo "Error: Couldn't get the Docker images tag" >&2
+    exit 1
+fi
+
+# TODO GENERATE JAVASCRIPT REPL
+
+# These variables are used by Goreleaser
+export API_IMAGE \
+    INITIALIZER_IMAGE \
+    INTERNAL_TESTSUITE_IMAGE \
+    WRAPPER_GENERATOR_BINARY_OUTPUT_FILENAME \
+    JAVASCRIPT_REPL_IMAGE \
+    CLI_BINARY_FILENAME
+export DOCKER_IMAGE_TAG="${docker_image_tag}"
+
+# We want to run goreleaser from the root
+cd "${root_dirpath}"
+
+go test ./...
+
+# TODO TODO TODO REMOVE ALL THIS WHEN THE CLI WILL DO EVERYTHING
+# Generate the wrapper script
+if ! goreleaser build --rm-dist --snapshot --id "${WRAPPER_SCRIPT_GENERATOR_GORELEASER_BUILD_ID}" --single-target; then
+    echo "Error: Couldn't build the wrapper script-generating binary" >&2
+    exit 1
+fi
+if ! "${root_dirpath}/${GORELEASER_OUTPUT_DIRNAME}/${WRAPPER_GENERATOR_BINARY_OUTPUT_FILENAME}" \
+        -kurtosis-core-version "${docker_image_tag}" \
+        -template "${WRAPPER_TEMPLATE_REL_FILEPATH}" \
+        -output "${WRAPPER_OUTPUT_REL_FILEPATH}"; then
+    echo "Error: Failed to generate wrapper script" >&2
+    exit 1
+fi
+
+# Build all the Docker images
+if ! goreleaser release --rm-dist --snapshot; then
+    echo "Error: Goreleaser build of all binaries & Docker images failed" >&2
+    exit 1
+fi
+
+# Build a CLI binary, compatible with the current OS & arch, so that we can run Interactive locally
+if ! goreleaser build --rm-dist --snapshot --id "${GORELEASER_CLI_BUILD_ID}" --single-target; then
+    echo "Error: Couldn't build the wrapper script-generating binary" >&2
+    exit 1
+fi
