535164d3bbbd246db04845d3c163e723fd53fda6
mieubrisse <mieubrisse@gmail.com>
2021-10-05 18:15:40 -0500

    Pre-release changes for version 1.22.9

--- core/testsuite_scripts/build-and-run-core.sh
+++ core/testsuite_scripts/build-and-run-core.sh
@@ -22,7 +22,7 @@ HELP_ACTION="help"
 
 KURTOSIS_DOCKER_ORG="kurtosistech"
 API_CONTAINER_DOCKER_REPO="kurtosis-core_api"
-API_CONTAINER_VERSION="1.22.8"
+API_CONTAINER_VERSION="1.22.9"
 
 # ====================== ARG PARSING =======================================================
 show_help_and_exit() {
