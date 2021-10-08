32efda47532f53d3e35909e469ad086ddc6f2b26
mieubrisse <mieubrisse@gmail.com>
2021-10-07 20:56:38 -0500

    Port _constants.sh and build.sh from kurtosis-core

---
+++ cli/scripts/_constants.sh
@@ -0,0 +1,20 @@
+# This script is intended to be sources by the other scripts in this directory
+DOCKER_ORG="kurtosistech"
+REPO_BASE="kurtosis-core"
+
+API_IMAGE="${DOCKER_ORG}/${REPO_BASE}_api"
+
+KURTOSIS_DIRPATH="$HOME/.kurtosis"
+
+BUILD_DIRNAME="build"
+GORELEASER_OUTPUT_DIRNAME="dist"
+
+GET_FIXED_DOCKER_IMAGES_TAG_SCRIPT_FILENAME="get-fixed-docker-images-tag.sh"
+
+# ------------------------ Testing  -------------------------------------------------------
+INTERNAL_TESTSUITE_IMAGE_SUFFIX="internal-testsuite"
+
+# ---------------------- Interactive  -----------------------------------------------------
+GORELEASER_CLI_BUILD_ID="cli"
+CLI_BINARY_FILENAME="kurtosis"  # Name of the CLI binary
+JAVASCRIPT_REPL_IMAGE="${DOCKER_ORG}/javascript-interactive-repl"
--- cli/scripts/build.sh
+++ cli/scripts/build.sh
@@ -3,18 +3,95 @@
 
 set -euo pipefail   # Bash "strict mode"
 script_dirpath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-lang_root_dirpath="$(dirname "${script_dirpath}")"
+root_dirpath="$(dirname "${script_dirpath}")"
 
 
 
 # ==================================================================================================
 #                                             Constants
 # ==================================================================================================
+source "${script_dirpath}/_constants.sh"
 
+GET_AUTOUPDATING_DOCKER_IMAGES_TAG_SCRIPT_FILENAME="get-autoupdating-docker-images-tag.sh"
+
+WRAPPER_GENERATOR_DIRNAME="wrapper_generator"
+WRAPPER_GENERATOR_BINARY_OUTPUT_FILENAME="wrapper-generator"
+WRAPPER_TEMPLATE_REL_FILEPATH="${WRAPPER_GENERATOR_DIRNAME}/kurtosis.template.sh"
+
+WRAPPER_SCRIPT_GENERATOR_GORELEASER_BUILD_ID="wrapper-generator"
+
+DEFAULT_SHOULD_PUBLISH_ARG="false"
+
+
+# ==================================================================================================
+#                                       Arg Parsing & Validation
+# ==================================================================================================
+show_helptext_and_exit() {
+    echo "Usage: $(basename "${0}") [should_publish_arg]"
+    echo ""
+    echo "  should_publish_arg  Whether the build artifacts should be published (default: ${DEFAULT_SHOULD_PUBLISH_ARG})"
+    echo ""
+    exit 1  # Exit with an error so that if this is accidentally called by CI, the script will fail
+}
+
+should_publish_arg="${1:-"${DEFAULT_SHOULD_PUBLISH_ARG}"}"
+if [ "${should_publish_arg}" != "true" ] && [ "${should_publish_arg}" != "false" ]; then
+    echo "Error: Invalid should-publish arg '${should_publish_arg}'" >&2
+    show_helptext_and_exit
+fi
 
 # ==================================================================================================
 #                                             Main Logic
 # ==================================================================================================
-cd "${lang_root_dirpath}"
-go build ./...
+build_dirpath="${root_dirpath}/${BUILD_DIRNAME}"
+if ! mkdir -p "${build_dirpath}"; then
+    echo "Error: Couldn't create build output dir '${build_dirpath}'" >&2
+    exit 1
+fi
+
+if ! fixed_docker_image_tag="$(bash "${script_dirpath}/${GET_FIXED_DOCKER_IMAGES_TAG_SCRIPT_FILENAME}")"; then
+    echo "Error: Couldn't get fixed Docker image tag" >&2
+    exit 1
+fi
+if ! autoupdating_docker_image_tag="$(bash "${script_dirpath}/${GET_AUTOUPDATING_DOCKER_IMAGES_TAG_SCRIPT_FILENAME}")"; then
+    echo "Error: Couldn't get autoupdating Docker image tag" >&2
+    exit 1
+fi
+
+# TODO GENERATE JAVASCRIPT REPL
+
+# These variables are used by Goreleaser
+export API_IMAGE \
+    DOCKER_ORG \
+    INTERNAL_TESTSUITE_IMAGE_SUFFIX \
+    JAVASCRIPT_REPL_IMAGE \
+    CLI_BINARY_FILENAME
+export FIXED_DOCKER_IMAGE_TAG="${fixed_docker_image_tag}"
+export AUTOUPDATING_DOCKER_IMAGE_TAG="${autoupdating_docker_image_tag}"
+if "${should_publish_arg}"; then
+    # This environment variable will be set ONLY when publishing, in the CI environment
+    # See the CI config for details on how this gets set
+    export FURY_TOKEN
+fi
+
+# We want to run goreleaser from the root
+cd "${root_dirpath}"
+
 go test ./...
+
+# Build all the Docker images
+if "${should_publish_arg}"; then
+    goreleaser_release_extra_args=""
+else
+    goreleaser_release_extra_args="--snapshot"
+fi
+if ! goreleaser release --rm-dist --skip-announce ${goreleaser_release_extra_args}; then
+    echo "Error: Goreleaser release of all binaries & Docker images failed" >&2
+    exit 1
+fi
+
+# Build a CLI binary, compatible with the current OS & arch, so that we can run interactive & testing locally
+if ! goreleaser build --rm-dist --snapshot --id "${GORELEASER_CLI_BUILD_ID}" --single-target; then
+    echo "Error: Couldn't build the wrapper script-generating binary" >&2
+    exit 1
+fi
