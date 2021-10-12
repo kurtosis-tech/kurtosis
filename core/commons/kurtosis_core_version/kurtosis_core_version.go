9156281eaf383b180a7fdb92a25839a65e6dbfa5
mieubrisse <mieubrisse@gmail.com>
2021-10-12 11:03:53 -0500

    First pass at own version constant

---
+++ core/commons/kurtosis_core_version/kurtosis_core_version.go
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021 - present Kurtosis Technologies Inc.
+ * All Rights Reserved.
+ */
+
+package kurtosis_core_version
+
+const (
+	// Used for library that consume this and need to know which version of the Docker image to pull
+	KurtosisCoreVersion = "1.23.2"
+)
