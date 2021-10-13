8052e3bd5edfd5e724d3e7ecff0982fa74e935f6
mieubrisse <mieubrisse@gmail.com>
2021-10-13 18:03:16 -0500

    PR review fixes

--- cli/cli/commands/repl/new/new.go
+++ cli/cli/commands/repl/new/new.go
@@ -2,13 +2,11 @@ package new
 
 import (
 	"context"
-	"fmt"
 	"github.com/docker/docker/client"
 	"github.com/kurtosis-tech/container-engine-lib/lib/docker_manager"
 	"github.com/kurtosis-tech/kurtosis-cli/cli/best_effort_image_puller"
 	"github.com/kurtosis-tech/kurtosis-cli/cli/defaults"
 	"github.com/kurtosis-tech/kurtosis-cli/cli/enclave_manager"
-	"github.com/kurtosis-tech/kurtosis-cli/cli/logrus_log_levels"
 	"github.com/kurtosis-tech/kurtosis-cli/cli/positional_arg_parser"
 	"github.com/kurtosis-tech/kurtosis-cli/cli/repl_runner"
 	"github.com/palantir/stacktrace"
@@ -19,36 +17,23 @@ import (
 
 const (
 	enclaveIDArg           = "enclave-id"
-	kurtosisLogLevelArg    = "kurtosis-log-level"
 	javascriptReplImageArg = "js-repl-image"
 )
 
-var defaultKurtosisLogLevel = logrus.InfoLevel.String()
 var positionalArgs = []string{
 	enclaveIDArg,
 }
 
-var kurtosisLogLevelStr string
 var jsReplImage string
 
 var NewCmd = &cobra.Command{
 	Use:                   "new [flags] " + strings.Join(positionalArgs, " "),
 	DisableFlagsInUseLine: true,
-	Short:                 "Create a new Javascript REPL inside a Kurtosis Enclave",
+	Short:                 "Create a new Javascript REPL inside the given Kurtosis enclave",
 	RunE:                  run,
 }
 
 func init() {
-	NewCmd.Flags().StringVarP(
-		&kurtosisLogLevelStr,
-		kurtosisLogLevelArg,
-		"l",
-		defaultKurtosisLogLevel,
-		fmt.Sprintf(
-			"The log level that Kurtosis itself should log at (%v)",
-			strings.Join(logrus_log_levels.GetAcceptableLogLevelStrs(), "|"),
-		),
-	)
 	NewCmd.Flags().StringVarP(
 		&jsReplImage,
 		javascriptReplImageArg,
@@ -59,11 +44,7 @@ func init() {
 }
 
 func run(cmd *cobra.Command, args []string) error {
-	kurtosisLogLevel, err := logrus.ParseLevel(kurtosisLogLevelStr)
-	if err != nil {
-		return stacktrace.Propagate(err, "An error occurred parsing Kurtosis loglevel string '%v' to a log level object", kurtosisLogLevelStr)
-	}
-	logrus.SetLevel(kurtosisLogLevel)
+	// TODO Set CLI loglevel from a global flag
 
 	dockerClient, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
@@ -84,7 +65,7 @@ func run(cmd *cobra.Command, args []string) error {
 
 	enclaveManager := enclave_manager.NewEnclaveManager(dockerClient)
 
-	enclaveCtx, err := enclaveManager.GetEnclaveContext(context.Background(), enclaveId, logrus.StandardLogger())
+	enclaveCtx, err := enclaveManager.GetEnclave(context.Background(), enclaveId, logrus.StandardLogger())
 	if err != nil {
 		return stacktrace.Propagate(err, "An error occurred getting enclave context for enclave ID '%v'", enclaveId)
 	}
