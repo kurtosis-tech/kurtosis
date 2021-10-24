bff49ed2ccf7d96801d55d65aa07e338c046018c
mieubrisse <mieubrisse@gmail.com>
2021-10-23 20:56:25 -0500

    Pre-release changes for version 0.5.0

--- cli/cli/kurtosis_cli_version/kurtosis_cli_version.go
+++ cli/cli/kurtosis_cli_version/kurtosis_cli_version.go
@@ -2,5 +2,5 @@ package kurtosis_cli_version
 
 const (
 	// !!!!!!!! DO NOT UPDATE THIS! It will be updated automatically during release !!!!!!!!!!
-	KurtosisCLIVersion = "0.4.3"
+	KurtosisCLIVersion = "0.5.0"
 )
