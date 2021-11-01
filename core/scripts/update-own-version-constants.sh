5d8e2367dd2723417f7394b0edb7b07e06199bb4
mieubrisse <mieubrisse@gmail.com>
2021-11-01 15:26:39 -0600

    Attempting to fix own-version constants update script

--- core/scripts/update-own-version-constants.sh
+++ core/scripts/update-own-version-constants.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 # 2021-07-08 WATERMARK, DO NOT REMOVE - This script was generated from the Kurtosis Bash script template
 
+# In order for testsuites and Lambdas to report the version of Kurtosis API that they depend on, we provide a constant
+#  that contains the version of this library. This script is responsible for updating that constant in all the various
+#  language, and will be run as part of the release flow of this repo.
+
 set -euo pipefail   # Bash "strict mode"
 script_dirpath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 root_dirpath="$(dirname "${script_dirpath}")"
@@ -10,10 +14,23 @@ root_dirpath="$(dirname "${script_dirpath}")"
 # ==================================================================================================
 #                                             Constants
 # ==================================================================================================
+API_DIRNAME="api"
+SUPPORTED_LANGS_FILENAME="supported-languages.txt"
 UPDATE_VERSION_IN_FILE_SCRIPT_FILENAME="update-version-in-file.sh" # From devtools; expected to be on PATH
 
-CONSTANT_FILE_RELATIVE_FILEPATH="commons/kurtosis_core_version/kurtosis_core_version.go"
-CONSTANT_PATTERN="KurtosisCoreVersion = \"%s\""
+# Per-language filepath that needs updating to update the constant, RELATIVE TO THE LANGUAGE ROOT
+declare -A CONSTANT_FILE_RELATIVE_FILEPATHS
+
+# Per-language patterns matching the constant line, which will be used for updating the version
+declare -A CONSTANT_PATTERNS
+
+# Golang
+CONSTANT_FILE_RELATIVE_FILEPATHS["golang"]="lib/kurtosis_api_version_const/kurtosis_api_version_const.go"
+CONSTANT_PATTERNS["golang"]="KurtosisApiVersion = \"%s\""
+
+# Typescript
+CONSTANT_FILE_RELATIVE_FILEPATHS["typescript"]="src/lib/kurtosis_api_version_const.ts"
+CONSTANT_PATTERNS["typescript"]="KURTOSIS_API_VERSION: string = \"%s\""
 
 
 # ==================================================================================================
@@ -39,8 +56,27 @@ fi
 # ==================================================================================================
 #                                             Main Logic
 # ==================================================================================================
-constant_file_abs_filepath="${root_dirpath}/${CONSTANT_FILE_RELATIVE_FILEPATH}"
-if ! bash "${UPDATE_VERSION_IN_FILE_SCRIPT_FILENAME}" "${constant_file_abs_filepath}" "${CONSTANT_PATTERN}" "${new_version}"; then
-    echo "Error: Couldn't update file '${constant_file_abs_filepath}' with new version '${new_version}' using pattern '${CONSTANT_PATTERN}'" >&2
-    exit 1
-fi
+echo "Updating the constants containing this library's version for all supported languages..."
+api_dirpath="${root_dirpath}/${API_DIRNAME}"
+supported_langs_filepath="${api_dirpath}/${SUPPORTED_LANGS_FILENAME}"
+for lang in $(cat "${supported_langs_filepath}"); do
+    constant_file_rel_filepath="${CONSTANT_FILE_RELATIVE_FILEPATHS["${lang}"]}"
+    if [ -z "${constant_file_rel_filepath}" ]; then
+        echo "Error: No relative filepath to a constant file that needs replacing was found for language '${lang}'; this script needs to be updated with this information" >&2
+        exit 1
+    fi
+
+    constant_file_abs_filepath="${api_dirpath}/${lang}/${constant_file_rel_filepath}"
+
+    pattern="${CONSTANT_PATTERNS["${lang}"]}"
+    if [ -z "${pattern}" ]; then
+        echo "Error: No replacement pattern was found for language '${lang}'; this script needs to be updated with this information" >&2
+        exit 1
+    fi
+
+    if ! "${UPDATE_VERSION_IN_FILE_SCRIPT_FILENAME}" "${constant_file_abs_filepath}" "${pattern}" "${new_version}"; then
+        echo "Error: An error occurred setting new version '${new_version}' in '${lang}' constants file '${to_update_abs_filepath}' using pattern '${replacement_pattern}'" >&2
+        exit 1
+    fi
+done
+echo "Successfully updated the constants containing this library's version have been updated for all supported languages"
