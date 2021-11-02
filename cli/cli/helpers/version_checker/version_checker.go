0c37e9b7501064f78c36f6b6d523f4489c8d9c23
Leandro Poroli <leandro.poroli@kurtosistech.com>
2021-11-02 17:11:37 -0300

    Changes after PR review

--- cli/cli/helpers/version_checker/version_checker.go
+++ cli/cli/helpers/version_checker/version_checker.go
@@ -28,11 +28,13 @@ func CheckLatestVersion() {
 	isLatestVersion, latestVersion, err := isLatestVersion()
 	if err != nil {
 		logrus.Warning("An error occurred trying to check if you are running the lates Kurtosis CLI version.")
+		logrus.Debugf("Checking latest version error: '%v", err)
 		logrus.Warningf("Your current version is '%v'", kurtosis_cli_version.KurtosisCLIVersion)
 		logrus.Warningf("And you can manually check if your current version is the latest through this page: https://github.com/kurtosis-tech/kurtosis-cli-release-artifacts/releases")
+		return
 	}
-	if !isLatestVersion && err == nil {
-		logrus.Warningf("You are running an old version of the Kurtosis CLI, we suggest you to update it to the latest version '%v'", latestVersion)
+	if !isLatestVersion {
+		logrus.Warningf("You are running an old version of the Kurtosis CLI; we suggest you to update it to the latest version, '%v'", latestVersion)
 		logrus.Warning("You can update it to the latest version doing the following steps")
 		logrus.Warning("If you installed Kurtosis CLI with Brew, execute:")
 		logrus.Warning("sudo brew uninstall kurtosis-tech/tap/kurtosis")
@@ -47,6 +49,7 @@ func CheckLatestVersion() {
 		logrus.Warning("If you manually installed Kurtosis CLI with a DEB, RPM or an APL package:")
 		logrus.Warning("Download the latest released package from the artifact page https://github.com/kurtosis-tech/kurtosis-cli-release-artifacts/releases")
 	}
+	return
 }
 
 func isLatestVersion() (bool, string, error) {
@@ -84,19 +87,25 @@ func getLatestReleaseVersionFromGitHub() (string, error) {
 	if err != nil {
 		return "", stacktrace.Propagate(err, "An error occurred executing HTTP GET request to URL '%v' ", latestReleaseOnGitHubURL)
 	}
-	defer response.Body.Close()
+	defer func() {
+		if err := response.Body.Close(); err != nil {
+			logrus.Warnf("We tried to close the response body, but doing so threw an error:\n%v", err)
+		}
+	}()
 
 	bodyBytes, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		return "", stacktrace.Propagate(err, "An error occurred reading the HTTP response body")
 	}
-	json.Unmarshal(bodyBytes, &responseObject)
 
 	if err := json.Unmarshal(bodyBytes, &responseObject); err != nil {
 		return "", stacktrace.Propagate(err, "An error occurred deserializing the latest release body response")
 	}
 
 	latestVersion := responseObject.TagName
+	if latestVersion == "" {
+		return "", stacktrace.Propagate(err, "The latest release version got from GitHub releases is empty")
+	}
 
 	return latestVersion, nil
 }
