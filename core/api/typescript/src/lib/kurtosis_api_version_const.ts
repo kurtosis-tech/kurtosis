3e43aba2dd7518e0d98134ad4bd565cb4bc1a0f0
mieubrisse <mieubrisse@gmail.com>
2021-11-07 15:05:23 -0600

    Pre-release changes for version 1.29.1

--- core/api/typescript/src/lib/kurtosis_api_version_const.ts
+++ core/api/typescript/src/lib/kurtosis_api_version_const.ts
@@ -1,2 +1,2 @@
 // DO NOT UPDATE THIS VALUE! It will get updated automatically during the release process
-export const KURTOSIS_API_VERSION: string = "1.29.0"
+export const KURTOSIS_API_VERSION: string = "1.29.1"
