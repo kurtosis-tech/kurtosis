f82be6fa961665dec4dfdc8aadfdee66d63df3bd
mieubrisse <mieubrisse@gmail.com>
2021-11-07 20:19:26 -0600

    New BasicDatastoreTest compiles

--- cli/go.mod
+++ cli/go.mod
@@ -13,7 +13,10 @@ require (
 	github.com/kurtosis-tech/container-engine-lib v0.0.0-20211106215242-31501d8d7f8d
 	github.com/kurtosis-tech/example-api-server/api/golang v0.0.0-20211101152411-a56fef9e73dd
 	github.com/kurtosis-tech/example-datastore-server/api/golang v0.0.0-20211101145825-570cf60ea641
-	github.com/kurtosis-tech/kurtosis-engine-api-lib/api/golang v0.0.0-20211107211522-ef09c391d535 // indirect
+	github.com/kurtosis-tech/kurtosis-core-api-lib/api/golang v0.0.0-20211104015055-c5e7cefde0aa
+	github.com/kurtosis-tech/kurtosis-engine-api-lib/api/golang v0.0.0-20211107211522-ef09c391d535
+	github.com/kurtosis-tech/kurtosis-testsuite-api-lib/golang v0.0.0-20211027222833-7233d903873e
+	github.com/kurtosis-tech/stacktrace v0.0.0-20211028211901-1c67a77b5409
 	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/cobra v1.2.1
 	github.com/stretchr/testify v1.7.0
--- cli/golang_internal_testsuite/testsuite/basic_datastore_test.go
+++ cli/golang_internal_testsuite/testsuite/basic_datastore_test.go
@@ -6,13 +6,10 @@ import (
 	"github.com/kurtosis-tech/example-datastore-server/api/golang/datastore_rpc_api_bindings"
 	"github.com/kurtosis-tech/kurtosis-cli/golang_internal_testsuite/client_helpers"
 	"github.com/kurtosis-tech/kurtosis-core-api-lib/api/golang/lib/enclaves"
-	"github.com/kurtosis-tech/kurtosis-core-api-lib/api/golang/lib/enclaves"
 	"github.com/kurtosis-tech/kurtosis-core-api-lib/api/golang/lib/services"
 	"github.com/kurtosis-tech/kurtosis-engine-api-lib/api/golang/lib/kurtosis_context"
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
-	"io/ioutil"
-	"os"
 	"testing"
 	"time"
 )
@@ -40,8 +37,8 @@ func TestBasicDatastoreTest(t *testing.T) {
 	assert.NoError(t, err, "An error occurred creating enclave '%v'", enclaveId)
 	defer func() {
 		if err := kurtosisCtx.DestroyEnclave(context.Background(), enclaveId); err != nil {
-			logrus.Error("An error occurred destroying enclave '%v' that we created for this test:\n%v", err)
-			logrus.Error("ACTION REQUIRED: You'll need to delete enclave '%v' manually!!!!")
+			logrus.Errorf("An error occurred destroying enclave '%v' that we created for this test:\n%v", enclaveId, err)
+			logrus.Errorf("ACTION REQUIRED: You'll need to delete enclave '%v' manually!!!!", enclaveId)
 		}
 	}()
 
@@ -49,61 +46,29 @@ func TestBasicDatastoreTest(t *testing.T) {
 	datastoreContainerConfigSupplier := client_helpers.GetDatastoreContainerConfigSupplier(datastoreImage)
 
 	serviceContext, hostPortBindings, err := enclaveCtx.AddService(datastoreServiceId, datastoreContainerConfigSupplier)
-	if err != nil {
-		return nil, stacktrace.Propagate(err, "An error occurred adding the datastore service")
-	}
+	assert.NoError(t, err, "An error occurred adding the datastore service")
 
 	datastoreClient, datastoreClientConnCloseFunc, err := client_helpers.NewDatastoreClient(serviceContext.GetIPAddress())
-	if err != nil {
-		return nil, stacktrace.Propagate(err, "An error occurred creating a new datastore client for service with ID '%v' and IP address '%v'", datastoreServiceId, serviceContext.GetIPAddress())
-	}
+	assert.NoError(t, err, "An error occurred creating a new datastore client for service with ID '%v' and IP address '%v'", datastoreServiceId, serviceContext.GetIPAddress())
 	defer func() {
 		if err := datastoreClientConnCloseFunc(); err != nil {
 			logrus.Warnf("We tried to close the datastore client, but doing so threw an error:\n%v", err)
 		}
 	}()
 
-	err = client_helpers.WaitForHealthy(ctx, datastoreClient, waitForStartupMaxPolls, waitForStartupDelayMilliseconds)
-	if err != nil {
-		return nil, stacktrace.Propagate(err, "An error occurred waiting for the datastore service to become available")
-	}
+	err = client_helpers.WaitForHealthy(context.Background(), datastoreClient, waitForStartupMaxPolls, waitForStartupDelayMilliseconds)
+	assert.NoError(t, err, "An error occurred waiting for the datastore service to become available")
 
 	logrus.Infof("Added datastore service with host port bindings: %+v", hostPortBindings)
-	return networkCtx, nil
-}
 
-func (test BasicDatastoreTest) Run(network networks.Network) error {
-	ctx := context.Background()
-
-	// Necessary because Go doesn't have generics
-	castedNetwork := network.(*networks.NetworkContext)
-
-	serviceContext, err := castedNetwork.GetServiceContext(datastoreServiceId)
-	if err != nil {
-		return stacktrace.Propagate(err, "An error occurred getting the datastore service info")
-	}
-
-	datastoreClient, datastoreClientConnCloseFunc, err := client_helpers.NewDatastoreClient(serviceContext.GetIPAddress())
-	if err != nil {
-		return stacktrace.Propagate(err, "An error occurred creating a new datastore client for service with ID '%v' and IP address '%v'", datastoreServiceId, serviceContext.GetIPAddress())
-	}
-	defer func() {
-		if err := datastoreClientConnCloseFunc(); err != nil {
-			logrus.Warnf("We tried to close the datastore client, but doing so threw an error:\n%v", err)
-		}
-	}()
-
+	// ------------------------------------- TEST RUN ----------------------------------------------
 	logrus.Infof("Verifying that key '%v' doesn't already exist...", testKey)
 	existsArgs := &datastore_rpc_api_bindings.ExistsArgs{
 		Key: testKey,
 	}
-	existsResponse, err := datastoreClient.Exists(ctx, existsArgs)
-	if err != nil {
-		return stacktrace.Propagate(err, "An error occurred checking if the test key exists")
-	}
-	if existsResponse.GetExists() {
-		return stacktrace.NewError("Test key should not exist yet")
-	}
+	existsResponse, err := datastoreClient.Exists(context.Background(), existsArgs)
+	assert.NoError(t, err, "An error occurred checking if the test key exists")
+	assert.False(t, existsResponse.GetExists(), "Test key should not exist yet")
 	logrus.Infof("Confirmed that key '%v' doesn't already exist", testKey)
 
 	logrus.Infof("Inserting value '%v' at key '%v'...", testKey, testValue)
@@ -111,22 +76,16 @@ func (test BasicDatastoreTest) Run(network networks.Network) error {
 		Key:   testKey,
 		Value: testValue,
 	}
-	if _, err = datastoreClient.Upsert(ctx, upsertArgs); err != nil {
-		return stacktrace.Propagate(err, "An error occurred upserting the test key")
-	}
+	_, err = datastoreClient.Upsert(context.Background(), upsertArgs)
+	assert.NoError(t, err, "An error occurred upserting the test key")
 	logrus.Infof("Inserted value successfully")
 
 	logrus.Infof("Getting the key we just inserted to verify the value...")
 	getArgs := &datastore_rpc_api_bindings.GetArgs{
 		Key: testKey,
 	}
-	getResponse, err := datastoreClient.Get(ctx, getArgs)
-	if err != nil {
-		return stacktrace.Propagate(err, "An error occurred getting the test key after upload")
-	}
-	if getResponse.GetValue() != testValue {
-		return stacktrace.NewError("Returned value '%v' != test value '%v'", getResponse.GetValue(), testValue)
-	}
+	getResponse, err := datastoreClient.Get(context.Background(), getArgs)
+	assert.NoError(t, err, "An error occurred getting the test key after upload")
+	assert.Equal(t, testValue, getResponse.GetValue(), "Returned value '%v' != test value '%v'", getResponse.GetValue(), testValue)
 	logrus.Info("Value verified")
-	return nil
 }
