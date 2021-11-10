2bfe3c2c3b52b3aabf2102cdcf2e18823beb835a
mieubrisse <mieubrisse@gmail.com>
2021-11-09 20:11:41 -0600

    Pre-release changes for version 1.2.2

--- engine/api/typescript/src/kurtosis_engine_api_version/kurtosis_engine_api_version.ts
+++ engine/api/typescript/src/kurtosis_engine_api_version/kurtosis_engine_api_version.ts
@@ -1,2 +1,2 @@
 // DO NOT UPDATE THIS VALUE! It will get updated automatically during the release process
-export const KURTOSIS_ENGINE_API_VERSION: string = "1.2.1";
+export const KURTOSIS_ENGINE_API_VERSION: string = "1.2.2";
