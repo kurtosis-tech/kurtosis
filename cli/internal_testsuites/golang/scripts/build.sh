d072715eb30d08d032bf04b9d1edc4271026dd58
mieubrisse <mieubrisse@gmail.com>
2021-11-12 18:06:28 -0600

    Almost-working files test

--- cli/internal_testsuites/golang/scripts/build.sh
+++ cli/internal_testsuites/golang/scripts/build.sh
@@ -18,4 +18,4 @@ lang_root_dirpath="$(dirname "${script_dirpath}")"
 cd "${lang_root_dirpath}"
 go build ./...
 # The count=1 disables caching for the testsuite (which we want, because the engine server might have changed even though the test code didn't)
-go test ./... -count=1
+go test ./... -count=1 -timeout 3m
