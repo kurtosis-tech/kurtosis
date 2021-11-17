a9aeb892b3f3c1a6ae9deaec19b420c9566083eb
mieubrisse <mieubrisse@gmail.com>
2021-11-16 19:34:35 -0600

    Pre-release changes for version 1.5.6

--- engine/launcher/engine_server_launcher/engine_server_launcher.go
+++ engine/launcher/engine_server_launcher/engine_server_launcher.go
@@ -20,7 +20,7 @@ import (
 
 const (
 	// !!!!!!!!!!!!!!!!!! DO NOT MODIFY THIS! IT WILL BE UPDATED AUTOMATICALLY DURING THE RELEASE PROCESS !!!!!!!!!!!!!!!
-	DefaultVersion = "1.5.5"
+	DefaultVersion = "1.5.6"
 	// !!!!!!!!!!!!!!!!!! DO NOT MODIFY THIS! IT WILL BE UPDATED AUTOMATICALLY DURING THE RELEASE PROCESS !!!!!!!!!!!!!!!
 
 	// TODO This should come from the same logic that builds the server image!!!!!
