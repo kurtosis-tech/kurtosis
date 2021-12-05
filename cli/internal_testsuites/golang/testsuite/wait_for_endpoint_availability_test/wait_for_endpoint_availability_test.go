64cdce694831fb7d87663f8ed3a99dc9f791883a
mieubrisse <mieubrisse@gmail.com>
2021-12-05 10:16:46 -0600

    Passing testsuite

--- cli/internal_testsuites/golang/testsuite/wait_for_endpoint_availability_test/wait_for_endpoint_availability_test.go
+++ cli/internal_testsuites/golang/testsuite/wait_for_endpoint_availability_test/wait_for_endpoint_availability_test.go
@@ -2,7 +2,6 @@ package wait_for_endpoint_availability_test
 
 import (
 	"context"
-	"fmt"
 	"github.com/kurtosis-tech/kurtosis-cli/golang_internal_testsuite/test_helpers"
 	"github.com/kurtosis-tech/kurtosis-core-api-lib/api/golang/lib/services"
 	"github.com/sirupsen/logrus"
@@ -14,15 +13,20 @@ const (
 	testName = "wait-for-endpoint-availability-test"
 	isPartitioningEnabled = false
 
-	dockerGettingStartedImage                             = "docker/getting-started"
-	exampleServiceId                   services.ServiceID = "docker-getting-started"
-	exampleServicePortNumInsideNetwork                    = 80
-	healthCheckUrlSlug                                    = ""
-	healthyValue                                          = ""
+	dockerGettingStartedImage                       = "docker/getting-started"
+	exampleServiceId             services.ServiceID = "docker-getting-started"
+	exampleServicePortId = "http"
+	exampleServicePrivatePortNum                    = 80
+	healthCheckUrlSlug                              = ""
+	healthyValue                                    = ""
 
 	waitForStartupTimeBetweenPolls = 1
 	waitForStartupMaxPolls         = 15
 	waitInitialDelayMilliseconds   = 500
+)
+var exampleServicePrivatePortSpec = services.NewPortSpec(
+	exampleServicePrivatePortNum,
+	services.PortProtocol_TCP,
 )
 
 func TestWaitForEndpointAvailabilityFunction(t *testing.T) {
@@ -36,12 +40,12 @@ func TestWaitForEndpointAvailabilityFunction(t *testing.T) {
 	// ------------------------------------- TEST SETUP ----------------------------------------------
 	configSupplier := getExampleServiceConfigSupplier()
 
-	_, _, err = enclaveCtx.AddService(exampleServiceId, configSupplier)
+	_, err = enclaveCtx.AddService(exampleServiceId, configSupplier)
 	require.NoError(t, err, "An error occurred adding the datastore service")
 
 	// ------------------------------------- TEST RUN ----------------------------------------------
 
-	portUint32 := uint32(exampleServicePortNumInsideNetwork)
+	portUint32 := uint32(exampleServicePrivatePortNum)
 
 	require.NoError(
 		t,
@@ -58,9 +62,9 @@ func getExampleServiceConfigSupplier() func(ipAddr string, sharedDirectory *serv
 	containerConfigSupplier := func(ipAddr string, sharedDirectory *services.SharedPath) (*services.ContainerConfig, error) {
 		containerConfig := services.NewContainerConfigBuilder(
 			dockerGettingStartedImage,
-		).WithUsedPorts(
-			map[string]bool{fmt.Sprintf("%v/tcp", exampleServicePortNumInsideNetwork): true},
-		).Build()
+		).WithUsedPorts(map[string]*services.PortSpec{
+			exampleServicePortId: exampleServicePrivatePortSpec,
+		}).Build()
 		return containerConfig, nil
 	}
 	return containerConfigSupplier
