7a61b94e7557608d4e6a61fbd1b9464d73325bc6
mieubrisse <mieubrisse@gmail.com>
2021-12-05 17:17:31 -0600

    Ready to make module test pass

--- cli/internal_testsuites/golang/testsuite/module_test/module_test.go
+++ cli/internal_testsuites/golang/testsuite/module_test/module_test.go
@@ -3,7 +3,6 @@ package module_test
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"github.com/kurtosis-tech/example-datastore-server/api/golang/datastore_rpc_api_bindings"
 	"github.com/kurtosis-tech/kurtosis-cli/golang_internal_testsuite/test_helpers"
 	"github.com/kurtosis-tech/kurtosis-core-api-lib/api/golang/lib/modules"
