3d8d877f7232b272022717f33bc0e97b99db18cd
mieubrisse <mieubrisse@gmail.com>
2021-12-05 16:07:40 -0600

    Pre-release changes for version 1.7.1

--- engine/launcher/engine_server_launcher/engine_server_launcher.go
+++ engine/launcher/engine_server_launcher/engine_server_launcher.go
@@ -22,7 +22,7 @@ import (
 
 const (
 	// !!!!!!!!!!!!!!!!!! DO NOT MODIFY THIS! IT WILL BE UPDATED AUTOMATICALLY DURING THE RELEASE PROCESS !!!!!!!!!!!!!!!
-	DefaultVersion = "1.7.0"
+	DefaultVersion = "1.7.1"
 	// !!!!!!!!!!!!!!!!!! DO NOT MODIFY THIS! IT WILL BE UPDATED AUTOMATICALLY DURING THE RELEASE PROCESS !!!!!!!!!!!!!!!
 
 	// TODO This should come from the same logic that builds the server image!!!!!
