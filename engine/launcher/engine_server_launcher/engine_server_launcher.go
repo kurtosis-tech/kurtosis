54323b5019c25031647dee8592a4959d8372b800
Leandro Poroli <leandro.poroli@kurtosistech.com>
2021-12-07 10:19:29 -0300

    Pre-release changes for version 1.7.4

--- engine/launcher/engine_server_launcher/engine_server_launcher.go
+++ engine/launcher/engine_server_launcher/engine_server_launcher.go
@@ -22,7 +22,7 @@ import (
 
 const (
 	// !!!!!!!!!!!!!!!!!! DO NOT MODIFY THIS! IT WILL BE UPDATED AUTOMATICALLY DURING THE RELEASE PROCESS !!!!!!!!!!!!!!!
-	DefaultVersion = "1.7.3"
+	DefaultVersion = "1.7.4"
 	// !!!!!!!!!!!!!!!!!! DO NOT MODIFY THIS! IT WILL BE UPDATED AUTOMATICALLY DURING THE RELEASE PROCESS !!!!!!!!!!!!!!!
 
 	// TODO This should come from the same logic that builds the server image!!!!!
