e8d22063e0e5d74e3346082eb06ecdf9362bd824
mario fernandez <mario.fernandez@kurtosistech.com>
2021-12-07 11:42:53 -0300

    Pre-release changes for version 1.7.5

--- engine/launcher/engine_server_launcher/engine_server_launcher.go
+++ engine/launcher/engine_server_launcher/engine_server_launcher.go
@@ -22,7 +22,7 @@ import (
 
 const (
 	// !!!!!!!!!!!!!!!!!! DO NOT MODIFY THIS! IT WILL BE UPDATED AUTOMATICALLY DURING THE RELEASE PROCESS !!!!!!!!!!!!!!!
-	DefaultVersion = "1.7.4"
+	DefaultVersion = "1.7.5"
 	// !!!!!!!!!!!!!!!!!! DO NOT MODIFY THIS! IT WILL BE UPDATED AUTOMATICALLY DURING THE RELEASE PROCESS !!!!!!!!!!!!!!!
 
 	// TODO This should come from the same logic that builds the server image!!!!!
