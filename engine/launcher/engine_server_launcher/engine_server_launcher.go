9df344e240f7a81c9ce58a632070949d41915abf
Leandro Poroli <leandro.poroli@kurtosistech.com>
2021-12-13 09:36:03 -0300

    Pre-release changes for version 1.8.0

--- engine/launcher/engine_server_launcher/engine_server_launcher.go
+++ engine/launcher/engine_server_launcher/engine_server_launcher.go
@@ -22,7 +22,7 @@ import (
 
 const (
 	// !!!!!!!!!!!!!!!!!! DO NOT MODIFY THIS! IT WILL BE UPDATED AUTOMATICALLY DURING THE RELEASE PROCESS !!!!!!!!!!!!!!!
-	KurtosisEngineVersion = "1.7.7"
+	KurtosisEngineVersion = "1.8.0"
 	// !!!!!!!!!!!!!!!!!! DO NOT MODIFY THIS! IT WILL BE UPDATED AUTOMATICALLY DURING THE RELEASE PROCESS !!!!!!!!!!!!!!!
 
 	// TODO This should come from the same logic that builds the server image!!!!!
