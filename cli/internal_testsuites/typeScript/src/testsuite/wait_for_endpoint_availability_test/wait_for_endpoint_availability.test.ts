4979d770443f9c9f04256cfdb24f7955b3f78e85
kzhama@genium.io <kzhama@genium.io>
2021-12-14 13:46:42 -0300

    add test

---
+++ cli/internal_testsuites/typeScript/src/testsuite/wait_for_endpoint_availability_test/wait_for_endpoint_availability.test.ts
@@ -0,0 +1,83 @@
+import { ContainerConfig, ContainerConfigBuilder, PortProtocol, PortSpec, ServiceID, SharedPath } from "kurtosis-core-api-lib"
+import { ok, Result } from "neverthrow"
+
+import { createEnclave } from "../../test_helpers/enclave_setup"
+
+const TEST_NAME = "wait-for-endpoint-availability-test"
+const IS_PARTITIONING_ENABLED = false
+
+const DOCKER_GETTING_STARTED_IMAGE = "docker/getting-started"
+const EXAMPLE_SERVICE_ID:ServiceID = "docker-getting-started"
+const EXAMPLE_SERVICE_PORT_ID = "http"
+const EXAMPLE_SERVICE_PRIVATE_PORT_NUM = 80
+const HEALTH_CHECK_URL_SLUG = ""
+const HEALTHY_VALUE = ""
+
+const WAIT_FOR_STARTUP_TIME_BETWEEN_POLLS = 1
+const WAIT_FOR_STARTUP_MAX_POLLS = 15
+const WAIT_INITIAL_DELAY_MILLISECONDS = 500
+
+const exampleServicePrivatePortSpec = new PortSpec(EXAMPLE_SERVICE_PRIVATE_PORT_NUM, PortProtocol.TCP)
+
+jest.setTimeout(30000)
+
+test("Test wait for endpoint availability", async () => {
+    // ------------------------------------- ENGINE SETUP ----------------------------------------------
+    const createEnclaveResult = await createEnclave(TEST_NAME, IS_PARTITIONING_ENABLED)
+
+    if(createEnclaveResult.isErr()) { throw createEnclaveResult.error }
+
+    const { enclaveContext, stopEnclaveFunction } = createEnclaveResult.value
+
+    try {
+        // ------------------------------------- TEST SETUP ----------------------------------------------
+
+        const configSupplier = getExampleServiceConfigSupplier()
+
+        const addServiceResult = await enclaveContext.addService(EXAMPLE_SERVICE_ID, configSupplier)
+
+        if(addServiceResult.isErr()){
+            throw addServiceResult.error
+        }
+
+        // ------------------------------------- TEST RUN ----------------------------------------------
+
+        const waitWaitForHttpGetEndpointAvailabilityResult = 
+            await enclaveContext.waitForHttpGetEndpointAvailability(
+                    EXAMPLE_SERVICE_ID, 
+                    EXAMPLE_SERVICE_PRIVATE_PORT_NUM, 
+                    HEALTH_CHECK_URL_SLUG, 
+                    WAIT_INITIAL_DELAY_MILLISECONDS, 
+                    WAIT_FOR_STARTUP_MAX_POLLS, 
+                    WAIT_FOR_STARTUP_TIME_BETWEEN_POLLS, 
+                    HEALTHY_VALUE 
+            );
+        
+        
+
+
+
+    }finally{
+        stopEnclaveFunction()
+    }
+})
+
+
+// ====================================================================================================
+//                                       Private helper functions
+// ====================================================================================================
+
+function getExampleServiceConfigSupplier(): (ipAddr:string, shareDirectory: SharedPath) => Result<ContainerConfig, Error> {
+    const containerConfigSupplier = (ipAddr:string, shareDirectory: SharedPath):Result<ContainerConfig, Error> => {
+        const exampleServicePort = new Map<string, PortSpec>()
+        exampleServicePort.set(EXAMPLE_SERVICE_PORT_ID, exampleServicePrivatePortSpec)
+        
+        const containerConfig = new ContainerConfigBuilder(DOCKER_GETTING_STARTED_IMAGE)
+            .withUsedPorts(exampleServicePort)
+            .build()
+
+        return ok(containerConfig)
+    }
+
+    return containerConfigSupplier
+}
