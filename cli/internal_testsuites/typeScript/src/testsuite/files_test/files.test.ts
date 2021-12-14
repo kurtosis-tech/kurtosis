37491228831ee056368695af3d7cd8fc831d21df
kzhama@genium.io <kzhama@genium.io>
2021-12-14 16:11:27 -0300

    resovle comments

--- cli/internal_testsuites/typeScript/src/testsuite/files_test/files.test.ts
+++ cli/internal_testsuites/typeScript/src/testsuite/files_test/files.test.ts
@@ -2,6 +2,7 @@ import { ContainerConfig, ContainerConfigBuilder, ServiceID, SharedPath } from "
 import log from "loglevel"
 import { ok, Result, err } from "neverthrow";
 import * as fs from "fs"
+import * as utf8 from "utf8"
 
 import { createEnclave } from "../../test_helpers/enclave_setup";
 
@@ -17,7 +18,6 @@ const EXPECTED_TEST_FILE2_CONTENTS = "This is another test file"
 const GENERATED_FILE_PERM_BITS = 0o644
 
 // Mapping of filepath_rel_to_shared_dir_root -> contents
-
 const generatedFileRelPathsAndContents = new Map<string,string>();
 generatedFileRelPathsAndContents.set("test1.txt", EXPECTED_TEST_FILE1_CONTENTS)
 generatedFileRelPathsAndContents.set("test2.txt", EXPECTED_TEST_FILE2_CONTENTS)
@@ -112,10 +112,10 @@ function generateFileInServiceContainer(relativePath: string, contents: string, 
     const sharedFilepath = sharedDirectory.getChildPath(relativePath)
     const absFilepathOnThisContainer = sharedFilepath.getAbsPathOnThisContainer()
 
-    const bytesArray = new Uint8Array(toUTF8Array(contents))
+    const bytesArray = utf8.encode(contents)
 
     try {
-        fs.writeFileSync(sharedFilepath.getAbsPathOnThisContainer(),bytesArray, { mode: 0o644 })
+        fs.writeFileSync(sharedFilepath.getAbsPathOnThisContainer(),bytesArray, { mode: GENERATED_FILE_PERM_BITS })
     }catch(error){
         log.error(`An error occurred writing contents "${contents}" to file "${absFilepathOnThisContainer}" with perms "${GENERATED_FILE_PERM_BITS}"`)
         if(error instanceof Error){
@@ -126,35 +126,4 @@ function generateFileInServiceContainer(relativePath: string, contents: string, 
     }
 
     return ok(null)
-}
-
-function toUTF8Array(str:string) {
-    let utf8 = [];
-    for (let i=0; i < str.length; i++) {
-        let charcode = str.charCodeAt(i);
-        if (charcode < 0x80) utf8.push(charcode);
-        else if (charcode < 0x800) {
-            utf8.push(0xc0 | (charcode >> 6), 
-                      0x80 | (charcode & 0x3f));
-        }
-        else if (charcode < 0xd800 || charcode >= 0xe000) {
-            utf8.push(0xe0 | (charcode >> 12), 
-                      0x80 | ((charcode>>6) & 0x3f), 
-                      0x80 | (charcode & 0x3f));
-        }
-        // surrogate pair
-        else {
-            i++;
-            // UTF-16 encodes 0x10000-0x10FFFF by
-            // subtracting 0x10000 and splitting the
-            // 20 bits of 0x0-0xFFFFF into two halves
-            charcode = 0x10000 + (((charcode & 0x3ff)<<10)
-                      | (str.charCodeAt(i) & 0x3ff))
-            utf8.push(0xf0 | (charcode >>18), 
-                      0x80 | ((charcode>>12) & 0x3f), 
-                      0x80 | ((charcode>>6) & 0x3f), 
-                      0x80 | (charcode & 0x3f));
-        }
-    }
-    return utf8;
-}
+};
