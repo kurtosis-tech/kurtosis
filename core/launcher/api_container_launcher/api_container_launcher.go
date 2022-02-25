e33f6aed8a31c490cf3746760027172d0ebcb07d
kzhama@genium.io <kzhama@genium.io>
2022-02-25 14:25:56 -0300

    Pre-release changes for version 1.39.5

--- core/launcher/api_container_launcher/api_container_launcher.go
+++ core/launcher/api_container_launcher/api_container_launcher.go
@@ -22,7 +22,7 @@ import (
 
 const (
 	// !!!!!!!!!!!!!!!!!! DO NOT MODIFY THIS! IT WILL BE UPDATED AUTOMATICALLY DURING THE RELEASE PROCESS !!!!!!!!!!!!!!!
-	DefaultVersion = "1.39.4"
+	DefaultVersion = "1.39.5"
 	// !!!!!!!!!!!!!!!!!! DO NOT MODIFY THIS! IT WILL BE UPDATED AUTOMATICALLY DURING THE RELEASE PROCESS !!!!!!!!!!!!!!!
 
 	portProtocolToMonitorWhenWaitingForAvailability = "tcp"
