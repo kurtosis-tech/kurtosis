7f5953675dd60070c873984c407395149f10b4ef
mieubrisse <mieubrisse@gmail.com>
2022-03-03 15:29:17 -0300

    Explanatory comment for tools.go

--- container-engine-lib/tools.go
+++ container-engine-lib/tools.go
@@ -2,7 +2,8 @@
 
 package main
 
-// This file is a convention to make sure that go.mod continues to have the module we need in our 'go generate' commands
+// This file is to make sure that go.mod continues to record the tools we need in our 'go generate' commands
+// See: https://marcofranssen.nl/manage-go-tools-via-go-modules
 import (
     _ "github.com/dmarkham/enumer"
 )
