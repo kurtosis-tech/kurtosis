7dc17584ac0a649f410a862264b49c1109d4a5c2
Codie <petersen.codie@gmail.com>
2022-04-07 12:02:21 -0700

    Added CGO_ENABLED variables for testing. 3 Tests fail, network_parition, network_soft_parition, and bulk_command_execution, but it seems that is known and ok for now.

--- cli/internal_testsuites/golang/scripts/build.sh
+++ cli/internal_testsuites/golang/scripts/build.sh
@@ -20,4 +20,4 @@ TIMEOUT="3m"   # This must be Go-parseable timeout
 cd "${lang_root_dirpath}"
 go build ./...
 # The count=1 disables caching for the testsuite (which we want, because the engine server might have changed even though the test code didn't)
-go test ./... -p "${PARALLELISM}" -count=1 -timeout "${TIMEOUT}"
+CGO_ENABLED=0 go test ./... -p "${PARALLELISM}" -count=1 -timeout "${TIMEOUT}"
