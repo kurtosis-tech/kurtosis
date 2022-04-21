a9d257ecb7c15e3f5dccbfa97a78df3838388b62
Codie Petersen <codie@Codies-MBP.lan>
2022-04-21 14:43:32 -0700

    Refactored file_artifact_store to properly interact with file_cache instead of file_handler. Migrated any useful functionality from handler to file_cache.

--- core/server/commons/enclave_data_directory/file_artifact_store.go
+++ core/server/commons/enclave_data_directory/file_artifact_store.go
@@ -8,8 +8,9 @@ package enclave_data_directory
 import (
 	"github.com/google/uuid"
 	"strings"
-	"fmt"
 	"sync"
+	"io"
+	"github.com/kurtosis-tech/stacktrace"
 )
 
 const (
@@ -22,39 +23,33 @@ type FileStore struct {
 	mutex 		*sync.Mutex
 }
 
-func newFileStore(uuid string, absolutePath string, dirpathRelativeToDataDirRoot string) *FileStore {
-	return &FileStore {
-		uuid: 							uuid,
-		fileCache: newFileCache(absolutePath, dirpathRelativeToDataDirRoot),
-	}
-}
+func newFileStore(uuid string, absoluteDirpath string, dirpathRelativeToDataDirRoot string) (*FileStore, error) {
+	uuid, err := getUniversallyUniqueID()
 
-// StoreFile: Saves file to disk and returns a FileStore struct.
-// Just rename via uuid only and appen tgz extension.
-func (store FileStore) StoreFile(fileName string, filedata []byte) (*FileStore, error) {
-	generatedUUID, _ := getUniversallyUniqueID()
-	uuidString := strings.Replace(generatedUUID.String(), "-", "",-1)
-	uuidFileName := strings.Join([]string{uuidString, artifactExtension}, ".")
-	relativeFolder := "" //Don't need a folder?
-
-	handler, err := createFileHandler()
 	if err != nil {
-		fmt.Println(err.Error())
-		return nil, err
+		return nil, stacktrace.Propagate(err, "Could not create Universally Unique ID for FileStore")
 	}
 
-	absolutePath, saveErr := handler.SaveBytesToPath(uuidFileName, relativeFolder, filedata)
-
-	if saveErr != nil {
-		fmt.Println(saveErr.Error())
-		return nil, saveErr
-	}
-	return newFileStore(uuidString, absolutePath, relativeFolder), nil
+	return &FileStore {
+		uuid: 	 	uuid,
+		fileCache: 	newFileCache(absoluteDirpath, dirpathRelativeToDataDirRoot),
+	}, nil
 }
 
-//There are some suggestions that go's implementation of uuid is not RFC compliant
-//If we can verify it is, it would be better to use ipv6 as nodeID and interface name where the data came in.
+// StoreFile: Saves file to disk.
+func (store FileStore) StoreFile(reader io.Reader) (*EnclaveDataDirFile, error) {
+	store.mutex.Lock()
+	defer store.mutex.Unlock()
+
+	uuidKeyedFileName := strings.Join([]string{store.uuid, artifactExtension}, ".")
+	return store.fileCache.AddFile(uuidKeyedFileName, reader)
+}
+
+//There are some suggestions that go's implementation of uuid is not RFC compliant.
+//If we can verify it is compliant, it would be better to use ipv6 as nodeID and interface name where the data came in.
 //Just generating a random one for now.
-func getUniversallyUniqueID() (uuid.UUID, error) {
-	return uuid.NewRandom()
+func getUniversallyUniqueID() (string, error) {
+	generatedUUID, err := uuid.NewRandom()
+	uuidString := strings.Replace(generatedUUID.String(), "-", "",-1)
+	return uuidString, err
 }
