a87ab58e5afb0d3c25ad20d81b62a8cd08375a3b
galenmarchetti <galenmarchetti@gmail.com>
2022-04-24 14:46:22 -0500

    adding pause unpause test logic

--- cli/internal_testsuites/golang/testsuite/service_pause_test/service_pause_test.go
+++ cli/internal_testsuites/golang/testsuite/service_pause_test/service_pause_test.go
@@ -1,7 +1,59 @@
 package service_pause_test
 
-import "testing"
+import (
+	"context"
+	"github.com/kurtosis-tech/kurtosis-cli/golang_internal_testsuite/test_helpers"
+	"github.com/kurtosis-tech/kurtosis-core-api-lib/api/golang/lib/services"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
 
-func TestFiles(t *testing.T) {
+const (
+	testName                  = "pauseUnpause"
+	isPartitioningEnabled     = false
+	pauseUnpauseTestImageName = "alpine:3.12.4"
+	testServiceId             = "test"
+)
 
+func TestPauseUnpause(t *testing.T) {
+	ctx := context.Background()
+
+	// ------------------------------------- ENGINE SETUP ----------------------------------------------
+	enclaveCtx, stopEnclaveFunc, _, err := test_helpers.CreateEnclave(t, ctx, testName, isPartitioningEnabled)
+	require.NoError(t, err, "An error occurred creating an enclave")
+	defer stopEnclaveFunc()
+
+	// ------------------------------------- TEST SETUP ----------------------------------------------
+	containerConfigSupplier := getContainerConfigSupplier()
+
+	_, err = enclaveCtx.AddService(testServiceId, containerConfigSupplier)
+	require.NoError(t, err, "An error occurred adding the file server service")
+
+	// ------------------------------------- TEST RUN ----------------------------------------------
+	// pause/unpause using servicectx
+	// serviceCtx.pause(..)
+}
+
+// ====================================================================================================
+//                                       Private helper functions
+// ====================================================================================================
+func getContainerConfigSupplier() func(ipAddr string, sharedDirectory *services.SharedPath) (*services.ContainerConfig, error) {
+	containerConfigSupplier := func(ipAddr string, sharedDirectory *services.SharedPath) (*services.ContainerConfig, error) {
+
+		// We sleep because the only function of this container is to test Docker executing a command while it's running
+		// NOTE: We could just as easily combine this into a single array (rather than splitting between ENTRYPOINT and CMD
+		// args), but this provides a nice little regression test of the ENTRYPOINT overriding
+		entrypointArgs := []string{"sleep"}
+		cmdArgs := []string{"30"}
+
+		containerConfig := services.NewContainerConfigBuilder(
+			pauseUnpauseTestImageName,
+		).WithEntrypointOverride(
+			entrypointArgs,
+		).WithCmdOverride(
+			cmdArgs,
+		).Build()
+		return containerConfig, nil
+	}
+	return containerConfigSupplier
 }
