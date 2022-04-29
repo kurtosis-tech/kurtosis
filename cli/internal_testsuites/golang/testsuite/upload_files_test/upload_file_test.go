df59a9aebe13569a78e38b0a8831a95908b56360
Codie Petersen <codie@Codies-MBP.lan>
2022-04-29 16:46:55 -0700

    Test infrastructure.

---
+++ cli/internal_testsuites/golang/testsuite/upload_files_test/upload_file_test.go
@@ -0,0 +1,65 @@
+package upload_files_test
+
+import (
+	"testing"
+	"io/ioutil"
+	"github.com/kurtosis-tech/stacktrace"
+)
+
+const archiveRootDirectoryTestPattern = "upload-test-"
+const archiveSubDirectoryTestPattern = "sub-folder-"
+const archiveFileTestPattern = "test-file-"
+const archiveTestFileContent = "This is file is for testing purposes."
+
+const numberOfTempTestFilesToCreateInSubDir = 3
+const numberOfTempTestFilesToCreateInRootDir = 1
+
+func TestUploadFiles(t *testing.T) {
+
+}
+//========================================================================
+// Helpers
+//========================================================================
+func createTestFiles(pathToCreateAt string, fileCount int) error {
+	for i := 0; i < fileCount; i++ {
+		tempFile, err := ioutil.TempFile(pathToCreateAt, archiveFileTestPattern)
+		if err != nil {
+			return stacktrace.Propagate(err, "Failed to create archive test files in '%s'.", pathToCreateAt)
+		}
+		defer tempFile.Close()
+
+		_, err = tempFile.Write([]byte(archiveTestFileContent))
+		if err != nil {
+			return stacktrace.Propagate(err, "Failed to archive test file '%s' at '%s'.", tempFile.Name(), pathToCreateAt)
+		}
+	}
+	return nil
+}
+
+//Creates a temporary folder with x files and 1 sub folder that has y files each.
+//Where x is numberOfTempTestFilesToCreateInRootDir
+//Where y is numberOfTempTestFilesToCreateInSubDir
+func createTestFolderToUpload() (string, error) {
+	baseTempDirPath, err := ioutil.TempDir("",archiveRootDirectoryTestPattern)
+	if err != nil {
+		return "", stacktrace.Propagate(err, "Failed to create a temporary root directory for testing.")
+	}
+
+	//Create a temporary subdirectory.
+	tempSubDirectory, err := ioutil.TempDir(baseTempDirPath, archiveSubDirectoryTestPattern)
+	if err != nil {
+		return "", stacktrace.Propagate(err, "Failed to create a temporary archive directory within '%s'.",
+			baseTempDirPath)
+	}
+
+	if err = createTestFiles(tempSubDirectory, numberOfTempTestFilesToCreateInSubDir);  err != nil {
+		return "", stacktrace.Propagate(err, "Failed to create archive test files at '%s'.",
+			tempSubDirectory)
+	}
+
+	if err := createTestFiles(baseTempDirPath, numberOfTempTestFilesToCreateInRootDir); err != nil {
+		return "", stacktrace.Propagate(err,"Failed to create archive test files in your root directory at '%s'.",
+			baseTempDirPath)
+	}
+	return baseTempDirPath, nil
+}
