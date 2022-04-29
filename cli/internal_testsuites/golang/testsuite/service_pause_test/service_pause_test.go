e4bbee35306ecdc111c34e97a926579593c9ba55
galenmarchetti <galenmarchetti@gmail.com>
2022-04-28 20:20:30 -0500

    modifying e2e test

--- cli/internal_testsuites/golang/testsuite/service_pause_test/service_pause_test.go
+++ cli/internal_testsuites/golang/testsuite/service_pause_test/service_pause_test.go
@@ -40,10 +40,12 @@ func TestPauseUnpause(t *testing.T) {
 	time.Sleep(10 * time.Second)
 	// ------------------------------------- TEST RUN ----------------------------------------------
 	// pause/unpause using servicectx
-	serviceCtx.PauseService()
+	err = serviceCtx.PauseService()
 	logrus.Infof("Paused service!")
+	require.NoError(t, err, "An error occurred unpausing")
 	time.Sleep(10 * time.Second)
 	serviceCtx.UnpauseService()
+	require.NoError(t, err, "An error occurred unpausing")
 	logrus.Infof("Unpaused service!")
 	time.Sleep(10 * time.Second)
 }
@@ -57,8 +59,8 @@ func getContainerConfigSupplier() func(ipAddr string, sharedDirectory *services.
 		// We sleep because the only function of this container is to test Docker executing a command while it's running
 		// NOTE: We could just as easily combine this into a single array (rather than splitting between ENTRYPOINT and CMD
 		// args), but this provides a nice little regression test of the ENTRYPOINT overriding
-		entrypointArgs := []string{"sleep"}
-		cmdArgs := []string{"30"}
+		entrypointArgs := []string{"/bin/sh", "-c"}
+		cmdArgs := []string{"while sleep 1; do ts=$(date +\"%s\") ; echo \"Time: $ts\" ; done"}
 
 		containerConfig := services.NewContainerConfigBuilder(
 			pauseUnpauseTestImageName,
