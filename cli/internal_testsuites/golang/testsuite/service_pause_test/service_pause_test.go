188a0bdefe107a7661390268b591f91a2856437d
galenmarchetti <galenmarchetti@gmail.com>
2022-05-03 18:20:18 -0500

    adding find gap logic

--- cli/internal_testsuites/golang/testsuite/service_pause_test/service_pause_test.go
+++ cli/internal_testsuites/golang/testsuite/service_pause_test/service_pause_test.go
@@ -6,6 +6,8 @@ import (
 	"github.com/kurtosis-tech/kurtosis-core-api-lib/api/golang/lib/services"
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/require"
+	"strconv"
+	"strings"
 	"testing"
 	"time"
 )
@@ -30,17 +32,33 @@ func TestPauseUnpause(t *testing.T) {
 	serviceCtx, err := enclaveCtx.AddService(testServiceId, containerConfigSupplier)
 	require.NoError(t, err, "An error occurred adding the file server service")
 
-	time.Sleep(10 * time.Second)
+	time.Sleep(4 * time.Second)
 	// ------------------------------------- TEST RUN ----------------------------------------------
 	// pause/unpause using servicectx
 	err = enclaveCtx.PauseService(serviceCtx.GetServiceID())
 	logrus.Infof("Paused service!")
-	require.NoError(t, err, "An error occurred unpausing")
-	time.Sleep(10 * time.Second)
+	require.NoError(t, err, "An error occurred pausing")
+	time.Sleep(4 * time.Second)
 	err = enclaveCtx.UnpauseService(serviceCtx.GetServiceID())
 	require.NoError(t, err, "An error occurred unpausing")
 	logrus.Infof("Unpaused service!")
-	time.Sleep(10 * time.Second)
+	time.Sleep(4 * time.Second)
+	_, results, err := serviceCtx.ExecCommand([]string{"cat", "/time.log"})
+	require.NoError(t, err, "An error occurred reading the logs.")
+	secondCounter := strings.Split(strings.TrimSuffix(results, "\n"), "\n")
+	foundGap := false
+	for i, line := range secondCounter {
+		if i > 0 {
+			currentSecondCount, err := strconv.Atoi(line)
+			require.NoError(t, err, "An error occurred converting seconds to int.")
+			previousSecondCount, err := strconv.Atoi(secondCounter[i-1])
+			require.NoError(t, err, "An error occurred converting seconds to int.")
+			if currentSecondCount-previousSecondCount > 2 {
+				foundGap = true
+			}
+		}
+	}
+	require.True(t, foundGap)
 }
 
 // ====================================================================================================
@@ -51,7 +69,7 @@ func getContainerConfigSupplier() func(ipAddr string) (*services.ContainerConfig
 
 		// We spam timestamps so that we can measure pausing processes (no more log output) and unpausing (log output resumes)
 		entrypointArgs := []string{"/bin/sh", "-c"}
-		cmdArgs := []string{"while sleep 1; do ts=$(date +\"%s\") ; echo \"Time: $ts\" ; done"}
+		cmdArgs := []string{"while sleep 1; do ts=$(date +\"%s\") ; echo \"$ts\" >> /time.log ; done"}
 
 		containerConfig := services.NewContainerConfigBuilder(
 			pauseUnpauseTestImageName,
