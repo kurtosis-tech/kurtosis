5a7ed9f20b68934059dec8abbc504f29156db605
Blaise Marchetti <blaise.marchetti@kurtosistech.com>
2022-05-09 18:23:11 -0400

    improved error messages

--- cli/cli/helpers/backend_for_cmd/get_backend_for_command.go
+++ cli/cli/helpers/backend_for_cmd/get_backend_for_command.go
@@ -3,15 +3,21 @@ package backend_for_cmd
 import (
 	"github.com/kurtosis-tech/container-engine-lib/lib"
 	"github.com/kurtosis-tech/container-engine-lib/lib/backend_interface"
-)
-
-const (
-	withKubernetesFlagName = "with-kubernetes"
+	"github.com/kurtosis-tech/stacktrace"
 )
 
 func GetBackendForCmd(useKubernetes bool) (backend_interface.KurtosisBackend, error) {
 	if useKubernetes {
-		return lib.GetLocalKubernetesKurtosisBackend()
+		kubernetesBackend, err := lib.GetLocalKubernetesKurtosisBackend()
+		if err != nil {
+			return nil, stacktrace.Propagate(err, "Expected to be able to get a kubernetes backend, instead a non-nil error was returned")
+		}
+		return kubernetesBackend, nil
 	}
-	return lib.GetLocalDockerKurtosisBackend()
+	dockerBackend, err := lib.GetLocalDockerKurtosisBackend()
+	if err != nil {
+		return nil, stacktrace.Propagate(err, "Expected to be able to get docker backend, instead a non-nil error was returned")
+	}
+
+	return dockerBackend, err
 }
