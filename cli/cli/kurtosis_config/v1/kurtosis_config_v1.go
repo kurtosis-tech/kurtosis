618fe17970c94ca65244b79642960cfc58a71103
galenmarchetti <galenmarchetti@gmail.com>
2022-05-12 13:41:42 -0500

    comment

--- cli/cli/kurtosis_config/v1/kurtosis_config_v1.go
+++ cli/cli/kurtosis_config/v1/kurtosis_config_v1.go
@@ -84,6 +84,7 @@ func (kurtosisConfigV1 *KurtosisConfigV1) OverlayOverrides(overrides *KurtosisCo
 
 type KurtosisClusterV1 struct {
 	Type *string                      `yaml:"type,omitempty"`
+	// If we ever get another type of cluster that has configuration, this will need to be polymorphically deserialized
 	Config *KubernetesClusterConfigV1 `yaml:"config,omitempty"`
 }
 
