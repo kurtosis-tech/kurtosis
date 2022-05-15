a297497d74cf4c118588c39cc7ff472ac1325aea
galenmarchetti <galenmarchetti@gmail.com>
2022-05-15 09:02:15 -0500

    adding prefixes

--- core/launcher/args/kurtosis_backend_type/kurtosis_backend_type.go
+++ core/launcher/args/kurtosis_backend_type/kurtosis_backend_type.go
@@ -5,10 +5,10 @@
 
 package kurtosis_backend_type
 
-//go:generate go run github.com/dmarkham/enumer -type=KurtosisBackendType -transform=lower -json
+//go:generate go run github.com/dmarkham/enumer -trimprefix "KurtosisBackendType_" -type=KurtosisBackendType -transform=lower -json
 type KurtosisBackendType uint
 const (
 	// To add new values, just add a new version to the end WITHOUT WHITESPACE
-	Docker KurtosisBackendType = iota
-	Kubernetes
+	KurtosisBackendType_Docker KurtosisBackendType = iota
+	KurtosisBackendType_Kubernetes
 )
