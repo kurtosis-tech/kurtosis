41d4d81d30ab4a602f5d93bcbc1f068dc250a9c2
galenmarchetti <galenmarchetti@gmail.com>
2022-05-15 13:06:40 -0500

    fixing comment in backend-type

--- core/launcher/args/kurtosis_backend_type.go
+++ core/launcher/args/kurtosis_backend_type.go
@@ -8,7 +8,7 @@ package args
 //go:generate go run github.com/dmarkham/enumer -trimprefix "KurtosisBackendType_" -type=KurtosisBackendType -transform=lower -json
 type KurtosisBackendType uint
 const (
-	// To add new values, just add a new version to the end WITHOUT WHITESPACE
+	// To add new values, just add a new value to the end WITHOUT WHITESPACE
 	KurtosisBackendType_Docker KurtosisBackendType = iota
 	KurtosisBackendType_Kubernetes
 )
