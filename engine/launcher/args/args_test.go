3cc06a6905f1fa90c27750a92a2433d7055eeba7
galenmarchetti <galenmarchetti@gmail.com>
2022-05-17 13:12:54 -0500

    testing

--- engine/launcher/args/args_test.go
+++ engine/launcher/args/args_test.go
@@ -2,14 +2,31 @@ package args
 
 import (
 	"encoding/json"
+	"github.com/kurtosis-tech/kurtosis-engine-server/launcher/args/kurtosis_backend_config"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
 
-const argsJson = "{\"grpcListenPortNum\":9710,\"grpcProxyListenPortNum\":9711,\"logLevelStr\":\"debug\",\"imageVersionTag\":\"X.X.X\",\"metricsUserId\":\"5e9d668ad9b004ba16def3ee14c271f5134e1df57a4d4996924e6544e6b0e9be\",\"didUserAcceptSendingMetrics\":true,\"kurtosisBackendType\":\"kubernetes\",\"kurtosisBackendConfig\":{\"storageClass\":\"standard\",\"enclaveSizeInMegabytes\":10}}"
+const (
+	kubernetesArgsJson   = "{\"grpcListenPortNum\":9710,\"grpcProxyListenPortNum\":9711,\"logLevelStr\":\"debug\",\"imageVersionTag\":\"X.X.X\",\"metricsUserId\":\"5e9d668ad9b004ba16def3ee14c271f5134e1df57a4d4996924e6544e6b0e9be\",\"didUserAcceptSendingMetrics\":true,\"kurtosisBackendType\":\"kubernetes\",\"kurtosisBackendConfig\":{\"storageClass\":\"standard\",\"enclaveSizeInMegabytes\":10}}"
+	dockerArgsJson   = "{\"grpcListenPortNum\":9710,\"grpcProxyListenPortNum\":9711,\"logLevelStr\":\"debug\",\"imageVersionTag\":\"X.X.X\",\"metricsUserId\":\"5e9d668ad9b004ba16def3ee14c271f5134e1df57a4d4996924e6544e6b0e9be\",\"didUserAcceptSendingMetrics\":true,\"kurtosisBackendType\":\"docker\",\"kurtosisBackendConfig\":{}}"
+	expectedStorageClass = "standard"
+	expectedEnclaveSize = uint(10)
+)
 
-func TestArgsUnmarshal(t *testing.T) {
-	paramsJsonBytes := []byte(argsJson)
+func TestArgsUnmarshalKubernetes(t *testing.T) {
+	paramsJsonBytes := []byte(kubernetesArgsJson)
+	var args EngineServerArgs
+	err := json.Unmarshal(paramsJsonBytes, &args)
+	require.NoError(t, err)
+	backendConfig := args.KurtosisBackendConfig
+	kubernetesBackendConfig := backendConfig.(kurtosis_backend_config.KubernetesBackendConfig)
+	require.Equal(t, expectedStorageClass, kubernetesBackendConfig.StorageClass)
+	require.Equal(t, expectedEnclaveSize, kubernetesBackendConfig.EnclaveSizeInMegabytes)
+}
+
+func TestArgsUnmarshalDocker(t *testing.T) {
+	paramsJsonBytes := []byte(dockerArgsJson)
 	var args EngineServerArgs
 	err := json.Unmarshal(paramsJsonBytes, &args)
 	require.NoError(t, err)
