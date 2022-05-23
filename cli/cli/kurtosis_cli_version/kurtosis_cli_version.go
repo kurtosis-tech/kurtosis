c8492f67aa9e913a90c0a23a363d3390c6d394ba
mieubrisse <mieubrisse@gmail.com>
2022-05-23 19:37:29 -0300

    Pre-release changes for version 0.20.1

--- cli/cli/kurtosis_cli_version/kurtosis_cli_version.go
+++ cli/cli/kurtosis_cli_version/kurtosis_cli_version.go
@@ -2,5 +2,5 @@ package kurtosis_cli_version
 
 const (
 	// !!!!!!!! DO NOT UPDATE THIS! It will be updated automatically during release !!!!!!!!!!
-	KurtosisCLIVersion = "0.20.0"
+	KurtosisCLIVersion = "0.20.1"
 )
