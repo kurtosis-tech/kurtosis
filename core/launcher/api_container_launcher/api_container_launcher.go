9b8ab211aa6a7bb96820ece5adfdbd9f208be038
Blaise Marchetti <blaise.marchetti@kurtosistech.com>
2022-06-01 10:55:25 -0400

    Pre-release changes for version 1.55.0

--- core/launcher/api_container_launcher/api_container_launcher.go
+++ core/launcher/api_container_launcher/api_container_launcher.go
@@ -18,7 +18,7 @@ import (
 
 const (
 	// !!!!!!!!!!!!!!!!!! DO NOT MODIFY THIS! IT WILL BE UPDATED AUTOMATICALLY DURING THE RELEASE PROCESS !!!!!!!!!!!!!!!
-	DefaultVersion = "1.54.1"
+	DefaultVersion = "1.55.0"
 	// !!!!!!!!!!!!!!!!!! DO NOT MODIFY THIS! IT WILL BE UPDATED AUTOMATICALLY DURING THE RELEASE PROCESS !!!!!!!!!!!!!!!
 
 	enclaveDataVolumeDirpath = "/kurtosis-data"
