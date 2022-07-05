9e4e1dc77982c61dee022edc6558ec946796f2ef
Tedi Mitiku <tedi.m52@gmail.com>
2022-07-05 12:52:27 -0300

    inline kudet get-docker-tag instead of using variable

--- engine/server/scripts/build.sh
+++ engine/server/scripts/build.sh
@@ -2,9 +2,9 @@
 # 2021-07-08 WATERMARK, DO NOT REMOVE - This script was generated from the Kurtosis Bash script template
 
 set -euo pipefail # Bash "strict mode"
-git_repo_dirpath="$(pwd)"
 script_dirpath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 server_root_dirpath="$(dirname "${script_dirpath}")"
+git_repo_dirpath="$(dirname "${server_root_dirpath}")"
 
 # ==================================================================================================
 #                                             Constants
@@ -49,10 +49,13 @@ echo "Successfully built server code"
 
 # Generate Docker image tag
 if ! cd "${git_repo_dirpath}"; then
-  echo "Couldn't cd to the git root dirpath '${server_root_dirpath}'" >&2
+  echo "Error: Couldn't cd to the git root dirpath '${server_root_dirpath}'" >&2
   exit 1
 fi
-docker_tag="$(${GET_DOCKER_IMAGE_TAG_CMD})"
+if ! docker_tag="$(kudet get-docker-tag)"; then
+    echo "Error: Couldn't get the Docker image tag" >&2
+    exit 1
+fi
 
 # Build Docker image
 dockerfile_filepath="${server_root_dirpath}/Dockerfile"
