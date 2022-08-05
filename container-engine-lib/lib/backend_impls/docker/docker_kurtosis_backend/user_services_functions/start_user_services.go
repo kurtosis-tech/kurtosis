538f5052a29508c2c5cc06b82da69db59071431b
Tedi Mitiku <tedi.m52@gmail.com>
2022-08-05 09:03:21 -0400

    chane createStartServiceOperations to createStartServiceOperation

--- container-engine-lib/lib/backend_impls/docker/docker_kurtosis_backend/user_services_functions/start_user_services.go
+++ container-engine-lib/lib/backend_impls/docker/docker_kurtosis_backend/user_services_functions/start_user_services.go
@@ -275,6 +275,9 @@ func StartUserServices(
 	map[service.ServiceGUID]error,
 	error,
 ) {
+	serviceRegistrationMutex.Lock()
+	defer serviceRegistrationMutex.Unlock()
+
 	// Sanity check for port bindings on all services
 	for guid, config := range services {
 	//TODO this is a huge hack to temporarily enable static ports for NEAR until we have a more productized solution
@@ -292,8 +295,6 @@ func StartUserServices(
 			}
 		}
 	}
-	serviceRegistrationMutex.Lock()
-	defer serviceRegistrationMutex.Unlock()
 
 	enclaveNetwork, err := shared_helpers.GetEnclaveNetworkByEnclaveId(ctx, enclaveID, dockerManager)
 	if err != nil {
@@ -358,7 +359,7 @@ func StartUserServices(
 		return nil, nil, stacktrace.Propagate(err, "Couldn't get an object attribute provider for enclave '%v'", enclaveID)
 	}
 
-	successfulStarts, failedStarts, err := runStartServicesOperationInParallel(
+	successfulStarts, failedStarts, err := runStartServiceOperationsInParallel(
 		ctx,
 		enclaveNetworkId,
 		services,
@@ -376,10 +377,10 @@ func StartUserServices(
 // ====================================================================================================
 //                       Private helper functions
 // ====================================================================================================
-func runStartServicesOperationInParallel(
+func runStartServiceOperationsInParallel(
 	ctx context.Context,
 	enclaveNetworkId string,
-	services map[service.ServiceGUID]*service.ServiceConfig,
+	serviceConfigs map[service.ServiceGUID]*service.ServiceConfig,
 	serviceRegistrations map[service.ServiceGUID]*service.ServiceRegistration,
 	enclaveObjAttrsProvider object_attributes_provider.DockerEnclaveObjectAttributesProvider,
 	freeIpAddrProvider *lib.FreeIpAddrTracker,
@@ -389,214 +390,208 @@ func runStartServicesOperationInParallel(
 	map[service.ServiceGUID]error,
 	error,
 ) {
-	operations := createStartServiceOperations(
-		ctx,
-		enclaveNetworkId,
-		services,
-		serviceRegistrations,
-		enclaveObjAttrsProvider,
-		freeIpAddrProvider,
-		dockerManager)
-
-	_, failedOps, successfulServicesData, err := operation_parallelizer.RunOperationsInParallel(operations)
-	if err != nil {
-		return nil, nil, stacktrace.Propagate(err, "An error occurred while trying to run start services operation in parallel.")
+	operations := map[operation_parallelizer.OperationID]operation_parallelizer.Operation{}
+	for guid, config := range serviceConfigs {
+		operations[operation_parallelizer.OperationID(guid)] = createStartServiceOperation(
+			ctx,
+			guid,
+			config,
+			serviceRegistrations[guid],
+			enclaveNetworkId,
+			enclaveObjAttrsProvider,
+			freeIpAddrProvider,
+			dockerManager)
 	}
+
+	successfulServicesObjs, failedOps := operation_parallelizer.RunOperationsInParallel(operations)
 
 	successfulServices := map[service.ServiceGUID]service.Service{}
 	failedServices := map[service.ServiceGUID]error{}
 
-	for id, data := range successfulServicesData {
+	for id, data := range successfulServicesObjs {
 		serviceGUID := service.ServiceGUID(id)
-		serviceObj := data.Data.(service.Service)
+		serviceObj, ok := data.(service.Service)
+		if !ok {
+			return nil, nil, stacktrace.NewError("Casting to a service object on data returned from the operation to start service with guid `%v` failed.", serviceGUID)
+		}
 		successfulServices[serviceGUID] = serviceObj
 	}
 
 	for id, err := range failedOps {
-		failedServices[service.ServiceGUID(id)] = err
+		serviceGUID := service.ServiceGUID(id)
+		failedServices[serviceGUID] = err
 	}
 
 	return successfulServices, failedServices, nil
 }
 
-func createStartServiceOperations(
+func createStartServiceOperation(
 	ctx context.Context,
+	serviceGUID service.ServiceGUID,
+	serviceConfig *service.ServiceConfig,
+	serviceRegistration *service.ServiceRegistration,
 	enclaveNetworkId string,
-	services map[service.ServiceGUID]*service.ServiceConfig,
-	serviceRegistrations map[service.ServiceGUID]*service.ServiceRegistration,
 	enclaveObjAttrsProvider object_attributes_provider.DockerEnclaveObjectAttributesProvider,
 	freeIpAddrProvider *lib.FreeIpAddrTracker,
-	dockerManager *docker_manager.DockerManager) map[operation_parallelizer.OperationID]operation_parallelizer.Operation {
-	operations := map[operation_parallelizer.OperationID]operation_parallelizer.Operation{}
+	dockerManager *docker_manager.DockerManager) func() (interface{}, error) {
+	id := serviceRegistration.GetID()
+	privateIpAddr := serviceRegistration.GetPrivateIP()
 
-	for guid, config := range services {
-		guid := guid
-		config := config
-		registration := serviceRegistrations[guid]
-		id := registration.GetID()
-		privateIpAddr := registration.GetPrivateIP()
-
-		var startServiceOp operation_parallelizer.Operation = func(dataChan chan operation_parallelizer.OperationData) error {
-			filesArtifactsExpansion := config.GetFilesArtifactsExpansion()
-			containerImageName := config.GetContainerImageName()
-			privatePorts := config.GetPrivatePorts()
-			publicPorts := config.GetPublicPorts()
-			entrypointArgs := config.GetEntrypointArgs()
-			cmdArgs := config.GetCmdArgs()
-			envVars := config.GetEnvVars()
-			cpuAllocationMillicpus := config.GetCPUAllocationMillicpus()
-			memoryAllocationMegabytes := config.GetMemoryAllocationMegabytes()
+	var startServiceOp operation_parallelizer.Operation = func() (interface{}, error) {
+		filesArtifactsExpansion := serviceConfig.GetFilesArtifactsExpansion()
+		containerImageName := serviceConfig.GetContainerImageName()
+		privatePorts := serviceConfig.GetPrivatePorts()
+		publicPorts := serviceConfig.GetPublicPorts()
+		entrypointArgs := serviceConfig.GetEntrypointArgs()
+		cmdArgs := serviceConfig.GetCmdArgs()
+		envVars := serviceConfig.GetEnvVars()
+		cpuAllocationMillicpus := serviceConfig.GetCPUAllocationMillicpus()
+		memoryAllocationMegabytes := serviceConfig.GetMemoryAllocationMegabytes()
 
 
-			volumeMounts := map[string]string{}
-			shouldDeleteVolumes := true
-			if filesArtifactsExpansion != nil {
-				candidateVolumeMounts, err := doFilesArtifactExpansionAndGetUserServiceVolumes(
-					ctx,
-					guid,
-					enclaveObjAttrsProvider,
-					freeIpAddrProvider,
-					enclaveNetworkId,
-					filesArtifactsExpansion.ExpanderImage,
-					filesArtifactsExpansion.ExpanderEnvVars,
-					filesArtifactsExpansion.ExpanderDirpathsToServiceDirpaths,
-					dockerManager,
-				)
-				if err != nil {
-					return stacktrace.Propagate(err, "An error occurred doing files artifacts expansion to get user service volumes")
-				}
-				defer func() {
-					if shouldDeleteVolumes {
-						for volumeName := range candidateVolumeMounts {
-							// Use background context so we delete these even if input context was cancelled
-							if err := dockerManager.RemoveVolume(context.Background(), volumeName); err != nil {
-								logrus.Errorf("Starting the service failed so we tried to delete files artifact expansion volume '%v' that we created, but doing so threw an error:\n%v", volumeName, err)
-								logrus.Errorf("You'll need to delete volume '%v' manually!", volumeName)
-							}
-						}
-					}
-				}()
-				volumeMounts = candidateVolumeMounts
-			}
-
-			containerAttrs, err := enclaveObjAttrsProvider.ForUserServiceContainer(
-				id,
-				guid,
-				privateIpAddr,
-				privatePorts,
-			)
-			if err != nil {
-				return stacktrace.Propagate(err, "An error occurred while trying to get the user service container attributes for user service with GUID '%v'", guid)
-			}
-			containerName := containerAttrs.GetName()
-
-			labelStrs := map[string]string{}
-			for labelKey, labelValue := range containerAttrs.GetLabels() {
-				labelStrs[labelKey.GetString()] = labelValue.GetString()
-			}
-
-			dockerUsedPorts := map[nat.Port]docker_manager.PortPublishSpec{}
-			for portId, privatePortSpec := range privatePorts {
-				dockerPort, err := shared_helpers.TransformPortSpecToDockerPort(privatePortSpec)
-				if err != nil {
-					return stacktrace.Propagate(err, "An error occurred converting private port spec '%v' to a Docker port", portId)
-				}
-				//TODO this is a huge hack to temporarily enable static ports for NEAR until we have a more productized solution
-				if publicPorts != nil && len(publicPorts) > 0 {
-					publicPortSpec, found := publicPorts[portId]
-					if !found {
-						return stacktrace.NewError("Expected to receive public port with ID '%v' bound to private port number '%v', but it was not found", portId, privatePortSpec.GetNumber())
-					}
-					dockerUsedPorts[dockerPort] = docker_manager.NewManualPublishingSpec(publicPortSpec.GetNumber())
-				} else {
-					dockerUsedPorts[dockerPort] = docker_manager.NewAutomaticPublishingSpec()
-				}
-			}
-
-			createAndStartArgsBuilder := docker_manager.NewCreateAndStartContainerArgsBuilder(
-				containerImageName,
-				containerName.GetString(),
+		volumeMounts := map[string]string{}
+		shouldDeleteVolumes := true
+		if filesArtifactsExpansion != nil {
+			candidateVolumeMounts, err := doFilesArtifactExpansionAndGetUserServiceVolumes(
+				ctx,
+				serviceGUID,
+				enclaveObjAttrsProvider,
+				freeIpAddrProvider,
 				enclaveNetworkId,
-			).WithStaticIP(
-				privateIpAddr,
-			).WithUsedPorts(
-				dockerUsedPorts,
-			).WithEnvironmentVariables(
-				envVars,
-			).WithLabels(
-				labelStrs,
-			).WithAlias(
-				string(id),
-			).WithCPUAllocationMillicpus(
-				cpuAllocationMillicpus,
-			).WithMemoryAllocationMegabytes(
-				memoryAllocationMegabytes,
+				filesArtifactsExpansion.ExpanderImage,
+				filesArtifactsExpansion.ExpanderEnvVars,
+				filesArtifactsExpansion.ExpanderDirpathsToServiceDirpaths,
+				dockerManager,
 			)
-
-			if entrypointArgs != nil {
-				createAndStartArgsBuilder.WithEntrypointArgs(entrypointArgs)
-			}
-			if cmdArgs != nil {
-				createAndStartArgsBuilder.WithCmdArgs(cmdArgs)
-			}
-			if volumeMounts != nil {
-				createAndStartArgsBuilder.WithVolumeMounts(volumeMounts)
-			}
-
-			createAndStartArgs := createAndStartArgsBuilder.Build()
-
-			// Best-effort pull attempt
-			if err = dockerManager.PullImage(ctx, containerImageName); err != nil {
-				logrus.Warnf("Failed to pull the latest version of user service container image '%v'; you may be running an out-of-date version", containerImageName)
-			}
-
-			containerId, hostMachinePortBindings, err := dockerManager.CreateAndStartContainer(ctx, createAndStartArgs)
 			if err != nil {
-				return stacktrace.Propagate(err, "An error occurred starting the user service container for user service with GUID '%v'", guid)
+				return nil, stacktrace.Propagate(err, "An error occurred doing files artifacts expansion to get user service volumes")
 			}
-			shouldKillContainer := true
 			defer func() {
-				if shouldKillContainer {
-					// TODO switch to removing the container, so that the service registration is still viable?
-					// NOTE: We use the background context here so that the kill will still go off even if the reason for
-					// the failure was the original context being cancelled
-					if err := dockerManager.KillContainer(context.Background(), containerId); err != nil {
-						logrus.Errorf(
-							"Launching user service container '%v' with container ID '%v' didn't complete successfully so we "+
-								"tried to kill the container we started, but doing so exited with an error:\n%v",
-							containerName.GetString(),
-							containerId,
-							err)
-						logrus.Errorf("ACTION REQUIRED: You'll need to manually stop user service container with ID '%v'!!!!!!", containerId)
+				if shouldDeleteVolumes {
+					for volumeName := range candidateVolumeMounts {
+						// Use background context so we delete these even if input context was cancelled
+						if err := dockerManager.RemoveVolume(context.Background(), volumeName); err != nil {
+							logrus.Errorf("Starting the service failed so we tried to delete files artifact expansion volume '%v' that we created, but doing so threw an error:\n%v", volumeName, err)
+							logrus.Errorf("You'll need to delete volume '%v' manually!", volumeName)
+						}
 					}
 				}
 			}()
-
-			_, _, maybePublicIp, maybePublicPortSpecs, err := shared_helpers.GetIpAndPortInfoFromContainer(
-				containerName.GetString(),
-				labelStrs,
-				hostMachinePortBindings,
-			)
-			if err != nil {
-				return stacktrace.Propagate(err, "An error occurred getting the public IP and ports from container '%v'", containerName)
-			}
-
-			serviceObj := service.NewService(
-				registration,
-				container_status.ContainerStatus_Running,
-				privatePorts,
-				maybePublicIp,
-				maybePublicPortSpecs)
-
-			dataChan <- operation_parallelizer.OperationData{ID: operation_parallelizer.OperationID(guid), Data: serviceObj}
-
-			shouldDeleteVolumes = false
-			shouldKillContainer = false
-			return nil
+			volumeMounts = candidateVolumeMounts
 		}
 
-		operations[operation_parallelizer.OperationID(guid)] = startServiceOp
-	}
+		containerAttrs, err := enclaveObjAttrsProvider.ForUserServiceContainer(
+			id,
+			serviceGUID,
+			privateIpAddr,
+			privatePorts,
+		)
+		if err != nil {
+			return nil, stacktrace.Propagate(err, "An error occurred while trying to get the user service container attributes for user service with GUID '%v'", serviceGUID)
+		}
+		containerName := containerAttrs.GetName()
 
-	return operations
+		labelStrs := map[string]string{}
+		for labelKey, labelValue := range containerAttrs.GetLabels() {
+			labelStrs[labelKey.GetString()] = labelValue.GetString()
+		}
+
+		dockerUsedPorts := map[nat.Port]docker_manager.PortPublishSpec{}
+		for portId, privatePortSpec := range privatePorts {
+			dockerPort, err := shared_helpers.TransformPortSpecToDockerPort(privatePortSpec)
+			if err != nil {
+				return nil, stacktrace.Propagate(err, "An error occurred converting private port spec '%v' to a Docker port", portId)
+			}
+			//TODO this is a huge hack to temporarily enable static ports for NEAR until we have a more productized solution
+			if publicPorts != nil && len(publicPorts) > 0 {
+				publicPortSpec, found := publicPorts[portId]
+				if !found {
+					return nil, stacktrace.NewError("Expected to receive public port with ID '%v' bound to private port number '%v', but it was not found", portId, privatePortSpec.GetNumber())
+				}
+				dockerUsedPorts[dockerPort] = docker_manager.NewManualPublishingSpec(publicPortSpec.GetNumber())
+			} else {
+				dockerUsedPorts[dockerPort] = docker_manager.NewAutomaticPublishingSpec()
+			}
+		}
+
+		createAndStartArgsBuilder := docker_manager.NewCreateAndStartContainerArgsBuilder(
+			containerImageName,
+			containerName.GetString(),
+			enclaveNetworkId,
+		).WithStaticIP(
+			privateIpAddr,
+		).WithUsedPorts(
+			dockerUsedPorts,
+		).WithEnvironmentVariables(
+			envVars,
+		).WithLabels(
+			labelStrs,
+		).WithAlias(
+			string(id),
+		).WithCPUAllocationMillicpus(
+			cpuAllocationMillicpus,
+		).WithMemoryAllocationMegabytes(
+			memoryAllocationMegabytes,
+		)
+
+		if entrypointArgs != nil {
+			createAndStartArgsBuilder.WithEntrypointArgs(entrypointArgs)
+		}
+		if cmdArgs != nil {
+			createAndStartArgsBuilder.WithCmdArgs(cmdArgs)
+		}
+		if volumeMounts != nil {
+			createAndStartArgsBuilder.WithVolumeMounts(volumeMounts)
+		}
+
+		createAndStartArgs := createAndStartArgsBuilder.Build()
+
+		// Best-effort pull attempt
+		if err = dockerManager.PullImage(ctx, containerImageName); err != nil {
+			logrus.Warnf("Failed to pull the latest version of user service container image '%v'; you may be running an out-of-date version", containerImageName)
+		}
+
+		containerId, hostMachinePortBindings, err := dockerManager.CreateAndStartContainer(ctx, createAndStartArgs)
+		if err != nil {
+			return nil, stacktrace.Propagate(err, "An error occurred starting the user service container for user service with GUID '%v'", serviceGUID)
+		}
+		shouldKillContainer := true
+		defer func() {
+			if shouldKillContainer {
+				// TODO switch to removing the container, so that the service registration is still viable?
+				// NOTE: We use the background context here so that the kill will still go off even if the reason for
+				// the failure was the original context being cancelled
+				if err := dockerManager.KillContainer(context.Background(), containerId); err != nil {
+					logrus.Errorf(
+						"Launching user service container '%v' with container ID '%v' didn't complete successfully so we "+
+							"tried to kill the container we started, but doing so exited with an error:\n%v",
+						containerName.GetString(),
+						containerId,
+						err)
+					logrus.Errorf("ACTION REQUIRED: You'll need to manually stop user service container with ID '%v'!!!!!!", containerId)
+				}
+			}
+		}()
+
+		_, _, maybePublicIp, maybePublicPortSpecs, err := shared_helpers.GetIpAndPortInfoFromContainer(
+			containerName.GetString(),
+			labelStrs,
+			hostMachinePortBindings,
+		)
+		if err != nil {
+			return nil, stacktrace.Propagate(err, "An error occurred getting the public IP and ports from container '%v'", containerName)
+		}
+
+		serviceObj := service.NewService(
+			serviceRegistration,
+			container_status.ContainerStatus_Running,
+			privatePorts,
+			maybePublicIp,
+			maybePublicPortSpecs)
+
+		shouldDeleteVolumes = false
+		shouldKillContainer = false
+		return serviceObj, nil
+	}
+	return startServiceOp
 }
