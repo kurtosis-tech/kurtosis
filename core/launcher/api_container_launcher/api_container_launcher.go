4d8176309f459569b7e2d9ee25d98effe9f0c966
tedim52 <hello@kurtosistech.com>
2022-08-15 01:36:59 +0000

    Finalize changes for release version '1.57.3'

--- core/launcher/api_container_launcher/api_container_launcher.go
+++ core/launcher/api_container_launcher/api_container_launcher.go
@@ -17,7 +17,7 @@ import (
 
 const (
 	// !!!!!!!!!!!!!!!!!! DO NOT MODIFY THIS! IT WILL BE UPDATED AUTOMATICALLY DURING THE RELEASE PROCESS !!!!!!!!!!!!!!!
-	DefaultVersion = "1.57.2"
+	DefaultVersion = "1.57.3"
 	// !!!!!!!!!!!!!!!!!! DO NOT MODIFY THIS! IT WILL BE UPDATED AUTOMATICALLY DURING THE RELEASE PROCESS !!!!!!!!!!!!!!!
 
 	enclaveDataVolumeDirpath = "/kurtosis-data"
