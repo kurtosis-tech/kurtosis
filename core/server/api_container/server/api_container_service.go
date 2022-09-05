5e3229fde6200e3d2fdfb60f38095701bbe66a42
Gyanendra Mishra <anomaly.the@gmail.com>
2022-09-05 21:26:18 +0530

    added a defer undo to remove file artifact in case of failure

--- core/server/api_container/server/api_container_service.go
+++ core/server/api_container/server/api_container_service.go
@@ -589,11 +589,21 @@ func (apicService ApiContainerService) RenderTemplatesToFilesArtifact(ctx contex
 	defer compressedFile.Close()
 
 	filesArtifactUuId, err := apicService.filesArtifactStore.StoreFile(compressedFile)
+	shouldDeleteFilesArtifact := true
 	if err != nil {
 		return nil, stacktrace.Propagate(err, "An error occurred while storing the file '%v' in the files artifact store", compressedFile)
 	}
+	defer func() {
+		if shouldDeleteFilesArtifact {
+			if err = apicService.filesArtifactStore.RemoveFile(filesArtifactUuId); err != nil {
+				logrus.Errorf("We tried to clean up the files artifact '%v' we had stored in the files artifact but failed:\n%v", filesArtifactUuId, err)
+			}
+		}
+	}()
 
 	response := &kurtosis_core_rpc_api_bindings.RenderTemplatesToFilesArtifactResponse{Uuid: string(filesArtifactUuId)}
+
+	shouldDeleteFilesArtifact = false
 	return response, nil
 }
 
