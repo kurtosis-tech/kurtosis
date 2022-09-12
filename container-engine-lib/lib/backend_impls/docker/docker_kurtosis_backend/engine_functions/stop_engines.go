20f3594d2320c42ad1af70bb1f6b2e23d30d066e
Leandro Poroli <leandro.poroli@kurtosistech.com>
2022-09-12 17:14:04 -0300

    Changes after PR review

--- container-engine-lib/lib/backend_impls/docker/docker_kurtosis_backend/engine_functions/stop_engines.go
+++ container-engine-lib/lib/backend_impls/docker/docker_kurtosis_backend/engine_functions/stop_engines.go
@@ -2,7 +2,6 @@ package engine_functions
 
 import (
 	"context"
-	"github.com/kurtosis-tech/container-engine-lib/lib/backend_impls/docker/docker_kurtosis_backend/shared_helpers"
 	"github.com/kurtosis-tech/container-engine-lib/lib/backend_impls/docker/docker_manager"
 	"github.com/kurtosis-tech/container-engine-lib/lib/backend_impls/docker/docker_operation_parallelizer"
 	"github.com/kurtosis-tech/container-engine-lib/lib/backend_interface/objects/engine"
@@ -71,40 +70,13 @@ func StopEngines(
 		)
 	}
 
-	if err := removeLogsComponentesGraceFully(ctx, dockerManager); err != nil {
-		return nil, nil, stacktrace.Propagate(err, "An error occurred removing the logs componentes containers")
+	//TODO we are removing the los components containers rather than stopping them because we are preparing the stage
+	//TODO for a single engine server, logs components containers have an static name, so if we stop the containers
+	//TODO the engine restart will fail because the container's name will be in use
+	if err := removeLogsComponentsGracefully(ctx, dockerManager); err != nil {
+		return nil, nil, stacktrace.Propagate(err, "An error occurred removing the logs components containers")
 	}
 
 	return successfulGuids, erroredGuids, nil
 }
 
-func removeLogsComponentesGraceFully(ctx context.Context, dockerManager *docker_manager.DockerManager) error {
-
-	logsCollectorContainer, err := shared_helpers.GetLogsCollectorContainer(ctx, dockerManager)
-	if err != nil {
-		return stacktrace.Propagate(err, "An error occurred getting the logs collector container")
-	}
-
-	if err := dockerManager.StopContainer(ctx, logsCollectorContainer.GetId(), stopLogsComponentsContainersTimeout); err != nil {
-		return stacktrace.Propagate(err, "An error occurred stopping the logs collector container with ID '%v'", logsCollectorContainer.GetId())
-	}
-
-	if err := dockerManager.RemoveContainer(ctx, logsCollectorContainer.GetId()); err != nil {
-		return stacktrace.Propagate(err, "An error occurred removing the logs collector container with ID '%v'", logsCollectorContainer.GetId())
-	}
-
-	logsDatabaseContainer, err := getLogsDatabaseContainer(ctx, dockerManager)
-	if err != nil {
-		return stacktrace.Propagate(err, "An error occurred getting the logs database container")
-	}
-
-	if err := dockerManager.StopContainer(ctx, logsDatabaseContainer.GetId(), stopLogsComponentsContainersTimeout); err != nil {
-		return stacktrace.Propagate(err, "An error occurred stopping the logs database container with ID '%v'", logsDatabaseContainer.GetId())
-	}
-
-	if err := dockerManager.RemoveContainer(ctx, logsDatabaseContainer.GetId()); err != nil {
-		return stacktrace.Propagate(err, "An error occurred removing the logs database container with ID '%v'", logsDatabaseContainer.GetId())
-	}
-
-	return nil
-}
