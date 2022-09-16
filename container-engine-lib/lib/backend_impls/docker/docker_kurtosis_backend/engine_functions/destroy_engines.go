60a86e0c3436b521b8da8b55249bb2c95165e82c
Leandro Poroli <leandro.poroli@kurtosistech.com>
2022-09-16 14:06:12 -0300

    Fixed adding engine filters

--- container-engine-lib/lib/backend_impls/docker/docker_kurtosis_backend/engine_functions/destroy_engines.go
+++ container-engine-lib/lib/backend_impls/docker/docker_kurtosis_backend/engine_functions/destroy_engines.go
@@ -70,7 +70,7 @@ func DestroyEngines(
 	//TODO we are removing the los components containers rather than stopping them because we are preparing the stage
 	//TODO for a single engine server, logs components containers have an static name, so if we stop the containers
 	//TODO the engine restart will fail because the container's name will be in use
-	if err := removeLogsComponentsGracefully(ctx, dockerManager); err != nil {
+	if err := removeLogsComponentsGracefully(ctx, filters, dockerManager); err != nil {
 		return nil, nil, stacktrace.Propagate(err, "An error occurred removing the logs components containers")
 	}
 
