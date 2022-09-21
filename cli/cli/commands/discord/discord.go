45e88a23b7c9e39eb8b0d90027b4ab48b3242119
Gyanendra Mishra <anomaly.the@gmail.com>
2022-09-21 15:13:20 -0300

    move to kurtosis-command

--- cli/cli/commands/discord/discord.go
+++ cli/cli/commands/discord/discord.go
@@ -1,31 +1,38 @@
 package discord
 
 import (
+	"context"
+	"github.com/kurtosis-tech/container-engine-lib/lib/backend_interface"
+	"github.com/kurtosis-tech/kurtosis-cli/cli/command_framework/highlevel/engine_consuming_kurtosis_command"
+	"github.com/kurtosis-tech/kurtosis-cli/cli/command_framework/lowlevel/args"
+	"github.com/kurtosis-tech/kurtosis-cli/cli/command_framework/lowlevel/flags"
 	"github.com/kurtosis-tech/kurtosis-cli/cli/command_str_consts"
+	"github.com/kurtosis-tech/kurtosis-engine-api-lib/api/golang/kurtosis_engine_rpc_api_bindings"
 	"github.com/kurtosis-tech/stacktrace"
-	"github.com/spf13/cobra"
 	"os/exec"
 	"runtime"
 )
 
 const (
-	linuxOSName = "linux"
-	macOSName = "darwin"
+	linuxOSName        = "linux"
+	macOSName          = "darwin"
 	windowsOSName      = "windows"
 	kurtosisDiscordUrl = "https://discord.com/channels/783719264308953108/783719264308953111"
 )
 
-var DiscordCmd = &cobra.Command{
-	Use:   command_str_consts.DiscordCmdStr,
-	Short: "Opens the Kurtosis Discord",
-	RunE:  run,
+var DiscordCmd = &engine_consuming_kurtosis_command.EngineConsumingKurtosisCommand{
+	CommandStr:       command_str_consts.DiscordCmdStr,
+	ShortDescription: "Opens the Kurtosis Discord",
+	RunFunc:          run,
 }
 
-func init() {
-	// No flags yet
-}
-
-func run(_ *cobra.Command, _ []string) error {
+func run(
+	_ context.Context,
+	_ backend_interface.KurtosisBackend,
+	_ kurtosis_engine_rpc_api_bindings.EngineServiceClient,
+	_ *flags.ParsedFlags,
+	_ *args.ParsedArgs,
+) error {
 	var args []string
 	switch runtime.GOOS {
 	case linuxOSName:
