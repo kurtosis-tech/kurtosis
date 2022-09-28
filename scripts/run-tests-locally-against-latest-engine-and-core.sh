be017f325f74775e8a809b9fe4e29b03d7d799f9
Gyanendra Mishra <anomaly.the@gmail.com>
2022-09-28 19:08:52 -0300

    add k8s proper

--- scripts/run-tests-locally-against-latest-engine-and-core.sh
+++ scripts/run-tests-locally-against-latest-engine-and-core.sh
@@ -23,9 +23,17 @@ TESTSUITE_CLUSTER_BACKEND_MINIKUBE="minikube"
 # By default, run testsuite against docker
 DEFAULT_TESTSUITE_CLUSTER_BACKEND="${TESTSUITE_CLUSTER_BACKEND_DOCKER}"
 
+
 # ==================================================================================================
-#                                             Main Logic
+#                                       Arg Parsing & Validation
 # ==================================================================================================
+show_helptext_and_exit() {
+    echo "Usage: $(basename "${0}") cli_cluster_backend_arg"
+    echo ""
+    echo "  cli_cluster_backend_arg   Optional argument describing the cluster backend tests are running against. Must be one of 'docker', 'minikube' (default: ${DEFAULT_TESTSUITE_CLUSTER_BACKEND})"
+    echo ""
+    exit 1  # Exit with an error so that if this is accidentally called by CI, the script will fail
+}
 
 testsuite_cluster_backend_arg="${1:-${DEFAULT_TESTSUITE_CLUSTER_BACKEND}}"
 if [ "${testsuite_cluster_backend_arg}" != "${TESTSUITE_CLUSTER_BACKEND_DOCKER}" ] &&
@@ -34,12 +42,27 @@ if [ "${testsuite_cluster_backend_arg}" != "${TESTSUITE_CLUSTER_BACKEND_DOCKER}"
     show_helptext_and_exit
 fi
 
+
+# ==================================================================================================
+#                                             Main Logic
+# ==================================================================================================
 # if the test suite is k8s we build & run images in k8s
+# we start minikube if it isn't running
+# we set the docker to be the one inside minikube
 if [ "${testsuite_cluster_backend_arg}" == "${TESTSUITE_CLUSTER_BACKEND_MINIKUBE}" ]; then
-    eval $(minikube docker-env)
-    minikube start
+    if ! minikube status; then
+      if ! minikube start; then
+          echo "Error starting minikube" >&2
+          exit 1
+      fi
+    fi
+    if ! eval $(minikube docker-env); then
+        echo "Error changing docker environment to minikube" >&2
+        exit 1
+    fi
 fi
 
+# build all server images
 for build_script_rel_filepath in "${BUILD_SCRIPT_RELATIVE_FILEPATHS[@]}"; do
     build_script_abs_filepath="${root_dirpath}/${build_script_rel_filepath}"
     if ! bash "${build_script_abs_filepath}"; then
@@ -48,24 +71,38 @@ for build_script_rel_filepath in "${BUILD_SCRIPT_RELATIVE_FILEPATHS[@]}"; do
     fi
 done
 
+# stop existing engine
+if ! bash "${cli_launch_path}" engine stop; then
+    echo "Error: Stopping the engine failed" >&2
+    exit 1
+fi
+
+# if k8s we set cluster to minikube
+if ! bash "${cli_launch_path}" cluster set minikube; then
+    echo "Setting the cluster to minikube failed" >&2
+    exit 1
+fi
 
 source ${root_dirpath}/_kurtosis_servers.env
 
 if ! bash "${cli_launch_path}" engine restart --version ${CORE_ENGINE_VERSION_TAG}; then
-    echo "Error: Build script '${cli_launch_path}' failed" >&2
+    echo "Restarting the engine failed" >&2
     exit 1
 fi
 
 # if minikube run engine gateway
+gateway_pid=""
 if [ "${testsuite_cluster_backend_arg}" == "${TESTSUITE_CLUSTER_BACKEND_MINIKUBE}" ]; then
-  if ! bash "${cli_launch_path}" gateway &; then
-      echo "Error: Build script '${cli_launch_path}' failed" >&2
-      exit 1
-  fi
-f
+  "${cli_launch_path}" "gateway" &
+  gateway_pid="${!}"
+fi
 
-if ! bash "${internal_test_suite_build_script_path}"; then
+if ! bash "${internal_test_suite_build_script_path}" "${testsuite_cluster_backend_arg}"; then
     echo "Error: Build script '${internal_test_suite_build_script_path}' failed" >&2
     exit 1
 fi
 
+# kill the gateway pid if its k8s
+if [ "${testsuite_cluster_backend_arg}" == "${TESTSUITE_CLUSTER_BACKEND_MINIKUBE}" ]; then
+  "kill" "${gateway_pid}"
+fi
