b81c62b103876e226a1e462a97443bec75180193
Victor Colombo <victor.colombo@kurtosistech.com>
2022-09-30 11:18:02 -0300

    Improve test

--- container-engine-lib/lib/free_ip_addr_tracker/free_ip_addr_tracker_test.go
+++ container-engine-lib/lib/free_ip_addr_tracker/free_ip_addr_tracker_test.go
@@ -7,7 +7,7 @@ package free_ip_addr_tracker
 
 import (
 	"github.com/sirupsen/logrus"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	bolt "go.etcd.io/bbolt"
 	"net"
 	"os"
@@ -15,84 +15,87 @@ import (
 )
 
 func TestGetIp(t *testing.T) {
-	db, err := bolt.Open("test.db", 0666, nil)
-	assert.Nil(t, err)
+	file, err := os.CreateTemp("/tmp", "*.db")
+	defer os.Remove(file.Name())
+	require.Nil(t, err)
+	db, err := bolt.Open(file.Name(), 0666, nil)
+	defer db.Close()
+	require.Nil(t, err)
 	subnetMask := "1.2.3.4/16"
 	_, parsedSubnetMask, err := net.ParseCIDR(subnetMask)
-	assert.Nil(t, err)
+	require.Nil(t, err)
 	addrTracker, err := GetOrCreateNewFreeIpAddrTracker(logrus.StandardLogger(), parsedSubnetMask, map[string]bool{
 		"1.2.0.2": true,
 		"1.2.0.3": true,
 	}, db)
-	assert.Nil(t, err)
+	require.Nil(t, err)
 
 	ip, err := addrTracker.GetFreeIpAddr()
-	assert.Nil(t, err)
-	assert.Equal(t, "1.2.0.1", ip.String())
+	require.Nil(t, err)
+	require.Equal(t, "1.2.0.1", ip.String())
 
 	ip2, err := addrTracker.GetFreeIpAddr()
-	assert.Nil(t, err)
-	assert.Equal(t, "1.2.0.4", ip2.String())
+	require.Nil(t, err)
+	require.Equal(t, "1.2.0.4", ip2.String())
 
 	ip3, err := addrTracker.GetFreeIpAddr()
-	assert.Nil(t, err)
-	assert.Equal(t, "1.2.0.5", ip3.String())
-	err = db.Close()
-	assert.Nil(t, err)
-	os.Remove("test.db")
+	require.Nil(t, err)
+	require.Equal(t, "1.2.0.5", ip3.String())
 }
 
 func TestReleaseIp(t *testing.T) {
-	db, err := bolt.Open("test.db", 0666, nil)
-	assert.Nil(t, err)
+	file, err := os.CreateTemp("/tmp", "*.db")
+	defer os.Remove(file.Name())
+	require.Nil(t, err)
+	db, err := bolt.Open(file.Name(), 0666, nil)
+	defer db.Close()
+	require.Nil(t, err)
 	subnetMask := "1.2.3.4/16"
 	_, parsedSubnetMask, err := net.ParseCIDR(subnetMask)
-	assert.Nil(t, err)
+	require.Nil(t, err)
 	addrTracker, err := GetOrCreateNewFreeIpAddrTracker(logrus.StandardLogger(), parsedSubnetMask, map[string]bool{}, db)
-	assert.Nil(t, err)
+	require.Nil(t, err)
 
 	ip, err := addrTracker.GetFreeIpAddr()
-	assert.Nil(t, err)
-	assert.Equal(t, "1.2.0.1", ip.String())
+	require.Nil(t, err)
+	require.Equal(t, "1.2.0.1", ip.String())
 
 	ip2, err := addrTracker.GetFreeIpAddr()
-	assert.Nil(t, err)
-	assert.Equal(t, "1.2.0.2", ip2.String())
+	require.Nil(t, err)
+	require.Equal(t, "1.2.0.2", ip2.String())
 
-	addrTracker.ReleaseIpAddr(ip)
+	err = addrTracker.ReleaseIpAddr(ip)
+	require.Nil(t, err)
 
 	ip3, err := addrTracker.GetFreeIpAddr()
-	assert.Nil(t, err)
-	assert.Equal(t, "1.2.0.1", ip3.String())
-	err = db.Close()
-	assert.Nil(t, err)
-	os.Remove("test.db")
+	require.Nil(t, err)
+	require.Equal(t, "1.2.0.1", ip3.String())
 }
 
 func TestIpTrackerDiskPersistence(t *testing.T) {
-	db, err := bolt.Open("test.db", 0666, nil)
-	assert.Nil(t, err)
+	file, err := os.CreateTemp("/tmp", "*.db")
+	defer os.Remove(file.Name())
+	require.Nil(t, err)
+	db, err := bolt.Open(file.Name(), 0666, nil)
+	defer db.Close()
+	require.Nil(t, err)
 	subnetMask := "1.2.3.4/16"
 	_, parsedSubnetMask, err := net.ParseCIDR(subnetMask)
-	assert.Nil(t, err)
+	require.Nil(t, err)
 	addrTracker, err := GetOrCreateNewFreeIpAddrTracker(logrus.StandardLogger(), parsedSubnetMask, map[string]bool{
 		"1.2.0.2": true,
 		"1.2.0.3": true,
 	}, db)
-	assert.Nil(t, err)
+	require.Nil(t, err)
 
 	ip, err := addrTracker.GetFreeIpAddr()
-	assert.Nil(t, err)
-	assert.Equal(t, "1.2.0.1", ip.String())
+	require.Nil(t, err)
+	require.Equal(t, "1.2.0.1", ip.String())
 
 	addrTracker2, err := GetOrCreateNewFreeIpAddrTracker(logrus.StandardLogger(), parsedSubnetMask, map[string]bool{}, db)
-	assert.Nil(t, err)
+	require.Nil(t, err)
 
 	ip2, err := addrTracker2.GetFreeIpAddr()
-	assert.Nil(t, err)
-	assert.Equal(t, "1.2.0.4", ip2.String())
-
-	assert.Nil(t, err)
-
-	os.Remove("test.db")
+	require.Nil(t, err)
+	require.Equal(t, "1.2.0.4", ip2.String())
 }
