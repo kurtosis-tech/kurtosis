a81e41d036416f1ab957a0e8277680b6c7ac3374
Gyanendra Mishra <anomaly.the@gmail.com>
2022-09-30 10:18:15 -0300

    change version script

--- scripts/generate-kurtosis-version.sh
+++ scripts/generate-kurtosis-version.sh
@@ -11,9 +11,46 @@ root_dirpath="$(dirname "${script_dirpath}")"
 
 KURTOSIS_VERSION_PACKAGE_DIR="kurtosis_version"
 KURTOSIS_VERSION_PACKAGE_NAME="github.com/kurtosis-tech/kurtosis/kurtosis_version"
-KURTOSIS_VERSION_PACKAGE_GOSUM_PATH="kurtosis_version/go.sum"
+KURTOSIS_VERSION_PACKAGE_GOSUM_PATH="go.sum"
+
+
+# ==================================================================================================
+#                                       Arg Parsing & Validation
+# ==================================================================================================
+show_helptext_and_exit() {
+    echo "Usage: $(basename "${0}")"
+    exit 1
+}
+
 
 # ==================================================================================================
 #                                             Main Logic
 # ==================================================================================================
+
+if ![--d "${root_dirpath}/${KURTOSIS_VERSION_PACKAGE_DIR}"]; then
+  if ! bash "mkdir" "${root_dirpath}/${KURTOSIS_VERSION_PACKAGE_DIR}" ; then
+    echo "Failed to create directory '${root_dirpath}/${KURTOSIS_VERSION_PACKAGE_DIR}'" >&2
+    exit 1
+  fi
+fi
+
+if ! bash "cd" "${root_dirpath}/${KURTOSIS_VERSION_PACKAGE_DIR}"; then
+  echo "Failed to cd into directory '${root_dirpath}/${KURTOSIS_VERSION_PACKAGE_DIR}'" >&2
+fi
+
+if ! bash "go" "mod" "init" "${KURTOSIS_VERSION_PACKAGE_NAME}" ; then
+  echo "Failed to create package '${KURTOSIS_VERSION_PACKAGE_NAME}'" >&2
+  exit 1
+fi
+
+go_sum_path="${root_dirpath}/${KURTOSIS_VERSION_PACKAGE_DIR}/${KURTOSIS_VERSION_PACKAGE_GOSUM_PATH}"
+if ! bash "go" "touch" "${go_sum_path}" ; then
+  echo "Failed to create package '${go_sum_path}'" >&2
+  exit 1
+fi
+
+if ! docker_tag="$(kudet get-docker-tag)"; then
+    echo "Error: Couldn't get the Docker image tag" >&2
+    exit 1
+fi
 
