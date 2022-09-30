33b3375439532180359a0cc8955621d3a8bf6c99
Gyanendra Mishra <anomaly.the@gmail.com>
2022-09-30 10:38:39 -0300

    add version generation script

--- scripts/generate-kurtosis-version.sh
+++ scripts/generate-kurtosis-version.sh
@@ -10,6 +10,7 @@ root_dirpath="$(dirname "${script_dirpath}")"
 # ==================================================================================================
 
 KURTOSIS_VERSION_PACKAGE_DIR="kurtosis_version"
+KURTOSIS_VERSION_GO_FILE="kurtosis_version.go"
 KURTOSIS_VERSION_PACKAGE_NAME="github.com/kurtosis-tech/kurtosis/kurtosis_version"
 KURTOSIS_VERSION_PACKAGE_GOSUM_PATH="go.sum"
 
@@ -27,25 +28,29 @@ show_helptext_and_exit() {
 #                                             Main Logic
 # ==================================================================================================
 
-if ![--d "${root_dirpath}/${KURTOSIS_VERSION_PACKAGE_DIR}"]; then
-  if ! bash "mkdir" "${root_dirpath}/${KURTOSIS_VERSION_PACKAGE_DIR}" ; then
-    echo "Failed to create directory '${root_dirpath}/${KURTOSIS_VERSION_PACKAGE_DIR}'" >&2
-    exit 1
-  fi
+if ! rm "-f" "${root_dirpath}/${KURTOSIS_VERSION_PACKAGE_DIR}"; then
+  echo "Failed to remove '${root_dirpath}/${KURTOSIS_VERSION_PACKAGE_DIR}'"
+  exit 1
+fi
+
+if ! mkdir "${root_dirpath}/${KURTOSIS_VERSION_PACKAGE_DIR}" ; then
+  echo "Failed to create directory '${root_dirpath}/${KURTOSIS_VERSION_PACKAGE_DIR}'" >&2
+  exit 1
 fi
 
 if ! bash "cd" "${root_dirpath}/${KURTOSIS_VERSION_PACKAGE_DIR}"; then
   echo "Failed to cd into directory '${root_dirpath}/${KURTOSIS_VERSION_PACKAGE_DIR}'" >&2
+  exit 1
 fi
 
-if ! bash "go" "mod" "init" "${KURTOSIS_VERSION_PACKAGE_NAME}" ; then
+if ! go "mod" "init" "${KURTOSIS_VERSION_PACKAGE_NAME}" ; then
   echo "Failed to create package '${KURTOSIS_VERSION_PACKAGE_NAME}'" >&2
   exit 1
 fi
 
-go_sum_path="${root_dirpath}/${KURTOSIS_VERSION_PACKAGE_DIR}/${KURTOSIS_VERSION_PACKAGE_GOSUM_PATH}"
-if ! bash "go" "touch" "${go_sum_path}" ; then
-  echo "Failed to create package '${go_sum_path}'" >&2
+go_sum_abs_path="${root_dirpath}/${KURTOSIS_VERSION_PACKAGE_DIR}/${KURTOSIS_VERSION_PACKAGE_GOSUM_PATH}"
+if ! touch "${go_sum_abs_path}" ; then
+  echo "Failed to create go sum '${go_sum_abs_path}'" >&2
   exit 1
 fi
 
@@ -53,4 +58,13 @@ if ! docker_tag="$(kudet get-docker-tag)"; then
     echo "Error: Couldn't get the Docker image tag" >&2
     exit 1
 fi
+
+kurtosis_version_go_file_abs_path="${root_dirpath}/${KURTOSIS_VERSION_PACKAGE_DIR}/${KURTOSIS_VERSION_GO_FILE}"
+echo "package ${KURTOSIS_VERSION_PACKAGE_DIR}" > "${kurtosis_version_go_file_abs_path}"
+echo "" >> "${kurtosis_version_go_file_abs_path}"
+echo "const (" >> "${kurtosis_version_go_file_abs_path}"
+echo "	// !!!!!!!!!!!!!!!!!! DO NOT MODIFY THIS! IT WILL BE UPDATED AUTOMATICALLY DURING THE RELEASE PROCESS !!!!!!!!!!!!!!!" >> "${kurtosis_version_go_file_abs_path}"
+echo "	KurtosisVersion = "${docker_tag}"" >> "${kurtosis_version_go_file_abs_path}"
+echo "  // !!!!!!!!!!!!!!!!!! DO NOT MODIFY THIS! IT WILL BE UPDATED AUTOMATICALLY DURING THE RELEASE PROCESS !!!!!!!!!!!!!!!"> "${kurtosis_version_go_file_abs_path}"
+echo ")" >> "${kurtosis_version_go_file_abs_path}"
 
