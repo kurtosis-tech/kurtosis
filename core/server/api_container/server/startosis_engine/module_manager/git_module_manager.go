df9966d0d17e766c7fd76143a8d360687bb5eb66
Gyanendra Mishra <anomaly.the@gmail.com>
2022-10-03 13:08:49 -0300

    save state

--- core/server/api_container/server/startosis_engine/module_manager/git_module_manager.go
+++ core/server/api_container/server/startosis_engine/module_manager/git_module_manager.go
@@ -27,24 +27,14 @@ func NewGitModuleManager(moduleDir string, tmpDir string) *GitModuleManager {
 	}
 }
 
-func (moduleManager *GitModuleManager) GetModule(githubURL string) (string, error) {
-	parsedUrl, err := url.Parse(githubURL)
+func (moduleManager *GitModuleManager) GetModule(packageURL string) (string, error) {
+	// TODO move validate URL to parse and return things
+	err := validateURL(packageURL)
 	if err != nil {
-		return "", stacktrace.Propagate(err, "Error parsing the url '%v'", githubURL)
-	}
-	if parsedUrl.Scheme != "https" {
-		return "", stacktrace.NewError("Expected the scheme to be 'https' got '%v'", parsedUrl.Scheme)
-	}
-	if parsedUrl.Host != "github.com" {
-		return "", stacktrace.NewError("We only support modules on Github for now")
+		return "", stacktrace.Propagate(err, "An error occurred while validating URL")
 	}
 
 	splitURLPath := removeEmpty(strings.Split(parsedUrl.Path, "/"))
-
-	if len(splitURLPath) < 2 {
-		return "", stacktrace.NewError("URL path should contain at least 2 parts")
-	}
-
 	contents, err := os.ReadFile(moduleManager.getPathToStartosisFile(splitURLPath))
 	if err == nil {
 		return string(contents), nil
@@ -88,6 +78,27 @@ func (moduleManager *GitModuleManager) getPathToStartosisFile(splitUrlPath []str
 	splitUrlPath = append([]string{moduleManager.moduleDir}, splitUrlPath...)
 	filePath := path.Join(splitUrlPath...)
 	return filePath
+}
+
+func validateURL(packageURL string) error {
+	parsedUrl, err := url.Parse(packageURL)
+	if err != nil {
+		return stacktrace.Propagate(err, "Error parsing the url '%v'", packageURL)
+	}
+	if parsedUrl.Scheme != "https" {
+		return stacktrace.NewError("Expected the scheme to be 'https' got '%v'", parsedUrl.Scheme)
+	}
+	if parsedUrl.Host != "github.com" {
+		return stacktrace.NewError("We only support modules on Github for now")
+	}
+
+	splitURLPath := removeEmpty(strings.Split(parsedUrl.Path, "/"))
+
+	if len(splitURLPath) < 2 {
+		return stacktrace.NewError("URL path should contain at least 2 parts")
+	}
+
+	return nil
 }
 
 func removeEmpty(splitPath []string) []string {
