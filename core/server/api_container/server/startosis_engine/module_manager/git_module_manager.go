cebafad7f2c1145d5d052333b2be6ebedd4cd957
Gyanendra Mishra <anomaly.the@gmail.com>
2022-10-03 15:58:35 -0300

    cleanup git module manager

--- core/server/api_container/server/startosis_engine/module_manager/git_module_manager.go
+++ core/server/api_container/server/startosis_engine/module_manager/git_module_manager.go
@@ -1,6 +1,7 @@
 package module_manager
 
 import (
+	"fmt"
 	"github.com/go-git/go-git/v5"
 	"github.com/kurtosis-tech/stacktrace"
 	"io"
@@ -13,11 +14,32 @@ import (
 const (
 	moduleDirPermission     = 0755
 	temporaryRepoDirPattern = "tmp-repo-dir-*"
+	githubDomain            = "github.com"
+	httpsSchema             = "https"
 )
 
 type GitModuleManager struct {
 	moduleTmpDir string
 	moduleDir    string
+	gitURL       string
+}
+
+type ParsedGitURL struct {
+	moduleAuthor       string
+	moduleName         string
+	gitURL             string
+	relativeModulePath string
+	relativeFilePath   string
+}
+
+func NewParsedGITURL(moduleAuthor, moduleName, gitURL, relativeModulePath, relativeFilePath string) *ParsedGitURL {
+	return &ParsedGitURL{
+		moduleAuthor:       moduleAuthor,
+		moduleName:         moduleName,
+		gitURL:             gitURL,
+		relativeModulePath: relativeModulePath,
+		relativeFilePath:   relativeFilePath,
+	}
 }
 
 func NewGitModuleManager(moduleDir string, tmpDir string) *GitModuleManager {
@@ -28,35 +50,30 @@ func NewGitModuleManager(moduleDir string, tmpDir string) *GitModuleManager {
 }
 
 func (moduleManager *GitModuleManager) GetModule(packageURL string) (string, error) {
-	// TODO move validate URL to parse and return things
-	err := validateURL(packageURL)
+	parsedURL, err := parseGitURL(packageURL)
 	if err != nil {
-		return "", stacktrace.Propagate(err, "An error occurred while validating URL")
+		return "", stacktrace.Propagate(err, "An error occurred while parsing URL")
 	}
 
-	parsedUrl, _ := url.Parse(packageURL)
-	splitURLPath := removeEmpty(strings.Split(parsedUrl.Path, "/"))
-	contents, err := os.ReadFile(moduleManager.getPathToStartosisFile(splitURLPath))
+	pathToStartosisFile := path.Join(moduleManager.moduleDir, parsedURL.relativeFilePath)
+
+	contents, err := os.ReadFile(pathToStartosisFile)
 	if err == nil {
 		return string(contents), nil
 	}
-
-	firstTwoSubPaths := strings.Join(splitURLPath[:2], "/")
-	authorName := splitURLPath[0]
-	gitRepo := "https://github.com/" + firstTwoSubPaths
 
 	tempRepoDirPath, err := os.MkdirTemp(moduleManager.moduleTmpDir, temporaryRepoDirPattern)
 	if err != nil {
 		return "", stacktrace.Propagate(err, "Error creating temporary directory for the repository to be cloned into")
 	}
 	defer os.RemoveAll(tempRepoDirPath)
-	gitClonePath := path.Join(tempRepoDirPath, firstTwoSubPaths)
-	_, err = git.PlainClone(gitClonePath, false, &git.CloneOptions{URL: gitRepo, Progress: io.Discard})
+	gitClonePath := path.Join(tempRepoDirPath, parsedURL.relativeModulePath)
+	_, err = git.PlainClone(gitClonePath, false, &git.CloneOptions{URL: parsedURL.gitURL, Progress: io.Discard})
 	if err != nil {
-		return "", stacktrace.Propagate(err, "Error in cloning git repository '%v'", gitRepo)
+		return "", stacktrace.Propagate(err, "Error in cloning git repository '%v' to '%v'", parsedURL.gitURL, gitClonePath)
 	}
-	moduleAuthorPath := path.Join(moduleManager.moduleDir, authorName)
-	modulePath := path.Join(moduleManager.moduleDir, firstTwoSubPaths)
+	moduleAuthorPath := path.Join(moduleManager.moduleDir, parsedURL.moduleAuthor)
+	modulePath := path.Join(moduleManager.moduleDir, parsedURL.relativeModulePath)
 	_, err = os.Stat(moduleAuthorPath)
 	if err != nil {
 		if err = os.Mkdir(moduleAuthorPath, moduleDirPermission); err != nil {
@@ -67,7 +84,7 @@ func (moduleManager *GitModuleManager) GetModule(packageURL string) (string, err
 		return "", stacktrace.Propagate(err, "An error occurred while moving module at temporary destination to final destination")
 	}
 
-	contents, err = os.ReadFile(moduleManager.getPathToStartosisFile(splitURLPath))
+	contents, err = os.ReadFile(pathToStartosisFile)
 	if err != nil {
 		return "", stacktrace.Propagate(err, "An error occurred in reading contents of the StarLark file")
 	}
@@ -75,39 +92,47 @@ func (moduleManager *GitModuleManager) GetModule(packageURL string) (string, err
 	return string(contents), nil
 }
 
-func (moduleManager *GitModuleManager) getPathToStartosisFile(splitUrlPath []string) string {
-	splitUrlPath = append([]string{moduleManager.moduleDir}, splitUrlPath...)
-	filePath := path.Join(splitUrlPath...)
-	return filePath
-}
-
-func validateURL(packageURL string) error {
+func parseGitURL(packageURL string) (*ParsedGitURL, error) {
 	parsedUrl, err := url.Parse(packageURL)
 	if err != nil {
-		return stacktrace.Propagate(err, "Error parsing the url '%v'", packageURL)
+		return nil, stacktrace.Propagate(err, "Error parsing the url '%v'", packageURL)
 	}
-	if parsedUrl.Scheme != "https" {
-		return stacktrace.NewError("Expected the scheme to be 'https' got '%v'", parsedUrl.Scheme)
+	if parsedUrl.Scheme != httpsSchema {
+		return nil, stacktrace.NewError("Expected the scheme to be 'https' got '%v'", parsedUrl.Scheme)
 	}
-	if parsedUrl.Host != "github.com" {
-		return stacktrace.NewError("We only support modules on Github for now")
+	if parsedUrl.Host != githubDomain {
+		return nil, stacktrace.NewError("We only support modules on Github for now")
 	}
 
-	splitURLPath := removeEmpty(strings.Split(parsedUrl.Path, "/"))
+	splitURLPath := removeEmptyStringsFromSlice(strings.Split(parsedUrl.Path, "/"))
 
 	if len(splitURLPath) < 2 {
-		return stacktrace.NewError("URL path should contain at least 2 parts")
+		return nil, stacktrace.NewError("URL path should contain at least 2 parts")
 	}
 
-	return nil
+	moduleAuthor := splitURLPath[0]
+	moduleName := splitURLPath[1]
+	gitURL := fmt.Sprintf("%v://%v/%v/%v.git", httpsSchema, githubDomain, moduleAuthor, moduleName)
+	relativeModulePath := path.Join(moduleAuthor, moduleName)
+	relativeFilePath := path.Join(splitURLPath...)
+
+	parsedGitURL := NewParsedGITURL(
+		moduleAuthor,
+		moduleName,
+		gitURL,
+		relativeModulePath,
+		relativeFilePath,
+	)
+
+	return parsedGitURL, nil
 }
 
-func removeEmpty(splitPath []string) []string {
-	var splitWithoutEmpties []string
-	for _, subPath := range splitPath {
+func removeEmptyStringsFromSlice(stringSlice []string) []string {
+	var sliceWithoutEmptyStrings []string
+	for _, subPath := range stringSlice {
 		if subPath != "" {
-			splitWithoutEmpties = append(splitWithoutEmpties, subPath)
+			sliceWithoutEmptyStrings = append(sliceWithoutEmptyStrings, subPath)
 		}
 	}
-	return splitWithoutEmpties
+	return sliceWithoutEmptyStrings
 }
