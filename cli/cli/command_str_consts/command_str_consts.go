0c2c6f34224036c37488fdc523313cb4836ec49d
guillaume.bouvignies <guillaume.bouvignies@kurtosistech.com>
2022-10-06 19:54:01 +0200

    Revert formatting in command_str_consts.go

--- cli/cli/command_str_consts/command_str_consts.go
+++ cli/cli/command_str_consts/command_str_consts.go
@@ -6,70 +6,47 @@ import (
 )
 
 // We put all the command strings here so that when we need to give users remediation instructions, we can give them the
-// commands they need to run
+//  commands they need to run
 var KurtosisCmdStr = path.Base(os.Args[0])
 
 const (
-	// clean
 	CleanCmdStr = "clean"
-
-	// cluster
-	ClusterCmdStr    = "cluster"
-	ClusterSetCmdStr = "set"
-	ClusterGetCmdStr = "get"
-	ClusterLsCmdStr  = "ls"
-
-	// discord
+	ClusterCmdStr = "cluster"
+		ClusterSetCmdStr = "set"
+		ClusterGetCmdStr = "get"
+		ClusterLsCmdStr = "ls"
 	DiscordCmdStr = "discord"
-
-	// enclave
-	EnclaveCmdStr        = "enclave"
-	EnclaveInspectCmdStr = "inspect"
-	EnclaveLsCmdStr      = "ls"
-	EnclaveAddCmdStr     = "add"
-	EnclaveStopCmdStr    = "stop"
-	EnclaveRmCmdStr      = "rm"
-	EnclaveDumpCmdStr    = "dump"
-
-	// engine
-	EngineCmdStr        = "engine"
-	EngineStartCmdStr   = "start"
-	EngineStatusCmdStr  = "status"
-	EngineStopCmdStr    = "stop"
-	EngineRestartCmdStr = "restart"
-
-	// files
-	FilesCmdStr             = "files"
-	FilesUploadCmdStr       = "upload"
-	FilesStoreWebCmdStr     = "storeweb"
-	FilesStoreServiceCmdStr = "storeservice"
-	FilesRenderTemplate     = "rendertemplate"
-
-	// module
-	ModuleCmdStr     = "module"
-	ModuleExecCmdStr = "exec"
-
-	// service
-	ServiceCmdStr        = "service"
-	ServiceAddCmdStr     = "add"
-	ServiceLogsCmdStr    = "logs"
-	ServicePauseCmdStr   = "pause"
-	ServiceRmCmdStr      = "rm"
-	ServiceUnpauseCmdStr = "unpause"
-	ServiceShellCmdStr   = "shell"
-
-	// startosis
+	EnclaveCmdStr = "enclave"
+		EnclaveInspectCmdStr = "inspect"
+		EnclaveLsCmdStr   = "ls"
+		EnclaveAddCmdStr  = "add"
+		EnclaveStopCmdStr = "stop"
+		EnclaveRmCmdStr = "rm"
+		EnclaveDumpCmdStr = "dump"
+	EngineCmdStr              = "engine"
+		EngineStartCmdStr   = "start"
+		EngineStatusCmdStr  = "status"
+		EngineStopCmdStr    = "stop"
+		EngineRestartCmdStr = "restart"
+	FilesCmdStr           = "files"
+		FilesUploadCmdStr = "upload"
+		FilesStoreWebCmdStr = "storeweb"
+		FilesStoreServiceCmdStr = "storeservice"
+		FilesRenderTemplate     = "rendertemplate"
+	ModuleCmdStr = "module"
+		ModuleExecCmdStr = "exec"
+	ServiceCmdStr = "service"
+		ServiceAddCmdStr = "add"
+		ServiceLogsCmdStr = "logs"
+		ServicePauseCmdStr = "pause"
+		ServiceRmCmdStr = "rm"
+		ServiceUnpauseCmdStr = "unpause"
+		ServiceShellCmdStr   = "shell"
 	StartosisCmdStr     = "startosis"
-	StartosisExecCmdStr = "exec"
-
-	// config
-	ConfigCmdStr = "config"
-	InitCmdStr   = "init"
-	PathCmdStr   = "path"
-
-	// version
+		StartosisExecCmdStr = "exec"
+	ConfigCmdStr             = "config"
+		InitCmdStr = "init"
+		PathCmdStr = "path"
 	VersionCmdStr = "version"
-
-	// gateway
 	GatewayCmdStr = "gateway"
 )
