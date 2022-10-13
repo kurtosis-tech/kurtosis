13be66ad2537dc11d7e269e3b5625d531cd541f9
Gyanendra Mishra <anomaly.the@gmail.com>
2022-10-13 13:45:14 -0300

    fix ip address referral

--- core/server/api_container/server/startosis_engine/kurtosis_types/service_test.go
+++ core/server/api_container/server/startosis_engine/kurtosis_types/service_test.go
@@ -9,7 +9,8 @@ import (
 func TestService_StringRepresentation(t *testing.T) {
 	ipAddressStr := "{{kurtosis:service_id.ip_address}}"
 	ports := starlark.NewDict(1)
-	ports.SetKey(starlark.String("grpc"), NewPortSpec(starlark.MakeInt(123), "TCP"))
+	err := ports.SetKey(starlark.String("grpc"), NewPortSpec(starlark.MakeInt(123), "TCP"))
+	require.Nil(t, err)
 	service := NewService(starlark.String(ipAddressStr), ports)
 	expectedStr := `service(ip_address="{{kurtosis:service_id.ip_address}}", ports={"grpc": port_spec(number=123, protocol="TCP")})`
 	require.Equal(t, expectedStr, service.String())
