990da600a5eeda4c262f5d65d29debd44dab8007
Victor Colombo <victor.colombo@kurtosistech.com>
2022-10-19 11:39:29 -0300

    Comment

--- core/server/api_container/server/startosis_engine/kurtosis_instruction/exec/exec.go
+++ core/server/api_container/server/startosis_engine/kurtosis_instruction/exec/exec.go
@@ -91,10 +91,8 @@ func (instruction *ExecInstruction) String() string {
 }
 
 func (instruction *ExecInstruction) ValidateAndUpdateEnvironment(environment *startosis_validator.ValidatorEnvironment) error {
-	// this doesn't do anything but can't return an error as the validator runs this regardless
-	// this is a no-op
 	if !environment.DoesServiceIdExist(instruction.serviceId) {
-		return stacktrace.NewError("There was an error validating '%v': service ID '%v' does not exists", instruction.String(), instruction.serviceId)
+		return stacktrace.NewError("There was an error validating '%v': service ID '%v' does not exist", instruction.String(), instruction.serviceId)
 	}
 	return nil
 }
