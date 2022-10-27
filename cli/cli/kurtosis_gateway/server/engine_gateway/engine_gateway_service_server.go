f39ef2235b8da6c6a0137a9243949253d0f3c102
Leandro Poroli <leandro.poroli@kurtosistech.com>
2022-10-27 16:25:41 -0300

    Implemented StreamUserServiceLogs in the gateway

--- cli/cli/kurtosis_gateway/server/engine_gateway/engine_gateway_service_server.go
+++ cli/cli/kurtosis_gateway/server/engine_gateway/engine_gateway_service_server.go
@@ -219,6 +219,21 @@ func (service *EngineGatewayServiceServer) GetUserServiceLogs(
 	return remoteEngineResponse, nil
 }
 
+func (service *EngineGatewayServiceServer) StreamUserServiceLogs(
+	args *kurtosis_engine_rpc_api_bindings.GetUserServiceLogsArgs,
+	stream kurtosis_engine_rpc_api_bindings.EngineService_StreamUserServiceLogsServer,
+) error {
+	remoteEngineClient, err := service.engineClientSupplier.GetEngineClient()
+	if err != nil {
+		return stacktrace.Propagate(err, "Expected to be able to get a client for a live Kurtosis engine, instead a non nil error was returned")
+	}
+
+	if _, err := remoteEngineClient.StreamUserServiceLogs(stream.Context(), args); err != nil {
+		return stacktrace.Propagate(err, "An error occurred streaming user service logs through the remote engine, using these arguments '%+v'", args)
+	}
+	return nil
+}
+
 // Private functions for managing our running enclave api container gateways
 func (service *EngineGatewayServiceServer) startRunningGatewayForEnclave(enclaveInfo *kurtosis_engine_rpc_api_bindings.EnclaveInfo) (*runningApiContainerGateway, error) {
 	service.mutex.Lock()
