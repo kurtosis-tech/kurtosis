822675a6d76de7525de09a9bbaf23ef889247a7b
mieubrisse <mieubrisse@gmail.com>
2022-10-27 13:59:32 -0300

    Simplify regenerate-protobuf-bindings script

--- api/scripts/regenerate-protobuf-bindings.sh
+++ api/scripts/regenerate-protobuf-bindings.sh
@@ -13,45 +13,29 @@ PROTOBUF_DIRNAME="protobuf"
 GOLANG_DIRNAME="golang"
 TYPESCRIPT_DIRNAME="typescript"
 
-CORE_RPC_BINDINGS_DIRNAME="kurtosis_core_rpc_api_bindings"
-CORE_DIR_NAME="core"
-
-ENGINE_RPC_BINDINGS_DIRNAME="kurtosis_engine_rpc_api_bindings"
-ENGINE_DIR_NAME="engine"
+OUTPUT_DIRNAMES=(
+    "engine"
+    "core"
+)
 
 # =============================== MAIN LOGIC =======================================================
-core_input_dirpath="${api_dirpath}/${PROTOBUF_DIRNAME}/${CORE_DIR_NAME}"
-engine_input_dirpath="${api_dirpath}/${PROTOBUF_DIRNAME}/${ENGINE_DIR_NAME}"
+for output_dirname in "${OUTPUT_DIRNAMES[@]}"; do
+    input_dirpath="${api_dirpath}/${PROTOBUF_DIRNAME}/${output_dirname}"
+    rpc_bindings_dirname="kurtosis_${output_dirname}_rpc_api_bindings"
 
-# Core Golang
-core_go_output_dirpath="${api_dirpath}/${GOLANG_DIRNAME}/${CORE_DIR_NAME}/${CORE_RPC_BINDINGS_DIRNAME}"
-if ! GO_MOD_FILEPATH="${api_dirpath}/${GOLANG_DIRNAME}/go.mod" "${GENERATOR_SCRIPT_FILENAME}" "${core_input_dirpath}" "${core_go_output_dirpath}" golang; then
-    echo "Error: An error occurred generating Go bindings in directory '${core_go_output_dirpath}'" >&2
-    exit 1
-fi
-echo "Successfully generated Go bindings in directory '${core_go_output_dirpath}'"
+    # Golang
+    go_output_dirpath="${api_dirpath}/${GOLANG_DIRNAME}/${output_dirname}/${rpc_bindings_dirname}"
+    if ! GO_MOD_FILEPATH="${api_dirpath}/${GOLANG_DIRNAME}/go.mod" "${GENERATOR_SCRIPT_FILENAME}" "${input_dirpath}" "${go_output_dirpath}" golang; then
+        echo "Error: An error occurred generating ${output_dirname} Go bindings in directory '${go_output_dirpath}'" >&2
+        exit 1
+    fi
+    echo "Successfully generated ${output_dirname} Go bindings in directory '${go_output_dirpath}'"
 
-# Core TypeScript
-core_typescript_output_dirpath="${api_dirpath}/${TYPESCRIPT_DIRNAME}/src/${CORE_DIR_NAME}/${CORE_RPC_BINDINGS_DIRNAME}"
-if ! "${GENERATOR_SCRIPT_FILENAME}" "${core_input_dirpath}" "${core_typescript_output_dirpath}" typescript; then
-    echo "Error: An error occurred generating TypeScript bindings in directory '${core_typescript_output_dirpath}'" >&2
-    exit 1
-fi
-echo "Successfully generated TypeScript bindings in directory '${core_typescript_output_dirpath}'"
-
-
-# Engine Golang
-engine_go_output_dirpath="${api_dirpath}/${GOLANG_DIRNAME}/${ENGINE_DIR_NAME}/${ENGINE_RPC_BINDINGS_DIRNAME}"
-if ! GO_MOD_FILEPATH="${api_dirpath}/${GOLANG_DIRNAME}/go.mod" "${GENERATOR_SCRIPT_FILENAME}" "${engine_input_dirpath}" "${engine_go_output_dirpath}" golang; then
-    echo "Error: An error occurred generating Go bindings in directory '${engine_go_output_dirpath}'" >&2
-    exit 1
-fi
-echo "Successfully generated Go bindings in directory '${engine_go_output_dirpath}'"
-
-# Engine TypeScript
-engine_typescript_output_dirpath="${api_dirpath}/${TYPESCRIPT_DIRNAME}/src/${ENGINE_DIR_NAME}/${ENGINE_RPC_BINDINGS_DIRNAME}"
-if ! "${GENERATOR_SCRIPT_FILENAME}" "${engine_input_dirpath}" "${engine_typescript_output_dirpath}" typescript; then
-    echo "Error: An error occurred generating TypeScript bindings in directory '${engine_typescript_output_dirpath}'" >&2
-    exit 1
-fi
-echo "Successfully generated TypeScript bindings in directory '${engine_typescript_output_dirpath}'"
+    # TypeScript
+    typescript_output_dirpath="${api_dirpath}/${TYPESCRIPT_DIRNAME}/src/${output_dirname}/${rpc_bindings_dirname}"
+    if ! "${GENERATOR_SCRIPT_FILENAME}" "${input_dirpath}" "${typescript_output_dirpath}" typescript; then
+        echo "Error: An error occurred generating ${output_dirname} TypeScript bindings in directory '${typescript_output_dirpath}'" >&2
+        exit 1
+    fi
+    echo "Successfully generated ${output_dirname} TypeScript bindings in directory '${typescript_output_dirpath}'"
+done
