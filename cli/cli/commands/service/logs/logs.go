65a164449649de1ab846e02031e6eef235fc41df
Leandro Poroli <leandro.poroli@kurtosistech.com>
2022-11-01 09:25:56 -0300

    Added bypass for Kubernetes cluster in service logs command

--- cli/cli/commands/service/logs/logs.go
+++ cli/cli/commands/service/logs/logs.go
@@ -17,9 +17,14 @@ import (
 	"github.com/kurtosis-tech/kurtosis/cli/cli/command_framework/lowlevel/args"
 	"github.com/kurtosis-tech/kurtosis/cli/cli/command_framework/lowlevel/flags"
 	"github.com/kurtosis-tech/kurtosis/cli/cli/command_str_consts"
+	"github.com/kurtosis-tech/kurtosis/cli/cli/helpers/kurtosis_config_getter"
+	"github.com/kurtosis-tech/kurtosis/cli/cli/kurtosis_config/resolved_config"
 	"github.com/kurtosis-tech/kurtosis/container-engine-lib/lib/backend_interface"
+	"github.com/kurtosis-tech/kurtosis/container-engine-lib/lib/backend_interface/objects/enclave"
+	"github.com/kurtosis-tech/kurtosis/container-engine-lib/lib/backend_interface/objects/service"
 	"github.com/kurtosis-tech/stacktrace"
 	"github.com/sirupsen/logrus"
+	"io"
 	"strconv"
 )
 
@@ -96,6 +101,61 @@ func run(
 		serviceGuid: true,
 	}
 
+	clusterConfig, err := kurtosis_config_getter.GetKurtosisClusterConfig()
+	if err != nil {
+		return stacktrace.Propagate(err, "And error occurred getting the Kurtosis cluster config")
+	}
+
+	clusterType := clusterConfig.GetClusterType()
+
+	//Print logs for Kubernetes cluster
+	//TODO HUGE HACK!
+	//TODO this is momentarily used here until we implement centralized logs for Kubernetes
+	if clusterType == resolved_config.KurtosisClusterType_Kubernetes {
+		//These Kurtosis primitives came from the backend (container-engine-lib) and this is the reason
+		//why are different from the same defined earlier (which came from the Kurtosis SDK)
+		kurtosisBackendEnclaveId := enclave.EnclaveID(enclaveIdStr)
+		kurtosisBackendServiceGUID := service.ServiceGUID(serviceGuidStr)
+
+		userServiceFilters := &service.ServiceFilters{
+			GUIDs: map[service.ServiceGUID]bool{
+				kurtosisBackendServiceGUID: true,
+			},
+		}
+
+		successfulUserServiceLogs, erroredUserServiceGuids, err := kurtosisBackend.GetUserServiceLogs(ctx, kurtosisBackendEnclaveId, userServiceFilters, shouldFollowLogs)
+		if err != nil {
+			return stacktrace.Propagate(err, "An error occurred getting user service logs using filters '%+v'", userServiceFilters)
+		}
+		defer func() {
+			for _, userServiceLogsReadCloser := range successfulUserServiceLogs {
+				if err := userServiceLogsReadCloser.Close(); err != nil{
+					logrus.Warnf("We tried to close the user service logs read-closer-objects after we're done using it, but doing so threw an error:\n%v", err)
+				}
+			}
+		}()
+
+		if len(erroredUserServiceGuids) > 0 {
+			err, found := erroredUserServiceGuids[kurtosisBackendServiceGUID]
+			if !found {
+				return stacktrace.NewError("Expected to find an error for user service with GUID '%v' on user service error map '%+v' but was not found; this should never happen, and is a bug in Kurtosis", kurtosisBackendServiceGUID, erroredUserServiceGuids)
+			}
+			return stacktrace.Propagate(err, "An error occurred getting user service logs for user service with GUID '%v'", kurtosisBackendServiceGUID)
+		}
+
+		userServiceReadCloserLog, found := successfulUserServiceLogs[kurtosisBackendServiceGUID]
+		if !found {
+			return stacktrace.NewError("Expected to find logs for user service with GUID '%v' on user service logs map '%+v' but was not found; this should never happen, and is a bug in Kurtosis", kurtosisBackendServiceGUID, userServiceReadCloserLog)
+		}
+
+		if _, err := io.Copy(logrus.StandardLogger().Out, userServiceReadCloserLog); err != nil {
+			return stacktrace.Propagate(err, "An error occurred copying the service logs to STDOUT")
+		}
+
+		return nil
+	}
+
+	//Print logs for Docker cluster which uses the Kurtosis centralized logs feature
 	kurtosisCtx, err := kurtosis_context.NewKurtosisContextFromLocalEngine()
 	if err != nil {
 		return stacktrace.Propagate(err, "An error occurred connecting to the local Kurtosis engine")
