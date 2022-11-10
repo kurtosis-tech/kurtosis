231779044fd9284be1fa507d6dc0d71cac9f4f2d
Victor Colombo <victor.colombo@kurtosistech.com>
2022-11-10 16:32:06 +0000

    Fix test

--- internal_testsuites/golang/testsuite/facts_engine_test/facts_engine_test.go
+++ internal_testsuites/golang/testsuite/facts_engine_test/facts_engine_test.go
@@ -83,7 +83,7 @@ func TestFactsEngine(t *testing.T) {
 	require.Equal(t, expectedOutputForExecFactOutput, getFactValuesResponse.GetFactValues()[0].GetStringValue())
 
 	logrus.Infof("Defining HTTP request fact...")
-	_, err = enclaveCtx.DefineFact(binding_constructors.NewGetHttpRequestFactRecipeWithDefaultRefresh(testServiceId, httpRequestFactName, containerPortId, "/"))
+	_, err = enclaveCtx.DefineFact(&kurtosis_core_rpc_api_bindings.FactRecipe{FactRecipeDefinition: binding_constructors.NewGetHttpRequestFactRecipeDefinition(testServiceId, httpRequestFactName, containerPortId, "/")})
 	require.Nil(t, err)
 	time.Sleep(5 * time.Second)
 
