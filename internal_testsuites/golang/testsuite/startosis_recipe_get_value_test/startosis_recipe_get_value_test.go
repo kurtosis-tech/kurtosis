0579ff1c2f3205b5479e40510a5a1a62707bcea2
Victor Colombo <victor.colombo@kurtosistech.com>
2022-11-22 20:06:24 -0300

    E2E test

--- internal_testsuites/golang/testsuite/startosis_recipe_get_value_test/startosis_recipe_get_value_test.go
+++ internal_testsuites/golang/testsuite/startosis_recipe_get_value_test/startosis_recipe_get_value_test.go
@@ -8,18 +8,8 @@ import (
 	"testing"
 )
 
-package startosis_remove_service_test
-
-import (
-"context"
-"github.com/kurtosis-tech/kurtosis-cli/golang_internal_testsuite/test_helpers"
-"github.com/sirupsen/logrus"
-"github.com/stretchr/testify/require"
-"testing"
-)
-
 const (
-	testName              = "startosis_remove_service_test"
+	testName              = "startosis_recipe_get_value_test"
 	isPartitioningEnabled = false
 	defaultDryRun         = false
 
@@ -27,9 +17,25 @@ const (
 	portId    = "grpc"
 
 	startosisScript = `
-value = get_value()
-print(value)
-add_service()
+service_config = struct(
+    image = "httpd:latest",
+    ports = {
+        "http_port": struct(number = 80, protocol = "TCP")
+    }
+)
+add_service(service_id = "web-server", config = service_config)
+recipe = struct(
+    service_id = "web-server",
+    port_id = "http_port",
+    endpoint = "/",
+    method = "GET",
+)
+response = get_value(recipe)
+print(response["body"])
+print(response["code"])
+assert(response["code"], "==", "200")
+assert(response["body"], "==", "<html><body><h1>It works!</h1></body></html>
+")
 `
 )
 
@@ -48,8 +54,8 @@ func TestStartosis(t *testing.T) {
 	executionResult, err := enclaveCtx.ExecuteStartosisScript(startosisScript, defaultDryRun)
 	require.NoError(t, err, "Unexpected error executing startosis script")
 
-	expectedScriptOutput := `Adding service example-datastore-server-1.
-Service example-datastore-server-1 deployed successfully.
+	expectedScriptOutput := `<html><body><h1>It works!</h1></body></html>
+200
 `
 	require.Empty(t, executionResult.InterpretationError, "Unexpected interpretation error. This test requires you to be online for the read_file command to run")
 	require.Lenf(t, executionResult.ValidationErrors, 0, "Unexpected validation error")
@@ -57,33 +63,4 @@ Service example-datastore-server-1 deployed successfully.
 	require.Equal(t, expectedScriptOutput, executionResult.SerializedScriptOutput)
 	logrus.Infof("Successfully ran Startosis script")
 
-	// Check that the service added by the script is functional
-	logrus.Infof("Checking that services are all healthy")
-	require.NoError(
-		t,
-		test_helpers.ValidateDatastoreServiceHealthy(context.Background(), enclaveCtx, serviceId, portId),
-		"Error validating datastore server '%s' is healthy",
-		serviceId,
-	)
-
-	logrus.Infof("All services added via the module work as expected")
-
-	// we run the remove script and see if things still work
-	executionResult, err = enclaveCtx.ExecuteStartosisScript(removeScript, defaultDryRun)
-	require.NoError(t, err, "Unexpected error executing remove script")
-	require.Empty(t, executionResult.InterpretationError, "Unexpected interpretation error")
-	require.Lenf(t, executionResult.ValidationErrors, 0, "Unexpected validation error")
-	require.Empty(t, executionResult.ExecutionError, "Unexpected execution error")
-
-	require.Error(
-		t,
-		test_helpers.ValidateDatastoreServiceHealthy(context.Background(), enclaveCtx, serviceId, portId),
-		"Error validating datastore server '%s' is not healthy",
-		serviceId,
-	)
-
-	// Ensure that service listing is empty too
-	serviceIds, err := enclaveCtx.GetServices()
-	require.Nil(t, err)
-	require.Empty(t, serviceIds)
 }
