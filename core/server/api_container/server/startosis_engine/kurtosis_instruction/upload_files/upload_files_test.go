94b3bcc5600f9e3306dab1ce18a09698be3bf7ad
Gyanendra Mishra <anomaly.the@gmail.com>
2022-11-23 12:36:55 +0530

    some mroe changes

--- core/server/api_container/server/startosis_engine/kurtosis_instruction/upload_files/upload_files_test.go
+++ core/server/api_container/server/startosis_engine/kurtosis_instruction/upload_files/upload_files_test.go
@@ -9,18 +9,18 @@ import (
 
 func TestUploadFiles_StringRepresentation(t *testing.T) {
 	filePath := "github.com/kurtosis/module/lib/lib.star"
-	artifactUuid, err := enclave_data_directory.NewFilesArtifactUUID()
+	artifactId, err := enclave_data_directory.NewFilesArtifactUUID()
 	require.Nil(t, err)
 	uploadInstruction := NewUploadFilesInstruction(
 		kurtosis_instruction.NewInstructionPosition(1, 13, "dummyFile"),
-		nil, nil, filePath, "dummyPathOnDisk", artifactUuid,
+		nil, nil, filePath, "dummyPathOnDisk", artifactId,
 	)
 	expectedMultiLineStrRep := `# from: dummyFile[1:13]
 upload_files(
-	artifact_id="` + string(artifactUuid) + `",
+	artifact_id="` + string(artifactId) + `",
 	src="` + filePath + `"
 )`
 	require.Equal(t, expectedMultiLineStrRep, uploadInstruction.GetCanonicalInstruction())
-	expectedSingleLineStrRep := `upload_files(artifact_id="` + string(artifactUuid) + `", src="` + filePath + `")`
+	expectedSingleLineStrRep := `upload_files(artifact_id="` + string(artifactId) + `", src="` + filePath + `")`
 	require.Equal(t, expectedSingleLineStrRep, uploadInstruction.String())
 }
