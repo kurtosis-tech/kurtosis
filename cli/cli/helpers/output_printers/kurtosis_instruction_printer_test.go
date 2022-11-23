bed3168c0ca609ab6f3d3733f99e575d35783f5f
guillaume.bouvignies <guillaume.bouvignies@kurtosistech.com>
2022-11-23 09:49:23 +0100

    Address PR comment

--- cli/cli/helpers/output_printers/kurtosis_instruction_printer_test.go
+++ cli/cli/helpers/output_printers/kurtosis_instruction_printer_test.go
@@ -10,7 +10,7 @@ func TestFormatInstruction(t *testing.T) {
 	instruction := binding_constructors.NewKurtosisInstruction(
 		binding_constructors.NewKurtosisInstructionPosition("dummyFile", 12, 4),
 		`my_instruction("foo", ["bar", "doo"], kwarg1="serviceA", kwarg2=struct(bonjour=42, hello="world"))`,
-		// for now result is appended manually in the exec command code. This is change once we start doing streaming where the result is displayed right after the instruction code
+		// TODO(gb): for now result is appended manually in the exec command code. This is change once we start doing streaming where the result is displayed right after the instruction code
 		nil)
 	formattedInstruction := FormatInstruction(instruction)
 	expectedResult := `# from dummyFile[12:4]
