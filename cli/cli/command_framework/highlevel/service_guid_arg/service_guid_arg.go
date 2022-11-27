5e3d094ee87687ffe0c2fd3475758e99b2e0e060
Anders Schwartz <anders.schwartz@kurtosistech.com>
2022-11-27 16:23:35 -0500

    service-guid: add validation

--- cli/cli/command_framework/highlevel/service_guid_arg/service_guid_arg.go
+++ cli/cli/command_framework/highlevel/service_guid_arg/service_guid_arg.go
@@ -3,11 +3,13 @@ package service_guid_arg
 import (
 	"context"
 	"github.com/kurtosis-tech/kurtosis/api/golang/core/lib/enclaves"
+	"github.com/kurtosis-tech/kurtosis/api/golang/core/lib/services"
 	"github.com/kurtosis-tech/kurtosis/api/golang/engine/lib/kurtosis_context"
 	"github.com/kurtosis-tech/kurtosis/cli/cli/command_framework/lowlevel/args"
 	"github.com/kurtosis-tech/kurtosis/cli/cli/command_framework/lowlevel/flags"
 	"github.com/kurtosis-tech/stacktrace"
 	"sort"
+	"strings"
 )
 
 const (
@@ -32,10 +34,7 @@ func NewServiceGUIDArg(
 	}
 }
 
-func getCompletions(ctx context.Context, flags *flags.ParsedFlags, previousArgs *args.ParsedArgs) ([]string, error) {
-	enclaveIdStr, err := previousArgs.GetNonGreedyArg(enclaveIdArgKey)
-	enclaveId := enclaves.EnclaveID(enclaveIdStr)
-
+func getServiceGUIDsForEnclave(ctx context.Context, enclaveID enclaves.EnclaveID) (map[services.ServiceGUID]bool, error) {
 	kurtosisCtx, err := kurtosis_context.NewKurtosisContextFromLocalEngine()
 	if err != nil {
 		return nil, stacktrace.Propagate(
@@ -43,12 +42,26 @@ func getCompletions(ctx context.Context, flags *flags.ParsedFlags, previousArgs 
 			"An error occurred connecting to the Kurtosis engine for retrieving the enclave IDs for tab completion",
 		)
 	}
-	_, serviceGUIDs, err := kurtosisCtx.GetServices(ctx, enclaveId)
+	_, serviceGUIDs, err := kurtosisCtx.GetServices(ctx, enclaveID)
 	if err != nil {
 		return nil, stacktrace.Propagate(
 			err,
 			"An error occurred getting the services retrieving for enclave ID tab completion",
 		)
+	}
+	return serviceGUIDs, nil
+}
+
+func getCompletions(ctx context.Context, flags *flags.ParsedFlags, previousArgs *args.ParsedArgs) ([]string, error) {
+	enclaveIdStr, err := previousArgs.GetNonGreedyArg(enclaveIdArgKey)
+	if err != nil {
+		return nil, stacktrace.Propagate(err, "An error occurred getting the enclave ID using key '%v'", enclaveIdArgKey)
+	}
+
+	enclaveID := enclaves.EnclaveID(enclaveIdStr)
+	serviceGUIDs, err := getServiceGUIDsForEnclave(ctx, enclaveID)
+	if err != nil {
+		return nil, stacktrace.Propagate(err, "An error occurred getting the services retrieving for enclave ID tab completion")
 	}
 
 	result := []string{}
@@ -61,42 +74,49 @@ func getCompletions(ctx context.Context, flags *flags.ParsedFlags, previousArgs 
 }
 
 // Create a validation function using the previously-created
-func getValidationFunc(argKey string, engineClientCtxKey string, isGreedy bool) func(context.Context, *flags.ParsedFlags, *args.ParsedArgs) error {
+func getValidationFunc(
+	argKey string,
+	engineClientCtxKey string,
+	isGreedy bool,
+) func(context.Context, *flags.ParsedFlags, *args.ParsedArgs) error {
 	return func(ctx context.Context, flags *flags.ParsedFlags, args *args.ParsedArgs) error {
-		//uncastedEngineClient := ctx.Value(engineClientCtxKey)
-		//if uncastedEngineClient == nil {
-		//	return stacktrace.NewError("Expected an engine client to have been stored in the context under key '%v', but none was found; this is a bug in Kurtosis!", engineClientCtxKey)
-		//}
-		//engineClient, ok := uncastedEngineClient.(kurtosis_engine_rpc_api_bindings.EngineServiceClient)
-		//if !ok {
-		//	return stacktrace.NewError("Found an object that should be the engine client stored in the context under key '%v', but this object wasn't of the correct engine client type", engineClientCtxKey)
-		//}
-		//
-		//var enclaveIdsToValidate []string
-		//if isGreedy {
-		//	enclaveIds, err := args.GetGreedyArg(argKey)
-		//	if err != nil {
-		//		return stacktrace.Propagate(err, "Expected a value for greedy arg '%v' but didn't find one", argKey)
-		//	}
-		//	enclaveIdsToValidate = enclaveIds
-		//} else {
-		//	enclaveId, err := args.GetNonGreedyArg(argKey)
-		//	if err != nil {
-		//		return stacktrace.Propagate(err, "Expected a value for non-greedy arg '%v' but didn't find one", argKey)
-		//	}
-		//	enclaveIdsToValidate = []string{enclaveId}
-		//}
-		//
-		//getEnclavesResp, err := engineClient.GetEnclaves(ctx, &emptypb.Empty{})
-		//if err != nil {
-		//	return stacktrace.Propagate(err, "An error occurred getting enclaves, which is necessary to check if the enclaves exist")
-		//}
-		//
-		//for _, enclaveId := range enclaveIdsToValidate {
-		//	if _, found := getEnclavesResp.EnclaveInfo[enclaveId]; !found {
-		//		return stacktrace.NewError("No enclave found with ID '%v'", enclaveId)
-		//	}
-		//}
+		var serviceGUIDsToValidate []string
+		if isGreedy {
+			serviceGUID, err := args.GetGreedyArg(argKey)
+			if err != nil {
+				return stacktrace.Propagate(err, "Expected a value for greedy arg '%v' but didn't find one", argKey)
+			}
+			serviceGUIDsToValidate = serviceGUID
+		} else {
+			serviceGUID, err := args.GetNonGreedyArg(argKey)
+			if err != nil {
+				return stacktrace.Propagate(err, "Expected a value for non-greedy arg '%v' but didn't find one", argKey)
+			}
+			serviceGUIDsToValidate = []string{serviceGUID}
+		}
+
+		serviceGUIDs, err := getCompletions(ctx, flags, args)
+		if err != nil {
+			return stacktrace.Propagate(err, "An error occurred while getting the services for the enclave")
+		}
+
+		var errorServiceGUIDs []string
+		for _, serviceGUIDtoValidate := range serviceGUIDsToValidate {
+			var contains = false
+			for _, serviceGUID := range serviceGUIDs {
+				if found := serviceGUIDtoValidate == serviceGUID; found {
+					contains = true
+					break
+				}
+			}
+			if !contains {
+				errorServiceGUIDs = append(errorServiceGUIDs, serviceGUIDtoValidate)
+			}
+		}
+
+		if len(errorServiceGUIDs) > 0 {
+			return stacktrace.NewError("One or more service GUIDs do not exist in the enclave: '%s'", strings.Join(errorServiceGUIDs, ", "))
+		}
 		return nil
 	}
 }
