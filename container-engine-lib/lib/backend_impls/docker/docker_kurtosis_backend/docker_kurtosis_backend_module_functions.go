9def1a23b8906f403c0aae4575c7abf32424b3fa
Leandro Poroli <leandro.poroli@kurtosistech.com>
2022-12-01 11:46:42 -0300

    Filtering modules by enclave id

--- container-engine-lib/lib/backend_impls/docker/docker_kurtosis_backend/docker_kurtosis_backend_module_functions.go
+++ container-engine-lib/lib/backend_impls/docker/docker_kurtosis_backend/docker_kurtosis_backend_module_functions.go
@@ -254,7 +254,7 @@ func (backend *DockerKurtosisBackend) GetModules(
 	map[module.ModuleGUID]*module.Module,
 	error,
 ) {
-	matchingModuleContainers, err := backend.getMatchingModules(ctx, filters)
+	matchingModuleContainers, err := backend.getMatchingModules(ctx, enclaveId, filters)
 	if err != nil {
 		return nil, stacktrace.Propagate(err, "An error occurred getting module containers matching the following filters: %+v", filters)
 	}
@@ -277,7 +277,7 @@ func (backend *DockerKurtosisBackend) GetModuleLogs(
 	map[module.ModuleGUID]error,
 	error,
 ) {
-	matchingModulesByContainerId, err := backend.getMatchingModules(ctx, filters)
+	matchingModulesByContainerId, err := backend.getMatchingModules(ctx, enclaveId, filters)
 	if err != nil {
 		return nil, nil, stacktrace.Propagate(err, "An error occurred getting modules matching filters '%+v'", filters)
 	}
@@ -323,7 +323,7 @@ func (backend *DockerKurtosisBackend) StopModules(
 	resultErroredModuleGuids map[module.ModuleGUID]error,
 	resultErr error,
 ) {
-	matchingModulesByContainerId, err := backend.getMatchingModules(ctx, filters)
+	matchingModulesByContainerId, err := backend.getMatchingModules(ctx, enclaveId, filters)
 	if err != nil {
 		return nil, nil, stacktrace.Propagate(err, "An error occurred getting modules matching filters '%+v'", filters)
 	}
@@ -377,7 +377,7 @@ func (backend *DockerKurtosisBackend) DestroyModules(
 	erroredModuleIds map[module.ModuleGUID]error,
 	resultErr error,
 ) {
-	matchingModulesByContainerId, err := backend.getMatchingModules(ctx, filters)
+	matchingModulesByContainerId, err := backend.getMatchingModules(ctx, enclaveId, filters)
 	if err != nil {
 		return nil, nil, stacktrace.Propagate(err, "An error occurred getting module containers matching the following filters: %+v", filters)
 	}
@@ -426,11 +426,12 @@ func (backend *DockerKurtosisBackend) DestroyModules(
 //                                     Private Helper Methods
 // ====================================================================================================
 // Gets modules matching the search filters, indexed by their container ID
-func (backend *DockerKurtosisBackend) getMatchingModules(ctx context.Context, filters *module.ModuleFilters) (map[string]*module.Module, error) {
+func (backend *DockerKurtosisBackend) getMatchingModules(ctx context.Context, enclaveId enclave.EnclaveID, filters *module.ModuleFilters) (map[string]*module.Module, error) {
 
 	moduleContainerSearchLabels := map[string]string{
 		label_key_consts.AppIDDockerLabelKey.GetString():         label_value_consts.AppIDDockerLabelValue.GetString(),
 		label_key_consts.ContainerTypeDockerLabelKey.GetString(): label_value_consts.ModuleContainerTypeDockerLabelValue.GetString(),
+		label_key_consts.EnclaveIDDockerLabelKey.GetString(): string(enclaveId),
 	}
 	matchingModuleContainers, err := backend.dockerManager.GetContainersByLabels(ctx, moduleContainerSearchLabels, consts.ShouldFetchAllContainersWhenRetrievingContainers)
 	if err != nil {
