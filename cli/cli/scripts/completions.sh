dcdc7f92f093a9073f93b72c834e96e6246a9acb
Anders Schwartz <anders.schwartz@kurtosistech.com>
2022-12-07 09:36:42 -0500

    remove-artifact: use the completions folder

--- cli/cli/scripts/completions.sh
+++ cli/cli/scripts/completions.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 
-set -e
+set -eu pipefail
 if [ -z "${CLI_BINARY_FILENAME}" ]; then
     echo "Environment variable CLI_BINARY_FILENAME must not be empty" >&2
     exit 1
 fi
+COMPLETIONS_BUILD_DIR="completions/"
+COMPLETIONS_BINARY_DIR="${COMPLETIONS_BUILD_DIR}/cli/"
+COMPLETIONS_SCRIPT_DIR="${COMPLETIONS_BUILD_DIR}/scripts/"
+CLI_BINARY_PATH="${COMPLETIONS_BINARY_DIR}/${CLI_BINARY_FILENAME}"
 
-rm -rf completions
-mkdir completions
+rm -rf "${COMPLETIONS_BUILD_DIR}"
+mkdir -p "${COMPLETIONS_BINARY_DIR}"
+mkdir -p "${COMPLETIONS_SCRIPT_DIR}"
 
-go build -o "${CLI_BINARY_FILENAME}" main.go
+go build -o "${CLI_BINARY_PATH}" main.go
 
 for sh in bash zsh fish; do
-	"./${CLI_BINARY_FILENAME}" completion "$sh" > "completions/kurtosis.$sh"
+	"${CLI_BINARY_PATH}" completion "$sh" > "${COMPLETIONS_SCRIPT_DIR}/kurtosis.$sh"
 done
 
-rm "${CLI_BINARY_FILENAME}"
+#trap 'rm -rf "${COMPLETIONS_BINARY_DIR}"' exit
