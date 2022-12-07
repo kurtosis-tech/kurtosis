57759648c7f9c261cbf3f7e6bf18ada903960c0a
Gyanendra Mishra <anomaly.the@gmail.com>
2022-12-07 11:02:52 +0530

    added typescript tests

---
+++ internal_testsuites/typescript/src/testsuite/starlark_exec_test/starlark_exec.test.ts
@@ -0,0 +1,56 @@
+import {createEnclave} from "../../test_helpers/enclave_setup";
+import log from "loglevel";
+
+const STARLARK_EXEC_TEST = "starlark_exec_test"
+const IS_PARTITIONING_ENABLED = false
+const DEFAULT_DRY_RUN = false
+const EMPTY_ARGS = "{}"
+
+const STARLARK_SCRIPT = `
+def run(args):
+	service_config = struct(
+		image = "mendhak/http-https-echo:26",
+		ports = {
+			"http-port": struct(number = 8080, protocol = "TCP")
+		}
+	)
+
+	add_service(service_id = "web-server", config = service_config)
+	response = exec("web-server", ["echo", "hello", "world"])
+	assert(response.code, "==", 0)
+	assert(response.output, "==", "hello world\n")
+`
+
+jest.setTimeout(180000)
+
+test("Test Starlark Exec", TestAddServiceWithEmptyAndWithoutPorts)
+
+async function TestAddServiceWithEmptyAndWithoutPorts() {
+
+    // ------------------------------------- ENGINE SETUP ----------------------------------------------
+    const createEnclaveResult = await createEnclave(STARLARK_EXEC_TEST, IS_PARTITIONING_ENABLED)
+
+    if(createEnclaveResult.isErr()) { throw createEnclaveResult.error }
+
+    const { enclaveContext, stopEnclaveFunction } = createEnclaveResult.value
+
+    try {
+        // ------------------------------------- TEST SETUP ----------------------------------------------
+        log.info("Executing Starlark script...")
+        const runResult = await enclaveContext.runStarlarkScriptBlocking(STARLARK_SCRIPT, EMPTY_ARGS, DEFAULT_DRY_RUN)
+        if (runResult.isErr()) {
+            log.error("Unexpected error executing Starlark script");
+            throw runResult.error;
+        }
+
+        expect(runResult.value.interpretationError).toBeUndefined();
+        expect(runResult.value.validationErrors).toEqual([]);
+        expect(runResult.value.executionError).toBeUndefined();
+        log.info("Successfully ran Starlark script");
+    }
+    finally {
+        stopEnclaveFunction()
+    }
+
+    jest.clearAllTimers()
+}
