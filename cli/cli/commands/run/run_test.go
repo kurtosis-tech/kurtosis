718c161af99f739f6ed4944ebe2b08d012ecf066
Gyanendra Mishra <anomaly.the@gmail.com>
2022-12-08 13:36:45 +0530

    this looks good

--- cli/cli/commands/run/run_test.go
+++ cli/cli/commands/run/run_test.go
@@ -1,18 +1,24 @@
 package run
 
 import (
+	"github.com/kurtosis-tech/kurtosis/cli/cli/command_framework/lowlevel/args"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
 
+const testScriptArg = "."
+
 func TestValidateArgs_valid(t *testing.T) {
-	input := `{"hello": "world!"}`
-	err := validatePackageArgs(input)
+	inputArgs := `{"hello": "world!"}`
+	args, err := args.ParseArgsForValidation(StarlarkRunCmd.Args, []string{testScriptArg, inputArgs})
 	require.Nil(t, err)
+	require.NotNil(t, args)
 }
 
 func TestValidateArgs_invalid(t *testing.T) {
-	input := `"hello": "world!"` // missing { }
-	err := validatePackageArgs(input)
-	require.NotNil(t, err)
+	inputArgs := `"hello": "world!"` // missing { }
+	args, err := args.ParseArgsForValidation(StarlarkRunCmd.Args, []string{testScriptArg, inputArgs})
+	require.Nil(t, err)
+	require.NotNil(t, args)
+
 }
