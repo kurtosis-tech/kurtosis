7d5d5a4512b9f0cfb5cb54d432e0e5f37cb891fb
Anders Schwartz <anders.schwartz@kurtosistech.com>
2022-12-09 13:33:42 -0500

    remove-artifact: address comments

--- cli/cli/scripts/completions.sh
+++ cli/cli/scripts/completions.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
-set -eu pipefail
+set -euo pipefail   # Bash "strict mode"
+script_dirpath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 if [ -z "${CLI_BINARY_FILENAME}" ]; then
     echo "Environment variable CLI_BINARY_FILENAME must not be empty" >&2
     exit 1
 fi
-COMPLETIONS_BUILD_DIR="completions/"
-COMPLETIONS_BINARY_DIR="${COMPLETIONS_BUILD_DIR}/cli/"
-COMPLETIONS_SCRIPT_DIR="${COMPLETIONS_BUILD_DIR}/scripts/"
+COMPLETIONS_BUILD_DIR="${script_dirpath}/completions"
+COMPLETIONS_BINARY_DIR="${COMPLETIONS_BUILD_DIR}/cli"
+COMPLETIONS_SCRIPT_DIR="${COMPLETIONS_BUILD_DIR}/scripts"
 CLI_BINARY_PATH="${COMPLETIONS_BINARY_DIR}/${CLI_BINARY_FILENAME}"
 
-rm -rf "${COMPLETIONS_BUILD_DIR}"
+if [ "${COMPLETIONS_BUILD_DIR}/" = "/" ]; then
+    echo "ERROR: variable COMPLETIONS_BUILD_DIR is empty. Halting. Would have ran 'rm -rf /'!!" >&2
+    exit 1
+fi
+rm -rf "${COMPLETIONS_BUILD_DIR}"q
 mkdir -p "${COMPLETIONS_BINARY_DIR}"
 mkdir -p "${COMPLETIONS_SCRIPT_DIR}"
 
