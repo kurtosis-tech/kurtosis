8a078520470a745ec5931f1033fd7e75020787e0
Victor Colombo <victor.colombo@kurtosistech.com>
2022-12-12 14:08:30 -0300

    Cleanup tests

--- internal_testsuites/golang/testsuite/startosis_request_wait_assert_test/startosis_assert_fail_test.go
+++ internal_testsuites/golang/testsuite/startosis_request_wait_assert_test/startosis_assert_fail_test.go
@@ -8,8 +8,8 @@ import (
 )
 
 const (
-	timeoutWaitTestName        = "startosis_timeout_wait_test"
-	timeoutWaitStartosisScript = `
+	assertFailTestName = "startosis_assert_fail_test"
+	assertFailScript   = `
 def run(args):
 	service_config = struct(
 		image = "mendhak/http-https-echo:26",
@@ -29,15 +29,16 @@ def run(args):
 		}
 	)
 	response = wait(get_recipe, "code", "==", 200, interval="100ms", timeout="30s")
+	assert(response["code"], "!=", 200)
 `
 )
 
-func TestStartosis_TimeoutWait(t *testing.T) {
+func TestStartosis_AssertFail(t *testing.T) {
 	ctx := context.Background()
-	runResult := test_helpers.SetupSimpleEnclaveAndRunScript(t, ctx, timeoutWaitTestName, timeoutWaitStartosisScript)
+	runResult := test_helpers.SetupSimpleEnclaveAndRunScript(t, ctx, assertFailTestName, assertFailScript)
 
 	require.Nil(t, runResult.InterpretationError, "Unexpected interpretation error. This test requires you to be online for the read_file command to run")
 	require.Empty(t, runResult.ValidationErrors, "Unexpected validation error")
-	require.NotEmpty(t, runResult.ExecutionError, "Expected execution error coming from wait timeout")
+	require.NotEmpty(t, runResult.ExecutionError, "Expected execution error coming from assert fail")
 
 }
