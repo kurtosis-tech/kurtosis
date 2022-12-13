fd3ede11e068fc83a4bc21feb151597d22bc2fec
Leandro Poroli <leandro.poroli@kurtosistech.com>
2022-12-13 15:19:57 -0300

    Renamed textPattern field in LokiLineFilter

--- engine/server/engine/centralized_logs/loki_line_filter.go
+++ engine/server/engine/centralized_logs/loki_line_filter.go
@@ -3,32 +3,32 @@ package centralized_logs
 import "fmt"
 
 type LokiLineFilter struct {
-	operator    lokiLineFilterOperator
-	textPattern string
+	operator     lokiLineFilterOperator
+	textOrRegexp string
 }
 
 func NewDoesContainTextLokiLineFilter(text string) *LokiLineFilter {
 	operator := lokiLineFilterOperatorDoesContainText
-	return &LokiLineFilter{operator: operator, textPattern: text}
+	return &LokiLineFilter{operator: operator, textOrRegexp: text}
 }
 
 func NewDoesNotContainTextLokiLineFilter(text string) *LokiLineFilter {
 	operator := lokiLineFilterOperatorDoesNotContainText
-	return &LokiLineFilter{operator: operator, textPattern: text}
+	return &LokiLineFilter{operator: operator, textOrRegexp: text}
 }
 
 //Loki accepts re2 regex syntax type, more here: https://github.com/google/re2/wiki/Syntax
 func NewDoesContainMatchRegexLokiLineFilter(regex string) *LokiLineFilter {
 	operator := lokiLineFilterOperatorDoesContainMatchRegex
-	return &LokiLineFilter{operator: operator, textPattern: regex}
+	return &LokiLineFilter{operator: operator, textOrRegexp: regex}
 }
 
 //Loki accepts re2 regex syntax type, more here: https://github.com/google/re2/wiki/Syntax
 func NewDoesNotContainMatchRegexLokiLineFilter(regex string) *LokiLineFilter {
 	operator := lokiLineFilterOperatorDoesNotContainMatchRegex
-	return &LokiLineFilter{operator: operator, textPattern: regex}
+	return &LokiLineFilter{operator: operator, textOrRegexp: regex}
 }
 
 func (lineFilter *LokiLineFilter) String() string {
-	return fmt.Sprintf(`%s "%s"`, lineFilter.operator, lineFilter.textPattern)
+	return fmt.Sprintf(`%s "%s"`, lineFilter.operator, lineFilter.textOrRegexp)
 }
