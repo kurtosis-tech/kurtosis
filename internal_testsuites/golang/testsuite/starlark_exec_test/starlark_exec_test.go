43aa4c97aa17a1594f25263d534cdd65bfed4155
Victor Colombo <victorcolombo@users.noreply.github.com>
2022-12-22 10:34:56 -0300

    refactor: Migrate E2E exec test completly to Starlark (#785)
    Changelog picked up from commits here:
    
    refactor: Migrate E2E exec test completly to Starlark
    
    Co-authored-by: Victor Colombo <victor.colombo@kurtosistech.com>

--- internal_testsuites/golang/testsuite/starlark_exec_test/starlark_exec_test.go
+++ internal_testsuites/golang/testsuite/starlark_exec_test/starlark_exec_test.go
@@ -2,9 +2,11 @@ package starlark_exec_test
 
 import (
 	"context"
+	"fmt"
 	"github.com/kurtosis-tech/kurtosis-cli/golang_internal_testsuite/test_helpers"
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/require"
+	"strings"
 	"testing"
 )
 
@@ -12,25 +14,53 @@ const (
 	testName              = "starlark_exec_test"
 	isPartitioningEnabled = false
 	defaultDryRun         = false
+	emptyParams           = "{}"
+)
 
-	emptyParams    = "{}"
-	starlarkScript = `
-def run(plan):
-	service_config = ServiceConfig(
-		image = "mendhak/http-https-echo:26",
-		ports = {
-			"http-port": PortSpec(number = 8080, transport_protocol = "TCP")
-		}
+const (
+	noExecOutput              = ""
+	expectedLogOutput         = "hello\\n"
+	expectedAdvancedLogOutput = "hello && hello\\n"
+
+	successExitCode int32 = 0
+	failureExitCode int32 = 1
+
+	setupStarlarkScript = `
+def run(plan, args):
+	service_config = struct(
+		image = "alpine:3.12.4",
+		entrypoint = ["sleep"],
+		cmd = ["30"]
 	)
-	plan.add_service(service_id = "web-server", config = service_config)
-	exec_recipe = struct(
-		service_id = "web-server",
-		command = ["echo", "hello", "world"]
-	)
-	response = plan.exec(exec_recipe)
-	plan.assert(response["code"], "==", 0)
-	plan.assert(response["output"], "==", "hello world\n")
+	plan.add_service(service_id = "test", config = service_config)
 `
+	testStarlarkScriptTemplate = `
+def run(plan, args):
+	exec_recipe = struct(
+		service_id = "test",
+		command = %v,
+	)
+	exec_result = plan.exec(exec_recipe)
+	plan.assert(exec_result["code"], "==", %d)
+	plan.assert(exec_result["output"], "==", "%s")
+`
+)
+
+var (
+	execCommandThatShouldWork          = []string{"true"}
+	execCommandThatShouldFail          = []string{"false"}
+	execCommandThatShouldHaveLogOutput = []string{"echo", "hello"}
+
+	// This command tests to ensure that the commands the user is running get passed exactly as-is to the Docker
+	// container. If Kurtosis code is magically wrapping the code with "sh -c", this will fail.
+	execCommandThatWillFailIfShWrapped = []string{"echo", "hello && hello"}
+
+	testStarlarkScripts = []string{
+		fmt.Sprintf(testStarlarkScriptTemplate, sliceToStarlarkString(execCommandThatShouldWork), successExitCode, noExecOutput),
+		fmt.Sprintf(testStarlarkScriptTemplate, sliceToStarlarkString(execCommandThatShouldFail), failureExitCode, noExecOutput),
+		fmt.Sprintf(testStarlarkScriptTemplate, sliceToStarlarkString(execCommandThatShouldHaveLogOutput), successExitCode, expectedLogOutput),
+		fmt.Sprintf(testStarlarkScriptTemplate, sliceToStarlarkString(execCommandThatWillFailIfShWrapped), successExitCode, expectedAdvancedLogOutput),
+	}
 )
 
 func TestStarlarkExec(t *testing.T) {
@@ -41,15 +71,33 @@ func TestStarlarkExec(t *testing.T) {
 	require.NoError(t, err, "An error occurred creating an enclave")
 	defer destroyEnclaveFunc()
 
-	// ------------------------------------- TEST RUN ----------------------------------------------
-	logrus.Infof("Executing Starlark script...")
+	// -------------------------------------- TEST SETUP -----------------------------------------------
+	logrus.Infof("Executing Starlark setup script...")
 
-	runResult, err := enclaveCtx.RunStarlarkScriptBlocking(ctx, starlarkScript, emptyParams, defaultDryRun)
+	setupRunResult, err := enclaveCtx.RunStarlarkScriptBlocking(ctx, setupStarlarkScript, emptyParams, defaultDryRun)
 	require.NoError(t, err, "Unexpected error executing Starlark script")
+	require.Nil(t, setupRunResult.InterpretationError, "Unexpected interpretation error")
+	require.Empty(t, setupRunResult.ValidationErrors, "Unexpected validation error")
+	require.Nil(t, setupRunResult.ExecutionError, "Unexpected execution error")
 
-	require.Nil(t, runResult.InterpretationError, "Unexpected interpretation error.")
-	require.Empty(t, runResult.ValidationErrors, "Unexpected validation error")
-	require.Nil(t, runResult.ExecutionError, "Unexpected execution error")
-	logrus.Infof("Successfully ran Starlark script")
+	// ------------------------------------- TEST RUN ----------------------------------------------
 
+	for testIndex, testStarlarkScript := range testStarlarkScripts {
+		logrus.Infof("Executing Starlark test script %d:\n%s", testIndex, testStarlarkScript)
+		runResult, err := enclaveCtx.RunStarlarkScriptBlocking(ctx, testStarlarkScript, emptyParams, defaultDryRun)
+		require.NoError(t, err, "Unexpected error executing Starlark script")
+		require.Nil(t, runResult.InterpretationError, "Unexpected interpretation error")
+		require.Empty(t, runResult.ValidationErrors, "Unexpected validation error")
+		require.Nil(t, runResult.ExecutionError, "Unexpected execution error")
+		logrus.Infof("Successfully ran Starlark test script")
+	}
+
+}
+
+func sliceToStarlarkString(slice []string) string {
+	quotedSlice := []string{}
+	for _, s := range slice {
+		quotedSlice = append(quotedSlice, fmt.Sprintf(`"%s"`, s))
+	}
+	return fmt.Sprintf("[%v]", strings.Join(quotedSlice, ","))
 }
