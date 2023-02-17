fcd3a4ba2c4b25556bcd49ab91c8aeb4dc333061
Peeeekay <15133250+Peeeekay@users.noreply.github.com>
2023-02-17 00:11:39 -0500

    feat! : this is a breaking change where we are deprecating PacketDelay to introduce latency in favour of PacketDelayDistribution. Instead of using packet delay, use UniformPacketDelayDistribution for constant delays or NormalPacketDelayDistribution for normally distributed latencies (#17)

--- core/server/api_container/server/startosis_engine/kurtosis_types/connection_config_test.go
+++ core/server/api_container/server/startosis_engine/kurtosis_types/connection_config_test.go
@@ -15,46 +15,39 @@ var (
 )
 
 func TestConnectionConfig_StringRepresentation(t *testing.T) {
-	connectionConfig := NewConnectionConfig(50.5, nil, NoPacketDelay)
-	expectedRepresentation := fmt.Sprintf("%s(%s=50.5, packet_delay=PacketDelay(delay_ms=0))", ConnectionConfigTypeName, packetLossPercentageAttr)
+	connectionConfig := NewConnectionConfig(50.5, nil)
+	expectedRepresentation := fmt.Sprintf("%s(%s=50.5)", ConnectionConfigTypeName, packetLossPercentageAttr)
 	require.Equal(t, expectedRepresentation, connectionConfig.String())
 
-	connectionConfig = NewConnectionConfig(50.5, noPacketDelayDistribution, NoPacketDelay)
+	connectionConfig = NewConnectionConfig(50.5, noPacketDelayDistribution)
 	expectedRepresentation = fmt.Sprintf("%s(%s=50.5, packet_delay_distribution=UniformPacketDelayDistribution(ms=0))", ConnectionConfigTypeName, packetLossPercentageAttr)
 	require.Equal(t, expectedRepresentation, connectionConfig.String())
 
-	connectionConfig = NewConnectionConfig(50.5, NewNormalPacketDelayDistribution(100, 10, 10), NoPacketDelay)
+	connectionConfig = NewConnectionConfig(50.5, NewNormalPacketDelayDistribution(100, 10, 10))
 	expectedRepresentation = fmt.Sprintf("%s(%s=50.5, packet_delay_distribution=NormalPacketDelayDistribution(mean_ms=100, std_dev_ms=10, correlation=10.0))", ConnectionConfigTypeName, packetLossPercentageAttr)
 	require.Equal(t, expectedRepresentation, connectionConfig.String())
 }
 
 func TestConnectionConfig_Type(t *testing.T) {
-	connectionConfig := NewConnectionConfig(50.5, nil, NoPacketDelay)
+	connectionConfig := NewConnectionConfig(50.5, nil)
 	require.Equal(t, ConnectionConfigTypeName, connectionConfig.Type())
 }
 
 func TestConnectionConfig_Truth_False(t *testing.T) {
-	connectionConfig := NewConnectionConfig(-1, nil, NoPacketDelay)
-	require.Equal(t, starlark.Bool(false), connectionConfig.Truth())
-
-	connectionConfig = NewConnectionConfig(50, nil, nil)
+	connectionConfig := NewConnectionConfig(-1, nil)
 	require.Equal(t, starlark.Bool(false), connectionConfig.Truth())
 }
 
 func TestConnectionConfig_Truth_True(t *testing.T) {
-	connectionConfig := NewConnectionConfig(50.5, nil, NoPacketDelay)
+	connectionConfig := NewConnectionConfig(50.5, nil)
 	require.Equal(t, starlark.Bool(true), connectionConfig.Truth())
 }
 
 func TestConnectionConfig_Attr_Exists(t *testing.T) {
-	connectionConfig := NewConnectionConfig(50.5, nil, NoPacketDelay)
+	connectionConfig := NewConnectionConfig(50.5, nil)
 	attr, err := connectionConfig.Attr(packetLossPercentageAttr)
 	require.Nil(t, err)
 	require.Equal(t, starlark.Float(50.5), attr)
-
-	attr, err = connectionConfig.Attr(packetDelayAttr)
-	require.Nil(t, err)
-	require.Equal(t, NoPacketDelay, attr)
 
 	attr, err = connectionConfig.Attr(packetDelayDistributionAttr)
 	require.Nil(t, err)
@@ -62,7 +55,7 @@ func TestConnectionConfig_Attr_Exists(t *testing.T) {
 }
 
 func TestConnectionConfig_Attr_DoesNotExist(t *testing.T) {
-	connectionConfig := NewConnectionConfig(50.5, nil, NoPacketDelay)
+	connectionConfig := NewConnectionConfig(50.5, nil)
 	attr, err := connectionConfig.Attr("do-not-exist")
 	expectedError := fmt.Sprintf("'%s' has no attribute 'do-not-exist'", ConnectionConfigTypeName)
 	require.Equal(t, expectedError, err.Error())
@@ -70,11 +63,10 @@ func TestConnectionConfig_Attr_DoesNotExist(t *testing.T) {
 }
 
 func TestConnectionConfig_AttrNames(t *testing.T) {
-	connectionConfig := NewConnectionConfig(50.5, nil, NoPacketDelay)
+	connectionConfig := NewConnectionConfig(50.5, nil)
 	attrs := connectionConfig.AttrNames()
 	expectedAttrs := []string{
 		packetLossPercentageAttr,
-		packetDelayAttr,
 		packetDelayDistributionAttr,
 	}
 	require.Equal(t, expectedAttrs, attrs)
@@ -87,19 +79,19 @@ func TestConnectionConfig_MakeWithArgs(t *testing.T) {
 	})
 	connectionConfig, err := MakeConnectionConfig(nil, builtin, args, noKwargs)
 	require.Nil(t, err)
-	expectedConnectionResult := NewConnectionConfig(50, nil, NoPacketDelay)
+	expectedConnectionResult := NewConnectionConfig(50, nil)
 	require.Equal(t, expectedConnectionResult, connectionConfig)
 
 	// for args, user would still need to pass 0 as the first argument
 	// assumption is that users use named args instead of positional args so should be good
-	packetDelay := NewPacketDelay(100)
+	packetDelay := NewUniformPacketDelayDistribution(100)
 	args = []starlark.Value{
 		starlark.Float(0),
 		packetDelay,
 	}
 	connectionConfig, err = MakeConnectionConfig(nil, builtin, args, noKwargs)
 	require.Nil(t, err)
-	expectedConnectionResult = NewConnectionConfig(0, nil, packetDelay)
+	expectedConnectionResult = NewConnectionConfig(0, packetDelay)
 	require.Equal(t, expectedConnectionResult, connectionConfig)
 }
 
@@ -115,25 +107,6 @@ func TestConnectionConfig_MakeWithArgs_FailureBadArgument(t *testing.T) {
 	require.Nil(t, connectionConfig)
 }
 
-func TestConnectionConfig_MakeWithKwargs_InvalidFieldsSet(t *testing.T) {
-	builtin := &starlark.Builtin{}
-	packetDelayDistribution := NewUniformPacketDelayDistribution(100)
-	kwargs := []starlark.Tuple{
-		[]starlark.Value{
-			starlark.String(packetDelayDistributionAttr),
-			packetDelayDistribution,
-		},
-		[]starlark.Value{
-			starlark.String(packetDelayAttr),
-			NoPacketDelay,
-		},
-	}
-	_, err := MakeConnectionConfig(nil, builtin, noArgs, kwargs)
-	require.NotNil(t, err)
-	expectedErr := fmt.Sprintf("%q can only have either %q or %q set exclusively. Note: %q field will be deprecated in a upcoming release", ConnectionConfigTypeName, packetDelayDistributionAttr, packetDelayAttr, packetDelayAttr)
-	require.EqualError(t, err, expectedErr)
-}
-
 func TestConnectionConfig_MakeWithKwargs_WithPacketLossAndNoDelay(t *testing.T) {
 	builtin := &starlark.Builtin{}
 	kwargs := []starlark.Tuple{
@@ -144,7 +117,7 @@ func TestConnectionConfig_MakeWithKwargs_WithPacketLossAndNoDelay(t *testing.T) 
 	}
 	connectionConfig, err := MakeConnectionConfig(nil, builtin, noArgs, kwargs)
 	require.Nil(t, err)
-	expectedConnectionResult := NewConnectionConfig(50, nil, NoPacketDelay)
+	expectedConnectionResult := NewConnectionConfig(50, nil)
 	require.Equal(t, expectedConnectionResult, connectionConfig)
 }
 
@@ -159,19 +132,19 @@ func TestConnectionConfig_MakeWithKwargs_WithNoPacketLossAndPacketDelay(t *testi
 	}
 	connectionConfig, err := MakeConnectionConfig(nil, builtin, noArgs, kwargs)
 	require.Nil(t, err)
-	expectedConnectionResult := NewConnectionConfig(0, packetDelayDistribution, NoPacketDelay)
+	expectedConnectionResult := NewConnectionConfig(0, packetDelayDistribution)
 	require.Equal(t, expectedConnectionResult, connectionConfig)
 
-	packetDelay := NewPacketDelay(100)
+	packetDelay := NewNormalPacketDelayDistribution(100, 10, 5)
 	kwargs = []starlark.Tuple{
 		[]starlark.Value{
-			starlark.String(packetDelayAttr),
+			starlark.String(packetDelayDistributionAttr),
 			packetDelay,
 		},
 	}
 	connectionConfig, err = MakeConnectionConfig(nil, builtin, noArgs, kwargs)
 	require.Nil(t, err)
-	expectedConnectionResult = NewConnectionConfig(0, nil, packetDelay)
+	expectedConnectionResult = NewConnectionConfig(0, packetDelay)
 	require.Equal(t, expectedConnectionResult, connectionConfig)
 }
 func TestConnectionConfig_MakeWithKwargs_WithPacketLossAndDelay(t *testing.T) {
@@ -191,13 +164,13 @@ func TestConnectionConfig_MakeWithKwargs_WithPacketLossAndDelay(t *testing.T) {
 
 	connectionConfig, err := MakeConnectionConfig(nil, builtin, noArgs, kwargs)
 	require.Nil(t, err)
-	expectedConnectionResult := NewConnectionConfig(50, packetDelayDistribution, NoPacketDelay)
+	expectedConnectionResult := NewConnectionConfig(50, packetDelayDistribution)
 	require.Equal(t, expectedConnectionResult, connectionConfig)
 
-	packetDelay := NewPacketDelay(100)
+	packetDelay := NewNormalPacketDelayDistribution(100, 10, 5)
 	kwargs = []starlark.Tuple{
 		[]starlark.Value{
-			starlark.String(packetDelayAttr),
+			starlark.String(packetDelayDistributionAttr),
 			packetDelay,
 		},
 		[]starlark.Value{
@@ -208,14 +181,14 @@ func TestConnectionConfig_MakeWithKwargs_WithPacketLossAndDelay(t *testing.T) {
 
 	connectionConfig, err = MakeConnectionConfig(nil, builtin, noArgs, kwargs)
 	require.Nil(t, err)
-	expectedConnectionResult = NewConnectionConfig(50, nil, packetDelay)
+	expectedConnectionResult = NewConnectionConfig(50, packetDelay)
 	require.Equal(t, expectedConnectionResult, connectionConfig)
 }
 func TestConnectionConfig_MakeWithKwargs_WithNoPacketLossAndNoDelay(t *testing.T) {
 	builtin := &starlark.Builtin{}
 	connectionConfig, err := MakeConnectionConfig(nil, builtin, noArgs, noKwargs)
 	require.Nil(t, err)
-	expectedConnectionResult := NewConnectionConfig(0, nil, NoPacketDelay)
+	expectedConnectionResult := NewConnectionConfig(0, nil)
 	require.Equal(t, expectedConnectionResult, connectionConfig)
 }
 
@@ -239,20 +212,20 @@ func TestConnectionConfig_MakeWithKwargs_FailureWrongArg(t *testing.T) {
 }
 
 func TestConnectionConfig_ToKurtosisType(t *testing.T) {
-	expectedUniformDelayFromPacketDelay := partition_topology.NewUniformPacketDelayDistribution(100)
+	expectedDefaultUniformDistribution := partition_topology.NewUniformPacketDelayDistribution(0)
 	expectedUniformDelayFromPacketDelayDistribution := partition_topology.NewUniformPacketDelayDistribution(500)
-	connectionConfigFromPacketDelay := NewConnectionConfig(50, nil, NewPacketDelay(100))
+	connectionConfigFromPacketDelay := NewConnectionConfig(50, nil)
 
 	expectedKurtosisTypeFromPacketDelay :=
 		partition_topology.NewPartitionConnection(
 			partition_topology.NewPacketLoss(50),
-			expectedUniformDelayFromPacketDelay,
+			expectedDefaultUniformDistribution,
 		)
 
 	require.Equal(t, expectedKurtosisTypeFromPacketDelay, *connectionConfigFromPacketDelay.ToKurtosisType())
 
 	connectionConfigFromPacketDelayDistribution :=
-		NewConnectionConfig(50, NewUniformPacketDelayDistribution(500), NewPacketDelay(100))
+		NewConnectionConfig(50, NewUniformPacketDelayDistribution(500))
 
 	expectedKurtosisTypeFromPacketDelayDistribution :=
 		partition_topology.NewPartitionConnection(
