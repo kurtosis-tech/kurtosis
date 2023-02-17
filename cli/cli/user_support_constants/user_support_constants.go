ddc6d77e223c6c2433a6cdbcaabbf3d287ae48e0
Leandro Poroli <leandro.poroli@kurtosistech.com>
2023-02-17 11:44:52 -0300

    Updated user support constants

--- cli/cli/user_support_constants/user_support_constants.go
+++ cli/cli/user_support_constants/user_support_constants.go
@@ -11,13 +11,17 @@ const (
 	Domain                       = "kurtosis.com"
 	DocumentationUrl             = "https://docs." + Domain
 	DiscordUrl                   = "https://discord.gg/6Jjp9c89z9"
-	GithubIssuesUrl              = "https://github.com/kurtosis-tech/kurtosis-sdk"
+	GithubIssuesUrl              = "https://github.com/kurtosis-tech/kurtosis/issues/new/choose"
 	CLICommandsReferenceURL      = DocumentationUrl + "/cli"
 	StarlarkPackagesReferenceURL = DocumentationUrl + "/reference/packages"
 	StarlarkLocatorsReferenceURL = DocumentationUrl + "/reference/locators"
 	UpgradeCLIInstructionsPage   = DocumentationUrl + "/install#upgrading"
 	GoogleRe2SyntaxDocumentation = "https://github.com/google/re2/wiki/Syntax"
 	MetricsPhilosophyDocs        = DocumentationUrl + "/explanations/metrics-philosophy"
+	KurtosisDiscordUrl           = "https://discord.com/channels/783719264308953108/783719264308953111"
+	KurtosisOnBoardCalendlyUrl   = "https://calendly.com/d/zgt-f2c-66p/kurtosis-onboarding"
+	FeedbackEmail                = "feedback@" + Domain
+	FeedbackEmailLink            = "mailto:" + FeedbackEmail
 	//    If you add new URLs above, make sure to add them to the urlsToValidateInTest below!!!
 	// WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING
 )
@@ -33,4 +37,6 @@ var urlsToValidateInTest = []string{
 	UpgradeCLIInstructionsPage,
 	GoogleRe2SyntaxDocumentation,
 	MetricsPhilosophyDocs,
+	KurtosisDiscordUrl,
+	KurtosisOnBoardCalendlyUrl,
 }
