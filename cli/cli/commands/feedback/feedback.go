cbbea7274d313b0896c2a413ee5297a1ad85df1e
Leandro Poroli <leandro.poroli@kurtosistech.com>
2023-03-01 10:35:12 -0300

    Added metrics ID and kurtosis version on the links

--- cli/cli/commands/feedback/feedback.go
+++ cli/cli/commands/feedback/feedback.go
@@ -7,9 +7,11 @@ import (
 	"github.com/kurtosis-tech/kurtosis/cli/cli/command_framework/lowlevel/args"
 	"github.com/kurtosis-tech/kurtosis/cli/cli/command_framework/lowlevel/flags"
 	"github.com/kurtosis-tech/kurtosis/cli/cli/command_str_consts"
+	"github.com/kurtosis-tech/kurtosis/cli/cli/helpers/metrics_user_id_store"
 	"github.com/kurtosis-tech/kurtosis/cli/cli/helpers/multi_os_command_executor"
 	"github.com/kurtosis-tech/kurtosis/cli/cli/helpers/output_printers"
 	"github.com/kurtosis-tech/kurtosis/cli/cli/user_support_constants"
+	"github.com/kurtosis-tech/kurtosis/kurtosis_version"
 	"github.com/kurtosis-tech/stacktrace"
 	"github.com/savioxavier/termlink"
 )
@@ -18,14 +20,17 @@ const (
 	commandShortDescription = "Give feedback"
 	commandDescription      = "Give feedback, file a bug report or feature request, or get help from the Kurtosis team."
 
-	githubFlagKey = "github"
-	emailFlagKey  = "email"
+	githubFlagKey   = "github"
+	emailFlagKey    = "email"
+	calendlyFlagKey = "calendly"
 
 	githubFlagUsageDescription = "Takes you to our Github where you can file a bug report, feature request, or get help."
 	emailFlagUsageDescription  = "Opens your mail client to send us feedback via email."
+	calendlyFlagDescription    = "When set, opens the link to our Calendly page to schedule a 1:1 session with a Kurtosis expert."
 
 	defaultOpenGitHubIssuePage = "false"
 	defaultOpenEmailLink       = "false"
+	defaultOpenCalendlyLink    = "false"
 
 	githubLinkText     = "let us know in our Github."
 	emailLinkText      = "click here to email us."
@@ -60,6 +65,13 @@ var FeedbackCmd = &lowlevel.LowlevelKurtosisCommand{
 			Type:      flags.FlagType_Bool,
 			Default:   defaultOpenEmailLink,
 		},
+		{
+			Key:       calendlyFlagKey,
+			Usage:     calendlyFlagDescription,
+			Shorthand: "",
+			Type:      flags.FlagType_Bool,
+			Default:   defaultOpenCalendlyLink,
+		},
 	},
 	Args:                     nil,
 	PreValidationAndRunFunc:  nil,
@@ -68,13 +80,26 @@ var FeedbackCmd = &lowlevel.LowlevelKurtosisCommand{
 }
 
 func run(_ context.Context, flags *flags.ParsedFlags, _ *args.ParsedArgs) error {
+	metricsUserIdStore := metrics_user_id_store.GetMetricsUserIDStore()
+	metricsUserId, err := metricsUserIdStore.GetUserID()
+	if err != nil {
+		return stacktrace.Propagate(err, "An error occurred getting metrics user id")
+	}
+
 	shouldOpenGitHubIssuesPage, err := flags.GetBool(githubFlagKey)
 	if err != nil {
 		return stacktrace.Propagate(err, "Expected a boolean flag with key '%v' but none was found; this is an error in Kurtosis!", githubFlagKey)
 	}
 
+	gitHubIssueURL := fmt.Sprintf(
+		"%s?version=%v&metrics-user-id=%v",
+		user_support_constants.GithubIssuesUrl,
+		kurtosis_version.KurtosisVersion,
+		metricsUserId,
+	)
+
 	if shouldOpenGitHubIssuesPage {
-		if err := multi_os_command_executor.OpenFile(user_support_constants.GithubIssuesUrl); err != nil {
+		if err := multi_os_command_executor.OpenFile(gitHubIssueURL); err != nil {
 			return stacktrace.Propagate(err, "An error occurred while opening the Kurtosis Github issue page")
 		}
 		return nil
@@ -85,11 +110,35 @@ func run(_ context.Context, flags *flags.ParsedFlags, _ *args.ParsedArgs) error 
 		return stacktrace.Propagate(err, "Expected a boolean flag with key '%v' but none was found; this is an error in Kurtosis!", emailFlagKey)
 	}
 
+	body := fmt.Sprintf(
+		"Thanks for sending us feedback, we value your thoughts.\n\n"+
+			"We've prefilled some information about your Kurtosis CLI instance. "+
+			"While it is optional to send this information to us, this metadata can be help our"+
+			" team debug issues and turn around deliver fixes/improvements faster.\n"+
+			"CLI version: %v\nMetrics User ID: %v", kurtosis_version.KurtosisVersion, metricsUserId)
+
+	feedbackEmailLink := fmt.Sprintf(
+		"%s?body=%v",
+		user_support_constants.FeedbackEmailLink,
+		body,
+	)
+
 	if shouldOpenEmailLink {
-		if err := multi_os_command_executor.OpenFile(user_support_constants.FeedbackEmailLink); err != nil {
+		if err := multi_os_command_executor.OpenFile(feedbackEmailLink); err != nil {
 			return stacktrace.Propagate(err, "An error occurred while opening the feedback email link")
 		}
 		return nil
+	}
+
+	shouldOpenCalendlyLink, err := flags.GetBool(calendlyFlagKey)
+	if err != nil {
+		return stacktrace.Propagate(err, "Expected a boolean flag with key '%v' but none was found; this is an error in Kurtosis!", calendlyFlagKey)
+	}
+
+	if shouldOpenCalendlyLink {
+		if err := multi_os_command_executor.OpenFile(user_support_constants.KurtosisOnBoardCalendlyUrl); err != nil {
+			return stacktrace.Propagate(err, "An error occurred while opening the Calendly email link")
+		}
 	}
 
 	spotlightMessagePrinter := output_printers.GetSpotlightMessagePrinter()
@@ -97,8 +146,8 @@ func run(_ context.Context, flags *flags.ParsedFlags, _ *args.ParsedArgs) error 
 
 	fmt.Printf(
 		feedbackMsg,
-		termlink.ColorLink(githubLinkText, user_support_constants.GithubIssuesUrl, greenColorStr),
-		termlink.ColorLink(emailLinkText, user_support_constants.FeedbackEmailLink, greenColorStr),
+		termlink.ColorLink(githubLinkText, gitHubIssueURL, greenColorStr),
+		termlink.ColorLink(emailLinkText, feedbackEmailLink, greenColorStr),
 		termlink.ColorLink(onboardingLinkText, user_support_constants.KurtosisOnBoardCalendlyUrl, greenColorStr),
 	)
 	return nil
--- cli/cli/user_support_constants/user_support_constants.go
+++ cli/cli/user_support_constants/user_support_constants.go
@@ -9,6 +9,7 @@ const (
 	// WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING
 	//    If you add new URLs below, make sure to add them to the urlsToValidateInTest below!!!
 	Domain                       = "kurtosis.com"
+	OldDomain                    = "kurtosistech.com" //This domain is still used for email accounts
 	DocumentationUrl             = "https://docs." + Domain
 	DiscordUrl                   = "https://discord.gg/6Jjp9c89z9"
 	GithubIssuesUrl              = "https://github.com/kurtosis-tech/kurtosis/issues/new/choose"
@@ -22,7 +23,7 @@ const (
 	GoogleRe2SyntaxDocumentation = "https://github.com/google/re2/wiki/Syntax"
 	KurtosisDiscordUrl           = "https://discord.com/channels/783719264308953108/783719264308953111"
 	KurtosisOnBoardCalendlyUrl   = "https://calendly.com/d/zgt-f2c-66p/kurtosis-onboarding"
-	FeedbackEmail                = "feedback@" + Domain
+	FeedbackEmail                = "feedback@" + OldDomain
 	FeedbackEmailLink            = "mailto:" + FeedbackEmail
 
 	//    If you add new URLs above, make sure to add them to the urlsToValidateInTest below!!!
