3f6f753e6f5a0b491001507b5e8a6a9e1aef174f
Guillaume Bouvignies <guillaume.bouvignies@kurtosistech.com>
2023-03-01 18:10:41 +0100

    refactor: Small refactor to Starlark framework tests (#86)
    Small refactor to Starlark framework tests

--- core/server/api_container/server/startosis_engine/kurtosis_starlark_framework/test_engine/add_services_framework_test.go
+++ core/server/api_container/server/startosis_engine/kurtosis_starlark_framework/test_engine/add_services_framework_test.go
@@ -17,14 +17,6 @@ import (
 	"testing"
 )
 
-const (
-	enclaveUuid  = "test-enclave"
-	service1     = service.ServiceName("service-1")
-	serviceUuid1 = service.ServiceUUID("service-1-uuid")
-	service2     = service.ServiceName("service-2")
-	serviceUuid2 = service.ServiceUUID("service-2-uuid")
-)
-
 type addServicesTestCase struct {
 	*testing.T
 }
@@ -47,22 +39,22 @@ func (t *addServicesTestCase) GetInstruction() *kurtosis_plan_instruction.Kurtos
 		mock.Anything,
 		mock.MatchedBy(func(configs map[service.ServiceName]*kurtosis_core_rpc_api_bindings.ServiceConfig) bool {
 			require.Len(t, configs, 2)
-			require.Contains(t, configs, service1)
-			require.Contains(t, configs, service2)
+			require.Contains(t, configs, TestServiceName)
+			require.Contains(t, configs, TestServiceName2)
 
-			expectedServiceConfig1 := services.NewServiceConfigBuilder("test-image-1").WithSubnetwork("my-subnetwork").Build()
-			actualServiceConfig1 := services.NewServiceConfigBuilderFromServiceConfig(configs[service1]).Build()
+			expectedServiceConfig1 := services.NewServiceConfigBuilder(TestContainerImageName).WithSubnetwork(string(TestSubnetwork)).Build()
+			actualServiceConfig1 := services.NewServiceConfigBuilderFromServiceConfig(configs[TestServiceName]).Build()
 			assert.Equal(t, expectedServiceConfig1, actualServiceConfig1)
-			actualServiceConfig2 := services.NewServiceConfigBuilderFromServiceConfig(configs[service2]).Build()
-			expectedServiceConfig2 := services.NewServiceConfigBuilder("test-image-2").WithCpuAllocationMillicpus(1000).WithMemoryAllocationMegabytes(2048).Build()
+			actualServiceConfig2 := services.NewServiceConfigBuilderFromServiceConfig(configs[TestServiceName2]).Build()
+			expectedServiceConfig2 := services.NewServiceConfigBuilder(TestContainerImageName).WithCpuAllocationMillicpus(TestCpuAllocation).WithMemoryAllocationMegabytes(TestMemoryAllocation).Build()
 			assert.Equal(t, expectedServiceConfig2, actualServiceConfig2)
 			return true
 		}),
 		mock.Anything,
 	).Times(1).Return(
 		map[service.ServiceName]*service.Service{
-			service1: service.NewService(service.NewServiceRegistration(service1, serviceUuid1, enclaveUuid, nil, string(service1)), container_status.ContainerStatus_Running, nil, nil, nil),
-			service2: service.NewService(service.NewServiceRegistration(service2, serviceUuid2, enclaveUuid, nil, string(service2)), container_status.ContainerStatus_Running, nil, nil, nil),
+			TestServiceName:  service.NewService(service.NewServiceRegistration(TestServiceName, TestServiceUuid, TestEnclaveUuid, nil, string(TestServiceName)), container_status.ContainerStatus_Running, nil, nil, nil),
+			TestServiceName2: service.NewService(service.NewServiceRegistration(TestServiceName2, TestServiceUuid2, TestEnclaveUuid, nil, string(TestServiceName2)), container_status.ContainerStatus_Running, nil, nil, nil),
 		},
 		map[service.ServiceName]error{},
 		nil,
@@ -72,19 +64,19 @@ func (t *addServicesTestCase) GetInstruction() *kurtosis_plan_instruction.Kurtos
 }
 
 func (t *addServicesTestCase) GetStarlarkCode() string {
-	serviceConfig1 := `ServiceConfig(image="test-image-1", subnetwork="my-subnetwork")`
-	serviceConfig2 := `ServiceConfig(image="test-image-2", cpu_allocation=1000, memory_allocation=2048)`
-	return fmt.Sprintf(`%s(%s={"service-1": %s, "service-2": %s})`, add_service.AddServicesBuiltinName, add_service.ConfigsArgName, serviceConfig1, serviceConfig2)
+	serviceConfig1 := fmt.Sprintf("ServiceConfig(image=%q, subnetwork=%q)", TestContainerImageName, TestSubnetwork)
+	serviceConfig2 := fmt.Sprintf("ServiceConfig(image=%q, cpu_allocation=%d, memory_allocation=%d)", TestContainerImageName, TestCpuAllocation, TestMemoryAllocation)
+	return fmt.Sprintf(`%s(%s={%q: %s, %q: %s})`, add_service.AddServicesBuiltinName, add_service.ConfigsArgName, TestServiceName, serviceConfig1, TestServiceName2, serviceConfig2)
 }
 
 func (t *addServicesTestCase) Assert(interpretationResult starlark.Value, executionResult *string) {
 	resultDict, ok := interpretationResult.(*starlark.Dict)
 	require.True(t, ok, "interpretation result should be a dictionary")
 	require.Equal(t, resultDict.Len(), 2)
-	require.Contains(t, resultDict.Keys(), starlark.String(service1))
-	require.Contains(t, resultDict.Keys(), starlark.String(service2))
+	require.Contains(t, resultDict.Keys(), starlark.String(TestServiceName))
+	require.Contains(t, resultDict.Keys(), starlark.String(TestServiceName2))
 
 	require.Contains(t, *executionResult, "Successfully added the following '2' services:")
-	require.Contains(t, *executionResult, "Service 'service-1' added with UUID 'service-1-uuid'")
-	require.Contains(t, *executionResult, "Service 'service-2' added with UUID 'service-2-uuid'")
+	require.Contains(t, *executionResult, fmt.Sprintf("Service '%s' added with UUID '%s'", TestServiceName, TestServiceUuid))
+	require.Contains(t, *executionResult, fmt.Sprintf("Service '%s' added with UUID '%s'", TestServiceName2, TestServiceUuid2))
 }
