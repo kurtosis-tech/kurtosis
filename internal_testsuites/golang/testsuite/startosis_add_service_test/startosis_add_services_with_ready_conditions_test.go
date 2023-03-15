09d9da9e294a1881a05fd3c04f6c90a3ed71e096
Leandro Poroli <leandro.poroli@kurtosistech.com>
2023-03-15 17:37:36 -0300

    Removed unit test that whas depending on the rollbacked service readiness check func

---
+++ internal_testsuites/golang/testsuite/startosis_add_service_test/startosis_add_services_with_ready_conditions_test.go
@@ -0,0 +1,70 @@
+package startosis_add_service
+
+import (
+	"context"
+	"fmt"
+	"github.com/kurtosis-tech/kurtosis-cli/golang_internal_testsuite/test_helpers"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+const (
+	addServiceWithReadyConditionsTestName = "service-test"
+
+	addServiceWithReadyConditionsScript = `
+def run(plan):
+	get_recipe = GetHttpRequestRecipe(
+		port_id = "http-port",
+		endpoint = "?input=foo/bar",
+	)
+
+	ready_conditions = ReadyConditions(
+        recipe=get_recipe,
+		field="code",
+		assertion="==",
+		target_value=%v,
+		interval="1s",
+		timeout="3s"
+    )
+
+	service_config = ServiceConfig(
+		image = "mendhak/http-https-echo:26",
+		ports = {
+			"http-port": PortSpec(number = 8080, transport_protocol = "TCP")
+		},
+        ready_conditions = ready_conditions
+	)
+
+	plan.add_service(service_name = "web-server", config = service_config)
+`
+
+	okStatusCode          = 200
+	serverErrorStatusCode = 500
+)
+
+func TestStartosis_AddServiceWithReadyConditionsCheck(t *testing.T) {
+	ctx := context.Background()
+
+	script := fmt.Sprintf(addServiceWithReadyConditionsScript, okStatusCode)
+
+	runResult := test_helpers.SetupSimpleEnclaveAndRunScript(t, ctx, addServiceWithReadyConditionsTestName, script)
+
+	require.Nil(t, runResult.InterpretationError, "Unexpected interpretation error")
+	require.Empty(t, runResult.ValidationErrors, "Unexpected validation error")
+	require.Empty(t, runResult.ExecutionError, "Unexpected execution error")
+}
+
+func TestStartosis_AddServiceWithReadyConditionsCheckFail(t *testing.T) {
+	ctx := context.Background()
+
+	expectedLastAssertionErrorStr := fmt.Sprintf("Assertion failed '%v' '==' '%v'", okStatusCode, serverErrorStatusCode)
+
+	script := fmt.Sprintf(addServiceWithReadyConditionsScript, serverErrorStatusCode)
+
+	runResult := test_helpers.SetupSimpleEnclaveAndRunScript(t, ctx, addServiceWithReadyConditionsTestName, script)
+
+	require.Nil(t, runResult.InterpretationError, "Unexpected interpretation error")
+	require.Empty(t, runResult.ValidationErrors, "Unexpected validation error")
+	require.NotEmpty(t, runResult.ExecutionError, "Expected execution error coming from failed ready conditions")
+	require.Contains(t, runResult.ExecutionError.ErrorMessage, expectedLastAssertionErrorStr)
+}
