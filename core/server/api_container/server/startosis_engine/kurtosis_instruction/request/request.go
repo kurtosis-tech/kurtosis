826f0727a43ca1acc05aaded41eed307b04c7d96
leoporoli <leandroporoli@gmail.com>
2023-03-15 09:13:22 -0300

    fix: wait instruction hanging forever when `service_name` field is not passed (#197)
    ## Description:
    Were are moving the validation to the interpretation layer to detect the
    missing field before executing the instructions.
    
    fixes #174
    
    Now if the  argument is not specified this error will be thrown:
    <img width="1387" alt="image"
    src="https://user-images.githubusercontent.com/29951623/225121657-469398f8-d44c-4787-b28c-08866105643c.png">

--- core/server/api_container/server/startosis_engine/kurtosis_instruction/request/request.go
+++ core/server/api_container/server/startosis_engine/kurtosis_instruction/request/request.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/kurtosis-tech/kurtosis/container-engine-lib/lib/backend_interface/objects/service"
 	"github.com/kurtosis-tech/kurtosis/core/server/api_container/server/service_network"
+	"github.com/kurtosis-tech/kurtosis/core/server/api_container/server/startosis_engine/kurtosis_instruction/shared_helpers"
 	"github.com/kurtosis-tech/kurtosis/core/server/api_container/server/startosis_engine/kurtosis_starlark_framework"
 	"github.com/kurtosis-tech/kurtosis/core/server/api_container/server/startosis_engine/kurtosis_starlark_framework/builtin_argument"
 	"github.com/kurtosis-tech/kurtosis/core/server/api_container/server/startosis_engine/kurtosis_starlark_framework/kurtosis_plan_instruction"
@@ -12,6 +13,7 @@ import (
 	"github.com/kurtosis-tech/kurtosis/core/server/api_container/server/startosis_engine/startosis_errors"
 	"github.com/kurtosis-tech/kurtosis/core/server/api_container/server/startosis_engine/startosis_validator"
 	"github.com/kurtosis-tech/stacktrace"
+	"github.com/sirupsen/logrus"
 	"go.starlark.net/starlark"
 )
 
@@ -72,19 +74,23 @@ type RequestCapabilities struct {
 }
 
 func (builtin *RequestCapabilities) Interpret(arguments *builtin_argument.ArgumentValuesSet) (starlark.Value, *startosis_errors.InterpretationError) {
-	var serviceName service.ServiceName
+	httpRequestRecipe, err := builtin_argument.ExtractArgumentValue[*recipe.HttpRequestRecipe](arguments, RecipeArgName)
+	if err != nil {
+		return nil, startosis_errors.WrapWithInterpretationError(err, "Unable to extract value for '%s' argument", RecipeArgName)
+	}
 
+	var serviceName service.ServiceName
 	if arguments.IsSet(ServiceNameArgName) {
 		serviceNameArgumentValue, err := builtin_argument.ExtractArgumentValue[starlark.String](arguments, ServiceNameArgName)
 		if err != nil {
 			return nil, startosis_errors.WrapWithInterpretationError(err, "Unable to extract value for '%s' argument", ServiceNameArgName)
 		}
 		serviceName = service.ServiceName(serviceNameArgumentValue.GoString())
-	}
-
-	httpRequestRecipe, err := builtin_argument.ExtractArgumentValue[*recipe.HttpRequestRecipe](arguments, RecipeArgName)
-	if err != nil {
-		return nil, startosis_errors.WrapWithInterpretationError(err, "Unable to extract value for '%s' argument", RecipeArgName)
+	} else if httpRequestRecipe.GetServiceName() != shared_helpers.EmptyServiceName {
+		serviceName = httpRequestRecipe.GetServiceName()
+		logrus.Warnf("The recipe.service_name field will be deprecated soon, users will have to pass the service name value direclty to the 'exec', 'request' and 'wait' instructions")
+	} else {
+		return nil, startosis_errors.NewInterpretationError("Service name is not set, either as a request instruction's argument or as a recipe field. You can fix it passing the 'service_name' argument in the 'request' call")
 	}
 
 	resultUuid, err := builtin.runtimeValueStore.CreateValue()
