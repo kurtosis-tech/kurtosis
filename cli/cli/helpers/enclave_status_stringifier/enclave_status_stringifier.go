8254c7fbf35e38840c1ff5182017f19184eccae5
Gyanendra Mishra <anomaly.the@gmail.com>
2023-03-15 09:14:57 +0000

    feat: colorize RUNNING|STOPPED statuses for Enclaves And Containers (#178)
    ## Description:
    Colorize RUNNING|STOPPED Statusses
    
    ## Is this change user facing?
    <!-- A user facing change is one that you should expect a day-to-day
    user to encounter or if the change requires user-action upon or before
    upgrading. If in doubt, select "Yes" -->
    * [x] Yes
    * [ ] No

--- cli/cli/helpers/enclave_status_stringifier/enclave_status_stringifier.go
+++ cli/cli/helpers/enclave_status_stringifier/enclave_status_stringifier.go
@@ -1,8 +1,14 @@
 package enclave_status_stringifier
 
 import (
+	"github.com/fatih/color"
 	"github.com/kurtosis-tech/kurtosis/api/golang/engine/kurtosis_engine_rpc_api_bindings"
 	"github.com/kurtosis-tech/stacktrace"
+)
+
+var (
+	colorizeRunning = color.New(color.FgGreen).SprintFunc()
+	colorizeStopped = color.New(color.FgYellow).SprintFunc()
 )
 
 func EnclaveContainersStatusStringifier(enclaveStatus kurtosis_engine_rpc_api_bindings.EnclaveContainersStatus) (string, error) {
@@ -10,9 +16,9 @@ func EnclaveContainersStatusStringifier(enclaveStatus kurtosis_engine_rpc_api_bi
 	case kurtosis_engine_rpc_api_bindings.EnclaveContainersStatus_EnclaveContainersStatus_EMPTY:
 		return "EMPTY", nil
 	case kurtosis_engine_rpc_api_bindings.EnclaveContainersStatus_EnclaveContainersStatus_RUNNING:
-		return "RUNNING", nil
+		return colorizeRunning("RUNNING"), nil
 	case kurtosis_engine_rpc_api_bindings.EnclaveContainersStatus_EnclaveContainersStatus_STOPPED:
-		return "STOPPED", nil
+		return colorizeStopped("STOPPED"), nil
 	default:
 		return "", stacktrace.NewError("Unrecognized enclave status '%v'; this is a bug in Kurtosis", enclaveStatus)
 	}
@@ -21,11 +27,11 @@ func EnclaveContainersStatusStringifier(enclaveStatus kurtosis_engine_rpc_api_bi
 func EnclaveAPIContainersStatusStringifier(enclaveStatus kurtosis_engine_rpc_api_bindings.EnclaveAPIContainerStatus) (string, error) {
 	switch enclaveStatus {
 	case kurtosis_engine_rpc_api_bindings.EnclaveAPIContainerStatus_EnclaveAPIContainerStatus_RUNNING:
-		return "RUNNING", nil
+		return colorizeRunning("RUNNING"), nil
 	case kurtosis_engine_rpc_api_bindings.EnclaveAPIContainerStatus_EnclaveAPIContainerStatus_NONEXISTENT:
 		return "NONEXISTENT", nil
 	case kurtosis_engine_rpc_api_bindings.EnclaveAPIContainerStatus_EnclaveAPIContainerStatus_STOPPED:
-		return "STOPPED", nil
+		return colorizeStopped("STOPPED"), nil
 	default:
 		return "", stacktrace.NewError("Unrecognized enclave API status '%v'; this is a bug in Kurtosis", enclaveStatus)
 	}
