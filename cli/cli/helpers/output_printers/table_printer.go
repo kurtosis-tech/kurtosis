51db6f8a7ae9a3fcb736f37d8fb948686e753b56
Gyanendra Mishra <anomaly.the@gmail.com>
2023-03-15 10:52:46 +0000

    dont pad pre padding strings

--- cli/cli/helpers/output_printers/table_printer.go
+++ cli/cli/helpers/output_printers/table_printer.go
@@ -3,6 +3,7 @@ package output_printers
 import (
 	"github.com/fatih/color"
 	"github.com/kurtosis-tech/stacktrace"
+	"strings"
 )
 
 type TablePrinter struct {
@@ -41,7 +42,12 @@ func (printer *TablePrinter) AddRow(data ...string) error {
 		)
 	}
 
+	// This is a hack; tabWriter doesn't support colored strings when it comes to formatting
+	// We format uncolored strings with Reset so that they have similar padding as colored strings
 	for index := range data {
+		if strings.HasPrefix(data[index], "\x1b") {
+			continue
+		}
 		data[index] = noColorForPadding(data[index])
 	}
 
