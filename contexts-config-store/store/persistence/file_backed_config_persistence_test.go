f0a35f920494771874c936c6fec40190dce80573
guillaume.bouvignies <guillaume.bouvignies@kurtosistech.com>
2023-03-17 19:49:02 +0100

    Fix undeterministic json serialization test

--- contexts-config-store/store/persistence/file_backed_config_persistence_test.go
+++ contexts-config-store/store/persistence/file_backed_config_persistence_test.go
@@ -3,6 +3,7 @@ package persistence
 import (
 	"github.com/kurtosis-tech/kurtosis/contexts-config-store/api/golang/generated"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/proto"
 	"os"
 	"testing"
@@ -30,7 +31,7 @@ var (
 		},
 	}
 
-	serializedContextConfig = "{\"currentContextUuid\":{\"value\":\"context-uuid\"},\"contexts\":[{\"uuid\":{\"value\":\"context-uuid\"},\"name\":\"context-name\",\"localOnlyContextV0\":{}}]}"
+	serializedContextsConfig = "{\"contexts\":[{\"localOnlyContextV0\":{},\"name\":\"context-name\",\"uuid\":{\"value\":\"context-uuid\"}}],\"currentContextUuid\":{\"value\":\"context-uuid\"}}"
 )
 
 func TestPersistConfig(t *testing.T) {
@@ -43,8 +44,19 @@ func TestPersistConfig(t *testing.T) {
 	require.Nil(t, err)
 
 	fileContent, err := os.ReadFile(tempFile.Name())
-	require.Nil(t, err)
-	require.Equal(t, serializedContextConfig, string(fileContent))
+	require.NoError(t, err)
+
+	// protojson provides no guarantee for the shape of the json produced, they just provide compatibility between
+	// protojson.Unmarshal and protojson.Marshal. To be able to validate output with the expected JSON, we need to
+	// compare the actual proto messages. See:
+	// - https://protobuf.dev/reference/go/faq/#unstable-json
+	// - https://github.com/golang/protobuf/issues/1082
+	writtenContextsConfig := new(generated.KurtosisContextsConfig)
+	require.NoError(t, protojson.Unmarshal(fileContent, writtenContextsConfig))
+	expectedContextsConfig := new(generated.KurtosisContextsConfig)
+	require.NoError(t, protojson.Unmarshal([]byte(serializedContextsConfig), expectedContextsConfig))
+
+	require.True(t, proto.Equal(writtenContextsConfig, expectedContextsConfig))
 }
 
 func TestLoadConfig(t *testing.T) {
@@ -52,7 +64,7 @@ func TestLoadConfig(t *testing.T) {
 	require.Nil(t, err)
 	defer os.Remove(tempFile.Name())
 
-	_, err = tempFile.Write([]byte(serializedContextConfig))
+	_, err = tempFile.Write([]byte(serializedContextsConfig))
 	require.Nil(t, err)
 
 	storage := newFileBackedConfigPersistenceForTesting(tempFile.Name())
