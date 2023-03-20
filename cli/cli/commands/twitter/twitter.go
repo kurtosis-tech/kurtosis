9bd2fd8ef80187fce1798cda8b43bfab32f99aab
Gyanendra Mishra <anomaly.the@gmail.com>
2023-03-20 12:01:33 +0100

    added Twitter

---
+++ cli/cli/commands/twitter/twitter.go
@@ -0,0 +1,30 @@
+package twitter
+
+import (
+	"context"
+	"github.com/kurtosis-tech/kurtosis/cli/cli/command_framework/lowlevel"
+	"github.com/kurtosis-tech/kurtosis/cli/cli/command_framework/lowlevel/args"
+	"github.com/kurtosis-tech/kurtosis/cli/cli/command_framework/lowlevel/flags"
+	"github.com/kurtosis-tech/kurtosis/cli/cli/command_str_consts"
+	"github.com/kurtosis-tech/kurtosis/cli/cli/helpers/multi_os_command_executor"
+	"github.com/kurtosis-tech/kurtosis/cli/cli/user_support_constants"
+	"github.com/kurtosis-tech/stacktrace"
+)
+
+var DiscordCmd = &lowlevel.LowlevelKurtosisCommand{
+	CommandStr:               command_str_consts.TwitterCmdStr,
+	ShortDescription:         "Opens the official Kurtosis Twitter page",
+	LongDescription:          "Opens the official Kurtosis Twitter page",
+	Flags:                    nil,
+	Args:                     nil,
+	PreValidationAndRunFunc:  nil,
+	RunFunc:                  run,
+	PostValidationAndRunFunc: nil,
+}
+
+func run(_ context.Context, _ *flags.ParsedFlags, _ *args.ParsedArgs) error {
+	if err := multi_os_command_executor.OpenFile(user_support_constants.KurtosisTechTwitterProfileLink); err != nil {
+		return stacktrace.Propagate(err, "An error occurred while opening the Kurtosis Twitter Channel")
+	}
+	return nil
+}
