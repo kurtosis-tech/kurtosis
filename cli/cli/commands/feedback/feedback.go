963e9dd055a3ceabafde11a4d942e16f300d827c
leoporoli <leandroporoli@gmail.com>
2023-03-27 11:32:01 -0300

    feat: Added `bug, feature and docs` flags in the `kurtosis feedback` command (#287)
    ## Description:
    - Adding `bug, fr and docs` kurtosis feedback flags
    - GitHub is the default feedback destination now.
    - Edited the kurtosis feedback command description text
    
    ## Is this change user-facing?
    YES
    
    ## References (if applicable):
    Fixes #219 & #226

--- cli/cli/commands/feedback/feedback.go
+++ cli/cli/commands/feedback/feedback.go
@@ -15,23 +15,41 @@ import (
 	"github.com/kurtosis-tech/stacktrace"
 	"github.com/savioxavier/termlink"
 	"net/url"
+	"strings"
 )
 
 const (
 	commandShortDescription = "Give feedback"
-	commandDescription      = "Give feedback, file a bug report or feature request, or get help from the Kurtosis team."
+	commandDescription      = "Give feedback, file a bug report or feature request, or get help from the Kurtosis team. " +
+		"See below for the many ways you can get in touch with us.\n" +
+		"TIP: You can quickly type and send us feedback directly from the CLI. For example, " +
+		"kurtosis feedback \"I enjoy the enclave naming theme\" will open the Kurtosis Github " +
+		"choose new issue type page with the description pre-filled with \"I enjoy the enclave naming theme\"."
 
-	githubFlagKey   = "github"
-	emailFlagKey    = "email"
-	calendlyFlagKey = "calendly"
+	githubFlagKey                 = "github"
+	emailFlagKey                  = "email"
+	calendlyFlagKey               = "calendly"
+	bugFeedbackFlagKey            = "bug"
+	featureRequestFeedbackFlagKey = "feature"
+	docsFeedbackFlagKey           = "docs"
 
-	githubFlagUsageDescription = "Takes you to our Github where you can file a bug report, feature request, or get help."
-	emailFlagUsageDescription  = "Opens your mail client to send us feedback via email."
-	calendlyFlagDescription    = "When set, opens the link to our Calendly page to schedule a 1:1 session with a Kurtosis expert."
+	githubFlagUsageDescription    = "Takes you to our Github where you can file a bug report, feature request, or get help."
+	emailFlagUsageDescription     = "Opens your mail client to send us feedback via email."
+	calendlyFlagDescription       = "When set, opens the link to our Calendly page to schedule a 1:1 session with a Kurtosis expert."
+	bugFlagDescription            = "To specify that this is a bug feedback type"
+	featureRequestFlagDescription = "To specify that this is a feature request feedback type"
+	docsFlagDescription           = "To specify that this is a docs feedback type"
+
+	bugEmailSubjectPrefix       = "[BUG]"
+	featureRequestSubjectPrefix = "[FEATURE_REQUEST]"
+	docsSubjectPrefix           = "[DOCS]"
 
 	defaultOpenGitHubIssuePage = "false"
 	defaultOpenEmailLink       = "false"
 	defaultOpenCalendlyLink    = "false"
+	defaultBug                 = "false"
+	defaultFeatureRequest      = "false"
+	defaultDocs                = "false"
 
 	githubLinkText     = "let us know in our Github."
 	emailLinkText      = "click here to email us."
@@ -40,6 +58,15 @@ const (
 	feedbackMsgTitle = "Your feedback is valuable and helps us improve Kurtosis. Thank you."
 
 	feedbackMsg = `
+This command can be used to deliver feedback to the Kurtosis team!
+
+* Pass in your feedback as an argument using double quotations (e.g. kurtosis feedback "my feedback").
+* Pass in the --github flag to open our Github Issues templates, pre-filled with your feedback/arg.
+* Pass in the --email flag to open a draft email, pre-filled with your feedback/arg, to send to feedback@kurtosistech.com.
+* Pass in the --calendly flag to open our Calendly link to schedule a 1:1 session with us for feedback and questions you may have!
+
+See below for the some direct links as well.
+
 * For bugs/issues, %v
 * For general feedback, %v
 * If you need help getting started, %v
@@ -50,6 +77,14 @@ const (
 	userMsgArgDefaultValue = "default-message-value"
 	userMsgArgIsOptional   = true
 	userMsgArgIsNotGreedy  = false
+
+	flagKeysStrSeparator = ", "
+
+	notDestinationTypeSelected = ""
+	notFeedbackTypeSelected    = ""
+	emptyUserMsg               = ""
+
+	defaultDestinationType = githubFlagKey
 )
 
 var FeedbackCmd = &lowlevel.LowlevelKurtosisCommand{
@@ -78,6 +113,27 @@ var FeedbackCmd = &lowlevel.LowlevelKurtosisCommand{
 			Type:      flags.FlagType_Bool,
 			Default:   defaultOpenCalendlyLink,
 		},
+		{
+			Key:       bugFeedbackFlagKey,
+			Usage:     bugFlagDescription,
+			Shorthand: "",
+			Type:      flags.FlagType_Bool,
+			Default:   defaultBug,
+		},
+		{
+			Key:       featureRequestFeedbackFlagKey,
+			Usage:     featureRequestFlagDescription,
+			Shorthand: "",
+			Type:      flags.FlagType_Bool,
+			Default:   defaultFeatureRequest,
+		},
+		{
+			Key:       docsFeedbackFlagKey,
+			Usage:     docsFlagDescription,
+			Shorthand: "",
+			Type:      flags.FlagType_Bool,
+			Default:   defaultDocs,
+		},
 	},
 	Args: []*args.ArgConfig{
 		{
@@ -94,18 +150,103 @@ var FeedbackCmd = &lowlevel.LowlevelKurtosisCommand{
 }
 
 func run(_ context.Context, flags *flags.ParsedFlags, args *args.ParsedArgs) error {
-	isGithubFlagActivated := false
-	isEmailFlagActivated := false
-	isCalendlyFlagActivated := false
-	doesUserFillMsg := true
-
 	// Args parsing and validation
+	userMsg, err := getUserMsgFromArgs(args)
+	if err != nil {
+		return stacktrace.Propagate(err, "An error occurred while extracting the user message from args '%+v'", args)
+	}
+
+	mutuallyExclusiveFeedbackDestinationTypeFlagKeys := []string{
+		githubFlagKey, emailFlagKey, calendlyFlagKey}
+
+	selectedDestinationType, err := validateMutuallyExclusiveBooleanFlagsAndGetSelectedKey(
+		flags,
+		mutuallyExclusiveFeedbackDestinationTypeFlagKeys,
+	)
+	if err != nil {
+		return stacktrace.Propagate(
+			err,
+			"An error occurred validating mutually exclusive flags '%+v'",
+			mutuallyExclusiveFeedbackDestinationTypeFlagKeys,
+		)
+	}
+
+	mutuallyExclusiveFeedbackTypeFlagKeys := []string{bugFeedbackFlagKey, featureRequestFeedbackFlagKey, docsFeedbackFlagKey}
+
+	selectedFeedbackType, err := validateMutuallyExclusiveBooleanFlagsAndGetSelectedKey(flags, mutuallyExclusiveFeedbackTypeFlagKeys)
+	if err != nil {
+		return stacktrace.Propagate(
+			err,
+			"An error occurred validating mutually exclusive flags '%+v'",
+			mutuallyExclusiveFeedbackTypeFlagKeys,
+		)
+	}
+
+	//Github destination is used by default if users fill the message or any feedback type but do not specify the destination
+	if selectedDestinationType == notDestinationTypeSelected &&
+		(userMsg != emptyUserMsg || selectedFeedbackType != notFeedbackTypeSelected) {
+		selectedDestinationType = defaultDestinationType
+	}
+
+	metricsUserIdStore := metrics_user_id_store.GetMetricsUserIDStore()
+	metricsUserId, err := metricsUserIdStore.GetUserID()
+	if err != nil {
+		return stacktrace.Propagate(err, "An error occurred getting metrics user id")
+	}
+
+	gitHubIssueURL, err := getGithubIssueURL(selectedFeedbackType, metricsUserId, userMsg)
+	if err != nil {
+		return stacktrace.Propagate(err, "An error occurred getting Github Issue URL")
+	}
+
+	if selectedDestinationType == githubFlagKey {
+		if err := multi_os_command_executor.OpenFile(gitHubIssueURL); err != nil {
+			return stacktrace.Propagate(err, "An error occurred while opening the Kurtosis Github issue page")
+		}
+		return nil
+	}
+
+	if selectedDestinationType == emailFlagKey {
+		emailLinkWithSubjectAndBody, err := getEmailLinkWithSubjectAndBody(userMsg, selectedFeedbackType)
+		if err != nil {
+			return stacktrace.Propagate(err, "An error occurred getting email link with subject and body")
+		}
+
+		if err := multi_os_command_executor.OpenFile(emailLinkWithSubjectAndBody); err != nil {
+			return stacktrace.Propagate(err, "An error occurred while opening the feedback email link")
+		}
+		return nil
+	}
+
+	if selectedDestinationType == calendlyFlagKey {
+		if err := multi_os_command_executor.OpenFile(user_support_constants.KurtosisOnBoardCalendlyUrl); err != nil {
+			return stacktrace.Propagate(err, "An error occurred while opening the Calendly email link")
+		}
+	}
+
+	//The following message is printed if destination is not set
+	spotlightMessagePrinter := output_printers.GetSpotlightMessagePrinter()
+	spotlightMessagePrinter.Print(feedbackMsgTitle)
+
+	fmt.Printf(
+		feedbackMsg,
+		termlink.ColorLink(githubLinkText, gitHubIssueURL, greenColorStr),
+		termlink.ColorLink(emailLinkText, user_support_constants.FeedbackEmailLink, greenColorStr),
+		termlink.ColorLink(onboardingLinkText, user_support_constants.KurtosisOnBoardCalendlyUrl, greenColorStr),
+	)
+	return nil
+}
+
+func getUserMsgFromArgs(args *args.ParsedArgs) (string, error) {
 	userMsg, err := args.GetNonGreedyArg(userMsgArgKey)
 	if err != nil {
-		return stacktrace.Propagate(err, "An error occurred getting the user message argument using flag key '%v'", userMsgArgKey)
+		return "", stacktrace.Propagate(
+			err,
+			"An error occurred getting the user message argument using flag key '%v'",
+			userMsgArgKey,
+		)
 	}
 	if userMsg == userMsgArgDefaultValue {
-		doesUserFillMsg = false
 		userMsg = ""
 	}
 	userEncodedMsg := &url.URL{
@@ -121,78 +262,105 @@ func run(_ context.Context, flags *flags.ParsedFlags, args *args.ParsedArgs) err
 		RawFragment: "",
 	}
 
-	metricsUserIdStore := metrics_user_id_store.GetMetricsUserIDStore()
-	metricsUserId, err := metricsUserIdStore.GetUserID()
-	if err != nil {
-		return stacktrace.Propagate(err, "An error occurred getting metrics user id")
+	return userEncodedMsg.String(), nil
+}
+
+func getEmailLinkWithSubjectAndBody(prefilledBody string, selectedFeedbackType string) (string, error) {
+	var prefilledSubject string
+	switch selectedFeedbackType {
+	case bugFeedbackFlagKey:
+		prefilledSubject = bugEmailSubjectPrefix
+	case featureRequestFeedbackFlagKey:
+		prefilledSubject = featureRequestSubjectPrefix
+	case docsFeedbackFlagKey:
+		prefilledSubject = docsSubjectPrefix
+	default:
+		prefilledSubject = ""
 	}
 
-	isEmailFlagActivated, err = flags.GetBool(emailFlagKey)
-	if err != nil {
-		return stacktrace.Propagate(err, "Expected a boolean flag with key '%v' but none was found; this is an error in Kurtosis!", emailFlagKey)
-	}
-
-	emailLink := fmt.Sprintf("%s?body=%s", user_support_constants.FeedbackEmailLink, userEncodedMsg)
-
-	isGithubFlagActivated, err = flags.GetBool(githubFlagKey)
-	if err != nil {
-		return stacktrace.Propagate(err, "Expected a boolean flag with key '%v' but none was found; this is an error in Kurtosis!", githubFlagKey)
-	}
-
-	gitHubIssueURL := fmt.Sprintf(
-		"%s?version=%v&metrics-user-id=%v&description=%s&background-and-motivation=%s",
-		user_support_constants.GithubIssuesUrl,
-		kurtosis_version.KurtosisVersion,
-		metricsUserId,
-		userEncodedMsg,
-		userEncodedMsg,
+	emailLink := fmt.Sprintf(
+		"%s?subject=%s&body=%s",
+		user_support_constants.FeedbackEmailLink,
+		prefilledSubject,
+		prefilledBody,
 	)
 
-	isCalendlyFlagActivated, err = flags.GetBool(calendlyFlagKey)
-	if err != nil {
-		return stacktrace.Propagate(err, "Expected a boolean flag with key '%v' but none was found; this is an error in Kurtosis!", calendlyFlagKey)
-	}
-
-	if isEmailFlagActivated || (doesUserFillMsg && !isGithubFlagActivated) {
-		if err := multi_os_command_executor.OpenFile(emailLink); err != nil {
-			return stacktrace.Propagate(err, "An error occurred while opening the feedback email link")
-		}
-		return nil
-	}
-
-	if isGithubFlagActivated {
-		if err := multi_os_command_executor.OpenFile(gitHubIssueURL); err != nil {
-			return stacktrace.Propagate(err, "An error occurred while opening the Kurtosis Github issue page")
-		}
-		return nil
-	}
-
-	if isCalendlyFlagActivated {
-		if err := multi_os_command_executor.OpenFile(user_support_constants.KurtosisOnBoardCalendlyUrl); err != nil {
-			return stacktrace.Propagate(err, "An error occurred while opening the Calendly email link")
-		}
-	}
-
-	spotlightMessagePrinter := output_printers.GetSpotlightMessagePrinter()
-	spotlightMessagePrinter.Print(feedbackMsgTitle)
-
-	fmt.Printf(
-		feedbackMsg,
-		termlink.ColorLink(githubLinkText, gitHubIssueURL, greenColorStr),
-		termlink.ColorLink(emailLinkText, emailLink, greenColorStr),
-		termlink.ColorLink(onboardingLinkText, user_support_constants.KurtosisOnBoardCalendlyUrl, greenColorStr),
-	)
-	return nil
+	return emailLink, nil
 }
 
 func validateUserMsgArg(_ context.Context, _ *flags.ParsedFlags, args *args.ParsedArgs) error {
 	userMsgArg, err := args.GetNonGreedyArg(userMsgArgKey)
 	if err != nil {
-		return stacktrace.Propagate(err, "An error occurred getting the user message arguments using flag key '%v'", userMsgArgKey)
+		return stacktrace.Propagate(
+			err,
+			"An error occurred getting the user message arguments using flag key '%v'",
+			userMsgArgKey,
+		)
 	}
 
 	if userMsgArg == "" {
 		return stacktrace.Propagate(err, "Error validating the user message argument, it can be an empty string")
 	}
 	return nil
+}
+
+func getGithubIssueURL(
+	selectedFeedbackType string,
+	metricsUserId string,
+	userEncodedMsgStr string,
+) (string, error) {
+
+	var gitHubIssueBaseUrl string
+	switch selectedFeedbackType {
+	case bugFeedbackFlagKey:
+		gitHubIssueBaseUrl = user_support_constants.GithubBugIssueUrl
+	case featureRequestFeedbackFlagKey:
+		gitHubIssueBaseUrl = user_support_constants.GithubFeatureRequestIssueUrl
+	case docsFeedbackFlagKey:
+		gitHubIssueBaseUrl = user_support_constants.GithubDocsIssueUrl
+	case notFeedbackTypeSelected:
+		gitHubIssueBaseUrl = user_support_constants.GithubChooseNewIssuesUrl
+	default:
+		return "", stacktrace.NewError(
+			"An error occurred while setting the Github URL, expected "+
+				"to receive a know feedback type but '%v' was received instead; this is a bug in Kurtosis",
+			selectedFeedbackType,
+		)
+	}
+
+	gitHubIssueURL := fmt.Sprintf(
+		"%s&version=%v&metrics-user-id=%v&description=%s&background-and-motivation=%s",
+		gitHubIssueBaseUrl,
+		kurtosis_version.KurtosisVersion,
+		metricsUserId,
+		userEncodedMsgStr,
+		userEncodedMsgStr,
+	)
+
+	return gitHubIssueURL, nil
+}
+
+//TODO we could add this to the command framework
+func validateMutuallyExclusiveBooleanFlagsAndGetSelectedKey(flags *flags.ParsedFlags, flagKeys []string) (string, error) {
+	anyPreviousFlagSet := false
+	selectedKey := ""
+	for _, flagKey := range flagKeys {
+		flagValue, err := flags.GetBool(flagKey)
+		if err != nil {
+			return "", stacktrace.Propagate(
+				err,
+				"Expected a boolean flag with key '%v' but none was found; this is an error in Kurtosis!",
+				flagValue,
+			)
+		}
+		if flagValue && anyPreviousFlagSet {
+			flagKeysStr := strings.Join(flagKeys, flagKeysStrSeparator)
+			return "", stacktrace.NewError("Flags '%s' are mutually exclusive, you have to pass only one of them", flagKeysStr)
+		}
+		if flagValue {
+			selectedKey = flagKey
+			anyPreviousFlagSet = true
+		}
+	}
+	return selectedKey, nil
 }
