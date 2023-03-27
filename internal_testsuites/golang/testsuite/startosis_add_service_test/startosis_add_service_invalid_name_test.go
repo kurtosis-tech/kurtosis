5717ce45ff04960844dc4431bbbd461043b42ad3
Gyanendra Mishra <anomaly.the@gmail.com>
2023-03-27 12:33:44 +0100

    some clenaup

--- internal_testsuites/golang/testsuite/startosis_add_service_test/startosis_add_service_invalid_name_test.go
+++ internal_testsuites/golang/testsuite/startosis_add_service_test/startosis_add_service_invalid_name_test.go
@@ -2,6 +2,7 @@ package startosis_add_service_test
 
 import (
 	"context"
+	"fmt"
 	"github.com/kurtosis-tech/kurtosis-cli/golang_internal_testsuite/test_helpers"
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/require"
@@ -9,8 +10,9 @@ import (
 )
 
 const (
-	addServiceWithInvalidServiceName = "invalid-service-name"
-	addServiceInvalidServiceNameTest = `
+	invalidServiceName                       = "this;.is:invalid"
+	addServiceWithInvalidServiceNameTestName = "add-service-invalid-service-name"
+	addServiceInvalidServiceNameTestScript   = `
 CONTAINER_IMAGE = "kurtosistech/example-datastore-server"
 GRPC_PORT = 1323
 
@@ -23,7 +25,7 @@ def run(plan):
 			"grpc": PortSpec(number = GRPC_PORT, transport_protocol = "TCP")
 		}
 	)
-	plan.add_service(service_name = "this;.is:invalid", config = config)
+	plan.add_service(service_name = "%s", config = config)
 `
 )
 
@@ -31,25 +33,25 @@ func TestAddServiceWithInvalidServiceNameFailsValidation(t *testing.T) {
 	ctx := context.Background()
 
 	// ------------------------------------- ENGINE SETUP ----------------------------------------------
-	enclaveCtx, _, destroyEnclaveFunc, err := test_helpers.CreateEnclave(t, ctx, addServiceWithInvalidServiceName, isPartitioningEnabled)
+	enclaveCtx, _, destroyEnclaveFunc, err := test_helpers.CreateEnclave(t, ctx, addServiceWithInvalidServiceNameTestName, isPartitioningEnabled)
 	require.NoError(t, err, "An error occurred creating an enclave")
 	defer func() {
 		destroyErr := destroyEnclaveFunc()
 		if destroyErr != nil {
 			logrus.Errorf("Error destroying enclave at the end of integration test '%s'",
-				addServiceWithInvalidServiceName)
+				addServiceWithInvalidServiceNameTestName)
 		}
 	}()
 
 	// ------------------------------------- TEST RUN ----------------------------------------------
 
 	logrus.Infof("Executing Starlark script...")
-	logrus.Debugf("Starlark script contents: \n%v", addServiceInvalidServiceNameTest)
+	logrus.Debugf("Starlark script contents: \n%v", fmt.Sprintf(addServiceInvalidServiceNameTestScript, invalidServiceName))
 
-	runResult, err := test_helpers.RunScriptWithDefaultConfig(ctx, enclaveCtx, addServiceInvalidServiceNameTest)
+	runResult, err := test_helpers.RunScriptWithDefaultConfig(ctx, enclaveCtx, fmt.Sprintf(addServiceInvalidServiceNameTestScript, invalidServiceName))
 	require.NoError(t, err, "Unexpected error executing Starlark script")
 
 	require.Nil(t, runResult.InterpretationError, "Unexpected interpretation error.")
 	require.NotEmpty(t, runResult.ValidationErrors, "Unexpected validation error")
-	require.Contains(t, runResult.ValidationErrors[0].ErrorMessage, "Service name 'this;.is:invalid' is invalid as it contains disallowed characters. Service names can only contain characters 'a-z', 'A-Z', '0-9', '-' & '_'")
+	require.Contains(t, runResult.ValidationErrors[0].ErrorMessage, fmt.Sprintf("Service name '%s' is invalid as it contains disallowed characters. Service names can only contain characters 'a-z', 'A-Z', '0-9', '-' & '_'", invalidServiceName))
 }
