1d367f1a29882d862e517380ecb9a83533fc7666
Gyanendra Mishra <anomaly.the@gmail.com>
2023-03-28 11:25:07 +0100

    this should work

--- cli/cli/commands/version/version.go
+++ cli/cli/commands/version/version.go
@@ -1,10 +1,18 @@
 package version
 
 import (
+	"context"
 	"github.com/kurtosis-tech/kurtosis/cli/cli/command_str_consts"
-	"github.com/kurtosis-tech/kurtosis/cli/cli/out"
+	"github.com/kurtosis-tech/kurtosis/cli/cli/helpers/engine_manager"
+	"github.com/kurtosis-tech/kurtosis/cli/cli/helpers/output_printers"
 	"github.com/kurtosis-tech/kurtosis/kurtosis_version"
+	"github.com/kurtosis-tech/stacktrace"
 	"github.com/spf13/cobra"
+)
+
+const (
+	cliVersionKey           = "CLI Version"
+	runningEngineVersionKey = "Running Engine Version"
 )
 
 // VersionCmd Suppressing exhaustruct requirement because this struct has ~40 properties
@@ -20,6 +28,25 @@ func init() {
 }
 
 func run(cmd *cobra.Command, args []string) error {
-	out.PrintOutLn(kurtosis_version.KurtosisVersion)
+	keyValuePrinter := output_printers.NewKeyValuePrinter()
+	keyValuePrinter.AddPair(cliVersionKey, kurtosis_version.KurtosisVersion)
+
+	ctx := context.Background()
+
+	engineManager, err := engine_manager.NewEngineManager(ctx)
+	if err != nil {
+		return stacktrace.Propagate(err, "An error occurred creating an engine manager")
+	}
+
+	status, _, maybeEngineVersion, err := engineManager.GetEngineStatus(ctx)
+	if err != nil {
+		return stacktrace.Propagate(err, "An error occurred getting the Kurtosis engine status")
+	}
+	if status == engine_manager.EngineStatus_Running {
+		keyValuePrinter.AddPair(runningEngineVersionKey, maybeEngineVersion)
+	}
+
+	keyValuePrinter.Print()
+
 	return nil
 }
