d6e60209bf4d91867f3c12b9fb4bf2976262aa7a
Victor Colombo <victorcolombo@users.noreply.github.com>
2023-04-06 18:25:04 +0200

    Timeout even if recipe is executing

--- core/server/api_container/server/startosis_engine/kurtosis_instruction/shared_helpers/service_helpers.go
+++ core/server/api_container/server/startosis_engine/kurtosis_instruction/shared_helpers/service_helpers.go
@@ -2,7 +2,6 @@ package shared_helpers
 
 import (
 	"context"
-	"github.com/cenkalti/backoff/v4"
 	"github.com/kurtosis-tech/kurtosis/container-engine-lib/lib/backend_interface/objects/service"
 	"github.com/kurtosis-tech/kurtosis/core/server/api_container/server/service_network"
 	"github.com/kurtosis-tech/kurtosis/core/server/api_container/server/startosis_engine/kurtosis_instruction/assert"
@@ -25,47 +24,63 @@ func ExecuteServiceAssertionWithRecipe(
 	interval time.Duration,
 	timeout time.Duration,
 ) (map[string]starlark.Comparable, int, error) {
-	var requestErr error
+	var recipeErr error
 	var assertErr error
 	tries := 0
-	timedOut := false
 	lastResult := map[string]starlark.Comparable{}
-	startTime := time.Now()
-
-	backoffObj := backoff.NewConstantBackOff(interval)
+	deadline := time.Now().Add(timeout)
+	contextWithDeadline, cancelContext := context.WithDeadline(ctx, deadline)
+	defer cancelContext()
+	timeoutChan := time.After(timeout)
+	tickChan := time.Tick(interval)
+	instantFirstRunChan := make(chan bool, 2)
+	instantFirstRunChan <- true
 
 	//TODO check if we can refactor this portion in order to use the time.Ticker(backoffDuration) pattern here:
 	for {
 		tries += 1
-		backoffDuration := backoffObj.NextBackOff()
-		if backoffDuration == backoff.Stop || time.Since(startTime) > timeout {
-			timedOut = true
-			break
+		select {
+		case <-timeoutChan:
+			if recipeErr != nil {
+				return lastResult, tries, stacktrace.NewError("Recipe execution timed-out waiting for the recipe execution to become valid on service '%v'. Tried '%v' times. Last recipe execution error was:\n '$%v'\n", serviceName, tries, recipeErr)
+			}
+			if assertErr != nil {
+				return lastResult, tries, stacktrace.NewError("Recipe execution timed-out waiting for the recipe execution to become valid on service '%v'. Tried '%v' times. Last assertion execution error was:\n '$%v'\n", serviceName, tries, assertErr)
+			}
+			return lastResult, tries, stacktrace.NewError("Recipe execution timed-out waiting for recipe execution on service '%v'. Tries '%v'", serviceName, tries)
+		case <-instantFirstRunChan:
+			lastResult, recipeErr, assertErr = runRecipe(contextWithDeadline, serviceNetwork, runtimeValueStore, serviceName, recipe, valueField, assertion, target)
+			if recipeErr == nil && assertErr == nil {
+				return lastResult, tries, nil
+			}
+		case <-tickChan:
+			lastResult, recipeErr, assertErr = runRecipe(contextWithDeadline, serviceNetwork, runtimeValueStore, serviceName, recipe, valueField, assertion, target)
+			if recipeErr == nil && assertErr == nil {
+				return lastResult, tries, nil
+			}
 		}
-		lastResult, requestErr = recipe.Execute(ctx, serviceNetwork, runtimeValueStore, serviceName)
-		if requestErr != nil {
-			time.Sleep(backoffDuration)
-			continue
-		}
-		value, found := lastResult[valueField]
-		if !found {
-			return lastResult, tries, stacktrace.NewError("Error extracting value from key '%v'", valueField)
-		}
-		assertErr = assert.Assert(value, assertion, target)
-		if assertErr == nil {
-			break
-		}
-		time.Sleep(backoffDuration)
 	}
-	if timedOut {
-		return lastResult, tries, stacktrace.NewError("Recipe execution timed-out waiting for the assertion to become valid on service '%v'. Waited for '%v'. Last assertion error was: \n%v", serviceName, time.Since(startTime), assertErr)
-	}
-	if requestErr != nil {
-		return lastResult, tries, stacktrace.Propagate(requestErr, "Error executing recipe on service '%v'", serviceName)
-	}
-	if assertErr != nil {
-		return lastResult, tries, stacktrace.Propagate(assertErr, "Error asserting recipe on service '%v'", serviceName)
-	}
+}
 
-	return lastResult, tries, nil
+func runRecipe(ctx context.Context,
+	serviceNetwork service_network.ServiceNetwork,
+	runtimeValueStore *runtime_value_store.RuntimeValueStore,
+	serviceName service.ServiceName,
+	recipe recipe.Recipe,
+	valueField string,
+	assertion string,
+	target starlark.Comparable) (map[string]starlark.Comparable, error, error) {
+	lastResult, recipeErr := recipe.Execute(ctx, serviceNetwork, runtimeValueStore, serviceName)
+	if recipeErr != nil {
+		return lastResult, recipeErr, nil
+	}
+	value, found := lastResult[valueField]
+	if !found {
+		return lastResult, stacktrace.NewError("Error extracting value from key '%v'. This is a bug in Kurtosis.", valueField), nil
+	}
+	assertErr := assert.Assert(value, assertion, target)
+	if assertErr != nil {
+		return lastResult, nil, assertErr
+	}
+	return lastResult, nil, nil
 }
