b418310d8278eb20e1ef265f090873a41ee3dd12
Leandro Poroli <leandro.poroli@kurtosistech.com>
2023-04-12 18:10:45 -0400

    Added wait in some tests

--- core/server/api_container/server/startosis_engine/kurtosis_types/service_test.go
+++ core/server/api_container/server/startosis_engine/kurtosis_types/service_test.go
@@ -15,19 +15,36 @@ const (
 	httpApplicationProtocol  = "http"
 	emptyApplicationProtocol = ""
 	serviceNameTestValue     = starlark.String("test-service")
+	emptyPortSpecWaitTimeout = ""
+	portSpecWaitTimeout      = "2s"
 )
 
 func TestService_StringRepresentation(t *testing.T) {
 	service, err := createTestServiceType()
 	require.Nil(t, err)
 	expectedStr := `Service(hostname = "datastore-1", ip_address = "{{kurtosis:service_name.ip_address}}", name = "test-service", ports = {"grpc": PortSpec(number=123, transport_protocol="TCP")})`
-	require.Equal(t, expectedStr, service.String())
+	serviceStr := service.String()
+	require.Equal(t, expectedStr, serviceStr)
 }
 
 func TestService_StringRepresentationWithApplicationProtocol(t *testing.T) {
 	service, err := createTestServiceTypeWithApplicationProtocol()
 	require.Nil(t, err)
 	expectedStr := `Service(hostname = "datastore-1", ip_address = "{{kurtosis:service_name.ip_address}}", name = "test-service", ports = {"grpc": PortSpec(number=123, transport_protocol="TCP", application_protocol="http")})`
+	require.Equal(t, expectedStr, service.String())
+}
+
+func TestService_StringRepresentationWithWait(t *testing.T) {
+	service, err := createTestServiceTypeWithWait()
+	require.Nil(t, err)
+	expectedStr := `Service(hostname = "datastore-1", ip_address = "{{kurtosis:service_name.ip_address}}", name = "test-service", ports = {"grpc": PortSpec(number=123, transport_protocol="TCP", wait="2s")})`
+	require.Equal(t, expectedStr, service.String())
+}
+
+func TestService_StringRepresentationWithApplicationProtocolAndWait(t *testing.T) {
+	service, err := createTestServiceTypeWithApplicationProtocolAndWait()
+	require.Nil(t, err)
+	expectedStr := `Service(hostname = "datastore-1", ip_address = "{{kurtosis:service_name.ip_address}}", name = "test-service", ports = {"grpc": PortSpec(number=123, transport_protocol="TCP", application_protocol="http", wait="2s")})`
 	require.Equal(t, expectedStr, service.String())
 }
 
@@ -84,7 +101,7 @@ func TestService_TestAttrNames(t *testing.T) {
 
 func createTestServiceType() (*Service, error) {
 	ports := starlark.NewDict(1)
-	portSpec, err := port_spec.CreatePortSpec(123, kurtosis_core_rpc_api_bindings.Port_TCP, emptyApplicationProtocol)
+	portSpec, err := port_spec.CreatePortSpec(123, kurtosis_core_rpc_api_bindings.Port_TCP, emptyApplicationProtocol, emptyPortSpecWaitTimeout)
 	if err != nil {
 		return nil, err
 	}
@@ -97,7 +114,33 @@ func createTestServiceType() (*Service, error) {
 
 func createTestServiceTypeWithApplicationProtocol() (*Service, error) {
 	ports := starlark.NewDict(1)
-	portSpec, err := port_spec.CreatePortSpec(123, kurtosis_core_rpc_api_bindings.Port_TCP, httpApplicationProtocol)
+	portSpec, err := port_spec.CreatePortSpec(123, kurtosis_core_rpc_api_bindings.Port_TCP, httpApplicationProtocol, emptyPortSpecWaitTimeout)
+	if err != nil {
+		return nil, err
+	}
+	if err := ports.SetKey(starlark.String("grpc"), portSpec); err != nil {
+		return nil, err
+	}
+	service := NewService(serviceNameTestValue, hostnameTestValue, ipAddressTestValue, ports)
+	return service, nil
+}
+
+func createTestServiceTypeWithWait() (*Service, error) {
+	ports := starlark.NewDict(1)
+	portSpec, err := port_spec.CreatePortSpec(123, kurtosis_core_rpc_api_bindings.Port_TCP, emptyApplicationProtocol, portSpecWaitTimeout)
+	if err != nil {
+		return nil, err
+	}
+	if err := ports.SetKey(starlark.String("grpc"), portSpec); err != nil {
+		return nil, err
+	}
+	service := NewService(serviceNameTestValue, hostnameTestValue, ipAddressTestValue, ports)
+	return service, nil
+}
+
+func createTestServiceTypeWithApplicationProtocolAndWait() (*Service, error) {
+	ports := starlark.NewDict(1)
+	portSpec, err := port_spec.CreatePortSpec(123, kurtosis_core_rpc_api_bindings.Port_TCP, httpApplicationProtocol, portSpecWaitTimeout)
 	if err != nil {
 		return nil, err
 	}
