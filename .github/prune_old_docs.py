a3703850176837ee03ef55926b8d4d0df6bb58e4
Gyanendra Mishra <anomaly.the@gmail.com>
2023-04-25 13:00:52 +0100

    ci: limit the number of versions of current minor version of docs kept & historical versions kept (#552)
    ## Description:
    Enforces limits using
    
    HISTORICAL_VERSIONS_TO_KEEP = 5
    MOST_TO_KEEP_OF_LATEST_VERSION = 5
    
    If they're set to -1 then we keep all of the current version.
    
    ## Is this change user facing?
    YES

--- .github/prune_old_docs.py
+++ .github/prune_old_docs.py
@@ -11,21 +11,33 @@ VERSIONS_FILE_NAME = "versions.json"
 
 Version = namedtuple("Version", "major minor patch")
 
+HISTORICAL_VERSIONS_TO_KEEP = 5
+MOST_TO_KEEP_OF_LATEST_VERSION = 5
 
 def main():
     versions_to_keep = []
     versioned_docs_to_delete = []
     versioned_sidebars_to_delete = []
     with open(VERSIONS_FILE_NAME, "r") as available_versions:
-        available_versions_json = json.loads(available_versions.read())
-        current_version = str_to_version(available_versions_json[0])
+        available_versions_list = json.loads(available_versions.read())
+        current_version = str_to_version(available_versions_list[0])
         latest_version = current_version
-        for version_str in available_versions_json:
+        historical_versions_kept = 0
+        current_versions_kept = 0
+        for version_str in available_versions_list:
             version = str_to_version(version_str)
             # if the version being processed is the same as the latest major and minor version do nothing
             if version.major == latest_version.major and version.minor == latest_version.minor:
+                if MOST_TO_KEEP_OF_LATEST_VERSION > 0 and current_versions_kept >= MOST_TO_KEEP_OF_LATEST_VERSION:
+                    continue
+                current_versions_kept += 1
                 versions_to_keep.append(version_str)
                 continue
+
+            if HISTORICAL_VERSIONS_TO_KEEP > 0 and historical_versions_kept >= HISTORICAL_VERSIONS_TO_KEEP:
+                break
+
+            historical_versions_kept += 1
 
             # as we go down the list if the minor version changes then we are on the highest patch with the minor
             # version so, we keep it
@@ -34,10 +46,12 @@ def main():
                 current_version = version
                 continue
 
-            # if the minor version is the same as the previous one then we are on a lower patch
-            if version.minor == current_version.minor:
-                versioned_docs_to_delete.append(f"versioned_docs/version-{version_str}")
-                versioned_sidebars_to_delete.append(f"versioned_sidebars/version-{version_str}-sidebars.json")
+    assert len(versions_to_keep) <= MOST_TO_KEEP_OF_LATEST_VERSION + HISTORICAL_VERSIONS_TO_KEEP
+
+    versions_to_delete = set(available_versions_list).difference(set(versions_to_keep))
+    for version in versions_to_delete:
+        versioned_docs_to_delete.append(f"versioned_docs/version-{version}")
+        versioned_sidebars_to_delete.append(f"versioned_sidebars/version-{version}-sidebars.json")
 
     for docs_folder_to_delete in versioned_docs_to_delete:
         shutil.rmtree(docs_folder_to_delete)
