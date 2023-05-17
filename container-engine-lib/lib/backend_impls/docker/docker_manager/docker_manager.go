19ec18e4174555b51c917e13f34f7275c6ddab1a
Gyanendra Mishra <anomaly.the@gmail.com>
2023-05-17 21:58:48 +0100

    fix: set MTU to 1440 to fix GitPod networking (#627)
    ## Description:
    Working off of
    https://www.gitpod.io/docs/configure#no-internet-access-over-https-port-443-on-docker-containers-with-custom-network
    to fix networking
    
    ## Is this change user facing?
    NO

--- container-engine-lib/lib/backend_impls/docker/docker_manager/docker_manager.go
+++ container-engine-lib/lib/backend_impls/docker/docker_manager/docker_manager.go
@@ -185,8 +185,10 @@ func (manager *DockerManager) CreateNetwork(context context.Context, name string
 		Ingress:    false,
 		ConfigOnly: false,
 		ConfigFrom: nil,
-		Options:    nil,
-		Labels:     labels,
+		Options: map[string]string{
+			"com.docker.network.driver.mtu": "1440",
+		},
+		Labels: labels,
 	})
 	if err != nil {
 		return "", stacktrace.Propagate(err, "Failed to create network %s with subnet %s", name, subnetMask)
