072c65d0ca838d3c1d940ac53619af59cfb37211
preetrawal <15133250+Peeeekay@users.noreply.github.com>
2023-06-06 13:05:48 -0400

    added a check

--- container-engine-lib/lib/backend_impls/kubernetes/kubernetes_kurtosis_backend/user_services_functions/start_user_services_test.go
+++ container-engine-lib/lib/backend_impls/kubernetes/kubernetes_kurtosis_backend/user_services_functions/start_user_services_test.go
@@ -1,7 +1,9 @@
 package user_services_functions
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/require"
+	apiv1 "k8s.io/api/core/v1"
 	"testing"
 )
 
@@ -10,4 +12,13 @@ func TestConvertMemoryAllocationToBytesReturnsCorrectValue(t *testing.T) {
 
 	memoryAllocationBytes := convertMegabytesToBytes(memoryAllocationMegabytes)
 	require.Equal(t, uint64(400000000), memoryAllocationBytes)
+}
+
+func Test_checkIfResourcesAreSetProperly(t *testing.T) {
+	resourceRequst := uint64(100)
+	resourceLimit := uint64(99)
+
+	err := checkIfResourcesAreSetProperly(resourceRequst, resourceLimit, apiv1.ResourceCPU)
+	require.NotNil(t, err)
+	require.ErrorContains(t, err, fmt.Sprintf("Minimum Resource Requirement for the container is set higher than Maximum resource requirement for resource: %v", apiv1.ResourceCPU))
 }
