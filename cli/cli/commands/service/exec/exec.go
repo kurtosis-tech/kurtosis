ece49371552f0f9c03fe73b879363c0599f65106
Guillaume Bouvignies <guillaume.bouvignies@kurtosistech.com>
2023-06-08 14:23:35 +0200

    feat: Add `kurtosis service exec` command (#690)
    ## Description:
    Users can now run `kurtosis service exec <ENCLAVE_ID> <SERVICE_ID> 'any
    sh command'`. The command will be executed inside the designated user
    service and the result will be printed in the CLI output.
    
    The main different with `kurtosis service shell <ENCLAVE_ID>
    <SERVICE_ID> --exec 'any sh command'` is that the former expects a zero
    exit code for the command otherwise throws an exception, whereas the
    latter returns gracefully whatever the exit code of the command was.
    
    In short, `kurtosis service shell` is designed for interactive sessions
    and will always return gracefully as long as the session terminated is
    ended successfully. `kurtosis service exec` provides a way to run a
    single command and returns immediately, taking the status of the command
    into account.
    
    Fixes #688 
    
    ## Is this change user facing?
    YES
    <!-- If yes, please add the "user facing" label to the PR -->
    <!-- If yes, don't forget to include docs changes where relevant -->
    
    ## References (if applicable):
    <!-- Add relevant Github Issues, Discord threads, or other helpful
    information. -->

---
+++ cli/cli/commands/service/exec/exec.go
@@ -0,0 +1,140 @@
+package exec
+
+import (
+	"context"
+	"fmt"
+	"github.com/kurtosis-tech/kurtosis/api/golang/engine/kurtosis_engine_rpc_api_bindings"
+	"github.com/kurtosis-tech/kurtosis/api/golang/engine/lib/kurtosis_context"
+	"github.com/kurtosis-tech/kurtosis/cli/cli/command_framework/highlevel/enclave_id_arg"
+	"github.com/kurtosis-tech/kurtosis/cli/cli/command_framework/highlevel/engine_consuming_kurtosis_command"
+	"github.com/kurtosis-tech/kurtosis/cli/cli/command_framework/highlevel/service_identifier_arg"
+	"github.com/kurtosis-tech/kurtosis/cli/cli/command_framework/lowlevel/args"
+	"github.com/kurtosis-tech/kurtosis/cli/cli/command_framework/lowlevel/flags"
+	"github.com/kurtosis-tech/kurtosis/cli/cli/command_str_consts"
+	"github.com/kurtosis-tech/kurtosis/cli/cli/out"
+	"github.com/kurtosis-tech/kurtosis/container-engine-lib/lib/backend_interface"
+	"github.com/kurtosis-tech/kurtosis/container-engine-lib/lib/backend_interface/objects/enclave"
+	"github.com/kurtosis-tech/kurtosis/container-engine-lib/lib/backend_interface/objects/service"
+	metrics_client "github.com/kurtosis-tech/metrics-library/golang/lib/client"
+	"github.com/kurtosis-tech/stacktrace"
+)
+
+const (
+	enclaveIdentifierArgKey = "enclave"
+	isEnclaveIdArgOptional  = false
+	isEnclaveIdArgGreedy    = false
+
+	serviceIdentifierArgKey  = "service"
+	isServiceGuidArgOptional = false
+	isServiceGuidArgGreedy   = false
+
+	execCommandArgKey        = "command"
+	isExecCommandArgOptional = false
+	isExecCommandArgGreedy   = false
+
+	kurtosisBackendCtxKey = "kurtosis-backend"
+	engineClientCtxKey    = "engine-client"
+
+	binShCommand     = "sh"
+	binShCommandFlag = "-c"
+)
+
+var ServiceShellCmd = &engine_consuming_kurtosis_command.EngineConsumingKurtosisCommand{
+	CommandStr:                command_str_consts.ServiceExecCmdStr,
+	ShortDescription:          "Executes a command in a service",
+	LongDescription:           "Execute a command in a service. Note if the command being run is multiple words you should wrap it in quotes",
+	KurtosisBackendContextKey: kurtosisBackendCtxKey,
+	EngineClientContextKey:    engineClientCtxKey,
+	Flags:                     []*flags.FlagConfig{},
+	Args: []*args.ArgConfig{
+		enclave_id_arg.NewEnclaveIdentifierArg(
+			enclaveIdentifierArgKey,
+			engineClientCtxKey,
+			isEnclaveIdArgOptional,
+			isEnclaveIdArgGreedy,
+		),
+		service_identifier_arg.NewServiceIdentifierArg(
+			serviceIdentifierArgKey,
+			isServiceGuidArgOptional,
+			isServiceGuidArgGreedy,
+		),
+		{
+			Key:        execCommandArgKey,
+			IsOptional: isExecCommandArgOptional,
+			IsGreedy:   isExecCommandArgGreedy,
+		},
+	},
+	RunFunc: run,
+}
+
+func run(
+	ctx context.Context,
+	kurtosisBackend backend_interface.KurtosisBackend,
+	_ kurtosis_engine_rpc_api_bindings.EngineServiceClient,
+	_ metrics_client.MetricsClient,
+	flags *flags.ParsedFlags,
+	args *args.ParsedArgs,
+) error {
+	enclaveIdentifier, err := args.GetNonGreedyArg(enclaveIdentifierArgKey)
+	if err != nil {
+		return stacktrace.Propagate(err, "An error occurred getting the enclave identifier using arg key '%v'", enclaveIdentifierArgKey)
+	}
+
+	serviceIdentifier, err := args.GetNonGreedyArg(serviceIdentifierArgKey)
+	if err != nil {
+		return stacktrace.Propagate(err, "An error occurred getting the service identifier using arg key '%v'", serviceIdentifierArgKey)
+	}
+
+	execCommandToRun, err := args.GetNonGreedyArg(execCommandArgKey)
+	if err != nil {
+		return stacktrace.Propagate(err, "An error occurred getting the exec command using arg key '%v'", execCommandArgKey)
+	}
+
+	kurtosisCtx, err := kurtosis_context.NewKurtosisContextFromLocalEngine()
+	if err != nil {
+		return stacktrace.Propagate(err, "An error occurred connecting to the local Kurtosis engine")
+	}
+
+	enclaveCtx, err := kurtosisCtx.GetEnclaveContext(ctx, enclaveIdentifier)
+	if err != nil {
+		return stacktrace.Propagate(err, "An error occurred while getting enclave context for enclave with identifier '%v' exists", enclaveIdentifier)
+	}
+
+	enclaveUuid := enclave.EnclaveUUID(enclaveCtx.GetEnclaveUuid())
+
+	serviceCtx, err := enclaveCtx.GetServiceContext(serviceIdentifier)
+	if err != nil {
+		return stacktrace.Propagate(err, "An error occurred while getting service context for service with identifier '%v'", serviceIdentifier)
+	}
+	serviceUuid := service.ServiceUUID(serviceCtx.GetServiceUUID())
+
+	results, resultErrors, err := kurtosisBackend.RunUserServiceExecCommands(ctx, enclaveUuid, map[service.ServiceUUID][]string{
+		serviceUuid: {
+			binShCommand,
+			binShCommandFlag,
+			execCommandToRun,
+		},
+	})
+
+	if err != nil {
+		return stacktrace.Propagate(err, "An error occurred executing command '%v' in user service with UUID '%v' in enclave '%v'",
+			execCommandToRun, serviceUuid, enclaveIdentifier)
+	}
+
+	if err, found := resultErrors[serviceUuid]; found {
+		return stacktrace.Propagate(err, "An error occurred executing command '%v' in user service with UUID '%v' in enclave '%v'",
+			execCommandToRun, serviceUuid, enclaveIdentifier)
+	}
+
+	successResult, found := results[serviceUuid]
+	if !found {
+		return stacktrace.NewError("The status of the command execution for '%s' in user service with UUID '%s' in enclave '%s' is unknown."+
+			" It wasn't returned neither as a success nor a failure. This is a bug in Kurtosis.", execCommandToRun, serviceUuid, enclaveIdentifier)
+	}
+
+	if successResult.GetExitCode() != 0 {
+		return stacktrace.NewError("The command was successfully executed but returned a non-zero exit code: '%d'. Output was:\n%v", successResult.GetExitCode(), successResult.GetOutput())
+	}
+	out.PrintOutLn(fmt.Sprintf("The command was successfully executed and returned '%d'. Output was:\n%v", successResult.GetExitCode(), successResult.GetOutput()))
+	return nil
+}
