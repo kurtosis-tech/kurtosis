6cbb0f6c5a90033b2f95f7c9ae04ae1b17957f49
Laurent Luce <laurentluce49@yahoo.com>
2023-06-08 21:11:47 -0400

    Disable start and stop service integration tests on k8s.

--- internal_testsuites/golang/testsuite/startosis_start_service_test/startosis_start_service_test.go
+++ internal_testsuites/golang/testsuite/startosis_start_service_test/startosis_start_service_test.go
@@ -1,3 +1,8 @@
+//go:build !minikube
+// +build !minikube
+
+// We don't run this test in Kubernetes because the gateway does not support start and stop services
+
 package startosis_stop_service_test
 
 import (
