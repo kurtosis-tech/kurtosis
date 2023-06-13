36f41881edeef4c236504173734ecf49f90fe418
preetrawal <15133250+Peeeekay@users.noreply.github.com>
2023-06-13 12:07:26 -0400

    updated test

--- internal_testsuites/golang/testsuite/startosis_run_sh_task_test/run_task_sh_task_test.go
+++ internal_testsuites/golang/testsuite/startosis_run_sh_task_test/run_task_sh_task_test.go
@@ -11,15 +11,15 @@ const (
 	runshTest     = "run-sh-test"
 	runshStarlark = `
 def run(plan):
-  result1 = plan.run_sh(run="echo kurtosis")
+  result1 = plan.run_sh(run="echo kurtosis", workdir="src")
   result2 = plan.run_sh(run="mkdir -p {0} && cd {0} && echo $(pwd)".format(result1["output"]))
-  plan.assert(result2["output"], "==", "/task/kurtosis\n")
+  plan.assert(result2["output"], "==", "/src/kurtosis\n")
 `
 )
 
 func TestStarlark_RunshTask(t *testing.T) {
 	ctx := context.Background()
 	runResult, _ := test_helpers.SetupSimpleEnclaveAndRunScript(t, ctx, runshTest, runshStarlark)
-	expectedOutput := "Command returned with exit code '0' and the following output:\n--------------------\nkurtosis\n\n--------------------\nCommand returned with exit code '0' and the following output:\n--------------------\n/task/kurtosis\n\n--------------------\nAssertion succeeded. Value is '\"/task/kurtosis\\n\"'.\n"
+	expectedOutput := "Command returned with exit code '0' and the following output:\n--------------------\nkurtosis\n\n--------------------\nCommand returned with exit code '0' and the following output:\n--------------------\n/src/kurtosis\n\n--------------------\nAssertion succeeded. Value is '\"/task/kurtosis\\n\"'.\n"
 	require.Equal(t, expectedOutput, string(runResult.RunOutput))
 }
