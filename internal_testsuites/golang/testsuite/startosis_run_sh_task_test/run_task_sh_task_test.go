5f40e3b6a8dff1b076c00db7f375b8306b25c44b
preetrawal <15133250+Peeeekay@users.noreply.github.com>
2023-06-13 23:21:41 -0400

    fixed the tests

--- internal_testsuites/golang/testsuite/startosis_run_sh_task_test/run_task_sh_task_test.go
+++ internal_testsuites/golang/testsuite/startosis_run_sh_task_test/run_task_sh_task_test.go
@@ -8,18 +8,29 @@ import (
 )
 
 const (
-	runshTest     = "run-sh-test"
-	runshStarlark = `
+	runshTest           = "run-sh-test"
+	runshStarlarkSimple = `
 def run(plan):
-  result1 = plan.run_sh(run="echo kurtosis")
-  result2 = plan.run_sh(run="mkdir -p {0} && cd {0} && echo $(pwd)".format(result1["output"]), workdir="src")
+  result1, _ = plan.run_sh(run="echo kurtosis")
+  result2, _ = plan.run_sh(run="mkdir -p {0} && cd {0} && echo $(pwd)".format(result1["output"]), workdir="src")
   plan.assert(result2["output"], "==", "/src/kurtosis\n")
 `
+	runshStarlarkFileArtifact = `def run(plan):
+  result, file_artifacts = plan.run_sh(run="echo kurtosis > tech.txt", store=["/task", "/task"])
+  result2, _ = plan.run_sh(run="cat ./task/tech.txt", files={"/src": file_artifacts[0]}, workdir="src")
+  plan.assert(result2["output"], "==", "kurtosis\n")`
 )
 
-func TestStarlark_RunshTask(t *testing.T) {
+func TestStarlark_RunshTaskSimple(t *testing.T) {
 	ctx := context.Background()
-	runResult, _ := test_helpers.SetupSimpleEnclaveAndRunScript(t, ctx, runshTest, runshStarlark)
+	runResult, _ := test_helpers.SetupSimpleEnclaveAndRunScript(t, ctx, runshTest, runshStarlarkSimple)
 	expectedOutput := "Command returned with exit code '0' and the following output:\n--------------------\nkurtosis\n\n--------------------\nCommand returned with exit code '0' and the following output:\n--------------------\n/src/kurtosis\n\n--------------------\nAssertion succeeded. Value is '\"/src/kurtosis\\n\"'.\n"
+	require.Equal(t, expectedOutput, string(runResult.RunOutput))
+}
+
+func TestStarlark_RunshTaskFileArtifact(t *testing.T) {
+	ctx := context.Background()
+	runResult, _ := test_helpers.SetupSimpleEnclaveAndRunScript(t, ctx, runshTest, runshStarlarkFileArtifact)
+	expectedOutput := "Command returned with exit code '0' and the following output:\n--------------------\nkurtosis\n\n--------------------\nCommand returned with exit code '0' and the following output:\n--------------------\nkurtosis\n\n--------------------\nAssertion succeeded. Value is '\"kurtosis\\n\"'.\n"
 	require.Equal(t, expectedOutput, string(runResult.RunOutput))
 }
