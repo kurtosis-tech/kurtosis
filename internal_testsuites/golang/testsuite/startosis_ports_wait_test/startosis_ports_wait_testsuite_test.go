9c0004346aedcea2fdba862e55e2ece12ac79bcc
Leandro Poroli <leandro.poroli@kurtosistech.com>
2023-06-15 15:47:18 -0300

    startosis_ports_wait package

---
+++ internal_testsuites/golang/testsuite/startosis_ports_wait_test/startosis_ports_wait_testsuite_test.go
@@ -0,0 +1,47 @@
+package startosis_request_wait_assert_test
+
+import (
+	"context"
+	"github.com/kurtosis-tech/kurtosis-cli/golang_internal_testsuite/test_helpers"
+	"github.com/kurtosis-tech/kurtosis/api/golang/core/lib/enclaves"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+const (
+	name                  = "startosis-ports-wait"
+	isPartitioningEnabled = false
+)
+
+type StartosisPortsWaitTestSuite struct {
+	suite.Suite
+	enclaveCtx         *enclaves.EnclaveContext
+	destroyEnclaveFunc func() error
+}
+
+func TestStartosisPortsWaitTestSuite(t *testing.T) {
+	suite.Run(t, new(StartosisPortsWaitTestSuite))
+}
+
+func (suite *StartosisPortsWaitTestSuite) SetupSuite() {
+	ctx := context.Background()
+	t := suite.T()
+	enclaveCtx, _, destroyEnclaveFunc, err := test_helpers.CreateEnclave(t, ctx, name, isPartitioningEnabled)
+	require.NoError(t, err, "An error occurred creating an enclave")
+	suite.enclaveCtx = enclaveCtx
+	suite.destroyEnclaveFunc = destroyEnclaveFunc
+}
+
+func (suite *StartosisPortsWaitTestSuite) TearDownSuite() {
+	err := suite.destroyEnclaveFunc()
+	require.NoError(suite.T(), err, "Destroying the test suite's enclave process has failed, you will have to remove it manually")
+}
+
+func (suite *StartosisPortsWaitTestSuite) RunScript(ctx context.Context, script string) (*enclaves.StarlarkRunResult, error) {
+	logrus.Infof("Executing Startosis script...")
+	logrus.Debugf("Startosis script content: \n%v", script)
+
+	return test_helpers.RunScriptWithDefaultConfig(ctx, suite.enclaveCtx, script)
+}
