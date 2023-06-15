f269d40fe6c4d0e7c1e4271c8485c57ef14271a1
guillaume.bouvignies <guillaume.bouvignies@kurtosistech.com>
2023-06-15 10:24:50 +0200

    Run exec in parallel in the backend

--- container-engine-lib/lib/backend_impls/kubernetes/kubernetes_kurtosis_backend/user_services_functions/run_user_service_exec_commands.go
+++ container-engine-lib/lib/backend_impls/kubernetes/kubernetes_kurtosis_backend/user_services_functions/run_user_service_exec_commands.go
@@ -10,7 +10,10 @@ import (
 	"github.com/kurtosis-tech/kurtosis/container-engine-lib/lib/backend_interface/objects/exec_result"
 	"github.com/kurtosis-tech/kurtosis/container-engine-lib/lib/backend_interface/objects/service"
 	"github.com/kurtosis-tech/kurtosis/container-engine-lib/lib/concurrent_writer"
+	"github.com/kurtosis-tech/kurtosis/container-engine-lib/lib/operation_parallelizer"
 	"github.com/kurtosis-tech/stacktrace"
+	v1 "k8s.io/api/core/v1"
+	"reflect"
 )
 
 // TODO Switch these to streaming methods, so that huge command outputs don't blow up the memory of the API container
@@ -46,69 +49,108 @@ func RunUserServiceExecCommands(
 		return nil, nil, stacktrace.Propagate(err, "An error occurred getting user services matching the requested UUIDs: %+v", requestedGuids)
 	}
 
-	for guid, commandArgs := range userServiceCommands {
-		objectsAndResources, found := matchingObjectsAndResources[guid]
-		if !found {
-			return nil, nil, stacktrace.NewError(
-				"Requested to execute command '%+v' on service '%v', but the service does not exist",
-				commandArgs,
-				guid,
-			)
-		}
-		serviceObj := objectsAndResources.Service
-		if serviceObj == nil {
-			return nil, nil, stacktrace.NewError(
-				"Cannot execute command '%+v' on service '%v' because the service is not started yet",
-				commandArgs,
-				guid,
-			)
-		}
-		if serviceObj.GetStatus() != container_status.ContainerStatus_Running {
-			return nil, nil, stacktrace.NewError(
-				"Cannot execute command '%+v' on service '%v' because the service status is '%v'",
-				commandArgs,
-				guid,
-				serviceObj.GetStatus().String(),
-			)
-		}
+	successfulExecs, failedExecs, err := runExecOperationsInParallel(namespaceName, userServiceCommands, matchingObjectsAndResources, kubernetesManager)
+	if err != nil {
+		return nil, nil, stacktrace.Propagate(err, "An unexpected error occurred running the exec commands in parallel")
 	}
+	return successfulExecs, failedExecs, nil
+}
 
-	// TODO Parallelize for perf
-	userServiceExecSuccess := map[service.ServiceUUID]*exec_result.ExecResult{}
-	userServiceExecErr := map[service.ServiceUUID]error{}
-	for serviceUuid, serviceCommand := range userServiceCommands {
-		userServiceObjectAndResources, found := matchingObjectsAndResources[serviceUuid]
+func runExecOperationsInParallel(
+	namespaceName string,
+	commandArgs map[service.ServiceUUID][]string,
+	userServiceKubernetesResources map[service.ServiceUUID]*shared_helpers.UserServiceObjectsAndKubernetesResources,
+	kubernetesManager *kubernetes_manager.KubernetesManager,
+) (
+	map[service.ServiceUUID]*exec_result.ExecResult,
+	map[service.ServiceUUID]error,
+	error,
+) {
+	successfulExecs := map[service.ServiceUUID]*exec_result.ExecResult{}
+	failedExecs := map[service.ServiceUUID]error{}
+
+	execOperations := map[operation_parallelizer.OperationID]operation_parallelizer.Operation{}
+	for serviceUuid, commandArg := range commandArgs {
+		userServiceKubernetesResource, found := userServiceKubernetesResources[serviceUuid]
 		if !found {
-			// Should never happen because we validate that the object exists earlier
-			return nil, nil, stacktrace.NewError("Validated that service '%v' has Kubernetes resources, but couldn't find them when we need to run the exec", serviceUuid)
-		}
-		// Don't need to validate that this is non-nil because we did so before we started executing
-		userServicePod := userServiceObjectAndResources.KubernetesResources.Pod
-		userServicePodName := userServicePod.Name
-
-		outputBuffer := &bytes.Buffer{}
-		concurrentBuffer := concurrent_writer.NewConcurrentWriter(outputBuffer)
-		exitCode, err := kubernetesManager.RunExecCommand(
-			namespaceName,
-			userServicePodName,
-			userServiceContainerName,
-			serviceCommand,
-			concurrentBuffer,
-			concurrentBuffer,
-		)
-		if err != nil {
-			userServiceExecErr[serviceUuid] = stacktrace.Propagate(
-				err,
-				"Expected to be able to execute command '%+v' in user service container '%v' in Kubernetes pod '%v' "+
-					"for Kurtosis service with guid '%v', instead a non-nil error was returned",
-				serviceCommand,
-				userServiceContainerName,
-				userServicePodName,
+			failedExecs[serviceUuid] = stacktrace.NewError(
+				"Cannot execute command '%+v' on service '%v' because no Kubernetes resources were found for it",
+				commandArgs,
 				serviceUuid,
 			)
 			continue
 		}
-		userServiceExecSuccess[serviceUuid] = exec_result.NewExecResult(exitCode, outputBuffer.String())
+
+		userServiceKubernetesService := userServiceKubernetesResource.Service
+		if userServiceKubernetesService == nil {
+			failedExecs[serviceUuid] = stacktrace.NewError(
+				"Cannot execute command '%+v' on service '%v' because the service is not started yet",
+				commandArgs,
+				serviceUuid,
+			)
+		}
+		if userServiceKubernetesService.GetStatus() != container_status.ContainerStatus_Running {
+			failedExecs[serviceUuid] = stacktrace.NewError(
+				"Cannot execute command '%+v' on service '%v' because the service status is '%v'",
+				commandArgs,
+				serviceUuid,
+				userServiceKubernetesService.GetStatus().String(),
+			)
+		}
+
+		userServiceKubernetesPod := userServiceKubernetesResource.KubernetesResources.Pod
+
+		execOperationId := operation_parallelizer.OperationID(serviceUuid)
+		execOperation := createExecOperation(namespaceName, serviceUuid, userServiceKubernetesPod, commandArg, kubernetesManager)
+		execOperations[execOperationId] = execOperation
 	}
-	return userServiceExecSuccess, userServiceExecErr, nil
+
+	successfulOperations, failedOperations := operation_parallelizer.RunOperationsInParallel(execOperations)
+	for operationUuid, operationResult := range successfulOperations {
+		serviceUuid := service.ServiceUUID(operationUuid)
+		execResult, ok := operationResult.(*exec_result.ExecResult)
+		if !ok {
+			return nil, nil, stacktrace.NewError("An error occurred processing the result of the exec command "+
+				"run on service '%s'. It seems the result object is of an unexpected type ('%v'). This is a Kurtosis "+
+				"internal bug.", serviceUuid, reflect.TypeOf(execResult))
+		}
+		successfulExecs[serviceUuid] = execResult
+	}
+	for operationId, err := range failedOperations {
+		serviceUuid := service.ServiceUUID(operationId)
+		failedExecs[serviceUuid] = err
+	}
+	return successfulExecs, failedExecs, nil
+}
+
+func createExecOperation(
+	namespaceName string,
+	serviceUuid service.ServiceUUID,
+	servicePod *v1.Pod,
+	commandArg []string,
+	kubernetesManager *kubernetes_manager.KubernetesManager,
+) operation_parallelizer.Operation {
+	return func() (interface{}, error) {
+		outputBuffer := &bytes.Buffer{}
+		concurrentBuffer := concurrent_writer.NewConcurrentWriter(outputBuffer)
+		exitCode, err := kubernetesManager.RunExecCommand(
+			namespaceName,
+			servicePod.Name,
+			userServiceContainerName,
+			commandArg,
+			concurrentBuffer,
+			concurrentBuffer,
+		)
+		if err != nil {
+			return nil, stacktrace.Propagate(err, "Expected to be able to execute command '%+v' in user "+
+				"service container '%v' in Kubernetes pod '%v' for Kurtosis service with guid '%v', instead a "+
+				"non-nil error was returned",
+				commandArg,
+				userServiceContainerName,
+				servicePod.Name,
+				serviceUuid,
+			)
+		}
+		return exec_result.NewExecResult(exitCode, outputBuffer.String()), nil
+	}
 }
