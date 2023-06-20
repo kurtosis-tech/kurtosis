6406f10bb1a96cdce429d2c4750977fb86f2d098
Peeeekay <15133250+Peeeekay@users.noreply.github.com>
2023-06-20 13:37:23 -0400

    fix!: removed workdir from run_sh and fixed some typos on the doc (#739)
    I have removed the `workdir` from run_sh task to keep it simple. The
    shell command will either run from root directory or workdir set in the
    image now. This also means that file artifacts will be accessed using
    absolute paths, not relative paths.
    
    Previously, `result = plan.run_sh(...)` would return
    `result.file_aritifacts` but now I fixed the typo for consistency and it
    returns `result.files_artifacts` and this can be used to access the
    files artifacts from the task.

--- internal_testsuites/golang/testsuite/startosis_run_sh_task_test/run_task_sh_task_test.go
+++ internal_testsuites/golang/testsuite/startosis_run_sh_task_test/run_task_sh_task_test.go
@@ -12,17 +12,24 @@ const (
 	runshStarlarkSimple = `
 def run(plan):
   result1 = plan.run_sh(run="echo kurtosis")
-  result2 = plan.run_sh(run="mkdir -p {0} && cd {0} && echo $(pwd)".format(result1.output), workdir="src")
+  result2 = plan.run_sh(run="mkdir -p /src/{0} && cd /src/{0} && echo $(pwd)".format(result1.output))
   plan.assert(result2.output, "==", "/src/kurtosis\n")
 `
 	runshStarlarkFileArtifact = `
 def run(plan):
-  result = plan.run_sh(run="mkdir -p src && echo kurtosis > /task/src/tech.txt", store=["src/tech.txt", "/task/src"])
-  file_artifacts = result.file_artifacts
-  result2 = plan.run_sh(run="cat /src/temp/tech.txt", files={"temp": file_artifacts[0]}, workdir="src")
+  result = plan.run_sh(run="mkdir -p /src && echo kurtosis > /src/tech.txt", store=["/src/tech.txt", "/src"], image="ethpandaops/ethereum-genesis-generator:1.0.14")
+  file_artifacts = result.files_artifacts
+  result2 = plan.run_sh(run="cat /temp/tech.txt", files={"/temp": file_artifacts[0]})
   plan.assert(result2.output, "==", "kurtosis\n")
-  result3 = plan.run_sh(run="cat ./src/tech.txt", files={"/task": file_artifacts[1]})
-  plan.assert(result3.output, "==", "kurtosis\n")`
+  result3 = plan.run_sh(run="cat /task/src/tech.txt", files={"/task": file_artifacts[1]})
+  plan.assert(result3.output, "==", "kurtosis\n")
+`
+
+	runshStarlarkFileArtifactFailure = `
+def run(plan):
+  result = plan.run_sh(run="cat /tmp/kurtosis.txt")
+  plan.assert(value=result.code, assertion="==", target_value="0")
+`
 )
 
 func TestStarlark_RunshTaskSimple(t *testing.T) {
@@ -37,4 +44,12 @@ func TestStarlark_RunshTaskFileArtifact(t *testing.T) {
 	runResult, _ := test_helpers.SetupSimpleEnclaveAndRunScript(t, ctx, runshTest, runshStarlarkFileArtifact)
 	expectedOutput := "Command returned with exit code '0' with no output\nCommand returned with exit code '0' and the following output:\n--------------------\nkurtosis\n\n--------------------\nAssertion succeeded. Value is '\"kurtosis\\n\"'.\nCommand returned with exit code '0' and the following output:\n--------------------\nkurtosis\n\n--------------------\nAssertion succeeded. Value is '\"kurtosis\\n\"'.\n"
 	require.Equal(t, expectedOutput, string(runResult.RunOutput))
+}
+
+func TestStarlark_RunshTaskFileArtifactFailure(t *testing.T) {
+	ctx := context.Background()
+	runResult, _ := test_helpers.SetupSimpleEnclaveAndRunScript(t, ctx, runshTest, runshStarlarkFileArtifactFailure)
+	expectedErrorMessage := "error occurred and shell command: \"cat /tmp/kurtosis.txt\" exited with code 1 with output \"cat: can't open '/tmp/kurtosis.txt': No such file or directory\\n"
+	require.NotNil(t, runResult.ExecutionError)
+	require.Contains(t, runResult.ExecutionError.GetErrorMessage(), expectedErrorMessage)
 }
