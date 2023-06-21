c98eaa4278d3aaeb4b38c36f8e1ff5ad760a1cd1
kurtosisbot <kurtosisbot@users.noreply.github.com>
2023-06-21 11:16:58 +0000

    Apply automatic changes

--- api/golang/kurtosis_version/kurtosis_version.go
+++ api/golang/kurtosis_version/kurtosis_version.go
@@ -9,6 +9,6 @@ const (
 	// !!!!!!!!!!! DO NOT UPDATE! WILL BE MANUALLY UPDATED DURING THE RELEASE PROCESS !!!!!!!!!!!!!!!!!!!!!!
 	// This is necessary so that Kurt Core consumers will know if they're compatible with the currently-running
 	//  API container
-	KurtosisVersion = "0.78.5"
+	KurtosisVersion = "0.79.0"
 	// !!!!!!!!!!! DO NOT UPDATE! WILL BE MANUALLY UPDATED DURING THE RELEASE PROCESS !!!!!!!!!!!!!!!!!!!!!!
 )
--- api/typescript/src/kurtosis_version/kurtosis_version.ts
+++ api/typescript/src/kurtosis_version/kurtosis_version.ts
@@ -1,5 +1,5 @@
 // !!!!!!!!!!! DO NOT UPDATE! WILL BE MANUALLY UPDATED DURING THE RELEASE PROCESS !!!!!!!!!!!!!!!!!!!!!!
 // This is necessary so that Kurt Core consumers (e.g. modules) will know if they're compatible with the currently-running
 //  API container
-export const KURTOSIS_VERSION: string = "0.78.5"
+export const KURTOSIS_VERSION: string = "0.79.0"
 // !!!!!!!!!!! DO NOT UPDATE! WILL BE MANUALLY UPDATED DURING THE RELEASE PROCESS !!!!!!!!!!!!!!!!!!!!!!
