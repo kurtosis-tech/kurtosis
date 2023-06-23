626820aebc98ab2ca24bce1cd346d0d58fa8235d
Victor Colombo <victorcolombo@users.noreply.github.com>
2023-06-23 15:26:28 +0100

    Parametrize build with git

--- api/rust/build.rs
+++ api/rust/build.rs
@@ -1,6 +1,15 @@
 use std::io::Result;
+use std::str;
+use std::process::Command;
+
 fn main() -> Result<()> {
-    tonic_build::compile_protos("../protobuf/engine/engine_service.proto")?;
-    tonic_build::compile_protos("../protobuf/core/api_container_service.proto")?;
-    Ok(())
+    let mut kurtosis_folder_command = Command::new("git");
+    kurtosis_folder_command.arg("rev-parse").arg("--show-toplevel");
+    let kurtosis_folder = kurtosis_folder_command.output()?;
+    let kurtosis_folder_str = str::from_utf8(&kurtosis_folder.stdout).unwrap().trim();
+    tonic_build::configure()
+        .compile(&[
+            format!("{}/api/protobuf/engine/engine_service.proto", kurtosis_folder_str),
+            format!("{}/api/protobuf/core/api_container_service.proto", kurtosis_folder_str)
+        ], &[kurtosis_folder_str])
 }
