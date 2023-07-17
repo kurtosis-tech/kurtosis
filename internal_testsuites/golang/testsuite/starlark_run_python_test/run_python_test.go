ec94fc61d32f66370bdbf867469d4c297878d271
Gyanendra Mishra <anomaly.the@gmail.com>
2023-07-17 14:42:24 +0100

    have a test that fails

--- internal_testsuites/golang/testsuite/starlark_run_python_test/run_python_test.go
+++ internal_testsuites/golang/testsuite/starlark_run_python_test/run_python_test.go
@@ -19,12 +19,35 @@ print("kurtosis")
 		run = python_script,
 	)
 `
+
+	runPythonWithPackagesTest   = "run-python-package-test"
+	runPythonWithPackagesScript = `
+def run(plan):
+	python_script = """
+import requests
+response = requests.get("https://docs.kurtosis.com")
+print(response.status_code)
+	"""
+
+	plan.run_python(
+		run = python_script,
+		packages = ["requests"]
+	)
+`
 )
 
 func TestStarlark_RunPython(t *testing.T) {
 	ctx := context.Background()
 	runResult, err := test_helpers.SetupSimpleEnclaveAndRunScript(t, ctx, runPythonTest, runPythonSimpleScript)
 	require.Nil(t, err)
-	expectedOutput := "Command returned with exit code '0' and the following output:\n--------------------\nkurtosis\n"
+	expectedOutput := "Command returned with exit code '0' and the following output:\n--------------------\nkurtosis\n\n--------------------\n"
+	require.Equal(t, expectedOutput, string(runResult.RunOutput))
+}
+
+func TestStarlark_RunPythonWithExternalPacakges(t *testing.T) {
+	ctx := context.Background()
+	runResult, err := test_helpers.SetupSimpleEnclaveAndRunScript(t, ctx, runPythonWithPackagesTest, runPythonWithPackagesScript)
+	require.Nil(t, err)
+	expectedOutput := "Command returned with exit code '0' and the following output:\n--------------------\n200\n\n--------------------\n"
 	require.Equal(t, expectedOutput, string(runResult.RunOutput))
 }
