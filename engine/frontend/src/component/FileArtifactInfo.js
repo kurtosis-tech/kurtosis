349f66d982e1440e44b6079cc46e5fee1ddb22ce
preetrawal <15133250+Peeeekay@users.noreply.github.com>
2023-07-25 10:53:51 -0400

    made last moment updates

--- engine/frontend/src/component/FileArtifactInfo.js
+++ engine/frontend/src/component/FileArtifactInfo.js
@@ -7,8 +7,9 @@ import NoData from "./NoData";
 import LeftPanel from "./LeftPanel";
 import RightPanel from "./RightPanel";
 import LoadingOverlay from "./LoadingOverflow";
+import e from "cors";
 
-const BreadCumbs = ({currentPath, handleOnClick}) => {
+const BreadCumbs = ({currentPath, handleOnClick, handleCleanButton}) => {
     const total = currentPath.length;
 
     const BreadCumb = ({text, last, color="text-slate-800", index, handleOnClick}) => {
@@ -24,7 +25,15 @@ const BreadCumbs = ({currentPath, handleOnClick}) => {
                 currentPath.map((path, index) => (
                     <BreadCumb key={index} text={path} last={total-1 === index} index={index} handleOnClick={handleOnClick}/>
                 ))
-            }   
+            } 
+            {
+                currentPath.length > 0 ? 
+                <div className="mx-3" onClick={handleCleanButton}> 
+                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                        <path color="gray" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg> 
+                </div> : null 
+            }  
         </div>
     )
 }
@@ -73,20 +82,30 @@ const FileArtifactInfo = ({enclaves}) => {
     const {fileArtifacts} = state;
     
     useEffect(() => {
-        setFileInfoLoading(true)
-        const fetch = async () => {
-            const selected = enclaves.filter(enclave => enclave.name === enclaveName);
-            if (selected.length > 0) {
-                const {files} = await getFileArtifactInfo(selected[0].apiClient, fileArtifactName);
-                setFiles(files)
-                setCurrentFiles(files)
-                setCurrentPath([])
-                setDetailInfo({})
-            }
-            setFileInfoLoading(false)
-        } 
-        fetch()
+        if (enclaves.length === 0) {
+            navigate(`/enclaves/${enclaveName}`)
+        } else {
+            setFileInfoLoading(true)
+            const fetch = async () => {
+                const selected = enclaves.filter(enclave => enclave.name === enclaveName);
+                if (selected.length > 0) {
+                    const {files} = await getFileArtifactInfo(selected[0].apiClient, fileArtifactName);
+                    setFiles(files)
+                    setCurrentFiles(files)
+                    setCurrentPath([])
+                    setDetailInfo({})
+                }
+                setFileInfoLoading(false)
+            } 
+            fetch()
+        }
     }, [fileArtifactName])
+
+    const handleCleanButton = () => {
+        setCurrentPath([])
+        setDetailInfo({})
+        setCurrentFiles(files)
+    }
 
     const handleFileClick = (key, file) => {
         if (file.path) {
@@ -132,7 +151,7 @@ const FileArtifactInfo = ({enclaves}) => {
                     (Object.keys(detailInfo).length !== 0) ?
                     <div className="flex h-3/4 flex-col"> 
                         <p className="text-lg font-bold text-right"> Size: {detailInfo.size}B </p>
-                        <p className="break-all overflow-y-auto"> {detailInfo.textPreview} </p> 
+                        <p className="break-all overflow-y-auto"> {detailInfo.textPreview.length > 0 ? detailInfo.textPreview : <h2 className="text-2xl text-center mt-20 text-red-800 font-bold">No Preview Available</h2>} </p> 
                     </div>  :                      
                     <div className="flex flex-col h-[85%] min-h-[85%] border-8">
                         <Heading content={"Files"} size={"text-xl"} />
@@ -156,7 +175,11 @@ const FileArtifactInfo = ({enclaves}) => {
                 <div className="h-[3rem] flex items-center justify-center m-2">
                     <Heading content={`${enclaveName}::${fileArtifactName}`} />
                 </div>
-                <BreadCumbs currentPath={currentPath} handleOnClick={handleBreadCrumbClick} />
+                <BreadCumbs 
+                    currentPath={currentPath} 
+                    handleOnClick={handleBreadCrumbClick} 
+                    handleCleanButton={handleCleanButton}
+                />
                 {fileInfoLoading ? 
                     <LoadingOverlay /> : 
                     <FileInfoComponent 
