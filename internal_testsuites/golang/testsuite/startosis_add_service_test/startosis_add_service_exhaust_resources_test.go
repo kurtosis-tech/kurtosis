45315ca7dabd1fce4d1a6241e783a6db05eca6f0
Gyanendra Mishra <anomaly.the@gmail.com>
2023-07-26 13:27:08 +0100

    added an integration test

---
+++ internal_testsuites/golang/testsuite/startosis_add_service_test/startosis_add_service_exhaust_resources_test.go
@@ -0,0 +1,40 @@
+package startosis_add_service_test
+
+import (
+	"context"
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	addServiceMemoryValidationTest = `
+CONTAINER_IMAGE = "kurtosistech/example-datastore-server"
+SERVICE_NAME = "` + serviceName + `"
+GRPC_PORT = 1323
+SUCCESS_CODE = 0
+
+def run(plan):
+	config = ServiceConfig(
+		image = CONTAINER_IMAGE,
+		max_cpu=500,
+		min_cpu=100,
+		memory_allocation=256,
+		min_memory=51200000,
+		ports = {
+			"grpc": PortSpec(number = GRPC_PORT, transport_protocol = "TCP")
+		}
+	)
+	datastore_1 = plan.add_service(name = SERVICE_NAME, config = config)`
+)
+
+func (suite *StartosisAddServiceTestSuite) TestAddServices_FailsIfWeConsumeMoreThanAvailableMemory() {
+	ctx := context.Background()
+	runResult, err := suite.RunScript(ctx, addServiceMemoryValidationTest)
+
+	t := suite.T()
+
+	require.NotNil(t, err)
+	require.Nil(t, runResult.InterpretationError, "Unexpected interpretation error.")
+	require.NotEmpty(t, runResult.ValidationErrors, "Expected validation errors to be non empty")
+	require.Contains(t, runResult.ValidationErrors[0].GetErrorMessage(), "There was an error validating 'add_service' as it required '51200000' megabytes of memory which is more than the amount available")
+	require.Nil(t, runResult.ExecutionError, "Unexpected execution error")
+}
