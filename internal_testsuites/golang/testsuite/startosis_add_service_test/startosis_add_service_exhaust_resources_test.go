d6d00643744cdd761b45099e3b556b7f3087cd8a
Gyanendra Mishra <anomaly.the@gmail.com>
2023-07-26 13:29:13 +0100

    added an integration test for cpu usage

--- internal_testsuites/golang/testsuite/startosis_add_service_test/startosis_add_service_exhaust_resources_test.go
+++ internal_testsuites/golang/testsuite/startosis_add_service_test/startosis_add_service_exhaust_resources_test.go
@@ -24,6 +24,24 @@ def run(plan):
 		}
 	)
 	datastore_1 = plan.add_service(name = SERVICE_NAME, config = config)`
+
+	addServiceCPUValidationTest = `
+CONTAINER_IMAGE = "kurtosistech/example-datastore-server"
+SERVICE_NAME = "` + serviceName + `"
+GRPC_PORT = 1323
+SUCCESS_CODE = 0
+
+def run(plan):
+	config = ServiceConfig(
+		image = CONTAINER_IMAGE,
+		min_cpu=1000000,
+		memory_allocation=256,
+		min_memory=512,
+		ports = {
+			"grpc": PortSpec(number = GRPC_PORT, transport_protocol = "TCP")
+		}
+	)
+	datastore_1 = plan.add_service(name = SERVICE_NAME, config = config)`
 )
 
 func (suite *StartosisAddServiceTestSuite) TestAddServices_FailsIfWeConsumeMoreThanAvailableMemory() {
@@ -36,5 +54,18 @@ func (suite *StartosisAddServiceTestSuite) TestAddServices_FailsIfWeConsumeMoreT
 	require.Nil(t, runResult.InterpretationError, "Unexpected interpretation error.")
 	require.NotEmpty(t, runResult.ValidationErrors, "Expected validation errors to be non empty")
 	require.Contains(t, runResult.ValidationErrors[0].GetErrorMessage(), "There was an error validating 'add_service' as it required '51200000' megabytes of memory which is more than the amount available")
+	require.Nil(t, runResult.ExecutionError, "Unexpected execution error")
+}
+
+func (suite *StartosisAddServiceTestSuite) TestAddServices_FailsIfWeConsumeMoreThanAvailableCPU() {
+	ctx := context.Background()
+	runResult, err := suite.RunScript(ctx, addServiceCPUValidationTest)
+
+	t := suite.T()
+
+	require.NotNil(t, err)
+	require.Nil(t, runResult.InterpretationError, "Unexpected interpretation error.")
+	require.NotEmpty(t, runResult.ValidationErrors, "Expected validation errors to be non empty")
+	require.Contains(t, runResult.ValidationErrors[0].GetErrorMessage(), "There was an error validating 'add_service' as it required '1000000' millicores of CPU which is more than the amount available")
 	require.Nil(t, runResult.ExecutionError, "Unexpected execution error")
 }
