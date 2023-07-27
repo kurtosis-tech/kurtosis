4df38911a915795be15283d30e1137a1f150d824
Gyanendra Mishra <anomaly.the@gmail.com>
2023-07-27 10:13:49 +0100

    cleanup

--- core/server/api_container/server/startosis_engine/startosis_validator/validator_environment_test.go
+++ core/server/api_container/server/startosis_engine/startosis_validator/validator_environment_test.go
@@ -20,7 +20,7 @@ const (
 
 func TestMultiplePortIdsForValidation(t *testing.T) {
 	emptyInitialMapping := map[service.ServiceName][]string{}
-	validatorEnvironment := NewValidatorEnvironment(false, nil, nil, emptyInitialMapping, availableCpuInMilliCores, availableMemoryInBytes, isResourceInformationComplete, isResourceInformationComplete)
+	validatorEnvironment := NewValidatorEnvironment(false, nil, nil, emptyInitialMapping, availableCpuInMilliCores, availableMemoryInBytes, isResourceInformationComplete)
 	portIds := []string{
 		fooPortId,
 		fizzPortId,
