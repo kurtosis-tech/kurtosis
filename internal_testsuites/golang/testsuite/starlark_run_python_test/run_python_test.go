6838295d2e5e218571d286d02bb14cbe4e75816c
Gyanendra Mishra <anomaly.the@gmail.com>
2023-07-28 00:19:31 +0100

    add a test for the large output

--- internal_testsuites/golang/testsuite/starlark_run_python_test/run_python_test.go
+++ internal_testsuites/golang/testsuite/starlark_run_python_test/run_python_test.go
@@ -37,6 +37,16 @@ print(sys.argv[1])
 		args = ["Kurtosis"]
 	)
 `
+
+	runPythonWithGiganticOutputGreaterThan4Mb    = "run-large-python-output-test"
+	runPythonWithLargeOutputGreaterThan4MbScript = `
+def run(plan):
+    plan.run_python(
+        run = """
+print("hello world"*2**20)
+"""
+    )
+`
 )
 
 func TestStarlark_RunPython(t *testing.T) {
@@ -53,4 +63,11 @@ func TestStarlark_RunPythonWithExternalPacakges(t *testing.T) {
 	require.Nil(t, err)
 	expectedOutput := "Command returned with exit code '0' and the following output:\n--------------------\n200\nKurtosis\n\n--------------------\n"
 	require.Equal(t, expectedOutput, string(runResult.RunOutput))
+}
+
+func TestStarlark_RunPythonWithLargeOutputGreaterThan4Mb(t *testing.T) {
+	ctx := context.Background()
+	runResult, err := test_helpers.SetupSimpleEnclaveAndRunScript(t, ctx, runPythonWithGiganticOutputGreaterThan4Mb, runPythonWithLargeOutputGreaterThan4MbScript)
+	require.Nil(t, err)
+	require.Greater(t, len(runResult.RunOutput), 2^20)
 }
