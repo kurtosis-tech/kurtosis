4acc0fc0f71f7eba269030d3c12bccfdfc4a54b0
leoporoli <leandroporoli@gmail.com>
2023-07-28 10:49:31 -0300

    refactor: implements GetServiceLogs engine's gateway endpoint (#1009)
    ## Description:
    This method was missing on the engine's gateway but it wasn't affecting
    the `kurtosis service logs` command because this command was calling
    directly to the Kubernetes backend, now with this endpoint the CLI
    command will be calling the engine's endpoint for getting the service
    logs
    
    ## Is this change user-facing?
    NO
    
    ## References (if applicable):
    It's related to this ticket:
    https://github.com/kurtosis-tech/kurtosis-private/issues/1069

---
+++ cli/cli/kurtosis_gateway/server/common/helpers.go
@@ -0,0 +1,27 @@
+package common
+
+import (
+	"github.com/kurtosis-tech/stacktrace"
+	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+	"io"
+)
+
+func ForwardKurtosisExecutionStream[T any](streamToReadFrom grpc.ClientStream, streamToWriteTo grpc.ServerStream) error {
+	for {
+		starlarkRunResponseLine := new(T)
+		// RecvMsg blocks until either a message is received or an error is thrown
+		readErr := streamToReadFrom.RecvMsg(starlarkRunResponseLine)
+		if readErr == io.EOF {
+			logrus.Debug("Finished reading from the Kurtosis response line stream.")
+			return nil
+		}
+		if readErr != nil {
+			return stacktrace.Propagate(readErr, "Error reading Kurtosis execution lines from Kurtosis core stream")
+		}
+
+		if writeErr := streamToWriteTo.SendMsg(starlarkRunResponseLine); writeErr != nil {
+			return stacktrace.Propagate(readErr, "Received a Kurtosis execution line but failed forwarding it back to the user")
+		}
+	}
+}
