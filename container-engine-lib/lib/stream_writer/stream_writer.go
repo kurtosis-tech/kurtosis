d45a23193568c7b81a6ae114feafac954b949306
Tedi Mitiku <tedi.m52@gmail.com>
2023-08-02 10:14:00 -0400

    add comment to stream writer

--- container-engine-lib/lib/stream_writer/stream_writer.go
+++ container-engine-lib/lib/stream_writer/stream_writer.go
@@ -2,11 +2,11 @@ package stream_writer
 
 import (
 	"io"
-	"strings"
 	"sync"
 	"time"
 )
 
+// Enables extracting output that is being written to an io.Writer via a channel
 type StreamWriter struct {
 	underlying    io.Writer
 	mutex         *sync.Mutex
@@ -25,7 +25,7 @@ func (writer *StreamWriter) Write(p []byte) (n int, err error) {
 	writer.mutex.Lock()
 	defer writer.mutex.Unlock()
 
-	writer.outputChannel <- strings.TrimSuffix(string(p), "\n")
+	writer.outputChannel <- string(p)
 
 	// TODO: remove this time.Sleep
 	// This sleep is a hack to make the streamed output UI deliver steadily as its
