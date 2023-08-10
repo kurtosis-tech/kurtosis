1c392bdaa8565b08bbffc063da33c7fc89e9fbbd
Tedi Mitiku <tedi.m52@gmail.com>
2023-08-10 17:00:36 -0400

    fix apic fluentd logging driver addr

--- container-engine-lib/lib/backend_impls/docker/docker_kurtosis_backend/docker_kurtosis_backend_api_container_functions.go
+++ container-engine-lib/lib/backend_impls/docker/docker_kurtosis_backend/docker_kurtosis_backend_api_container_functions.go
@@ -149,8 +149,12 @@ func (backend *DockerKurtosisBackend) CreateAPIContainer(
 	}
 
 	//The APIContainer will be configured to send the logs to the Fluentbit logs collector server
+	logCollectorAddr, err := enclaveLogsCollector.GetEnclaveNetworkTcpAddress()
+	if err != nil {
+		return nil, stacktrace.Propagate(err, "An error occurred retrieving the log collector address.")
+	}
 	fluentdLoggingDriverCnfg := docker_manager.NewFluentdLoggingDriver(
-		enclaveLogsCollector.GetEnclaveNetworkIpAddress().String(),
+		string(logCollectorAddr),
 		logs_collector_functions.GetKurtosisTrackedLogsCollectorLabels(),
 	)
 
