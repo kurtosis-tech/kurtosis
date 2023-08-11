45080b2e0734e32fe54a7f71db37994296829838
Tedi Mitiku <tedi.m52@gmail.com>
2023-08-11 09:52:26 -0400

    add comment

--- api/golang/engine/lib/enclave/consts.go
+++ api/golang/engine/lib/enclave/consts.go
@@ -1,3 +1,4 @@
 package enclave
 
+// We restrict enclave names to 60 characters bc K8s doesn't allow names beyond 63 chars so 63 - 3 ("kt-" prefix) = 60
 const AllowedEnclaveNameCharsRegexStr = `^[-A-Za-z0-9._]{1,60}$`
