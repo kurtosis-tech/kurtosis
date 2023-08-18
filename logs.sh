07da2a454c61a4a3703b4d662a3cac62cd2f2994
Tedi Mitiku <tedi.m52@gmail.com>
2023-08-18 16:29:43 -0400

    remove test script

--- logs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-cd /Users/tewodrosmitiku/Desktop/kurtosis
-kurtosis clean -a
-kurtosis engine stop
-./cli/cli/scripts/launch-cli.sh engine start --version=$1
-./cli/cli/scripts/launch-cli.sh run github.com/kurtosis-tech/mongodb-package
-
