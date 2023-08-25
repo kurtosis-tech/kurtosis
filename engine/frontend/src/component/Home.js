5cc2bf2cf6bdf270908dcc169fee1290168aeb2a
Anders Schwartz <anders.schwartz@kurtosistech.com>
2023-08-25 17:23:20 -0400

    update

--- engine/frontend/src/component/Home.js
+++ engine/frontend/src/component/Home.js
@@ -1,40 +1,24 @@
-import React, {useEffect, useMemo, useRef, useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import TitleBar from "./TitleBar"
 import Main from "./Main"
 import EnclaveInfo from "./EnclaveInfo";
 import ServiceInfo from "./ServiceInfo";
 import FileArtifactInfo from './FileArtifactInfo';
 import Enclaves from "./Enclaves";
-import CreateEnclave from "./CreateEnclave"
 import {getEnclavesFromKurtosis} from "../api/enclave";
-import {withRouter} from "react-router";
 
-import {Route, Routes, useLocation, useSearchParams} from 'react-router-dom';
+import {Route, Routes, useSearchParams} from 'react-router-dom';
 import {useAppContext} from "../context/AppState";
 import LoadingOverlay from "./LoadingOverflow";
+import CreateEnclave from "./CreateEnclave";
 
-const loading = (
-    <div className="flex-grow bg-#181926-100 flex-row flex mt-28 w-screen">
-        <div className='w-1/3'></div>
-        <div className="flex flex-col min-w-fit w-1/3">
-            <div className='flex justify-center items-center w-full'>
-                <div className='text-center w-full'>
-                    <h1 className="text-3xl font-bold text-[#24BA27]">Loading credentials...</h1>
-                    <br/>
-                    <LoadingOverlay/>
-                </div>
-            </div>
-        </div>
-        <div className='w-1/3'></div>
-    </div>
-)
+
 const queryParamToBool = (value) => {
     return ((value + '').toLowerCase() === 'true')
 }
 
 const DefaultApiHost = "localhost"
 const DefaultApiPort = 8081
-
 
 const createApiUrl = (apiHost, requireProxy) => {
     if (requireProxy) {
@@ -43,22 +27,33 @@ const createApiUrl = (apiHost, requireProxy) => {
     return `http://${DefaultApiHost}:${DefaultApiPort}`;
 }
 
+const makeUrl = () => {
+    let path = window.location.pathname
+    if (path.charAt(0) === "/") path = path.substr(1);
+    path = path.endsWith('/') ? path.slice(0, -1) : path;
+    return path;
+}
 const Home = () => {
     const [enclaves, setEnclaves] = useState([])
     const [enclaveLoading, setEnclaveLoading] = useState(false)
     const {appData, setAppData} = useAppContext()
     const [searchParams, setSearchParams] = useSearchParams();
-    const location = useLocation();
 
-    const makeUrl = () => {
-        let path = window.location.pathname
-        if (path.charAt(0) === "/") path = path.substr(1);
-        path = path.endsWith('/') ? path.slice(0, -1) : path;
-
-        // console.log("location.pathname", location.pathname)
-        console.log("path", path)
-        return path;
-    }
+    const loading = (
+        <div className="flex-grow bg-#181926-100 flex-row flex mt-28 w-screen">
+            <div className='w-1/3'></div>
+            <div className="flex flex-col min-w-fit w-1/3">
+                <div className='flex justify-center items-center w-full'>
+                    <div className='text-center w-full'>
+                        <h1 className="text-3xl font-bold text-[#24BA27]">Loading credentials...</h1>
+                        <br/>
+                        <LoadingOverlay/>
+                    </div>
+                </div>
+            </div>
+            <div className='w-1/3'></div>
+        </div>
+    )
 
     const urlPath = useMemo(() => {
         return makeUrl()
@@ -70,9 +65,9 @@ const Home = () => {
         switch (message) {
             case 'jwtToken':
                 const value = event.data.value
-                console.log("got event:", JSON.stringify(event.data))
+                console.log("got event: jwtToken")
                 if (value !== null && value !== undefined) {
-                    console.log("got token:", value)
+                    // console.log("got token:", value)
                     setAppData({
                         ...appData,
                         jwtToken: value,
@@ -90,7 +85,6 @@ const Home = () => {
         const requireProxy = requireAuth;
         const apiHost = createApiUrl(requestedApiHost, requireProxy)
         if (apiHost && apiHost.length > 0) {
-            console.log(`Setting API host = ${apiHost}`)
             setAppData({
                 ...appData,
                 apiHost: apiHost,
@@ -101,99 +95,64 @@ const Home = () => {
     const validJwtToken = () => appData.jwtToken && appData.jwtToken.length > 0
     const validApiHost = () => appData.apiHost && appData.apiHost.length > 0
 
+    const fetch = async () => {
+        console.log("submitting request for enclaves")
+        const response = await getEnclavesFromKurtosis(appData.jwtToken, appData.apiHost);
+        console.log("Got response for enclaves", response)
+        setEnclaves(response)
+        setEnclaveLoading(false)
+        console.log("finished fetch")
+    }
+
     useEffect(() => {
-        console.log("version G")
-
-        console.log("requireAuth=", requireAuth)
-        console.log("apiHost=", appData.apiHost)
-
-        if (!validApiHost()) {
-            console.log("Invalid api host: ", appData.apiHost)
-        } else {
-            console.log("Got valid api host: ", appData.apiHost)
-        }
-        if (!validJwtToken()) {
-            console.log("Invalid jwt token: ", appData.jwtToken)
-        } else {
-            console.log("Got valid jwt token: ", appData.jwtToken)
-        }
         if (requireAuth && !validJwtToken()) {
             console.log("Requires Auth and jwt token: waiting for jwt token")
         }
         if (requireAuth && validJwtToken() && validApiHost()) {
-            console.log("Requires Auth, jwt token and api host: Got jwt token", appData.jwtToken, appData.apiHost)
-            console.log("starting load and fetch")
-            console.log("appData.jwtToken", appData.jwtToken)
-            console.log("appData.apiHost", appData.apiHost)
+            console.log("Requires Auth, jwt token and api host: Got them all")
+            console.log("starting load for authenticated access")
             setEnclaveLoading(true)
-            const fetch = async () => {
-                console.log("submitting request for enclaves")
-                const response = await getEnclavesFromKurtosis(appData.jwtToken, appData.apiHost);
-                console.log("Got response for enclaves", response)
-                setEnclaves(response)
-                setEnclaveLoading(false)
-                console.log("finished fetch")
-            }
-            console.log("Run fetch")
             fetch()
-        }
-
-        if (!requireAuth && validApiHost()) {
-            console.log("Does not require auth")
-            console.log("appData.apiHost", appData.apiHost)
-            const fetch = async () => {
-                console.log("submitting request for enclaves for non auth")
-                const response = await getEnclavesFromKurtosis(appData.jwtToken, appData.apiHost);
-                console.log("Got response for enclaves for non auth", response)
-                setEnclaves(response)
-                setEnclaveLoading(false)
-            }
-            console.log("Run fetch for non auth")
-            fetch()
-            console.log("finished fetch for non auth")
-
         }
 
     }, [appData.jwtToken, appData.apiHost])
+
+    useEffect(() => {
+        if (!requireAuth && validApiHost()) {
+            console.log("Does not require auth. Got Api host:", appData.apiHost)
+            console.log("starting load for non authenticated access")
+            fetch()
+        }
+    }, [appData.apiHost])
 
     const addEnclave = (enclave) => {
         setEnclaves(enclaves => [...enclaves, enclave])
     }
 
     const checkAuth = (element) => {
-        return element;
-        // if (!requireAuth) {
-        //     return element;
-        // }
-        // if (requireAuth && appData.jwtToken) {
-        //     return element;
-        // }
-        // if (requireAuth && !appData.jwtToken) {
-        //     return loading;
-        // }
+        if (!requireAuth && appData.apiHost) {
+            return element;
+        }
+        if (requireAuth && appData.jwtToken && appData.apiHost) {
+            return element;
+        }
+        if (requireAuth && (!appData.jwtToken || appData.apiHost)) {
+            return loading;
+        }
     }
 
-
-    const basePath = urlPath;
-    const queryParams = window.location.search
     const constructPath = (path) => {
-        const temp = `${urlPath}${path}`
-        // console.log("constructPath", temp)
-        return temp
+        return `${urlPath}${path}`
     }
     return (
         <div className="h-screen flex flex-col bg-[#171923]">
             <TitleBar/>
-            {basePath}
-            <br/>
-            {queryParams}
-            <br/>
-            {urlPath}
             <div className="flex h-[calc(100vh-4rem)]">
                 <Routes>
                     <Route exact path={constructPath("/")}
                            element={checkAuth(<Main totalEnclaves={enclaves.length}/>)}/>
-                    {/*<Route exact path={constructPath("/enclave/*")} element={checkAuth(<CreateEnclave addEnclave={addEnclave}/>)}/>*/}
+                    <Route exact path={constructPath("/enclave/*")}
+                           element={checkAuth(<CreateEnclave addEnclave={addEnclave}/>)}/>
                     <Route exact path={constructPath("/enclaves")}
                            element={checkAuth(<Enclaves enclaves={enclaves} isLoading={enclaveLoading}
                                                         baseUrl={urlPath}/>)}/>
