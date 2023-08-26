abc032d48b13664828edd24e68eed4d7386c33d8
Anders Schwartz <anders.schwartz@kurtosistech.com>
2023-08-26 13:24:28 -0400

    add version

--- engine/frontend/src/App.js
+++ engine/frontend/src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 import Home from './component/Home';
-import { BrowserRouter as Router } from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 const App = () => {
-  return (
-      <div className="h-screen w-screen">
-        <Router>
-          <Home />
-        </Router>
-      </div>   
-  )
+    console.log("Enclave Manager version: 2023-08-26-1")
+    return (
+        <div className="h-screen w-screen">
+            <Router>
+                <Home/>
+            </Router>
+        </div>
+    )
 }
 
 export default App;
