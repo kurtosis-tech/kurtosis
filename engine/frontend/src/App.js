185da37553b722363f812caf36a8b8972d0590a0
Anders Schwartz <anders.schwartz@kurtosistech.com>
2023-08-28 08:09:06 -0400

    make new version

--- engine/frontend/src/App.js
+++ engine/frontend/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Home from './component/Home';
 
 const App = () => {
-    console.log("Enclave Manager version: 2023-08-27-1")
+    console.log("Enclave Manager version: 2023-08-28-1")
     return (
         <div className="h-screen w-screen">
             <Home/>
