933f92b119b4f4f73876b4bbee571261f3a2b3dc
Anders Schwartz <anders.schwartz@kurtosistech.com>
2023-08-29 16:41:23 -0400

    fix

--- engine/frontend/src/component/CreateEnclaveModal.js
+++ engine/frontend/src/component/CreateEnclaveModal.js
@@ -30,7 +30,7 @@ export const CreateEnclaveModal = ({handleSubmit, name, setName, args, setArgs, 
     <div className="flex justify-center w-full h-fit m-14">
       <form className="bg-gray-100 p-6 rounded-lg shadow-md w-1/3" on>
         <div className="text-center">
-          <label className="block mb-4 text-2xl">
+          <label className="block mb-4 text-2xl text-black">
             Package Id:
             <input
               type="text"
@@ -39,7 +39,7 @@ export const CreateEnclaveModal = ({handleSubmit, name, setName, args, setArgs, 
               className="block w-full rounded-md border-gray-300 py-2 px-3 mt-1 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
             />
           </label>
-          <label className="block mb-4">
+          <label className="block mb-4 text-black">
             Args: (Json)
             <textarea
               value={args}
@@ -53,7 +53,6 @@ export const CreateEnclaveModal = ({handleSubmit, name, setName, args, setArgs, 
             <Button
               onClick={handleFormSubmit}
               type="submit"
-              className="w-[50%] bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-blue-500 focus:ring-2 focus:ring-offset-2"
               bg='blue.500'
               w="50%"
               isLoading={runningPackage}
