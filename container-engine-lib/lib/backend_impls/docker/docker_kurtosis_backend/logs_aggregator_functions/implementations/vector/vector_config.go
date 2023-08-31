be0e7e7185462fc3b20516d1de2fcca4e580c4b4
Tedi Mitiku <tedi.m52@gmail.com>
2023-08-31 10:28:10 -0400

    modify vector config

--- container-engine-lib/lib/backend_impls/docker/docker_kurtosis_backend/logs_aggregator_functions/implementations/vector/vector_config.go
+++ container-engine-lib/lib/backend_impls/docker/docker_kurtosis_backend/logs_aggregator_functions/implementations/vector/vector_config.go
@@ -1,13 +1,16 @@
 package vector
 
 import (
+	"bytes"
 	"fmt"
+	"github.com/kurtosis-tech/stacktrace"
 	"strconv"
+	"text/template"
 )
 
 type VectorConfig struct {
 	Source *Source
-	Sink   *Sink
+	Sinks  []*Sink
 }
 
 type Source struct {
@@ -30,11 +33,51 @@ func newDefaultVectorConfig(listeningPortNumber uint16) *VectorConfig {
 			Type:    fluentBitSourceType,
 			Address: fmt.Sprintf("%s:%s", fluentBitSourceIpAddress, strconv.Itoa(int(listeningPortNumber))),
 		},
-		Sink: &Sink{
-			Id:       fileSinkId,
-			Type:     fileTypeId,
-			Inputs:   []string{fluentBitSourceId},
-			Filepath: logsFilepath,
+		Sinks: []*Sink{
+			{
+				Id:       fileSinkId,
+				Type:     fileTypeId,
+				Inputs:   []string{fluentBitSourceId},
+				Filepath: uuidLogsFilepath,
+			},
+			{
+				Id:       fileSinkId,
+				Type:     fileTypeId,
+				Inputs:   []string{fluentBitSourceId},
+				Filepath: nameLogsFilepath,
+			},
+			{
+				Id:       fileSinkId,
+				Type:     fileTypeId,
+				Inputs:   []string{fluentBitSourceId},
+				Filepath: shortUUIDLogsFilepath,
+			},
 		},
 	}
+}
+
+func (cfg *VectorConfig) getConfigFileContent() (string, error) {
+	srcCfgFileTemplate, err := template.New(sourceConfigFileTemplateName).Parse(srcConfigFileTemplate)
+	if err != nil {
+		return "", stacktrace.Propagate(err, "An error occurred parsing Vector's source config template.")
+	}
+	sinkCfgFileTemplate, err := template.New(sinkConfigFileTemplateName).Parse(sinkConfigFileTemplate)
+	if err != nil {
+		return "", stacktrace.Propagate(err, "An error occurred parsing Vector's sink config template.")
+	}
+
+	templateStrBuffer := &bytes.Buffer{}
+
+	if err := srcCfgFileTemplate.Execute(templateStrBuffer, cfg.Source); err != nil {
+		return "", stacktrace.Propagate(err, "An error occurred executing Vector's source config file template.")
+	}
+	for _, sink := range cfg.Sinks {
+		if err := sinkCfgFileTemplate.Execute(templateStrBuffer, sink); err != nil {
+			return "", stacktrace.Propagate(err, "An error occurred executing Vector's sink config file template.")
+		}
+	}
+
+	templateStr := templateStrBuffer.String()
+
+	return templateStr, nil
 }
