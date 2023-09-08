17a05f321c67eee436f52861e0ea6fe075d8dc43
Leandro Poroli <leandro.poroli@kurtosistech.com>
2023-09-08 18:03:45 -0300

    unit test updated

--- core/server/api_container/server/startosis_engine/runtime_value_store/recipe_result_repository_test.go
+++ core/server/api_container/server/startosis_engine/runtime_value_store/recipe_result_repository_test.go
@@ -1,7 +1,10 @@
 package runtime_value_store
 
 import (
+	port_spec_core "github.com/kurtosis-tech/kurtosis/container-engine-lib/lib/backend_interface/objects/port_spec"
 	"github.com/kurtosis-tech/kurtosis/container-engine-lib/lib/database_accessors/enclave_db"
+	"github.com/kurtosis-tech/kurtosis/core/server/api_container/server/startosis_engine/kurtosis_types/directory"
+	"github.com/kurtosis-tech/kurtosis/core/server/api_container/server/startosis_engine/kurtosis_types/port_spec"
 	"github.com/stretchr/testify/require"
 	bolt "go.etcd.io/bbolt"
 	"go.starlark.net/starlark"
@@ -17,6 +20,7 @@ const (
 	firstKey            = "mykey"
 	secondKey           = "mySecondKey"
 	thirdKey            = "myThirdKey"
+	fourthKey           = "myFourthKey"
 	starlarkStringValue = starlark.String("my-value")
 )
 
@@ -39,10 +43,23 @@ func TestRecipeResultSaveKey_Success(t *testing.T) {
 func TestRecipeResultSaveAndGet_Success(t *testing.T) {
 	repository := getRecipeResultRepositoryForTest(t)
 
+	startosisPortSpecType, interpretationErr := port_spec.CreatePortSpec(
+		uint16(443),
+		port_spec_core.TransportProtocol_TCP,
+		nil,
+		"10s",
+	)
+	require.Nil(t, interpretationErr)
+
+	startosisDirectoryType, interpretationErr := directory.CreateDirectoryFromFilesArtifact("fake-file-artifact-name")
+	require.Nil(t, interpretationErr)
+
 	resultValue := map[string]string{
-		firstKey:  starlarkStringValue.GoString(),
-		secondKey: starlarkIntValue.String(),
-		thirdKey:  starlarkBoolValue.String(),
+		firstKey:   starlarkStringValue.GoString(),
+		secondKey:  starlarkIntValue.String(),
+		thirdKey:   starlarkBoolValue.String(),
+		fourthKey:  startosisPortSpecType.String(),
+		"fifthKey": startosisDirectoryType.String(),
 	}
 
 	err := repository.Save(randomUuid, resultValue)
