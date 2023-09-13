59366c3204c8fc9b50b9c286fa16428832ea0d32
Laurent Luce <laurentluce49@yahoo.com>
2023-09-13 19:57:37 -0400

    Add CLI service inspect command.

---
+++ cli/cli/helpers/service_status_stringifier/service_status_stringifier.go
@@ -0,0 +1,23 @@
+package service_status_stringifier
+
+import (
+	"github.com/fatih/color"
+	"github.com/kurtosis-tech/kurtosis/api/golang/core/kurtosis_core_rpc_api_bindings"
+)
+
+var (
+	colorizeRunning = color.New(color.FgGreen).SprintFunc()
+	colorizeStopped = color.New(color.FgYellow).SprintFunc()
+)
+
+func ServiceStatusStringifier(serviceStatus kurtosis_core_rpc_api_bindings.ServiceStatus) string {
+	serviceStatusStr := kurtosis_core_rpc_api_bindings.ServiceStatus_name[int32(serviceStatus)]
+	switch serviceStatus {
+	case kurtosis_core_rpc_api_bindings.ServiceStatus_STOPPED:
+		return colorizeStopped(serviceStatusStr)
+	case kurtosis_core_rpc_api_bindings.ServiceStatus_RUNNING:
+		return colorizeRunning(serviceStatusStr)
+	default:
+		return serviceStatusStr
+	}
+}
