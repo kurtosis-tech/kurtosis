e2b0d2b50ffa7edd2ff50eeba4c0887aa38ff27b
Anders Schwartz <adschwartz@users.noreply.github.com>
2023-09-18 14:41:31 -0400

    feat: changes to the package manager config and the files artifact view (#1322)
    ## Description:
    This change improves the UX of the package manager and the files
    artifact view. It's not a breaking change
    
    ## Is this change user facing?
    YES
    
    ## References (if applicable):
    <!-- Add relevant Github Issues, Discord threads, or other helpful
    information. -->

--- engine/frontend/src/component/Heading.js
+++ engine/frontend/src/component/Heading.js
@@ -1,4 +1,4 @@
-const Heading = ({content, size="text-2xl", padding="p-4", margin="m-2", color="text-black"}) => {
+const Heading = ({content, size="text-2xl", padding="p-4", margin="m-2", color="text-white"}) => {
     const css = `${size} ${padding} ${margin} text-center font-bold ${color}`
     return (
         <div>
