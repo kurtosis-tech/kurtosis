7a832670d9fe7e2163960878d23f3b0b54f592a7
Leandro Poroli <leandro.poroli@kurtosistech.com>
2023-09-21 16:15:39 -0300

    commented code removed

--- internal_testsuites/golang/testsuite/startosis_package_test/startosis_package_testsuite_test.go
+++ internal_testsuites/golang/testsuite/startosis_package_test/startosis_package_testsuite_test.go
@@ -47,8 +47,8 @@ func (suite *StartosisPackageTestSuite) SetupTest() {
 }
 
 func (suite *StartosisPackageTestSuite) TearDownTest() {
-	//err := suite.destroyEnclaveFunc()
-	//require.NoError(suite.T(), err, "Destroying the test suite's enclave process has failed, you will have to remove it manually")
+	err := suite.destroyEnclaveFunc()
+	require.NoError(suite.T(), err, "Destroying the test suite's enclave process has failed, you will have to remove it manually")
 }
 
 func (suite *StartosisPackageTestSuite) RunPackage(ctx context.Context, packageRelativeDirpath string) (*enclaves.StarlarkRunResult, error) {
