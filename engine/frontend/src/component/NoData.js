723a1164777d4f4091c5cc2ba872208c3d634cc5
Anders Schwartz <anders.schwartz@kurtosistech.com>
2023-09-26 19:59:57 -0400

    fix: off center text

--- engine/frontend/src/component/NoData.js
+++ engine/frontend/src/component/NoData.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NoData = ({text="No Data Available", color="text-slate-500", size="text-4xl"}) => {
   return (
-    <div className="flex justify-center m-36">
+    <div className="flex justify-center my-36">
       <p className={`${size} ${color} font-bold`}>{text}</p>
     </div>
   );
