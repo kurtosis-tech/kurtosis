90f1cfb5e5ead735bc7fb535b57085d7c3180fa6
Anders Schwartz <anders.schwartz@kurtosistech.com>
2023-09-29 10:59:24 -0400

    refactor: move KeyValueTable out as its own component

--- engine/frontend/src/component/PackageCatalogForm.js
+++ engine/frontend/src/component/PackageCatalogForm.js
@@ -1,122 +1,13 @@
-import {
-    Box,
-    Button,
-    Center,
-    Checkbox,
-    Flex,
-    Grid,
-    GridItem,
-    HStack,
-    Input,
-    InputGroup,
-    InputLeftAddon,
-    Stack,
-    Text,
-    Tooltip,
-    useClipboard,
-} from '@chakra-ui/react'
+import {Box, Button, Center, Checkbox, Flex, Grid, GridItem, Input, Stack, Text, Tooltip,} from '@chakra-ui/react'
 import PackageCatalogOption from "./PackageCatalogOption";
 import {useLocation, useNavigate} from "react-router";
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import startCase from 'lodash/startCase'
 import {InfoOutlineIcon} from '@chakra-ui/icons'
-import {ObjectInput} from 'react-object-input'
 import {CodeEditor} from "./CodeEditor";
+import KeyValueTable from "./KeyValueTable";
 
 const yaml = require("js-yaml")
-
-const KeyValueTable = (dataCallBack) => {
-    const [value, setValue] = useState({})
-    const clipboard = useClipboard(value);
-
-    useEffect(() => {
-        dataCallBack(JSON.stringify(value))
-        clipboard.setValue(JSON.stringify(value, null, 2))
-    }, [value])
-
-    const paste = async () => {
-        const clipboard = await window.navigator.clipboard.readText()
-        try {
-            const json = JSON.parse(clipboard)
-            setValue(json)
-        } catch (e) {
-            alert("Could not process the content in the clipboard. Please verify it's valid JSON")
-        }
-    }
-
-    return (
-        <Box
-            border="1px"
-            borderColor='gray.700'
-            borderRadius="7"
-            margin={"1px"}
-            padding={1}
-        >
-            <ObjectInput
-                obj={value}
-                onChange={setValue}
-                renderItem={(key, value, updateKey, updateValue, deleteProperty) => (
-                    <Box
-                        margin={1}
-                    >
-                        <HStack
-                            spacing={1}
-                            direction="row"
-                        >
-                            <InputGroup>
-                                <InputLeftAddon children='Key'/>
-                                <Input
-                                    type="text"
-                                    value={key}
-                                    onChange={e => updateKey(e.target.value)}
-                                    size="md"
-                                    variant='filled'
-                                />
-                            </InputGroup>
-
-                            <InputGroup>
-                                <InputLeftAddon children='Value'/>
-                                <Input
-                                    type="text"
-                                    value={value || " "} // value will be undefined for new rows
-                                    onChange={e => updateValue(e.target.value)}
-                                    size="md"
-                                    variant='filled'
-                                />
-                            </InputGroup>
-                            <Button
-                                onClick={deleteProperty}
-                            >
-                                x
-                            </Button>
-                        </HStack>
-                    </Box>
-                )}
-                renderAdd={addItem => <Button size={"sm"} margin={1} onClick={addItem}>Add item</Button>}
-                // renderEmpty={() => <p></p>}
-            />
-            <Button
-                margin={1}
-                size={"sm"}
-                onClick={clipboard.onCopy}
-            >
-                <Tooltip label="Copy as JSON">
-                    {clipboard.hasCopied ? "Copied!" : "Copy"}
-                </Tooltip>
-
-            </Button>
-            <Button
-                margin={1}
-                size={"sm"}
-                onClick={paste}
-            >
-                <Tooltip label='Paste as a JSON key value map, e.g. `{ "key_1": "value", "key_2": 1 }` '>
-                    Paste
-                </Tooltip>
-            </Button>
-        </Box>
-    )
-}
 
 const prettyPrintTypeSpecialCases = (type, arg) => {
     if (type === "LIST") {
