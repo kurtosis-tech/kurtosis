700adfbe102e79286b1962e0934714e8899653c8
Gyanendra Mishra <anomaly.the@gmail.com>
2023-10-02 19:13:35 +0100

    ran linter on tests

--- internal_testsuites/golang/testsuite/startosis_package_test/startosis_package_testsuite_test.go
+++ internal_testsuites/golang/testsuite/startosis_package_test/startosis_package_testsuite_test.go
@@ -8,7 +8,6 @@ import (
 	"testing"
 
 	"github.com/kurtosis-tech/kurtosis-cli/golang_internal_testsuite/test_helpers"
-	"github.com/kurtosis-tech/kurtosis/api/golang/core/kurtosis_core_rpc_api_bindings"
 	"github.com/kurtosis-tech/kurtosis/api/golang/core/lib/enclaves"
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/require"
@@ -18,10 +17,6 @@ import (
 const (
 	name           = "startosis-package"
 	emptyRunParams = "{}"
-)
-
-var (
-	noExperimentalFeature = []kurtosis_core_rpc_api_bindings.KurtosisFeatureFlag{}
 )
 
 type StartosisPackageTestSuite struct {
