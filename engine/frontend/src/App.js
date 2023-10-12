9916f5723d55e9cd9edeb2d39797f1f84eec05fe
Anders Schwartz <anders.schwartz@kurtosistech.com>
2023-10-12 12:24:22 -0400

    new build

--- engine/frontend/src/App.js
+++ engine/frontend/src/App.js
@@ -3,7 +3,7 @@ import Home from './component/Home';
 import { ChakraProvider } from '@chakra-ui/react'
 
 const App = () => {
-  console.log("Enclave Manager version: 2023-10-05-01")
+  console.log("Enclave Manager version: 2023-10-12-01")
   return (
     <ChakraProvider>
       <div className="h-screen w-screen bg-[#171923]">
