52b939f89c6119780edf91183ec103350085981f
Omar <omar@omar>
2023-10-16 11:54:50 +0100

    This was testing runResult in places instead of runResult2.  Introduced err2 also to be safe.

--- internal_testsuites/golang/testsuite/startosis_replace_test/startosis_replace_with_local_and_then_with_remote_test.go
+++ internal_testsuites/golang/testsuite/startosis_replace_test/startosis_replace_with_local_and_then_with_remote_test.go
@@ -16,17 +16,21 @@ func (suite *StartosisReplaceTestSuite) TestStartosisReplaceWithLocalAndThenWith
 
 	t := suite.T()
 	require.NoError(t, err)
+	require.NotNil(t, runResult)
+
 	require.Nil(t, runResult.InterpretationError)
 	require.Empty(t, runResult.ValidationErrors)
 	require.Nil(t, runResult.ExecutionError)
 	expectedResult := "Replace with local package loaded.\nVerification succeeded. Value is '\"msg-loaded-from-local-dependency\"'.\n"
 	require.Equal(t, expectedResult, string(runResult.RunOutput))
 
-	runResult2, err := suite.RunPackageWithParams(ctx, packageWithoutReplaceRelPath, packageWithoutReplaceParams)
-	require.NoError(t, err)
-	require.Nil(t, runResult.InterpretationError)
-	require.Empty(t, runResult.ValidationErrors)
-	require.Nil(t, runResult.ExecutionError)
+	runResult2, err2 := suite.RunPackageWithParams(ctx, packageWithoutReplaceRelPath, packageWithoutReplaceParams)
+	require.NoError(t, err2)
+	require.NotNil(t, runResult2)
+
+	require.Nil(t, runResult2.InterpretationError)
+	require.Empty(t, runResult2.ValidationErrors)
+	require.Nil(t, runResult2.ExecutionError)
 	expectedResult2 := "Without replace package loaded.\nVerification succeeded. Value is '\"dependency-loaded-from-main\"'.\n"
 	require.Equal(t, expectedResult2, string(runResult2.RunOutput))
 }
