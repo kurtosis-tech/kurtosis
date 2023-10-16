3e1208bc9340302db49a041fc93b1e2d565e6abc
Gyanendra Mishra <anomaly.the@gmail.com>
2023-10-16 13:48:07 +0100

    fix: show container status instead of service status in enclave inspect (#1560)
    ## Description:
    If a service eventually dies we would still show `RUNNING` while in
    reality it would be stopped
    
    1. Create an enclave
    2. Stop a service
    3. Use inspect -> this prints service status Running (Kurtosis was told
    to keep it running)
    
    Post this PR
    
    3. This would print container status -> Stopped - actually reflecting
    reality
    
    
    ## Is this change user facing?
    YES
    
    ## References (if applicable):
    Closes #1351

--- cli/cli/commands/enclave/inspect/user_services.go
+++ cli/cli/commands/enclave/inspect/user_services.go
@@ -5,22 +5,17 @@ import (
 	"github.com/kurtosis-tech/kurtosis/api/golang/core/kurtosis_core_rpc_api_bindings"
 	"github.com/kurtosis-tech/kurtosis/api/golang/engine/kurtosis_engine_rpc_api_bindings"
 	"github.com/kurtosis-tech/kurtosis/api/golang/engine/lib/kurtosis_context"
+	"github.com/kurtosis-tech/kurtosis/cli/cli/helpers/container_status_stringifier"
 	"github.com/kurtosis-tech/kurtosis/cli/cli/helpers/output_printers"
-	"github.com/kurtosis-tech/kurtosis/cli/cli/helpers/service_status_stringifier"
 	"github.com/kurtosis-tech/kurtosis/cli/cli/helpers/user_services"
 	"github.com/kurtosis-tech/stacktrace"
 )
 
 const (
-	userServiceUUIDColHeader      = "UUID"
-	userServiceNameColHeader      = "Name"
-	userServicePortsColHeader     = "Ports"
-	userServiceStatusColHeader    = "Status"
-	defaultEmptyIPAddrForServices = ""
-	emptyApplicationProtocol      = ""
-	missingPortPlaceholder        = "<none>"
-	linkDelimeter                 = "://"
-	defaultEmptyIPAddrForAPIC     = ""
+	userServiceUUIDColHeader   = "UUID"
+	userServiceNameColHeader   = "Name"
+	userServicePortsColHeader  = "Ports"
+	userServiceStatusColHeader = "Status"
 )
 
 func printUserServices(ctx context.Context, _ *kurtosis_context.KurtosisContext, enclaveInfo *kurtosis_engine_rpc_api_bindings.EnclaveInfo, showFullUuids bool, isAPIContainerRunning bool) error {
@@ -49,8 +44,8 @@ func printUserServices(ctx context.Context, _ *kurtosis_context.KurtosisContext,
 			uuidToPrint = uuidStr
 		}
 
-		serviceStatus := userService.GetServiceStatus()
-		serviceStatusStr := service_status_stringifier.ServiceStatusStringifier(serviceStatus)
+		containerStatus := userService.GetContainer().GetStatus()
+		containerStatusStr := container_status_stringifier.ContainerStatusStringifier(containerStatus)
 
 		portBindingLines, err := user_services.GetUserServicePortBindingStrings(userService)
 		if err != nil {
@@ -59,7 +54,7 @@ func printUserServices(ctx context.Context, _ *kurtosis_context.KurtosisContext,
 		firstPortBindingLine := portBindingLines[0]
 		additionalPortBindingLines := portBindingLines[1:]
 
-		if err := tablePrinter.AddRow(uuidToPrint, serviceIdStr, firstPortBindingLine, serviceStatusStr); err != nil {
+		if err := tablePrinter.AddRow(uuidToPrint, serviceIdStr, firstPortBindingLine, containerStatusStr); err != nil {
 			return stacktrace.Propagate(
 				err,
 				"An error occurred adding row for user service with UUID '%v' to the table printer",
