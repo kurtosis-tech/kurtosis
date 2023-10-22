9bf025d1a9044270d801cb6ad8357cb6c729287d
Ben Gazzard <ben@dartoxia.com>
2023-10-22 15:22:18 +0100

    Style breadcrumbs

--- enclave-manager/web/src/emui/enclaves/EnclaveBreadcrumbs.tsx
+++ enclave-manager/web/src/emui/enclaves/EnclaveBreadcrumbs.tsx
@@ -1,6 +1,7 @@
 import { Breadcrumbs, EnclaveBreadCrumb } from "../../components/Breadcrumbs";
 import { isDefined } from "../../utils";
 import { useEnclaveRouteMatches } from "./types";
+import { Flex } from "@chakra-ui/react";
 
 export const EnclaveBreadcrumbs = () => {
   const matches = useEnclaveRouteMatches();
@@ -9,10 +10,12 @@ export const EnclaveBreadcrumbs = () => {
     .filter((match) => isDefined(match.handle?.name))
     .map((match) => ({ name: match.handle.name(match.data), destination: match.pathname }));
 
-  const crumbs: EnclaveBreadCrumb[] = [
-    { name: "Kurtosis Enclave Manager", destination: "/" },
-    ...(matchCrumbs.length > 1 ? matchCrumbs : []),
-  ];
+  const crumbs: EnclaveBreadCrumb[] = [...(matchCrumbs.length > 1 ? matchCrumbs : [])];
 
-  return <Breadcrumbs crumbs={crumbs} />;
+  return (
+    <Flex h="40px" p={"4px 0"} flexDirection={"column"} justifyContent={"center"} alignItems={"flex-start"}>
+      <Breadcrumbs crumbs={crumbs} />
+      &nbsp;
+    </Flex>
+  );
 };
