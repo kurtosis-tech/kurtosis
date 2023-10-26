4c8118b5071e2756e46348ae537c005b259d31f2
Ben Gazzard <ben@dartoxia.com>
2023-10-26 10:01:21 +0100

    Use env file

--- enclave-manager/web/src/client/constants.ts
+++ enclave-manager/web/src/client/constants.ts
@@ -1,3 +1,8 @@
-export const KURTOSIS_DEFAULT_HOST = "localhost";
-export const KURTOSIS_DEFAULT_PORT = 8081;
-export const KURTOSIS_DEFAULT_URL = `http://${KURTOSIS_DEFAULT_HOST}:${KURTOSIS_DEFAULT_PORT}`;
+import { isDefined } from "../utils";
+
+export const KURTOSIS_DEFAULT_HOST = process.env.REACT_APP_KURTOSIS_DEFAULT_HOST || "localhost";
+export const KURTOSIS_DEFAULT_PORT = isDefined(process.env.REACT_APP_KURTOSIS_DEFAULT_PORT)
+  ? parseInt(process.env.REACT_APP_KURTOSIS_DEFAULT_PORT)
+  : 8081;
+export const KURTOSIS_DEFAULT_URL =
+  process.env.REACT_APP_KURTOSIS_DEFAULT_URL || `http://${KURTOSIS_DEFAULT_HOST}:${KURTOSIS_DEFAULT_PORT}`;
