863d5b9318066ef6e773669abfb9d596108a53c8
Anders Schwartz <anders.schwartz@kurtosistech.com>
2023-11-06 15:10:42 -0500

    reset

--- scripts/docker-image-builder.sh
+++ scripts/docker-image-builder.sh
@@ -40,11 +40,11 @@ uname_arch=$(uname -m)
 
 architecture="amd64"
 
-#if [ "$uname_arch" == "x86_64" ] || [ "$uname_arch" == "amd64" ]; then
-#    architecture="amd64"
-#elif [ "$uname_arch" == "aarch64" ] || [ "$uname_arch" == "arm64" ]; then
-#    architecture="arm64"
-#fi
+if [ "$uname_arch" == "x86_64" ] || [ "$uname_arch" == "amd64" ]; then
+    architecture="amd64"
+elif [ "$uname_arch" == "aarch64" ] || [ "$uname_arch" == "arm64" ]; then
+    architecture="arm64"
+fi
 
 # Argument processing
 if "${push_to_registry_container}"; then
