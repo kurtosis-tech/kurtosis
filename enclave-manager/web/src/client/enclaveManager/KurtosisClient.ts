8613d2149052c7cd507845cee53733b38e94161d
Anders Schwartz <anders.schwartz@kurtosistech.com>
2023-11-09 17:39:30 -0500

    ui

--- enclave-manager/web/src/client/enclaveManager/KurtosisClient.ts
+++ enclave-manager/web/src/client/enclaveManager/KurtosisClient.ts
@@ -54,7 +54,7 @@ export abstract class KurtosisClient {
   }
 
   isRunningInCloud() {
-    return this.cloudUrl.host.toLowerCase().includes(KURTOSIS_CLOUD_HOST)
+    return this.cloudUrl.host.toLowerCase().includes(KURTOSIS_CLOUD_HOST);
   }
 
   abstract getHeaderOptions(): { headers?: Headers };
