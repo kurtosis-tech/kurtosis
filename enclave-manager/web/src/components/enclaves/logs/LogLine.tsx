126c7c424ffcd6c08719daa5ef2e73bd20546b43
Anders Schwartz <anders.schwartz@kurtosistech.com>
2023-11-10 13:13:44 -0500

    disable milliseconds

--- enclave-manager/web/src/components/enclaves/logs/LogLine.tsx
+++ enclave-manager/web/src/components/enclaves/logs/LogLine.tsx
@@ -50,7 +50,7 @@ export const LogLine = ({ timestamp, message, status }: LogLineProps) => {
           color={"grey"}
           minW={"200px"}
         >
-          <>{timestamp.toLocal().toFormat("yyyy-MM-dd HH:mm:ss.SSS ZZZZ")}</>
+          <>{timestamp.toLocal().toFormat("yyyy-MM-dd HH:mm:ss ZZZZ")}</>
         </Box>
       )}
       <Box
