824b085da9fd92bfacd3a464f2cedfb7704c1534
Anders Schwartz <anders.schwartz@kurtosistech.com>
2023-11-10 09:25:49 -0500

    clean up

--- enclave-manager/web/src/emui/enclaves/enclave/service/logs/ServiceLogs.tsx
+++ enclave-manager/web/src/emui/enclaves/enclave/service/logs/ServiceLogs.tsx
@@ -26,10 +26,10 @@ export async function reTryCatch(callback, times: number = 1, isRetry: boolean =
     return await callback(isRetry);
   } catch (error) {
     if (times > 0) {
-      console.log("retrying another times ", times);
+      console.info(`retrying another ${times} times`);
       return await reTryCatch(callback, times - 1, true);
     } else {
-      console.log("retry: giving up and throwing error");
+      console.info("retry: giving up and throwing error");
       throw error;
     }
   }
@@ -42,33 +42,31 @@ export const ServiceLogs = ({ enclave, service }: ServiceLogsProps) => {
   useEffect(() => {
     let canceled = false;
     const abortController = new AbortController();
-    setLogLines([]);
-    const callback = async (isRetry: boolean) => {
-      // TODO: when we have a way to track where we left off, we don't have to clear and re-read everything
-      if(isRetry) setLogLines([]);
-      try {
-        console.log("Creating a new logging stream");
-        for await (const lineGroup of await kurtosisClient.getServiceLogs(
-          abortController,
-          enclave,
-          [service],
-        )) {
-          if (canceled) return;
-          const lineGroupForService = lineGroup.serviceLogsByServiceUuid[service.serviceUuid];
-          if (!isDefined(lineGroupForService)) continue;
-          const parsedLines = serviceLogLineToLogLineProps(lineGroupForService.line, lineGroupForService.timestamp);
-          setLogLines((logLines) => [...logLines, ...parsedLines]);
+    if(kurtosisClient) {
+      setLogLines([]);
+      const callback = async (isRetry: boolean) => {
+        // TODO: when we have a way to track where we left off, we don't have to clear and re-read everything
+        if (isRetry) setLogLines([]);
+        console.info("Created a new logging stream");
+        try {
+          for await (const lineGroup of await kurtosisClient.getServiceLogs(abortController,enclave,[service])) {
+            if (canceled) return;
+            const lineGroupForService = lineGroup.serviceLogsByServiceUuid[service.serviceUuid];
+            if (!isDefined(lineGroupForService)) continue;
+            const parsedLines = serviceLogLineToLogLineProps(lineGroupForService.line, lineGroupForService.timestamp);
+            setLogLines((logLines) => [...logLines, ...parsedLines]);
+          }
+        } catch (error: any) {
+          if (canceled) {
+            console.info("The logging stream was successfully canceled (not an error)", error);
+            return;
+          }
+          console.error("An unhandled error occurred while streaming logs", error);
+          throw error;
         }
-      } catch (error: any) {
-        if (canceled) {
-          console.log("The logging stream was successfully canceled (not a real error)", error);
-          return;
-        }
-        console.error("An unhandled error occurred while streaming logs", error);
-        throw error;
-      }
-    };
-    reTryCatch(callback, 10000);
+      };
+      reTryCatch(callback, 100);
+    }
     return () => {
       canceled = true;
       abortController.abort();
