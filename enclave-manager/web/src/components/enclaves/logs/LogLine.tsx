db4f47be30ac54e4d0de732c920591c422ef9ecf
Anders Schwartz <anders.schwartz@kurtosistech.com>
2023-11-10 13:20:09 -0500

    fix width of timestamp

--- enclave-manager/web/src/components/enclaves/logs/LogLine.tsx
+++ enclave-manager/web/src/components/enclaves/logs/LogLine.tsx
@@ -48,7 +48,7 @@ export const LogLine = ({ timestamp, message, status }: LogLineProps) => {
           fontWeight={600}
           fontFamily={logFontFamily}
           color={"grey"}
-          minW={"200px"}
+          minW={"170px"}
         >
           <>{timestamp.toLocal().toFormat("yyyy-MM-dd HH:mm:ss ZZZZ")}</>
         </Box>
