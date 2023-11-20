7fce5b59d1060f99f8c2cbd54d7bb8483150310e
Anders Schwartz <adschwartz@users.noreply.github.com>
2023-11-20 12:40:07 -0500

    feat: search in service logs (#1830)
    ## Description:
    Adding a search bar to the service logs window
    
    ![image](https://github.com/kurtosis-tech/kurtosis/assets/11248996/60f962af-743d-49a4-87d5-74e2f02fed82)
    
    
    ## Is this change user facing?
    YES
    ## References (if applicable):
    Closes #1791

--- enclave-manager/web/src/components/enclaves/logs/LogLine.tsx
+++ enclave-manager/web/src/components/enclaves/logs/LogLine.tsx
@@ -4,6 +4,9 @@ import { DateTime } from "luxon";
 import { isDefined } from "../../../utils";
 // @ts-ignore
 import hasAnsi from "has-ansi";
+import { ReactElement } from "react";
+import { normalizeLogText } from "./LogViewer";
+
 const Convert = require("ansi-to-html");
 const convert = new Convert();
 
@@ -15,9 +18,21 @@ export type LogLineProps = {
   status?: LogStatus;
 };
 
+export type LogLineSearch = {
+  searchTerm: string;
+  pattern: RegExp;
+};
+
+export type LogLineInput = {
+  logLineProps: LogLineProps;
+  logLineSearch?: LogLineSearch;
+  selected: boolean | undefined;
+};
+
 const logFontFamily = "Menlo, Monaco, Inconsolata, Consolas, Courier, monospace";
 
-export const LogLine = ({ timestamp, message, status }: LogLineProps) => {
+export const LogLine = ({ logLineProps, logLineSearch, selected }: LogLineInput) => {
+  const { timestamp, message, status } = logLineProps;
   const statusToColor = (status?: LogStatus) => {
     switch (status) {
       case "error":
@@ -29,16 +44,58 @@ export const LogLine = ({ timestamp, message, status }: LogLineProps) => {
     }
   };
 
-  const processText = (message: string) => {
-    if (hasAnsi(message)) {
-      return parse(convert.toHtml(message));
+  const processText = (text: string, selected: boolean | undefined) => {
+    let reactComponent;
+    if (hasAnsi(text)) {
+      reactComponent = parse(convert.toHtml(text));
     } else {
-      return <>{message}</>;
+      reactComponent = <>{text}</>;
     }
+
+    if (logLineSearch) {
+      reactComponent = HighlightPattern({ text, regex: logLineSearch.pattern, selected });
+    }
+    return reactComponent;
+  };
+
+  const HighlightPattern = ({
+    text,
+    regex,
+    selected,
+  }: {
+    text: string;
+    regex: RegExp;
+    selected: boolean | undefined;
+  }) => {
+    const normalizedLogText = normalizeLogText(text);
+    const splitText = normalizedLogText.split(regex);
+    const matches = normalizedLogText.match(regex);
+
+    if (!isDefined(matches)) {
+      return <span>{text}</span>;
+    }
+
+    return (
+      <span>
+        {splitText.reduce(
+          (arr: (ReactElement | string)[], element, index) =>
+            matches[index]
+              ? [
+                  ...arr,
+                  element,
+                  <mark key={index}>
+                    {matches[index]}
+                  </mark>,
+                ]
+              : [...arr, element],
+          [],
+        )}
+      </span>
+    );
   };
 
   return (
-    <Flex p={"2px 0"} m={"0 16px"} gap={"8px"} alignItems={"top"}>
+    <Flex p={"2px 0"} m={"0 16px"} gap={"8px"} alignItems={"top"} backgroundColor={selected ? "gray.600" : ""}>
       {isDefined(timestamp) && (
         <Box
           as={"pre"}
@@ -62,8 +119,9 @@ export const LogLine = ({ timestamp, message, status }: LogLineProps) => {
         fontWeight={400}
         fontFamily={logFontFamily}
         color={statusToColor(status)}
+        _focus={{ boxShadow: "outline" }}
       >
-        {message && processText(message)}
+        {message && processText(message, selected)}
       </Box>
     </Flex>
   );
