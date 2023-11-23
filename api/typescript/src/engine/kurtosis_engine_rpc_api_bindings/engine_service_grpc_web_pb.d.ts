b4ad9fac46765ed549eab21d6331deedb904b154
Omar <omar@omar>
2023-11-23 13:52:56 +0000

    regen protobufs following install of tools

--- api/typescript/src/engine/kurtosis_engine_rpc_api_bindings/engine_service_grpc_web_pb.d.ts
+++ api/typescript/src/engine/kurtosis_engine_rpc_api_bindings/engine_service_grpc_web_pb.d.ts
@@ -1,7 +1,7 @@
 import * as grpcWeb from 'grpc-web';
 
-import * as engine_service_pb from './engine_service_pb';
-import * as google_protobuf_empty_pb from 'google-protobuf/google/protobuf/empty_pb';
+import * as engine_service_pb from './engine_service_pb'; // proto import: "engine_service.proto"
+import * as google_protobuf_empty_pb from 'google-protobuf/google/protobuf/empty_pb'; // proto import: "google/protobuf/empty.proto"
 
 
 export class EngineServiceClient {
