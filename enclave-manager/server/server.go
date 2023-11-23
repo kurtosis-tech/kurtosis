4dfc8d576e42d9de7f575098b97f72e998436731
Leandro Poroli <leandro.poroli@kurtosistech.com>
2023-11-23 16:51:21 -0300

    setting timeout in GRPC call in EM

--- enclave-manager/server/server.go
+++ enclave-manager/server/server.go
@@ -272,6 +272,7 @@ func (c *WebServer) CreateEnclave(ctx context.Context, req *connect.Request[kurt
 	if !auth {
 		return nil, stacktrace.Propagate(err, "User not authorized")
 	}
+	ctx, _ = context.WithTimeout(context.Background(), time.Second*120)
 	result, err := (*c.engineServiceClient).CreateEnclave(ctx, req)
 	if err != nil {
 		logrus.Infof("[LEO-DEBUG] create enclave error:\n%v", err)
