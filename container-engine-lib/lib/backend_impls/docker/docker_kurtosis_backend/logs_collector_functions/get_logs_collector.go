839c942c93dbaba192cf39200f397f72dc9bb473
Leandro Poroli <leandro.poroli@kurtosistech.com>
2023-11-24 10:28:41 -0300

    Create enclave network process removed

--- container-engine-lib/lib/backend_impls/docker/docker_kurtosis_backend/logs_collector_functions/get_logs_collector.go
+++ container-engine-lib/lib/backend_impls/docker/docker_kurtosis_backend/logs_collector_functions/get_logs_collector.go
@@ -2,7 +2,6 @@ package logs_collector_functions
 
 import (
 	"context"
-	"github.com/kurtosis-tech/kurtosis/container-engine-lib/lib/backend_impls/docker/docker_kurtosis_backend/shared_helpers"
 	"github.com/kurtosis-tech/kurtosis/container-engine-lib/lib/backend_impls/docker/docker_manager"
 	"github.com/kurtosis-tech/kurtosis/container-engine-lib/lib/backend_interface/objects/enclave"
 	"github.com/kurtosis-tech/kurtosis/container-engine-lib/lib/backend_interface/objects/logs_collector"
@@ -18,12 +17,19 @@ func GetLogsCollectorForEnclave(
 	resultMaybeLogsCollector *logs_collector.LogsCollector,
 	resultErr error,
 ) {
-	enclaveNetworkId, err := shared_helpers.GetEnclaveNetworkByEnclaveUuid(ctx, enclaveUuid, dockerManager)
+	/*enclaveNetworkId, err := shared_helpers.GetEnclaveNetworkByEnclaveUuid(ctx, enclaveUuid, dockerManager)
 	if err != nil {
 		return nil, stacktrace.Propagate(err, "An error occurred while retrieving the network id for the enclave '%v'", enclaveUuid)
 	}
 
-	maybeLogsCollectorObject, _, err := getLogsCollectorObjectAndContainerId(ctx, enclaveUuid, enclaveNetworkId, dockerManager)
+	*/
+
+	bridgeNetwork, err := dockerManager.GetNetworksByName(ctx, "bridge")
+	if err != nil {
+		return nil, stacktrace.Propagate(err, "An error occurred while retrieving the network id for the enclave '%v'", enclaveUuid)
+	}
+
+	maybeLogsCollectorObject, _, err := getLogsCollectorObjectAndContainerId(ctx, enclaveUuid, bridgeNetwork[0], dockerManager)
 	if err != nil {
 		return nil, stacktrace.Propagate(err, "An error occurred getting the logs collector")
 	}
