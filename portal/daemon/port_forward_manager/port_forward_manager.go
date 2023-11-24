4fb84806c9bceb155da41f747c9a0595df81aec7
Omar <omar@omar>
2023-11-24 11:19:29 +0000

    Basic validation of args based on enclave detail

--- portal/daemon/port_forward_manager/port_forward_manager.go
+++ portal/daemon/port_forward_manager/port_forward_manager.go
@@ -4,9 +4,12 @@ import (
 	"context"
 	"github.com/kurtosis-tech/kurtosis/api/golang/engine/lib/kurtosis_context"
 	"github.com/kurtosis-tech/stacktrace"
+	"github.com/sirupsen/logrus"
 )
 
-const ()
+const (
+	chiselPortInApicReplaceWithLookupLater = 9501
+)
 
 type PortForwardManager struct {
 	kurtosis *kurtosis_context.KurtosisContext
@@ -28,7 +31,30 @@ func (manager *PortForwardManager) Ping(ctx context.Context) error {
 	return nil
 }
 
+// TODO(omar): make a return struct - see what we end up using to represent port forwards
 func (manager *PortForwardManager) ForwardUserServiceToEphemeralPort(ctx context.Context, enclaveId string, serviceId string, portId string) (uint16, error) {
+	enclave, err := manager.kurtosis.GetEnclave(ctx, enclaveId)
+	if err != nil {
+		return 0, stacktrace.Propagate(err, "Failed to lookup enclave '%v' from Kurtosis Engine", enclaveId)
+	}
+
+	enclaveContext, err := manager.kurtosis.GetEnclaveContext(ctx, enclaveId)
+	if err != nil {
+		return 0, stacktrace.Propagate(err, "Failed to get enclave context for enclave '%v'", enclaveId)
+	}
+
+	serviceContext, err := enclaveContext.GetServiceContext(serviceId)
+	if err != nil {
+		return 0, stacktrace.Propagate(err, "Failed to get service context for service '%v' in enclave '%v'", serviceId, enclaveId)
+	}
+
+	serviceIpAddress := serviceContext.GetPrivateIPAddress()
+	privatePortSpec, exists := serviceContext.GetPrivatePorts()[portId]
+	if !exists {
+		return 0, stacktrace.NewError("Failed to find requested port id '%v' in service '%v' in enclave '%v'.  Available ports are: %v", portId, serviceId, enclaveId, serviceContext.GetPrivatePorts())
+	}
+
+	logrus.Debugf("Found service information for (%v, %v, %v): service running at %v:%d in enclave: %v", enclaveId, serviceId, portId, serviceIpAddress, privatePortSpec.GetNumber(), enclave.String())
 	return 0, nil
 }
 
