98b2b4b138c8095003e05fca8ae4cd71421bf869
Tedi Mitiku <tedi.m52@gmail.com>
2023-11-29 15:51:39 -0500

    move where logs collector is created

--- engine/server/engine/enclave_manager/enclave_creator.go
+++ engine/server/engine/enclave_manager/enclave_creator.go
@@ -13,6 +13,11 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+const (
+	defaultHttpLogsCollectorPortNum = uint16(9712)
+	defaultTcpLogsCollectorPortNum  = uint16(9713)
+)
+
 type EnclaveCreator struct {
 	kurtosisBackend                           backend_interface.KurtosisBackend
 	apiContainerKurtosisBackendConfigSupplier api_container_launcher.KurtosisBackendConfigSupplier
@@ -71,6 +76,20 @@ func (creator *EnclaveCreator) CreateEnclave(
 			for enclaveUuid, err := range destroyEnclaveErrs {
 				logrus.Errorf("Expected to be able to cleanup the enclave '%v', but an error was thrown:\n%v", enclaveUuid, err)
 				logrus.Errorf(manualActionRequiredStrFmt, enclaveUuid)
+			}
+		}
+	}()
+
+	// TODO the logs collector has a random private ip address in the enclave network that must be tracked
+	if _, err := creator.kurtosisBackend.CreateLogsCollectorForEnclave(setupCtx, enclaveUuid, defaultTcpLogsCollectorPortNum, defaultHttpLogsCollectorPortNum); err != nil {
+		return nil, stacktrace.Propagate(err, "An error occurred creating the logs collector with TCP port number '%v' and HTTP port number '%v'", defaultTcpLogsCollectorPortNum, defaultHttpLogsCollectorPortNum)
+	}
+	shouldDeleteLogsCollector := true
+	defer func() {
+		if shouldDeleteLogsCollector {
+			err = creator.kurtosisBackend.DestroyLogsCollectorForEnclave(teardownCtx, enclaveUuid)
+			if err != nil {
+				logrus.Errorf("Couldn't cleanup logs collector for enclave '%v' as the following error was thrown:\n%v", enclaveUuid, err)
 			}
 		}
 	}()
@@ -155,8 +174,9 @@ func (creator *EnclaveCreator) CreateEnclave(
 	}
 
 	// Everything started successfully, so the responsibility of deleting the enclave is now transferred to the caller
-	shouldDestroyEnclave = false
 	shouldStopApiContainer = false
+	shouldDeleteLogsCollector = false
+	shouldDestroyEnclave = false
 	return newEnclaveInfo, nil
 }
 
