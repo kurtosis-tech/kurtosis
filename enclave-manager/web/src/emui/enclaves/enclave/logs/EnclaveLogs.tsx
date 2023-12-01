7e2eed6c14745ca6d5891e2ed98c9be9422294ee
Ben Gazzard <ben@dartoxia.com>
2023-12-01 10:36:25 +0000

    Fix layout boxes

--- enclave-manager/web/src/emui/enclaves/enclave/logs/EnclaveLogs.tsx
+++ enclave-manager/web/src/emui/enclaves/enclave/logs/EnclaveLogs.tsx
@@ -124,7 +124,7 @@ export const EnclaveLogs = () => {
       : 0;
 
   return (
-    <AppPageLayout>
+    <AppPageLayout preventPageScroll>
       <LogViewer
         logLines={logLines}
         progressPercent={progressPercent}
