005f2a933b736179b48733beeef76582524aacf5
Laurent Luce <laurentluce49@yahoo.com>
2023-12-04 15:11:44 -0800

    Update Traefik port numbers.

--- container-engine-lib/lib/backend_impls/docker/docker_kurtosis_backend/reverse_proxy_functions/consts.go
+++ container-engine-lib/lib/backend_impls/docker/docker_kurtosis_backend/reverse_proxy_functions/consts.go
@@ -1,6 +1,6 @@
 package reverse_proxy_functions
 
 const (
-	defaultReverseProxyHttpPortNum      = uint16(9720)
-	defaultReverseProxyDashboardPortNum = uint16(9721)
+	defaultReverseProxyHttpPortNum      = uint16(9730)
+	defaultReverseProxyDashboardPortNum = uint16(9731)
 )
