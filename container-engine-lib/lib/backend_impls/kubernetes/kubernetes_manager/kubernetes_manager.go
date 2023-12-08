d3610c2dca2323bc61910ded0ca0f8c39f856776
Gyanendra Mishra <anomaly.the@gmail.com>
2023-12-08 12:04:32 +0530

    try a restart policy

--- container-engine-lib/lib/backend_impls/kubernetes/kubernetes_manager/kubernetes_manager.go
+++ container-engine-lib/lib/backend_impls/kubernetes/kubernetes_manager/kubernetes_manager.go
@@ -1139,7 +1139,7 @@ func (manager *KubernetesManager) CreatePod(
 		Containers:          podContainers,
 		EphemeralContainers: nil,
 		// We don't want Kubernetes auto-magically restarting our containers if they fail
-		RestartPolicy:                 apiv1.RestartPolicyNever,
+		RestartPolicy:                 apiv1.RestartPolicyOnFailure,
 		TerminationGracePeriodSeconds: nil,
 		ActiveDeadlineSeconds:         nil,
 		DNSPolicy:                     "",
