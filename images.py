1ccc4d9724f6151d6cee790e914d35d1515a3a55
Gyanendra Mishra <anomaly.the@gmail.com>
2023-12-08 11:51:02 +0530

    update images.py for generic case

--- images.py
+++ images.py
@@ -1,4 +1,4 @@
-ECR_ADDRESS = "258623609258.dkr.ecr.us-east-2.amazonaws.com"
+MY_USER_NAME = "h4ck3kr3y"
 
 import subprocess
 import sys
@@ -8,7 +8,7 @@ image_tag = sys.argv[1]
 def execute(command):
     return subprocess.check_output(command, shell=True, universal_newlines=True)
 
-login = execute(f"aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin {ECR_ADDRESS}")
+# login = execute(f"aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin {ECR_ADDRESS}")
 
 returned_text = execute(f"docker image ls | grep {image_tag}")
 
@@ -16,12 +16,13 @@ for line in returned_text.split("\n"):
     if line == "":
         continue
     image, tag, image_id = line.split()[:3]
+    author, repo = image.split("/")
     print(image, tag, image_id)
 
     if not image.startswith("kurtosistech"):
         continue
 
-    execute(f"docker tag {image_id} {ECR_ADDRESS}/{image}:{tag}")
-    execute(f"docker push {ECR_ADDRESS}/{image}:{tag}")
+    execute(f"docker tag {image_id} {MY_USER_NAME}/{repo}:{tag}")
+    execute(f"docker push {MY_USER_NAME}/{repo}:{tag}")
 
 
