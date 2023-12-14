dcf8d7d1d93373848e68c39b633aa22bc343fe14
Ben Gazzard <ben@dartoxia.com>
2023-12-14 12:49:00 +0000

    Remove hello from the emui

--- enclave-manager/web/packages/app/src/index.tsx
+++ enclave-manager/web/packages/app/src/index.tsx
@@ -1,9 +1,6 @@
-import { hello } from "kurtosis-ui-components";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { EmuiApp } from "./emui/App";
-
-hello();
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
