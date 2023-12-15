1c4863f4ec112e3c8ca6f095b3551883cbc8a213
Gyanendra Mishra <anomaly.the@gmail.com>
2023-12-15 21:31:18 +0530

    fix: ignore the current status of the service during a start/stop (#1965)

--- core/server/api_container/server/service_network/default_service_network.go
+++ core/server/api_container/server/service_network/default_service_network.go
@@ -463,9 +463,6 @@ func (network *DefaultServiceNetwork) StartServices(
 		if err != nil {
 			return nil, nil, stacktrace.Propagate(err, "An error occurred while getting service registration for identifier '%v'", serviceIdentifier)
 		}
-		if serviceRegistration.GetStatus() == service.ServiceStatus_Started {
-			return nil, nil, stacktrace.NewError("Service '%v' is already started", serviceRegistration.GetName())
-		}
 		serviceRegistrations[serviceRegistration.GetUUID()] = serviceRegistration
 	}
 
@@ -530,9 +527,6 @@ func (network *DefaultServiceNetwork) StopServices(
 		serviceRegistration, err := network.getServiceRegistrationForIdentifierUnlocked(serviceIdentifier)
 		if err != nil {
 			return nil, nil, stacktrace.Propagate(err, "An error occurred while getting service registration for identifier '%v'", serviceIdentifier)
-		}
-		if serviceRegistration.GetStatus() == service.ServiceStatus_Stopped {
-			return nil, nil, stacktrace.NewError("Service '%v' is already stopped", serviceRegistration.GetName())
 		}
 		serviceUuids[serviceRegistration.GetUUID()] = true
 		serviceNamesByUuid[serviceRegistration.GetUUID()] = serviceRegistration.GetName()
