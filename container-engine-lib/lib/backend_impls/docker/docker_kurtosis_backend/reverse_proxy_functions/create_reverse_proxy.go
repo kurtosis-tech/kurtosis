9dc33631c418849da901890a41aa3fb80ec6a732
Leandro Poroli <leandro.poroli@kurtosistech.com>
2023-12-20 11:58:52 -0300

    creating a new reverse proxy if the current is failing

--- container-engine-lib/lib/backend_impls/docker/docker_kurtosis_backend/reverse_proxy_functions/create_reverse_proxy.go
+++ container-engine-lib/lib/backend_impls/docker/docker_kurtosis_backend/reverse_proxy_functions/create_reverse_proxy.go
@@ -26,17 +26,22 @@ func CreateReverseProxy(
 	func(),
 	error,
 ) {
-	_, found, err := getReverseProxyContainer(ctx, dockerManager)
+	proxyDockerContainer, found, err := getReverseProxyContainer(ctx, dockerManager)
 	if err != nil {
 		return nil, nil, stacktrace.Propagate(err, "An error occurred getting reverse proxy container.")
 	}
 	if found {
 		logrus.Debugf("Found existing reverse proxy; cannot start a new one.")
-		reverseProxyObj, _, err := getReverseProxyObjectAndContainerId(ctx, dockerManager)
-		if err != nil {
-			return nil, nil, stacktrace.Propagate(err, "An error occurred getting existing reverse proxy.")
+		reverseProxyObj, _, getProxyObjErr := getReverseProxyObjectAndContainerId(ctx, dockerManager)
+		if getProxyObjErr == nil {
+			return reverseProxyObj, nil, nil
 		}
-		return reverseProxyObj, nil, nil
+		logrus.Debugf("Something failed while trying to create the reverse proxy object using container with ID '%s'. Error was:\n%s", proxyDockerContainer.GetId(), getProxyObjErr.Error())
+		logrus.Debugf("Destroying the failing reverse proxy to create a new one...")
+		if destroyProxyContainerErr := destroyReverseProxyWithContainerId(ctx, dockerManager, proxyDockerContainer.GetId()); destroyProxyContainerErr != nil {
+			return nil, nil, stacktrace.Propagate(err, "an error occurred destroying the current reverse proxy that was failing to create a new one")
+		}
+		logrus.Debugf("... current reverse proxy successfully destroyed, starting a new one now.")
 	}
 
 	reverseProxyNetwork, err := shared_helpers.GetEngineAndLogsComponentsNetwork(ctx, dockerManager)
