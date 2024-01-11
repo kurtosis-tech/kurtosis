7593a9c0fd407319e08ab48f4aa1d9f142abd993
Ben Gazzard <ben@dartoxia.com>
2024-01-11 14:42:31 +0000

    Prettier

--- enclave-manager/web/cypress/e2e/enclaveList.cy.ts
+++ enclave-manager/web/cypress/e2e/enclaveList.cy.ts
@@ -2,51 +2,50 @@ describe("Enclave List", () => {
   let enclaveName: string = "unknown";
 
   beforeEach(() => {
-    enclaveName = `cyrpress-test-${Math.floor(Math.random()*100000)}`
-  })
+    enclaveName = `cyrpress-test-${Math.floor(Math.random() * 100000)}`;
+  });
 
   afterEach(() => {
-    cy.deleteEnclave(enclaveName)
-  })
+    cy.deleteEnclave(enclaveName);
+  });
 
   it("Can create and update an enclave", () => {
-    cy.goToEnclaveList()
+    cy.goToEnclaveList();
     cy.contains("Enclaves");
 
     // Create a Postgres enclave
     cy.contains("New Enclave").click();
     cy.focused().type("postgres");
-    const configurationDrawer = cy.contains("[role='dialog']", "Enclave Configuration")
-    configurationDrawer.contains("Postgres Package").click()
+    const configurationDrawer = cy.contains("[role='dialog']", "Enclave Configuration");
+    configurationDrawer.contains("Postgres Package").click();
 
-    configurationDrawer.focusInputWithLabel("Enclave name")
-      .type(enclaveName)
+    configurationDrawer.focusInputWithLabel("Enclave name").type(enclaveName);
 
     cy.contains("button", "Run").click();
 
-    cy.url({timeout: 10 * 1000}).should("match", /enclave\/[^/]+\/logs/);
+    cy.url({ timeout: 10 * 1000 }).should("match", /enclave\/[^/]+\/logs/);
 
     cy.contains("button", "Edit").should("be.disabled");
-    cy.contains("Validating", {timeout: 10 * 1000})
-    cy.contains("Script completed", {timeout: 10 * 1000});
+    cy.contains("Validating", { timeout: 10 * 1000 });
+    cy.contains("Script completed", { timeout: 10 * 1000 });
     cy.contains("button", "Edit").should("be.enabled");
 
     // Go to the enclave overview
-    cy.contains("Go to Enclave Overview").click()
+    cy.contains("Go to Enclave Overview").click();
     cy.contains("[role='dialog'] button", "Continue").click();
     cy.url().should("match", /enclave\/[^/]+/);
-    cy.findCardWithName("Name").contains(enclaveName)
-    cy.contains("table", "postgres")
+    cy.findCardWithName("Name").contains(enclaveName);
+    cy.contains("table", "postgres");
     cy.get("table").contains("button", "postgres").click();
 
     cy.url().should("match", /enclave\/[^/]+\/service\/[^/]+/);
-    cy.findCardWithName("Name").contains("postgres")
-    cy.findCardWithName("Status").contains("Running", {matchCase: false});
+    cy.findCardWithName("Name").contains("postgres");
+    cy.findCardWithName("Status").contains("Running", { matchCase: false });
 
     // Update the postgres instance
     cy.contains("button", "Edit").click();
     cy.focusInputWithLabel("Max CPU").type("1024");
     cy.contains("button", "Update").click();
-    cy.contains("Script completed", {timeout: 10 * 1000});
+    cy.contains("Script completed", { timeout: 10 * 1000 });
   });
 });
--- enclave-manager/web/cypress/support/commands.ts
+++ enclave-manager/web/cypress/support/commands.ts
@@ -3,37 +3,33 @@
 export {};
 
 Cypress.Commands.add("goToEnclaveList", () => {
-  return cy.visit("http://localhost:9711")
-})
+  return cy.visit("http://localhost:9711");
+});
 
-Cypress.Commands.add('focusInputWithLabel', (label: string) => {
-  cy.contains("label", label, {matchCase: false})
-    .parents(".chakra-form-control")
-    .first()
-    .find("input")
-    .click()
-})
+Cypress.Commands.add("focusInputWithLabel", (label: string) => {
+  cy.contains("label", label, { matchCase: false }).parents(".chakra-form-control").first().find("input").click();
+});
 
-Cypress.Commands.add('findCardWithName', (name: string) => {
-  return cy.contains(".chakra-card", name, {matchCase: false})
-})
+Cypress.Commands.add("findCardWithName", (name: string) => {
+  return cy.contains(".chakra-card", name, { matchCase: false });
+});
 
-Cypress.Commands.add('deleteEnclave', (enclaveName: string) => {
+Cypress.Commands.add("deleteEnclave", (enclaveName: string) => {
   cy.goToEnclaveList();
-  cy.contains("tr", enclaveName).find(".chakra-checkbox").click()
+  cy.contains("tr", enclaveName).find(".chakra-checkbox").click();
   cy.contains("button", "Delete").click();
   cy.contains("[role='dialog'] button", "Delete").click();
-  cy.contains("[role='dialog']", "Delete", {timeout: 10 * 1000}).should("not.exist")
-  cy.contains("tr", enclaveName).should("not.exist")
-})
+  cy.contains("[role='dialog']", "Delete", { timeout: 10 * 1000 }).should("not.exist");
+  cy.contains("tr", enclaveName).should("not.exist");
+});
 
 declare global {
   namespace Cypress {
     interface Chainable {
-      deleteEnclave(enclaveName: string): Chainable<void>
-      focusInputWithLabel(label: string): Chainable<void>
-      findCardWithName(label: string): Chainable<JQuery<HTMLElement>>
-      goToEnclaveList(): Chainable<AUTWindow>
+      deleteEnclave(enclaveName: string): Chainable<void>;
+      focusInputWithLabel(label: string): Chainable<void>;
+      findCardWithName(label: string): Chainable<JQuery<HTMLElement>>;
+      goToEnclaveList(): Chainable<AUTWindow>;
     }
   }
 }
