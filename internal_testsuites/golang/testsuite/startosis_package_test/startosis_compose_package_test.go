97ea5844b83b4c4053012a082153af22daa1325a
Tedi Mitiku <tedi.m52@gmail.com>
2024-01-11 13:03:37 -0500

    fix ci tests

--- internal_testsuites/golang/testsuite/startosis_package_test/startosis_compose_package_test.go
+++ internal_testsuites/golang/testsuite/startosis_package_test/startosis_compose_package_test.go
@@ -27,39 +27,3 @@ func (suite *StartosisPackageTestSuite) TestStartosisPackage_DockerComposePackag
 	require.Contains(t, string(runResult.RunOutput), expectedScriptOutputSubstring)
 	require.Len(t, runResult.Instructions, 4)
 }
-
-func (suite *StartosisPackageTestSuite) TestStartosisPackage_DockerComposePackageWithImageBuildSpec() {
-	ctx := context.Background()
-	runResult, err := suite.RunPackage(ctx, imageBuildSpecDockerComposePackageRelPath)
-
-	t := suite.T()
-	require.Nil(t, err, "Unexpected error executing Starlark package")
-
-	require.NotNil(t, runResult)
-	require.Nil(t, runResult.InterpretationError)
-	require.Empty(t, runResult.ValidationErrors)
-	require.Nil(t, runResult.ExecutionError)
-
-	expectedScriptOutputSubstring := `Service 'web' added with service UUID `
-
-	require.Contains(t, string(runResult.RunOutput), expectedScriptOutputSubstring)
-	require.Len(t, runResult.Instructions, 2)
-}
-
-func (suite *StartosisPackageTestSuite) TestStartosisPackage_DockerComposePackageWithInvalidPathInVolumeErrors() {
-	ctx := context.Background()
-	runResult, err := suite.RunPackage(ctx, imageBuildSpecDockerComposePackageRelPath)
-
-	t := suite.T()
-	require.Nil(t, err, "Unexpected error executing Starlark package")
-
-	require.NotNil(t, runResult)
-	require.Nil(t, runResult.InterpretationError)
-	require.Empty(t, runResult.ValidationErrors)
-	require.Nil(t, runResult.ExecutionError)
-
-	expectedScriptOutputSubstring := `Service 'web' added with service UUID `
-
-	require.Contains(t, string(runResult.RunOutput), expectedScriptOutputSubstring)
-	require.Len(t, runResult.Instructions, 2)
-}
