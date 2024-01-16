7b86bffed94974dbcb8a9a437a0fbbdc6fb8bed8
Ben Gazzard <ben@dartoxia.com>
2024-01-16 17:48:54 +0000

    Don't it.only

--- enclave-manager/web/cypress/e2e/enclaves/packageSearch.cy.ts
+++ enclave-manager/web/cypress/e2e/enclaves/packageSearch.cy.ts
@@ -1,5 +1,5 @@
 describe("New Enclave package search", () => {
-  it.only("Can find an enclave by exact name", () => {
+  it("Can find an enclave by exact name", () => {
     cy.goToEnclaveList();
     cy.contains("New Enclave").click();
     cy.contains("Package Template Repo").should("not.exist");
