6292e1ebb6a3f6b2778fd541181f928720a1c588
Gyanendra Mishra <anomaly.the@gmail.com>
2024-01-17 18:38:20 +0000

    undo a change that was added mistakenly

--- internal_testsuites/golang/testsuite/startosis_directory_test/startosis_persistent_directory_test.go
+++ internal_testsuites/golang/testsuite/startosis_directory_test/startosis_persistent_directory_test.go
@@ -24,7 +24,7 @@ def run(plan):
 	)
 	service = plan.add_service(name=SERVICE_NAME, config=service_config)
 
-	plan.exec(w
+	plan.exec(
         service_name=SERVICE_NAME,
         recipe=ExecRecipe([
             "/bin/sh",
