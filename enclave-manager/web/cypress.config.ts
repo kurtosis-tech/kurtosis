7811158daf2ddcf5bbacb95dc734665de6d31984
Ben Gazzard <ben@dartoxia.com>
2024-01-25 16:19:05 +0000

    Attempt cypress retries

--- enclave-manager/web/cypress.config.ts
+++ enclave-manager/web/cypress.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     defaultCommandTimeout: 20000,
+    retries: {
+      runMode: 2,
+      openMode: 0,
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
