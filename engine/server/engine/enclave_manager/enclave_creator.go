a50c26dfc975cc4fa35e53837854aa58003d84b1
Tedi Mitiku <tedi.m52@gmail.com>
2024-02-06 15:45:01 -0500

    go fmt

--- engine/server/engine/enclave_manager/enclave_creator.go
+++ engine/server/engine/enclave_manager/enclave_creator.go
@@ -112,8 +112,7 @@ func (creator *EnclaveCreator) CreateEnclave(
 		isCI,
 		cloudUserID,
 		cloudInstanceID,
-		shouldAPICRunInDebugMode,
-	)
+		shouldAPICRunInDebugMode)
 	if err != nil {
 		return nil, stacktrace.Propagate(err, "An error occurred launching the API container")
 	}
@@ -222,7 +221,7 @@ func (creator *EnclaveCreator) launchApiContainer(
 			isCI,
 			cloudUserID,
 			cloudInstanceID,
-      shouldStartInDebugMode)
+			shouldStartInDebugMode)
 		if err != nil {
 			return nil, stacktrace.Propagate(err, "Expected to be able to launch api container for enclave '%v' with custom version '%v', but an error occurred", enclaveUuid, apiContainerImageVersionTag)
 		}
