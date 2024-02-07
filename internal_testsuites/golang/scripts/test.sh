4760076017363787f8a9cd872d0123f2c4a63813
Gyanendra Mishra <anomaly.the@gmail.com>
2024-02-07 16:13:12 +0000

    check CLI builds for all architectures

--- internal_testsuites/golang/scripts/test.sh
+++ internal_testsuites/golang/scripts/test.sh
@@ -63,8 +63,16 @@ if [ "${testsuite_cluster_backend_arg}" == "${TESTSUITE_CLUSTER_BACKEND_KUBERNET
     # The only reason this exists is because, some Kurtosis feature doesn't work on Kubernetes so we have to know to skip
     #  those tests
     # K8S is also slower than docker, so they have different timeouts
-    CGO_ENABLED=0 go test -v $(go list ./... | circleci tests split) -p "${PARALLELISM}" -count=1 -timeout "${KUBERNETES_TIMEOUT}" -tags kubernetes
+    if [ "${testsuite_is_running_on_circleci}" == "${TEST_IS_RUNNING_ON_CIRCLE_CI}" ]; then
+        CGO_ENABLED=0 go test -v $(go list ./... | circleci tests split) -p "${PARALLELISM}" -count=1 -timeout "${KUBERNETES_TIMEOUT}" -tags kubernetes
+    else
+        CGO_ENABLED=0 go test ./... -p "${PARALLELISM}" -count=1 -timeout "${KUBERNETES_TIMEOUT}" -tags kubernetes
+    fi
 else
-    CGO_ENABLED=0 go test -v $(go list ./... | circleci tests split) -p "${PARALLELISM}" -count=1 -timeout "${DOCKER_TIMEOUT}"
+    if [ "${testsuite_is_running_on_circleci}" == "${TEST_IS_RUNNING_ON_CIRCLE_CI}" ]; then
+        CGO_ENABLED=0 go test -v $(go list ./... | circleci tests split) -p "${PARALLELISM}" -count=1 -timeout "${DOCKER_TIMEOUT}"
+    else
+        CGO_ENABLED=0 go test ./... -p "${PARALLELISM}" -count=1 -timeout "${DOCKER_TIMEOUT}"
+    fi
 fi
 
