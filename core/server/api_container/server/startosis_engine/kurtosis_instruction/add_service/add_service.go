d661d5fa18a2bc226232c58d51a4b8142c8851b8
Tedi Mitiku <tedi.m52@gmail.com>
2024-03-05 19:58:59 -0500

    checkpoint

--- core/server/api_container/server/startosis_engine/kurtosis_instruction/add_service/add_service.go
+++ core/server/api_container/server/startosis_engine/kurtosis_instruction/add_service/add_service.go
@@ -12,6 +12,7 @@ import (
 	"github.com/kurtosis-tech/kurtosis/core/server/api_container/server/startosis_engine/kurtosis_starlark_framework/builtin_argument"
 	"github.com/kurtosis-tech/kurtosis/core/server/api_container/server/startosis_engine/kurtosis_starlark_framework/kurtosis_plan_instruction"
 	"github.com/kurtosis-tech/kurtosis/core/server/api_container/server/startosis_engine/kurtosis_types/service_config"
+	"github.com/kurtosis-tech/kurtosis/core/server/api_container/server/startosis_engine/plan_yaml"
 	"github.com/kurtosis-tech/kurtosis/core/server/api_container/server/startosis_engine/runtime_value_store"
 	"github.com/kurtosis-tech/kurtosis/core/server/api_container/server/startosis_engine/startosis_errors"
 	"github.com/kurtosis-tech/kurtosis/core/server/api_container/server/startosis_engine/startosis_packages"
@@ -246,6 +247,11 @@ func (builtin *AddServiceCapabilities) FillPersistableAttributes(builder *enclav
 	).AddServiceName(
 		builtin.serviceName,
 	)
+}
+
+func UpdatePlan(planYaml *plan_yaml.PlanYaml) error {
+
+	return nil
 }
 
 func (builtin *AddServiceCapabilities) Description() string {
