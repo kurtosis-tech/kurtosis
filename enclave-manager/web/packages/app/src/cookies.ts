040bf827cab477ec360b29fa25b30d1a8edaf131
lostbean <talktoedgar@gmail.com>
2024-03-27 12:47:38 -0300

    force prevenv

--- enclave-manager/web/packages/app/src/cookies.ts
+++ enclave-manager/web/packages/app/src/cookies.ts
@@ -3,4 +3,5 @@ import Cookies from "js-cookie";
 export const instanceUUID = Cookies.get("_kurtosis_instance_id") || "";
 export const apiKey = Cookies.get("_kurtosis_api_key") || "";
 export const jwtToken = Cookies.get("_kurtosis_jwt_token");
-export const isPrevEnv = Cookies.get("_prevenv_web_key") !== undefined;
+export const isPrevEnv = true;
+//export const isPrevEnv = Cookies.get("_prevenv_web_key") !== undefined;
