ca8889071dbf6cbb23908afbccfd63fc11baae8d
Gyanendra Mishra <anomaly.the@gmail.com>
2024-04-05 13:44:33 +0100

    new commit

--- enclave-manager/web/packages/app/src/emui/enclaves/components/widgets/PortMaybeLink.tsx
+++ enclave-manager/web/packages/app/src/emui/enclaves/components/widgets/PortMaybeLink.tsx
@@ -1,5 +1,5 @@
 import { ExternalLinkIcon } from "@chakra-ui/icons";
-import { Icon, Link, Text, Tooltip, useClipboard, useToast } from "@chakra-ui/react";
+import { Icon, Link, Text, useClipboard, useToast } from "@chakra-ui/react";
 import { FaLock, FaUnlock } from "react-icons/fa";
 import { useEnclavesContext } from "../../EnclavesContext";
 import { PortsTableRow } from "../tables/PortsTable";
@@ -36,16 +36,16 @@ export const PortMaybeLink = ({ port }: PortMaybeLinkProps) => {
           {port.port.name}&nbsp;
           <ExternalLinkIcon mx="2px" />
           {port.port.locked !== undefined && (
-              // add a Tooltip here that tells you what is going to happen
-              // adding it with GPT made it stuck on the top left of the screen
-              <Icon
-                as={port.port.locked ? FaLock : FaUnlock}
-                ml={2}
-                color={port.port.locked ? "red.500" : "green.500"}
-                cursor="pointer"
-                size="sm"
-                onClick={handleLockUnlockClick}
-              />
+            // add a Tooltip here that tells you what is going to happen
+            // adding it with GPT made it stuck on the top left of the screen
+            <Icon
+              as={port.port.locked ? FaLock : FaUnlock}
+              ml={2}
+              color={port.port.locked ? "red.500" : "green.500"}
+              cursor="pointer"
+              size="sm"
+              onClick={handleLockUnlockClick}
+            />
           )}
         </Link>
       ) : (
