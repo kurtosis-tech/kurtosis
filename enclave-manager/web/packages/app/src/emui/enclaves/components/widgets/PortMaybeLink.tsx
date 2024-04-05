87e9b1524aef1d29ec8c03a8878cd247d1660f02
Gyanendra Mishra <anomaly.the@gmail.com>
2024-04-05 13:28:26 +0100

    more gpt magic

--- enclave-manager/web/packages/app/src/emui/enclaves/components/widgets/PortMaybeLink.tsx
+++ enclave-manager/web/packages/app/src/emui/enclaves/components/widgets/PortMaybeLink.tsx
@@ -1,5 +1,5 @@
 import { ExternalLinkIcon } from "@chakra-ui/icons";
-import { Icon, Link, Text } from "@chakra-ui/react";
+import { Icon, Link, Text, Tooltip, useClipboard, useToast } from "@chakra-ui/react";
 import { FaLock, FaUnlock } from "react-icons/fa";
 import { useEnclavesContext } from "../../EnclavesContext";
 import { PortsTableRow } from "../tables/PortsTable";
@@ -10,12 +10,23 @@ type PortMaybeLinkProps = {
 
 export const PortMaybeLink = ({ port }: PortMaybeLinkProps) => {
   const { lockUnlockPort } = useEnclavesContext();
+  const toast = useToast();
+  const { onCopy } = useClipboard(port.link);
 
   const isHttpLink = port.port.applicationProtocol?.startsWith("http");
 
-  const handleLockUnlockClick = async () => {
+  const handleLockUnlockClick = async (e: React.MouseEvent<SVGElement>) => {
+    e.preventDefault();
     const lock = !port.port.locked;
     await lockUnlockPort(port.port.privatePort, port.port.serviceShortUuid, port.port.enclaveShortUuid, lock);
+    onCopy();
+    toast({
+      title: `Link ${lock ? "locked" : "unlocked"}`,
+      description: `The link has been ${lock ? "locked" : "unlocked"} and copied to the clipboard.`,
+      status: lock ? "success" : "warning",
+      duration: 3000,
+      isClosable: true,
+    });
   };
 
   return (
@@ -25,14 +36,20 @@ export const PortMaybeLink = ({ port }: PortMaybeLinkProps) => {
           {port.port.name}&nbsp;
           <ExternalLinkIcon mx="2px" />
           {port.port.locked !== undefined && (
-            <Icon
-              as={port.port.locked ? FaLock : FaUnlock}
-              ml={2}
-              color={port.port.locked ? "red.500" : "green.500"}
-              cursor="pointer"
-              size="sm"
-              onClick={handleLockUnlockClick}
-            />
+            <Tooltip
+              label={`Click to ${
+                port.port.locked ? "unlock" : "lock"
+              } the link. Unlocking will make it available to the public.`}
+            >
+              <Icon
+                as={port.port.locked ? FaLock : FaUnlock}
+                ml={2}
+                color={port.port.locked ? "red.500" : "green.500"}
+                cursor="pointer"
+                size="sm"
+                onClick={handleLockUnlockClick}
+              />
+            </Tooltip>
           )}
         </Link>
       ) : (
