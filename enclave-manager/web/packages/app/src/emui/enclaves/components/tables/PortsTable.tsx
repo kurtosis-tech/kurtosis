fc587cbc1abedb1fc7beab77205195a1efe7b9dd
Gyanendra Mishra <anomaly.the@gmail.com>
2024-04-05 12:23:48 +0100

    work in progress

--- enclave-manager/web/packages/app/src/emui/enclaves/components/tables/PortsTable.tsx
+++ enclave-manager/web/packages/app/src/emui/enclaves/components/tables/PortsTable.tsx
@@ -15,6 +15,7 @@ export type PortsTableRow = {
     publicPort: number;
     name: string;
     applicationProtocol: string;
+    locked: boolean | undefined,
   };
   link: string;
 };
@@ -47,6 +48,7 @@ export const getPortTableRows = (
         privatePort: port.number,
         publicPort: publicPorts[name].number,
         name: isDefined(serviceName) ? `${serviceName}:${name}` : name,
+        locked: privatePorts[name].locked
       },
       link: link,
     };
--- enclave-manager/web/packages/app/src/emui/enclaves/components/widgets/PortMaybeLink.tsx
+++ enclave-manager/web/packages/app/src/emui/enclaves/components/widgets/PortMaybeLink.tsx
@@ -1,10 +1,13 @@
 import { ExternalLinkIcon } from "@chakra-ui/icons";
 import { Link, Text } from "@chakra-ui/react";
 import { PortsTableRow } from "../tables/PortsTable";
+import { Icon } from "@chakra-ui/react";
+import { FaLock, FaUnlock } from "react-icons/fa";
 
 type PortMaybeLinkProps = {
   port: PortsTableRow;
 };
+
 export const PortMaybeLink = ({ port }: PortMaybeLinkProps) => {
   return (
     <Text>
@@ -15,6 +18,13 @@ export const PortMaybeLink = ({ port }: PortMaybeLinkProps) => {
         </Link>
       ) : (
         port.port.name
+      )}
+      {port.port.locked !== undefined && (
+        <Icon
+          as={port.port.locked ? FaLock : FaUnlock}
+          ml={2}
+          color={port.port.locked ? "red.500" : "green.500"}
+        />
       )}
     </Text>
   );
