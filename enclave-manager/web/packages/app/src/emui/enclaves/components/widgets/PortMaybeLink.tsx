eefb0c028ae280ab69c0d6435aff075c084e75e4
Gyanendra Mishra <anomaly.the@gmail.com>
2024-04-08 09:56:11 +0100

    ran prettier

--- enclave-manager/web/packages/app/src/emui/enclaves/components/widgets/PortMaybeLink.tsx
+++ enclave-manager/web/packages/app/src/emui/enclaves/components/widgets/PortMaybeLink.tsx
@@ -1,5 +1,5 @@
 import { CheckCircleIcon, ExternalLinkIcon, LockIcon, UnlockIcon } from "@chakra-ui/icons";
-import { Box, Flex, IconButton, Link, Heading, Text, Tooltip, useClipboard, useToast, Icon } from "@chakra-ui/react";
+import { Box, Flex, Heading, Icon, IconButton, Link, Text, Tooltip, useClipboard, useToast } from "@chakra-ui/react";
 import { useEnclavesContext } from "../../EnclavesContext";
 import { PortsTableRow } from "../tables/PortsTable";
 
@@ -26,27 +26,18 @@ export const PortMaybeLink = ({ port, disablePortLocking }: PortMaybeLinkProps) 
       isClosable: true,
       position: "bottom-right",
       render: () => (
-        <Flex
-          color='white'
-          p={3}
-          bg={lock ? "green.500" : "yellow.500"}
-          borderRadius={6}
-          gap={4}
-        >
+        <Flex color="white" p={3} bg={lock ? "green.500" : "yellow.500"} borderRadius={6} gap={4}>
           <Icon as={CheckCircleIcon} w={6} h={6} />
           <Box>
-            <Heading
-              as="h4"
-              fontSize="md"
-              fontWeight="500"
-              color="white"
-            >
+            <Heading as="h4" fontSize="md" fontWeight="500" color="white">
               Link {lock ? "locked" : "unlocked"}
             </Heading>
-            <Text marginTop={1} color="white">The link has been {lock ? "locked" : "unlocked"} and copied to the clipboard.</Text>
+            <Text marginTop={1} color="white">
+              The link has been {lock ? "locked" : "unlocked"} and copied to the clipboard.
+            </Text>
           </Box>
         </Flex>
-      )
+      ),
     });
   };
 
@@ -55,16 +46,13 @@ export const PortMaybeLink = ({ port, disablePortLocking }: PortMaybeLinkProps) 
       {isHttpLink ? (
         <Flex alignItems="center">
           {port.port.locked === undefined && (
-            <Tooltip
-              label={port.port.locked ? "Publish port" : "Make port private"}
-              fontSize="small"
-            >
+            <Tooltip label={port.port.locked ? "Publish port" : "Make port private"} fontSize="small">
               <IconButton
                 icon={port.port.locked ? <LockIcon /> : <UnlockIcon />}
                 variant={"ghost"}
                 size={"xs"}
                 cursor="pointer"
-                onClick={() =>  handleLockUnlockClick}
+                onClick={() => handleLockUnlockClick}
                 pointerEvents={disablePortLocking ? "none" : "auto"}
                 color={disablePortLocking ? "gray.200" : "white"}
                 aria-label={port.port.locked ? "Publish port" : "Make port private"}
