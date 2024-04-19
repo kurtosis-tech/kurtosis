cdae1cac1728b5b13ea9552149a16248ecd46b3a
Gyanendra Mishra <anomaly.the@gmail.com>
2024-04-19 15:14:28 +0100

    Revert "Revert "REVERT - build amd64""
    This reverts commit 88b671a19efaf5a6779eb420426c8181a2bcc382.

--- scripts/docker-image-builder.sh
+++ scripts/docker-image-builder.sh
@@ -43,7 +43,7 @@ architecture="amd64"
 if [ "$uname_arch" == "x86_64" ] || [ "$uname_arch" == "amd64" ]; then
     architecture="amd64"
 elif [ "$uname_arch" == "aarch64" ] || [ "$uname_arch" == "arm64" ]; then
-    architecture="arm64"
+    architecture="amd64"
 fi
 
 # Argument processing
