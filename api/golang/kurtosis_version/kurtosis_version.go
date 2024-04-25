e28ca704ad860f48598de2733f4ce303d5a21183
kurtosisbot <kurtosisbot@users.noreply.github.com>
2024-04-25 19:46:31 +0000

    Apply automatic changes

--- api/golang/kurtosis_version/kurtosis_version.go
+++ api/golang/kurtosis_version/kurtosis_version.go
@@ -9,6 +9,6 @@ const (
 	// !!!!!!!!!!! DO NOT UPDATE! WILL BE MANUALLY UPDATED DURING THE RELEASE PROCESS !!!!!!!!!!!!!!!!!!!!!!
 	// This is necessary so that Kurt Core consumers will know if they're compatible with the currently-running
 	//  API container
-	KurtosisVersion = "0.88.18"
+	KurtosisVersion = "0.88.19"
 	// !!!!!!!!!!! DO NOT UPDATE! WILL BE MANUALLY UPDATED DURING THE RELEASE PROCESS !!!!!!!!!!!!!!!!!!!!!!
 )
