3c2dd90a48f9fb34ca649e6c19bcaf45cea38893
Tedi Mitiku <tedi.m52@gmail.com>
2024-04-25 02:31:04 -0400

    Revert "update description of add service"
    This reverts commit 4a7cce8c503ea238f5d736cc391fe43a07ec3caa.

--- core/server/api_container/server/startosis_engine/kurtosis_instruction/add_service/add_service.go
+++ core/server/api_container/server/startosis_engine/kurtosis_instruction/add_service/add_service.go
@@ -184,7 +184,7 @@ func (builtin *AddServiceCapabilities) Validate(_ *builtin_argument.ArgumentValu
 	return nil
 }
 
-func (builtin *AddServiceCapabilities) Execute(ctx context.Context, arguments *builtin_argument.ArgumentValuesSet) (string, error) {
+func (builtin *AddServiceCapabilities) Execute(ctx context.Context, _ *builtin_argument.ArgumentValuesSet) (string, error) {
 	// update service config to use new service config set by a set_service instruction, if one exists
 	if builtin.interpretationTimeValueStore.ExistsNewServiceConfigForService(builtin.serviceName) {
 		newServiceConfig, err := builtin.interpretationTimeValueStore.GetNewServiceConfig(builtin.serviceName)
@@ -192,7 +192,6 @@ func (builtin *AddServiceCapabilities) Execute(ctx context.Context, arguments *b
 			return "", stacktrace.Propagate(err, "An error occurred retrieving a new service config '%s'.", builtin.serviceName)
 		}
 		builtin.serviceConfig = newServiceConfig
-		builtin.description = builtin_argument.GetDescriptionOrFallBack(arguments, fmt.Sprintf(addServiceDescriptionFormatStr, builtin.serviceName, newServiceConfig.GetContainerImageName()))
 	}
 	replacedServiceName, replacedServiceConfig, err := replaceMagicStrings(builtin.runtimeValueStore, builtin.serviceName, builtin.serviceConfig)
 	if err != nil {
