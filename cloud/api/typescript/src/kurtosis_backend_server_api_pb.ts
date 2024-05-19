17cac016a04ac42b4c4f9119698df395f50b3973
Laurent Luce <laurentluce49@yahoo.com>
2024-05-18 18:52:52 -0700

    Add instance id

--- cloud/api/typescript/src/kurtosis_backend_server_api_pb.ts
+++ cloud/api/typescript/src/kurtosis_backend_server_api_pb.ts
@@ -2131,6 +2131,11 @@ export class GetInstanceMetricsRequest extends Message<GetInstanceMetricsRequest
    */
   accessToken = "";
 
+  /**
+   * @generated from field: string instance_id = 2;
+   */
+  instanceId = "";
+
   constructor(data?: PartialMessage<GetInstanceMetricsRequest>) {
     super();
     proto3.util.initPartial(data, this);
@@ -2140,6 +2145,7 @@ export class GetInstanceMetricsRequest extends Message<GetInstanceMetricsRequest
   static readonly typeName = "kurtosis_cloud.GetInstanceMetricsRequest";
   static readonly fields: FieldList = proto3.util.newFieldList(() => [
     { no: 1, name: "access_token", kind: "scalar", T: 9 /* ScalarType.STRING */ },
+    { no: 2, name: "instance_id", kind: "scalar", T: 9 /* ScalarType.STRING */ },
   ]);
 
   static fromBinary(bytes: Uint8Array, options?: Partial<BinaryReadOptions>): GetInstanceMetricsRequest {
