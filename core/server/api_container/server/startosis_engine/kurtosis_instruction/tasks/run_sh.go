3fe8dc3d5d79c27c44e7f7a873fcd7a4af14823d
Tedi Mitiku <tedi.m52@gmail.com>
2024-06-06 17:24:29 -0400

    fix else stmt

--- core/server/api_container/server/startosis_engine/kurtosis_instruction/tasks/run_sh.go
+++ core/server/api_container/server/startosis_engine/kurtosis_instruction/tasks/run_sh.go
@@ -24,6 +24,7 @@ import (
 	"github.com/kurtosis-tech/kurtosis/core/server/api_container/server/startosis_engine/startosis_packages"
 	"github.com/kurtosis-tech/kurtosis/core/server/api_container/server/startosis_engine/startosis_validator"
 	"github.com/kurtosis-tech/stacktrace"
+	"github.com/xtgo/uuid"
 	"go.starlark.net/starlark"
 	"go.starlark.net/starlarkstruct"
 )
@@ -155,11 +156,6 @@ func (builtin *RunShCapabilities) Interpret(locatorOfModuleInWhichThisBuiltinIsB
 		randomUuid := uuid.NewRandom()
 		builtin.name = fmt.Sprintf("task-%v", randomUuid.String())
 	}
-	taskName, err := builtin_argument.ExtractArgumentValue[starlark.String](arguments, TaskNameArgName)
-	if err != nil {
-		return nil, startosis_errors.WrapWithInterpretationError(err, "Unable to extract value for '%s' argument", TaskNameArgName)
-	}
-	builtin.name = taskName.GoString()
 
 	runCommand, err := builtin_argument.ExtractArgumentValue[starlark.String](arguments, RunArgName)
 	if err != nil {
