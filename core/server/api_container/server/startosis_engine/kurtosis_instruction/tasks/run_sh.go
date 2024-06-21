27eb4d1fcc09bcf1d21036131816e6b07758fe41
Tedi Mitiku <tedi.m52@gmail.com>
2024-06-21 14:46:52 -0400

    get logs for tasks

--- core/server/api_container/server/startosis_engine/kurtosis_instruction/tasks/run_sh.go
+++ core/server/api_container/server/startosis_engine/kurtosis_instruction/tasks/run_sh.go
@@ -276,7 +276,7 @@ func (builtin *RunShCapabilities) Execute(ctx context.Context, _ *builtin_argume
 	if err != nil {
 		return "", stacktrace.Propagate(err, "error occurred while preparing the sh command to execute on the image")
 	}
-	fullCommandToRun := []string{shellWrapperCommand, "-c", commandToRun}
+	fullCommandToRun := []string{shellWrapperCommand, "-c", fmt.Sprintf("%v \"$(%v)\" %v %v", "printf \"%s\\n\"", commandToRun, ">>", "/proc/1/fd/1")}
 
 	// run the command passed in by user in the container
 	createDefaultDirectoryResult, err := executeWithWait(ctx, builtin.serviceNetwork, builtin.name, builtin.wait, fullCommandToRun)
