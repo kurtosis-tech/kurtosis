ddd274a7f054142ed22fd4a8cdb87d9a40af03ca
Tedi Mitiku <tedi.m52@gmail.com>
2024-08-19 10:55:08 -0400

    log retention period

--- engine/server/engine/main.go
+++ engine/server/engine/main.go
@@ -414,6 +414,7 @@ func getLogsDatabaseClient(kurtosisBackendType args.KurtosisBackendType, kurtosi
 		if logRetentionPeriodInWeeks < 1 {
 			logRetentionPeriodInWeeks = 1
 		}
+		logrus.Infof("Setting log retention period to '%v' weeks.", logRetentionPeriodInWeeks)
 		osFs := volume_filesystem.NewOsVolumeFilesystem()
 		perWeekFileLayout := file_layout.NewPerWeekFileLayout(realTime)
 		logFileManager := log_file_manager.NewLogFileManager(kurtosisBackend, osFs, perWeekFileLayout, realTime, logRetentionPeriodInWeeks)
