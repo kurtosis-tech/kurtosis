2cb4ea006fe9439eaff588e8b328931a1c3643f8
Tedi Mitiku <tedi.m52@gmail.com>
2025-03-04 11:39:19 -0500

    wait for namespace removal

--- container-engine-lib/lib/backend_impls/kubernetes/kubernetes_kurtosis_backend/logs_collector_functions/shared_helpers.go
+++ container-engine-lib/lib/backend_impls/kubernetes/kubernetes_kurtosis_backend/logs_collector_functions/shared_helpers.go
@@ -19,7 +19,7 @@ import (
 )
 
 const (
-	maxAvailabilityCheckRetries     = 30
+	maxRetries                      = 30
 	timeToWaitBetweenChecksDuration = 1 * time.Second
 )
 
@@ -332,11 +332,32 @@ func waitForLogsCollectorAvailability(
 			pod.Name,
 			fluentBitContainerName,
 			httpPortSpec,
-			maxAvailabilityCheckRetries,
+			maxRetries,
 			timeToWaitBetweenChecksDuration); err != nil {
 			return stacktrace.Propagate(err, "An error occurred while checking for availability of pod '%v' managed by logs collector daemon set '%v'", pod.Name, logsCollectorDaemonSet.Name)
 		}
 	}
 
 	return nil
+}
+
+func waitForNamespaceRemoval(
+	ctx context.Context,
+	namespace string,
+	kubernetesManager *kubernetes_manager.KubernetesManager) error {
+
+	for i := uint(0); i < maxRetries; i++ {
+		_, err := kubernetesManager.GetNamespace(ctx, namespace)
+		if err != nil {
+			// if err was returned, namespace doesn't exist, or it's been marked for deleted
+			return nil
+		}
+
+		// Tiny optimization to not sleep if we're not going to run the loop again
+		if i < maxRetries {
+			time.Sleep(timeToWaitBetweenChecksDuration)
+		}
+	}
+
+	return stacktrace.NewError("Attempted to wait for namespace '%v' removal or to be marked for deletion '%v' times but namespace was not removed.", namespace, maxRetries)
 }
