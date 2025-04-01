35c815e23a1ea235dd31ba482a9501232ef08bfb
Tedi Mitiku <tedi.m52@gmail.com>
2025-04-01 15:15:38 -0400

    add grafloki command

---
+++ cli/cli/commands/grafloki/grafloki.go
@@ -0,0 +1,21 @@
+package grafloki
+
+import (
+	"github.com/kurtosis-tech/kurtosis/cli/cli/command_str_consts"
+	"github.com/kurtosis-tech/kurtosis/cli/cli/commands/grafloki/start"
+	"github.com/kurtosis-tech/kurtosis/cli/cli/commands/grafloki/stop"
+	"github.com/spf13/cobra"
+)
+
+// ServiceCmd Suppressing exhaustruct requirement because this struct has ~40 properties
+// nolint: exhaustruct
+var GraflokiCmd = &cobra.Command{
+	Use:   command_str_consts.GraflokiCmdStr,
+	Short: "Grafana loki command",
+	RunE:  nil,
+}
+
+func init() {
+	GraflokiCmd.AddCommand(start.GraflokiStartCmd.MustGetCobraCommand())
+	GraflokiCmd.AddCommand(stop.GraflokiStopCmd.MustGetCobraCommand())
+}
---
+++ cli/cli/commands/grafloki/start/start.go
@@ -0,0 +1,30 @@
+package start
+
+import (
+	"context"
+	"github.com/kurtosis-tech/kurtosis/api/golang/engine/kurtosis_engine_rpc_api_bindings"
+	"github.com/kurtosis-tech/kurtosis/cli/cli/command_framework/highlevel/engine_consuming_kurtosis_command"
+	"github.com/kurtosis-tech/kurtosis/cli/cli/command_framework/lowlevel/args"
+	"github.com/kurtosis-tech/kurtosis/cli/cli/command_framework/lowlevel/flags"
+	"github.com/kurtosis-tech/kurtosis/cli/cli/command_str_consts"
+	"github.com/kurtosis-tech/kurtosis/container-engine-lib/lib/backend_interface"
+	"github.com/kurtosis-tech/kurtosis/metrics-library/golang/lib/metrics_client"
+)
+
+var GraflokiStartCmd = &engine_consuming_kurtosis_command.EngineConsumingKurtosisCommand{
+	CommandStr:       command_str_consts.GraflokiStartCmdStr,
+	ShortDescription: "Starts a grafana/loki instance.",
+	LongDescription:  "Starts a grafana/loki instance that the kurtosis engine will be configured to send logs to.",
+	RunFunc:          run,
+}
+
+func run(
+	ctx context.Context,
+	_ backend_interface.KurtosisBackend,
+	_ kurtosis_engine_rpc_api_bindings.EngineServiceClient,
+	_ metrics_client.MetricsClient,
+	flags *flags.ParsedFlags,
+	args *args.ParsedArgs,
+) error {
+	return nil
+}
