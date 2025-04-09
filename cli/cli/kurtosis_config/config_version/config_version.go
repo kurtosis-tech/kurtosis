3da0bf9476843a20b40810e26ff4693367c238ce
Tedi Mitiku <tedi.m52@gmail.com>
2025-04-09 18:44:30 -0400

    fmt

--- cli/cli/kurtosis_config/config_version/config_version.go
+++ cli/cli/kurtosis_config/config_version/config_version.go
@@ -8,7 +8,7 @@ const (
 	ConfigVersion_v0 ConfigVersion = iota
 	ConfigVersion_v1
 	ConfigVersion_v2 // Fixed a typo in Kubernetes config, `enclave-size-in-Megabytes` -> `enclave-size-in-megabytes`
-	ConfigVersion_v3	// adds logs aggregator config
+	ConfigVersion_v3 // adds logs aggregator config
 	ConfigVersion_v4 // adds engine-node-name to KubernetesClusterConfig
 	ConfigVersion_v5 // adds GraflokiConfig to KurtosisClusterConfig
 )
