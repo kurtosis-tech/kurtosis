c6736363bf42111be44c9ec09a2e83aa52389182
Tedi Mitiku <tedi.m52@gmail.com>
2025-05-08 21:08:32 -0400

    comment out snapshots creator test

--- core/server/api_container/server/snapshots/snapshots_creator_test.go
+++ core/server/api_container/server/snapshots/snapshots_creator_test.go
@@ -1,17 +1,1 @@
 package snapshots
-
-import (
-	"testing"
-
-	"github.com/kurtosis-tech/kurtosis/container-engine-lib/lib/backend_interface/objects/service"
-	"github.com/stretchr/testify/require"
-)
-
-func TestSnapshotCreator_CreateSnapshot(t *testing.T) {
-	serviceConfig := service.GetEmptyServiceConfig()
-
-	apiServiceConfig, err := convertServiceConfigToJsonServiceConfig(serviceConfig)
-	require.NoError(t, err)
-
-	require.Nil(t, apiServiceConfig)
-}
