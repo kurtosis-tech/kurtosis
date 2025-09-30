d885101be5c7373e4837c67c997aa46490a759f8
Tedi Mitiku <tedi.m52@gmail.com>
2025-09-30 16:03:43 -0400

    execute in parallel

--- core/server/api_container/server/startosis_engine/startosis_runner.go
+++ core/server/api_container/server/startosis_engine/startosis_runner.go
@@ -148,7 +148,7 @@ func (runner *StartosisRunner) Run(
 			return
 		}
 
-		_, interpretationErr = instructionsPlan.GenerateInstructionsDependencyGraph()
+		instructionDependencyGraph, interpretationErr := instructionsPlan.GenerateInstructionsDependencyGraph()
 		if interpretationErr != nil {
 			starlarkRunResponseLines <- binding_constructors.NewStarlarkRunResponseLineFromInterpretationError(interpretationErr.ToAPIType())
 			starlarkRunResponseLines <- binding_constructors.NewStarlarkRunResponseLineFromRunFailureEvent()
@@ -174,7 +174,14 @@ func (runner *StartosisRunner) Run(
 			startingExecutionMsg, defaultCurrentStepNumber, totalNumberOfInstructions)
 		starlarkRunResponseLines <- progressInfo
 
-		executionResponseLinesChan := runner.startosisExecutor.Execute(ctx, dryRun, parallelism, instructionsPlan.GetIndexOfFirstInstruction(), instructionsSequence, serializedScriptOutput)
+		var executionResponseLinesChan <-chan *kurtosis_core_rpc_api_bindings.StarlarkRunResponseLine
+		if parallelism > 1 {
+			logrus.Infof("Executing Kurtosis instructions in parallel with parallelism: %d", parallelism)
+			executionResponseLinesChan = runner.startosisExecutor.ExecuteInParallel(ctx, dryRun, parallelism, instructionsPlan.GetIndexOfFirstInstruction(), instructionsSequence, serializedScriptOutput, instructionDependencyGraph)
+		} else {
+			logrus.Infof("Executing Kurtosis instructions in serial")
+			executionResponseLinesChan = runner.startosisExecutor.Execute(ctx, dryRun, parallelism, instructionsPlan.GetIndexOfFirstInstruction(), instructionsSequence, serializedScriptOutput)
+		}
 		if isRunFinished, isRunSuccessful := forwardKurtosisResponseLineChannelUntilSourceIsClosed(executionResponseLinesChan, starlarkRunResponseLines); !isRunFinished {
 			logrus.Warnf("Execution finished but no 'RunFinishedEvent' was received through the stream. This is unexpected as every execution should be terminal.")
 		} else if !isRunSuccessful {
